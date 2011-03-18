1d97932a0727661a9c9a7455668e234e9f531bc3
Thomas Gleixner <tglx@linutronix.de>
2011-03-18 10:11:25 +0100

    fs: jbd/jbd2: Make state lock and journal head lock rt safe
    bit_spin_locks break under RT.
    
    Based on a previous patch from Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    --
     include/linux/buffer_head.h |    8 ++++++++
     include/linux/jbd2.h        |   24 ++++++++++++++++++++++++
     2 files changed, 32 insertions(+)

--- include/linux/buffer_head.h
+++ include/linux/buffer_head.h
@@ -78,6 +78,10 @@ struct buffer_head {
 	atomic_t b_count;		/* users using this buffer_head */
 #ifdef CONFIG_PREEMPT_RT_BASE
 	spinlock_t b_uptodate_lock;
+#if IS_ENABLED(CONFIG_JBD2)
+	spinlock_t b_state_lock;
+	spinlock_t b_journal_head_lock;
+#endif
 #endif
 };
 
@@ -109,6 +113,10 @@ static inline void buffer_head_init_locks(struct buffer_head *bh)
 {
 #ifdef CONFIG_PREEMPT_RT_BASE
 	spin_lock_init(&bh->b_uptodate_lock);
+#if IS_ENABLED(CONFIG_JBD2)
+	spin_lock_init(&bh->b_state_lock);
+	spin_lock_init(&bh->b_journal_head_lock);
+#endif
 #endif
 }
 
