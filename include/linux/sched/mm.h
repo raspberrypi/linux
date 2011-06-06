a3d71b5dc551f64b971b47bcef8b470b0e2b70b0
Thomas Gleixner <tglx@linutronix.de>
2011-06-06 12:20:33 +0200

    sched: Move mmdrop to RCU on RT
    Takes sleeping locks and calls into the memory allocator, so nothing
    we want to do in task switch and oder atomic contexts.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/sched/mm.h
+++ include/linux/sched/mm.h
@@ -49,6 +49,17 @@ static inline void mmdrop(struct mm_struct *mm)
 		__mmdrop(mm);
 }
 
+#ifdef CONFIG_PREEMPT_RT_BASE
+extern void __mmdrop_delayed(struct rcu_head *rhp);
+static inline void mmdrop_delayed(struct mm_struct *mm)
+{
+	if (atomic_dec_and_test(&mm->mm_count))
+		call_rcu(&mm->delayed_drop, __mmdrop_delayed);
+}
+#else
+# define mmdrop_delayed(mm)	mmdrop(mm)
+#endif
+
 /**
  * mmget() - Pin the address space associated with a &struct mm_struct.
  * @mm: The address space to pin.
