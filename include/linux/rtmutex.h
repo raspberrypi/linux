541ddad48bb4571cce1bdf1f687770fd71fd4232
Thomas Gleixner <tglx@linutronix.de>
2011-06-09 11:43:52 +0200

    rtmutex: Add rtmutex_lock_killable()
    Add "killable" type to rtmutex. We need this since rtmutex are used as
    "normal" mutexes which do use this type.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- include/linux/rtmutex.h
+++ include/linux/rtmutex.h
@@ -108,6 +108,7 @@ extern void rt_mutex_destroy(struct rt_mutex *lock);
 
 extern void rt_mutex_lock(struct rt_mutex *lock);
 extern int rt_mutex_lock_interruptible(struct rt_mutex *lock);
+extern int rt_mutex_lock_killable(struct rt_mutex *lock);
 extern int rt_mutex_timed_lock(struct rt_mutex *lock,
 			       struct hrtimer_sleeper *timeout);
 
