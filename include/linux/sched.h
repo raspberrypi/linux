73cce74a6da4a52140210acfc07a5ed7dcf38499
Peter Zijlstra <peterz@infradead.org>
2011-07-28 10:43:51 +0200

    mm, rt: kmap_atomic scheduling
    In fact, with migrate_disable() existing one could play games with
    kmap_atomic. You could save/restore the kmap_atomic slots on context
    switch (if there are any in use of course), this should be esp easy now
    that we have a kmap_atomic stack.
    
    Something like the below.. it wants replacing all the preempt_disable()
    stuff with pagefault_disable() && migrate_disable() of course, but then
    you can flip kmaps around like below.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    [dvhart@linux.intel.com: build fix]
    Link: http://lkml.kernel.org/r/1311842631.5890.208.camel@twins
    
    [tglx@linutronix.de: Get rid of the per cpu variable and store the idx
    		     and the pte content right away in the task struct.
    		     Shortens the context switch code. ]

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -28,6 +28,7 @@
 #include <linux/mm_types_task.h>
 #include <linux/task_io_accounting.h>
 #include <linux/rseq.h>
+#include <asm/kmap_types.h>
 
 /* task_struct member predeclarations (sorted alphabetically): */
 struct audit_context;
@@ -1205,6 +1206,12 @@ struct task_struct {
 	struct rcu_head			put_rcu;
 	int				softirq_nestcnt;
 	unsigned int			softirqs_raised;
+#endif
+#ifdef CONFIG_PREEMPT_RT_FULL
+# if defined CONFIG_HIGHMEM || defined CONFIG_X86_32
+	int				kmap_idx;
+	pte_t				kmap_pte[KM_TYPE_NR];
+# endif
 #endif
 #ifdef CONFIG_DEBUG_ATOMIC_SLEEP
 	unsigned long			task_state_change;
