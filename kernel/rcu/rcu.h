9f9e5436061e3d8e5d371301999b94c6d1e0e722
Thomas Gleixner <tglx@linutronix.de>
2011-10-05 11:59:38 -0700

    rcu: Merge RCU-bh into RCU-preempt
    The Linux kernel has long RCU-bh read-side critical sections that
    intolerably increase scheduling latency under mainline's RCU-bh rules,
    which include RCU-bh read-side critical sections being non-preemptible.
    This patch therefore arranges for RCU-bh to be implemented in terms of
    RCU-preempt for CONFIG_PREEMPT_RT_FULL=y.
    
    This has the downside of defeating the purpose of RCU-bh, namely,
    handling the case where the system is subjected to a network-based
    denial-of-service attack that keeps at least one CPU doing full-time
    softirq processing.  This issue will be fixed by a later commit.
    
    The current commit will need some work to make it appropriate for
    mainline use, for example, it needs to be extended to cover Tiny RCU.
    
    [ paulmck: Added a useful changelog ]
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20111005185938.GA20403@linux.vnet.ibm.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/rcu/rcu.h
+++ kernel/rcu/rcu.h
@@ -528,7 +528,6 @@ static inline void show_rcu_gp_kthreads(void) { }
 static inline int rcu_get_gp_kthreads_prio(void) { return 0; }
 #else /* #ifdef CONFIG_TINY_RCU */
 unsigned long rcu_get_gp_seq(void);
-unsigned long rcu_bh_get_gp_seq(void);
 unsigned long rcu_sched_get_gp_seq(void);
 unsigned long rcu_exp_batches_completed(void);
 unsigned long rcu_exp_batches_completed_sched(void);
@@ -536,10 +535,18 @@ unsigned long srcu_batches_completed(struct srcu_struct *sp);
 void show_rcu_gp_kthreads(void);
 int rcu_get_gp_kthreads_prio(void);
 void rcu_force_quiescent_state(void);
-void rcu_bh_force_quiescent_state(void);
 void rcu_sched_force_quiescent_state(void);
 extern struct workqueue_struct *rcu_gp_wq;
 extern struct workqueue_struct *rcu_par_gp_wq;
+
+#ifdef CONFIG_PREEMPT_RT_FULL
+#define rcu_bh_get_gp_seq		rcu_get_gp_seq
+#define rcu_bh_force_quiescent_state	rcu_force_quiescent_state
+#else
+unsigned long rcu_bh_get_gp_seq(void);
+void rcu_bh_force_quiescent_state(void);
+#endif
+
 #endif /* #else #ifdef CONFIG_TINY_RCU */
 
 #ifdef CONFIG_RCU_NOCB_CPU
