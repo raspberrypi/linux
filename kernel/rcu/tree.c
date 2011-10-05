84ea70cd498fd6940c80ad7a98aafd290a457d43
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2011-10-05 11:45:18 -0700

    rcu: Make ksoftirqd do RCU quiescent states
    Implementing RCU-bh in terms of RCU-preempt makes the system vulnerable
    to network-based denial-of-service attacks.  This patch therefore
    makes __do_softirq() invoke rcu_bh_qs(), but only when __do_softirq()
    is running in ksoftirqd context.  A wrapper layer in interposed so that
    other calls to __do_softirq() avoid invoking rcu_bh_qs().  The underlying
    function __do_softirq_common() does the actual work.
    
    The reason that rcu_bh_qs() is bad in these non-ksoftirqd contexts is
    that there might be a local_bh_enable() inside an RCU-preempt read-side
    critical section.  This local_bh_enable() can invoke __do_softirq()
    directly, so if __do_softirq() were to invoke rcu_bh_qs() (which just
    calls rcu_preempt_qs() in the PREEMPT_RT_FULL case), there would be
    an illegal RCU-preempt quiescent state in the middle of an RCU-preempt
    read-side critical section.  Therefore, quiescent states can only happen
    in cases where __do_softirq() is invoked directly from ksoftirqd.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Link: http://lkml.kernel.org/r/20111005184518.GA21601@linux.vnet.ibm.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/rcu/tree.c
+++ kernel/rcu/tree.c
@@ -244,7 +244,19 @@ void rcu_sched_qs(void)
 			   this_cpu_ptr(&rcu_sched_data), true);
 }
 
-#ifndef CONFIG_PREEMPT_RT_FULL
+#ifdef CONFIG_PREEMPT_RT_FULL
+static void rcu_preempt_qs(void);
+
+void rcu_bh_qs(void)
+{
+	unsigned long flags;
+
+	/* Callers to this function, rcu_preempt_qs(), must disable irqs. */
+	local_irq_save(flags);
+	rcu_preempt_qs();
+	local_irq_restore(flags);
+}
+#else
 void rcu_bh_qs(void)
 {
 	RCU_LOCKDEP_WARN(preemptible(), "rcu_bh_qs() invoked with preemption enabled!!!");
