2aa2155925a3989285a9cc0f804d57a2420bd28f
Dom Cobley <dc4@broadcom.com>
2012-01-20 18:23:29 +0000

    Fix headers for vchiq/vcos to be GPLv2
    Signed-off-by: Dom Cobley <dc4@broadcom.com>

--- drivers/misc/vc04_services/interface/vcos/vcos_lowlevel_thread.h
+++ drivers/misc/vc04_services/interface/vcos/vcos_lowlevel_thread.h
@@ -1,11 +1,22 @@
+/*
+ * Copyright (c) 2010-2011 Broadcom. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /*=============================================================================
-Copyright (c) 2009 Broadcom Europe Limited.
-All rights reserved.
-
-Project  :  vcfw
-Module   :  chip driver
-
-FILE DESCRIPTION
 VideoCore OS Abstraction Layer - low level thread support
 =============================================================================*/
 
