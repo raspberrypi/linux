0ae5d66e40cde5a95ed16d28a480d2ce91834bfc
Felipe Balbi <balbi@ti.com>
2012-03-21 11:44:00 +0200

    usb: dwc3: ep0: increment "actual" on bounced ep0 case
    commit cd423dd3634a5232a3019eb372b144619a61cd16 upstream.
    
    due to a HW limitation we have a bounce buffer for ep0
    out transfers which are not aligned with MaxPacketSize.
    
    On such case we were not increment r->actual as we should.
    
    This patch fixes that mistake.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/usb/dwc3/ep0.c
+++ drivers/usb/dwc3/ep0.c
@@ -572,8 +572,9 @@ static void dwc3_ep0_complete_data(struct dwc3 *dwc,
 		dwc->ep0_bounced = false;
 	} else {
 		transferred = ur->length - trb.length;
-		ur->actual += transferred;
 	}
+
+	ur->actual += transferred;
 
 	if ((epnum & 1) && ur->actual < ur->length) {
 		/* for some reason we did not get everything out */
