dbf3ef5dc68374947735af5f4647d82f5b0712e6
Stephen Warren <swarren@wwwdotorg.org>
2012-05-21 22:59:08 -0600

    ARM: bcm2708: instantiate I2C0 device
    The BCM2708 has 3 I2C adapters. This change instantiates the first of
    these, which is attached to the expansion header on the Raspberry Pi.
    
    Signed-off-by: Stephen Warren <swarren@wwwdotorg.org>

--- arch/arm/mach-bcm2708/include/mach/platform.h
+++ arch/arm/mach-bcm2708/include/mach/platform.h
@@ -63,6 +63,7 @@
 #define GPIO_BASE                (BCM2708_PERI_BASE + 0x200000) /* GPIO */
 #define UART0_BASE               (BCM2708_PERI_BASE + 0x201000)	/* Uart 0 */
 #define MMCI0_BASE               (BCM2708_PERI_BASE + 0x202000) /* MMC interface */
+#define I2C0_BASE                (BCM2708_PERI_BASE + 0x205000)
 #define UART1_BASE               (BCM2708_PERI_BASE + 0x215000) /* Uart 1 */
 #define EMMC_BASE                (BCM2708_PERI_BASE + 0x300000) /* eMMC interface */
 #define SMI_BASE		 (BCM2708_PERI_BASE + 0x600000) /* SMI */
