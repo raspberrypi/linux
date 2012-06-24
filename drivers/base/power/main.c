2a095a707c3ce005151ef2d31786508286c6d549
Mandeep Singh Baines <msb@chromium.org>
2012-06-24 23:31:09 +0200

    PM / Sleep: Prevent waiting forever on asynchronous suspend after abort
    commit 1f758b23177d588a71b96ad02990e715949bb82f upstream.
    
    __device_suspend() must always send a completion. Otherwise, parent
    devices will wait forever.
    
    Commit 1e2ef05b, "PM: Limit race conditions between runtime PM and
    system sleep (v2)", introduced a regression by short-circuiting the
    complete_all() for certain error cases.
    
    This patch fixes the bug by always signalling a completion.
    
    Addresses http://crosbug.com/31972
    
    Tested by injecting an abort.
    
    Signed-off-by: Mandeep Singh Baines <msb@chromium.org>
    Signed-off-by: Rafael J. Wysocki <rjw@sisk.pl>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -869,7 +869,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	dpm_wait_for_children(dev, async);
 
 	if (async_error)
-		return 0;
+		goto Complete;
 
 	pm_runtime_get_noresume(dev);
 	if (pm_runtime_barrier(dev) && device_may_wakeup(dev))
@@ -878,7 +878,7 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	if (pm_wakeup_pending()) {
 		pm_runtime_put_sync(dev);
 		async_error = -EBUSY;
-		return 0;
+		goto Complete;
 	}
 
 	device_lock(dev);
@@ -926,6 +926,8 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	}
 
 	device_unlock(dev);
+
+ Complete:
 	complete_all(&dev->power.completion);
 
 	if (error) {
