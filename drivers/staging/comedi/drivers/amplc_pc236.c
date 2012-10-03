6c04efd97dd25596a1bf80cc1c518fe7e451cc1e
Ian Abbott <abbotti@mev.co.uk>
2012-10-03 16:25:18 +0100

    staging: comedi: amplc_pc236: fix possible NULL deref during detach
    commit 2db012bd40b889d8e5483d3daa07af04c4c109d8 upstream.
    
    `pc236_detach()` is called by the comedi core to clean up if either
    `pc236_attach()` or `pc236_attach_pci()` returns an error.  It sets
    `thisboard` to the return value of `comedi_board(dev)` and assumes it is
    non-null.  This is a valid assumption if `pc236_attach()` fails, but not
    if `pc236_attach_pci()` fails, leading to a possible NULL pointer
    dereference.
    
    Check `thisboard` at the top of `pc236_detach()` and return early if it
    is `NULL`.  This is okay because the only other thing that could have
    been allocated is `dev->private` and that is freed by the comedi core,
    not by this function.
    
    Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/comedi/drivers/amplc_pc236.c
+++ drivers/staging/comedi/drivers/amplc_pc236.c
@@ -577,8 +577,11 @@ static int __devinit pc236_attach_pci(struct comedi_device *dev,
 
 static void pc236_detach(struct comedi_device *dev)
 {
+	const struct pc236_board *thisboard = comedi_board(dev);
 	struct pci_dev *pcidev = comedi_to_pci_dev(dev);
 
+	if (!thisboard)
+		return;
 	if (dev->iobase)
 		pc236_intr_disable(dev);
 	if (dev->irq)
