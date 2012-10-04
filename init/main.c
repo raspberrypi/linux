61ec464cb0c89b8b7fd37b287cc8defdd8c06bc1
Thomas Gleixner <tglx@linutronix.de>
2012-10-04 14:20:47 +0100

    softirq: Split softirq locks
    The 3.x RT series removed the split softirq implementation in favour
    of pushing softirq processing into the context of the thread which
    raised it. Though this prevents us from handling the various softirqs
    at different priorities. Now instead of reintroducing the split
    softirq threads we split the locks which serialize the softirq
    processing.
    
    If a softirq is raised in context of a thread, then the softirq is
    noted on a per thread field, if the thread is in a bh disabled
    region. If the softirq is raised from hard interrupt context, then the
    bit is set in the flag field of ksoftirqd and ksoftirqd is invoked.
    When a thread leaves a bh disabled region, then it tries to execute
    the softirqs which have been raised in its own context. It acquires
    the per softirq / per cpu lock for the softirq and then checks,
    whether the softirq is still pending in the per cpu
    local_softirq_pending() field. If yes, it runs the softirq. If no,
    then some other task executed it already. This allows for zero config
    softirq elevation in the context of user space tasks or interrupt
    threads.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- init/main.c
+++ init/main.c
@@ -543,6 +543,7 @@ asmlinkage __visible void __init start_kernel(void)
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
 	setup_per_cpu_areas();
+	softirq_early_init();
 	boot_cpu_state_init();
 	smp_prepare_boot_cpu();	/* arch-specific boot-cpu hooks */
 
