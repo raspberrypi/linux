5b0a6fb6c37e0ccfd3a4c3f3a33a48187e5e9113
popcornmix <popcornmix@gmail.com>
2012-10-24 22:00:43 +0100

    Fix reboot with new restart method of machine driver

--- arch/arm/mach-bcm2708/include/mach/system.h
+++ arch/arm/mach-bcm2708/include/mach/system.h
@@ -35,19 +35,4 @@ static inline void arch_idle(void)
 	cpu_do_idle();
 }
 
-static inline void arch_reset(char mode, const char *cmd)
-{
-	uint32_t pm_rstc, pm_wdog;
-	uint32_t timeout = 10;
-
-	/* Setup watchdog for reset */
-	pm_rstc = readl(IO_ADDRESS(PM_RSTC));
-
-	pm_wdog = PM_PASSWORD | (timeout & PM_WDOG_TIME_SET); // watchdog timer = timer clock / 16; need password (31:16) + value (11:0)
-	pm_rstc = PM_PASSWORD | (pm_rstc & PM_RSTC_WRCFG_CLR) | PM_RSTC_WRCFG_FULL_RESET;
-
-	writel(pm_wdog, IO_ADDRESS(PM_WDOG));
-	writel(pm_rstc, IO_ADDRESS(PM_RSTC));
-}
-
 #endif
