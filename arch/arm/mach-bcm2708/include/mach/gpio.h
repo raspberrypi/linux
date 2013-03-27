6632d18fcf68969195dff4d8a68767a543c5d845
popcornmix <popcornmix@gmail.com>
2013-03-27 20:27:09 +0000

    mach-bcm2708: Fix gpio header

--- arch/arm/mach-bcm2708/include/mach/gpio.h
+++ arch/arm/mach-bcm2708/include/mach/gpio.h
@@ -16,32 +16,9 @@
 #include <mach/irqs.h>
 
 #ifdef CONFIG_GPIOLIB
-
-static inline int gpio_get_value(unsigned gpio)
-{
-        return __gpio_get_value(gpio);
-}
-
-static inline void gpio_set_value(unsigned gpio, int value)
-{
-        __gpio_set_value(gpio, value);
-}
-
-static inline int gpio_cansleep(unsigned gpio)
-{
-        return __gpio_cansleep(gpio);
-}
-
-
 static inline unsigned irq_to_gpio(unsigned irq) {
 	return (irq-GPIO_IRQ_START);
 }
-
-static inline unsigned gpio_to_irq(unsigned gpio) {
-	return GPIO_IRQ_START+gpio;
-}
-#define gpio_to_irq gpio_to_irq
-
 #endif /* CONFIG_GPIOLIB */
 
 #endif
