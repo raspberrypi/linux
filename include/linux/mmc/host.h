e21a41779b151baa1ea67f50744c5b6c102d66b2
popcornmix <popcornmix@gmail.com>
2013-05-12 12:24:19 +0100

    Main bcm2708/bcm2709 linux port
    Signed-off-by: popcornmix <popcornmix@gmail.com>
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -311,6 +311,7 @@ struct mmc_host {
 #define MMC_CAP2_HS400_ES	(1 << 20)	/* Host supports enhanced strobe */
 #define MMC_CAP2_NO_SD		(1 << 21)	/* Do not send SD commands during initialization */
 #define MMC_CAP2_NO_MMC		(1 << 22)	/* Do not send (e)MMC commands during initialization */
+#define MMC_CAP2_FORCE_MULTIBLOCK (1 << 31)	/* Always use multiblock transfers */
 
 	mmc_pm_flag_t		pm_caps;	/* supported pm features */
 
