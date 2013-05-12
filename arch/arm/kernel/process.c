6f0468a9f44474189135066bbeb8eecf8d99bebf
popcornmix <popcornmix@gmail.com>
2013-05-12 12:24:19 +0100

    Main bcm2708/bcm2709 linux port
    Signed-off-by: popcornmix <popcornmix@gmail.com>
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -98,6 +98,16 @@ void arch_cpu_idle_dead(void)
 }
 #endif
 
+char bcm2708_reboot_mode = 'h';
+
+int __init reboot_setup(char *str)
+{
+	bcm2708_reboot_mode = str[0];
+	return 1;
+}
+
+__setup("reboot=", reboot_setup);
+
 void __show_regs(struct pt_regs *regs)
 {
 	unsigned long flags;
