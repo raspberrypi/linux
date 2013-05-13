01560c0490b28eb0eb9dbfacd8525a34054ab6c3
hutorny <eugene@hutorny.in.ua>
2013-05-13 10:26:14 +0300

    Update bcm2708.c to use CONFIG_BCM2708_SPIDEV rather than CONFIG_SPI

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -545,7 +545,7 @@ static struct platform_device bcm2708_spi_device = {
 	.resource = bcm2708_spi_resources,
 };
 
-#ifdef CONFIG_SPI
+#ifdef CONFIG_BCM2708_SPIDEV
 static struct spi_board_info bcm2708_spi_devices[] = {
 #ifdef CONFIG_SPI_SPIDEV
 	{
@@ -715,7 +715,7 @@ void __init bcm2708_init(void)
 	system_rev = boardrev;
 	system_serial_low = serial;
 
-#ifdef CONFIG_SPI
+#ifdef CONFIG_BCM2708_SPIDEV
 	spi_register_board_info(bcm2708_spi_devices,
 			ARRAY_SIZE(bcm2708_spi_devices));
 #endif
