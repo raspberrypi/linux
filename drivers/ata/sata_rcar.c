278cbfb57813031b472f6fba7293155f6d71d306
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-05-21 23:07:54 +0400

    sata_rcar: clear STOP bit in bmdma_start() method
    commit df7e131f6359f20ed8f0a37db039c4f6420a18c2 upstream.
    
    Iff bmdma_setup() has to stop a DMA transfer before starting a new
    one, then the STOP bit in the ATAPI_CONTROL1 register will remain set
    (it's only cleared when setting the START bit to 1) and then
    bmdma_start() method will set both START and STOP bits simultaneously
    which should abort the transfer being just started.  Avoid that by
    explicitly clearing the STOP bit in bmdma_start() method (in this case
    it will be ignored on write).
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/ata/sata_rcar.c
+++ drivers/ata/sata_rcar.c
@@ -548,6 +548,7 @@ static void sata_rcar_bmdma_start(struct ata_queued_cmd *qc)
 
 	/* start host DMA transaction */
 	dmactl = ioread32(priv->base + ATAPI_CONTROL1_REG);
+	dmactl &= ~ATAPI_CONTROL1_STOP;
 	dmactl |= ATAPI_CONTROL1_START;
 	iowrite32(dmactl, priv->base + ATAPI_CONTROL1_REG);
 }
