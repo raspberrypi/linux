70fb1fe00efa63272e196549386fc8ad56b9cb1c
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2013-08-29 18:21:04 +0200

    ptrace: fix ptrace vs tasklist_lock race
    As explained by Alexander Fyodorov <halcy@yandex.ru>:
    
    |read_lock(&tasklist_lock) in ptrace_stop() is converted to mutex on RT kernel,
    |and it can remove __TASK_TRACED from task->state (by moving  it to
    |task->saved_state). If parent does wait() on child followed by a sys_ptrace
    |call, the following race can happen:
    |
    |- child sets __TASK_TRACED in ptrace_stop()
    |- parent does wait() which eventually calls wait_task_stopped() and returns
    |  child's pid
    |- child blocks on read_lock(&tasklist_lock) in ptrace_stop() and moves
    |  __TASK_TRACED flag to saved_state
    |- parent calls sys_ptrace, which calls ptrace_check_attach() and wait_task_inactive()
    
    The patch is based on his initial patch where an additional check is
    added in case the __TASK_TRACED moved to ->saved_state. The pi_lock is
    taken in case the caller is interrupted between looking into ->state and
    ->saved_state.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1348,6 +1348,18 @@ out:
 }
 #endif /* CONFIG_NUMA_BALANCING */
 
+static bool check_task_state(struct task_struct *p, long match_state)
+{
+	bool match = false;
+
+	raw_spin_lock_irq(&p->pi_lock);
+	if (p->state == match_state || p->saved_state == match_state)
+		match = true;
+	raw_spin_unlock_irq(&p->pi_lock);
+
+	return match;
+}
+
 /*
  * wait_task_inactive - wait for a thread to unschedule.
  *
@@ -1392,7 +1404,7 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 		 * is actually now running somewhere else!
 		 */
 		while (task_running(rq, p)) {
-			if (match_state && unlikely(p->state != match_state))
+			if (match_state && !check_task_state(p, match_state))
 				return 0;
 			cpu_relax();
 		}
@@ -1407,7 +1419,8 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 		running = task_running(rq, p);
 		queued = task_on_rq_queued(p);
 		ncsw = 0;
-		if (!match_state || p->state == match_state)
+		if (!match_state || p->state == match_state ||
+		    p->saved_state == match_state)
 			ncsw = p->nvcsw | LONG_MIN; /* sets MSB */
 		task_rq_unlock(rq, p, &rf);
 
