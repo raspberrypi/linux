6aae3653929b63575918cfee93363e18de36e2c1
Joerg Faschingbauer <jf@faschingbauer.co.at>
2014-01-14 05:23:21 +0100

    bcm2708: alert mach/gpio.h:gpio_to_irq() users to bogusness
    people who upgrade from earlier version may still use the *ex*
    gpio_to_irq() of *ex* <mach/gpio.h>. in order to alert them to the
    bogusness of the situation, re-add <mach/gpio.> - for the sole purpose
    of placing a #error in it which clarifies what's to be done.

---
+++ arch/arm/mach-bcm2708/include/mach/gpio.h
@@ -0,0 +1,15 @@
+/*
+ * arch/arm/mach-bcm2708/include/mach/gpio.h
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef __ASM_ARCH_GPIO_H
+#define __ASM_ARCH_GPIO_H
+
+#error Use <mach/gpio_irq.h> and __bcm2708_gpio_to_irq() to achieve what you want. <mach/gpio.h> used to contain gpio_to_irq() which conflicted with the gpio subsystem function of the same name in subtle ways.
+
+#endif
+
