173d76aa5fd78eebd9893e201879d3a2df600c4c
Trond Myklebust <trond.myklebust@primarydata.com>
2014-02-02 14:36:42 -0500

    NFSv3: Fix return value of nfs3_proc_setacls
    commit 8f493b9cfcd8941c6b27d6ce8e3b4a78c094b3c1 upstream.
    
    nfs3_proc_setacls is used internally by the NFSv3 create operations
    to set the acl after the file has been created. If the operation
    fails because the server doesn't support acls, then it must return '0',
    not -EOPNOTSUPP.
    
    Reported-by: Russell King <linux@arm.linux.org.uk>
    Link: http://lkml.kernel.org/r/20140201010328.GI15937@n2100.arm.linux.org.uk
    Cc: Christoph Hellwig <hch@lst.de>
    Tested-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Trond Myklebust <trond.myklebust@primarydata.com>
    Acked-by: NeilBrown <neilb@suse.de>
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- fs/nfs/nfs3acl.c
+++ fs/nfs/nfs3acl.c
@@ -289,8 +289,8 @@ getout:
 	return acl;
 }
 
-static int nfs3_proc_setacls(struct inode *inode, struct posix_acl *acl,
-		  struct posix_acl *dfacl)
+static int __nfs3_proc_setacls(struct inode *inode, struct posix_acl *acl,
+			       struct posix_acl *dfacl)
 {
 	struct nfs_server *server = NFS_SERVER(inode);
 	struct nfs_fattr *fattr;
@@ -373,6 +373,15 @@ out:
 	return status;
 }
 
+int nfs3_proc_setacls(struct inode *inode, struct posix_acl *acl,
+		struct posix_acl *dfacl)
+{
+	int ret;
+	ret = __nfs3_proc_setacls(inode, acl, dfacl);
+	return (ret == -EOPNOTSUPP) ? 0 : ret;
+
+}
+
 int nfs3_proc_setacl(struct inode *inode, int type, struct posix_acl *acl)
 {
 	struct posix_acl *alloc = NULL, *dfacl = NULL;
@@ -406,7 +415,7 @@ int nfs3_proc_setacl(struct inode *inode, int type, struct posix_acl *acl)
 		if (IS_ERR(alloc))
 			goto fail;
 	}
-	status = nfs3_proc_setacls(inode, acl, dfacl);
+	status = __nfs3_proc_setacls(inode, acl, dfacl);
 	posix_acl_release(alloc);
 	return status;
 
