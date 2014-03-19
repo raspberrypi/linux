4d22eb083cf667129e9158b407dda5588c89c64a
Sekhar Nori <nsekhar@ti.com>
2014-03-19 11:25:50 +0530

    dma: edma: fix incorrect SG list handling
    commit 5fc68a6cad658e45dca3e0a6607df3a8e5df4ef9 upstream.
    
    The code to handle any length SG lists calls edma_resume()
    even before edma_start() is called. This is incorrect
    because edma_resume() enables edma events on the channel
    after which CPU (in edma_start) cannot clear posted
    events by writing to ECR (per the EDMA user's guide).
    
    Because of this EDMA transfers fail to start if due
    to some reason there is a pending EDMA event registered
    even before EDMA transfers are started. This can happen if
    an EDMA event is a byproduct of device initialization.
    
    Fix this by calling edma_resume() only if it is not the
    first batch of MAX_NR_SG elements.
    
    Without this patch, MMC/SD fails to function on DA850 EVM
    with DMA. The behaviour is triggered by specific IP and
    this can explain why the issue was not reported before
    (example with MMC/SD on AM335x).
    
    Tested on DA850 EVM and AM335x EVM-SK using MMC/SD card.
    
    Cc: Joel Fernandes <joelf@ti.com>
    Acked-by: Joel Fernandes <joelf@ti.com>
    Tested-by: Jon Ringle <jringle@gridpoint.com>
    Tested-by: Alexander Holler <holler@ahsoftware.de>
    Reported-by: Jon Ringle <jringle@gridpoint.com>
    Signed-off-by: Sekhar Nori <nsekhar@ti.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- drivers/dma/edma.c
+++ drivers/dma/edma.c
@@ -170,11 +170,13 @@ static void edma_execute(struct edma_chan *echan)
 	if (edesc->processed == edesc->pset_nr)
 		edma_link(echan->slot[nslots-1], echan->ecc->dummy_slot);
 
-	edma_resume(echan->ch_num);
-
 	if (edesc->processed <= MAX_NR_SG) {
 		dev_dbg(dev, "first transfer starting %d\n", echan->ch_num);
 		edma_start(echan->ch_num);
+	} else {
+		dev_dbg(dev, "chan: %d: completed %d elements, resuming\n",
+			echan->ch_num, edesc->processed);
+		edma_resume(echan->ch_num);
 	}
 
 	/*
