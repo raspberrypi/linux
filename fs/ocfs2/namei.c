6c2d09eef51c2a403ff782c0171f6d127ffd5fc8
Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
2014-04-03 14:47:07 -0700

    ocfs2: fix panic on kfree(xattr->name)
    commit f81c20158f8d5f7938d5eb86ecc42ecc09273ce6 upstream.
    
    Commit 9548906b2bb7 ('xattr: Constify ->name member of "struct xattr"')
    missed that ocfs2 is calling kfree(xattr->name).  As a result, kernel
    panic occurs upon calling kfree(xattr->name) because xattr->name refers
    static constant names.  This patch removes kfree(xattr->name) from
    ocfs2_mknod() and ocfs2_symlink().
    
    Signed-off-by: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Reported-by: Tariq Saeed <tariq.x.saeed@oracle.com>
    Tested-by: Tariq Saeed <tariq.x.saeed@oracle.com>
    Reviewed-by: Srinivas Eeda <srinivas.eeda@oracle.com>
    Cc: Joel Becker <jlbec@evilplan.org>
    Cc: Mark Fasheh <mfasheh@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- fs/ocfs2/namei.c
+++ fs/ocfs2/namei.c
@@ -430,7 +430,6 @@ leave:
 
 	brelse(new_fe_bh);
 	brelse(parent_fe_bh);
-	kfree(si.name);
 	kfree(si.value);
 
 	ocfs2_free_dir_lookup_result(&lookup);
@@ -1818,7 +1817,6 @@ bail:
 
 	brelse(new_fe_bh);
 	brelse(parent_fe_bh);
-	kfree(si.name);
 	kfree(si.value);
 	ocfs2_free_dir_lookup_result(&lookup);
 	if (inode_ac)
