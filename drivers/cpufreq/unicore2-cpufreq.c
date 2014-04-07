1ffc607aaacad8456aae7a76842a8c9c1a1a851a
Chen Gang <gang.chen.5i5j@gmail.com>
2014-04-07 20:04:21 +0800

    cpufreq: unicore32: fix typo issue for 'clk'
    commit b4ddad95020e65cfbbf9aee63d3bcdf682794ade upstream.
    
    Need use 'clk' instead of 'mclk', which is the original removed local
    variable.
    
    The related original commit:
    
      "652ed95 cpufreq: introduce cpufreq_generic_get() routine"
    
    The related error with allmodconfig for unicore32:
    
        CC      drivers/cpufreq/unicore2-cpufreq.o
      drivers/cpufreq/unicore2-cpufreq.c: In function ‘ucv2_target’:
      drivers/cpufreq/unicore2-cpufreq.c:48: error: ‘struct cpufreq_policy’ has no member named ‘mclk’
      make[2]: *** [drivers/cpufreq/unicore2-cpufreq.o] Error 1
      make[1]: *** [drivers/cpufreq] Error 2
      make: *** [drivers] Error 2
    
    Fixes: 652ed95d5fa6 (cpufreq: introduce cpufreq_generic_get() routine)
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/cpufreq/unicore2-cpufreq.c
+++ drivers/cpufreq/unicore2-cpufreq.c
@@ -45,7 +45,7 @@ static int ucv2_target(struct cpufreq_policy *policy,
 	freqs.new = target_freq;
 
 	cpufreq_notify_transition(policy, &freqs, CPUFREQ_PRECHANGE);
-	ret = clk_set_rate(policy->mclk, target_freq * 1000);
+	ret = clk_set_rate(policy->clk, target_freq * 1000);
 	cpufreq_notify_post_transition(policy, &freqs, ret);
 
 	return ret;
