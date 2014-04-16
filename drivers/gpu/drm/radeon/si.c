5bbd7bffabfb02b0467ccd3c6ade2b32c738eb70
Alex Deucher <alexdeucher@gmail.com>
2014-04-16 09:42:22 -0400

    drm/radeon/si: make sure mc ucode is loaded before checking the size
    commit 8c79bae6a30f606b7a4e17c994bc5f72f8fdaf11 upstream.
    
    Avoid a possible segfault.
    
    Noticed-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- drivers/gpu/drm/radeon/si.c
+++ drivers/gpu/drm/radeon/si.c
@@ -1475,10 +1475,12 @@ static int si_mc_load_microcode(struct radeon_device *rdev)
 	const __be32 *fw_data;
 	u32 running, blackout = 0;
 	u32 *io_mc_regs;
-	int i, regs_size, ucode_size = rdev->mc_fw->size / 4;
+	int i, regs_size, ucode_size;
 
 	if (!rdev->mc_fw)
 		return -EINVAL;
+
+	ucode_size = rdev->mc_fw->size / 4;
 
 	switch (rdev->family) {
 	case CHIP_TAHITI:
