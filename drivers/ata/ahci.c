0db2ae62e61221dd834d00d1e86f34eabbeac73f
Alexander Gordeev <agordeev@redhat.com>
2014-04-17 14:13:49 +0200

    ahci: Ensure "MSI Revert to Single Message" mode is not enforced
    commit ab0f9e78b97f5193dd38b3757b42b6fbded05fb7 upstream.
    
    The AHCI specification allows hardware to choose to revert to
    single MSI mode when fewer messages are allocated than requested.
    Yet, at least ICH10 chipset reverts to single MSI mode even when
    enough messages are allocated in some cases (see below).
    
    This update forces the driver to not rely on initialization of
    multiple MSIs mode alone and always check if "MSI Revert to
    Single Message" (MRSM) mode was enforced by the controller and
    fallback to the single MSI mode in case it did.
    
    That prevents a situation when the driver configured multiple
    per-port IRQ handlers, but the controller sends all port's
    interrupts to a single IRQ, which could easily screw up the
    interrupt handling and lead to delays and possibly crashes.
    
    The fix was tested on a 6-port controller that successfully
    reverted to the single MSI mode:
    
    00:1f.2 SATA controller: Intel Corporation 82801JI (ICH10 Family) SATA
    AHCI Controller (prog-if 01 [AHCI 1.0])
    	Subsystem: Super Micro Computer Inc Device 10a7
    	Flags: bus master, 66MHz, medium devsel, latency 0, IRQ 101
    	I/O ports at f110 [size=8]
    	I/O ports at f100 [size=4]
    	I/O ports at f0f0 [size=8]
    	I/O ports at f0e0 [size=4]
    	I/O ports at f020 [size=32]
    	Memory at fbf00000 (32-bit, non-prefetchable) [size=2K]
    	Capabilities: [80] MSI: Enable+ Count=1/16 Maskable- 64bit-
    	Capabilities: [70] Power Management version 3
    	Capabilities: [a8] SATA HBA v1.0
    	Capabilities: [b0] PCI Advanced Features
    	Kernel driver in use: ahci
    
    With 6 ports just 8 MSI vectors should be enough, but the adapter
    enforces the MRSM mode when less than 16 vectors are written to
    the Multiple Messages Enable PCI register. I instigated MRSM mode
    by forcing @nvec to 8 in ahci_init_interrupts().
    
    Signed-off-by: Alexander Gordeev <agordeev@redhat.com>
    Cc: linux-ide@vger.kernel.org
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/ata/ahci.c
+++ drivers/ata/ahci.c
@@ -1163,7 +1163,7 @@ static inline void ahci_gtf_filter_workaround(struct ata_host *host)
 #endif
 
 static int ahci_init_interrupts(struct pci_dev *pdev, unsigned int n_ports,
-			 struct ahci_host_priv *hpriv)
+				struct ahci_host_priv *hpriv)
 {
 	int rc, nvec;
 
@@ -1188,6 +1188,13 @@ static int ahci_init_interrupts(struct pci_dev *pdev, unsigned int n_ports,
 		goto intx;
 	else if (rc > 0)
 		goto single_msi;
+
+	/* fallback to single MSI mode if the controller enforced MRSM mode */
+	if (readl(hpriv->mmio + HOST_CTL) & HOST_MRSM) {
+		pci_disable_msi(pdev);
+		printk(KERN_INFO "ahci: MRSM is on, fallback to single MSI\n");
+		goto single_msi;
+	}
 
 	return nvec;
 
