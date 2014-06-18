a636c6386c08a840163b6b5319cfe5b35c80e053
Eric Anholt <eric@anholt.net>
2014-06-18 15:55:34 +0100

    drm/vc4: Start building the command validator.
    This rendered a triangle with an even more hacked-up hackdriver.
    
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -38,6 +38,49 @@ to_vc4_dev(struct drm_device *dev)
 	return (struct vc4_dev *)dev;
 }
 
+struct exec_info {
+	/* This is the array of BOs that were looked up at the start of exec.
+	 * Command validation will use indices into this array.
+	 */
+	struct drm_gem_cma_object **bo;
+	uint32_t bo_count;
+
+	/* Current indices into @bo loaded by the non-hardware packet
+	 * that passes in indices.  This can be used even without
+	 * checking that we've seen one of those packets, because
+	 * @bo_count is always >= 1, and this struct is initialized to
+	 * 0.
+	 */
+	uint32_t bo_index[2];
+	uint32_t max_width, max_height;
+
+	/**
+	 * This is the BO where we store the validated command lists
+	 * and shader records.
+	 */
+	struct drm_gem_cma_object *exec_bo;
+
+	/**
+	 * This tracks the per-shader-record state (packet 64) that
+	 * determines the length of the shader record and the offset
+	 * it's expected to be found at.  It gets read in from the
+	 * command lists.
+	 */
+	uint32_t *shader_state;
+	/** How many shader states the user declared they were using. */
+	uint32_t shader_state_size;
+	/** How many shader state records the validator has seen. */
+	uint32_t shader_state_count;
+
+	/**
+	 * Computed addresses pointing into exec_bo where we start the
+	 * bin thread (ct0) and render thread (ct1).
+	 */
+	uint32_t ct0ca, ct0ea;
+	uint32_t ct1ca, ct1ea;
+	uint32_t shader_paddr;
+};
+
 #define VC4_READ(offset) readl(to_vc4_dev(dev)->vc4_regs + offset)
 #define VC4_WRITE(offset, val) writel(val, to_vc4_dev(dev)->vc4_regs + offset)
 
@@ -51,3 +94,19 @@ int vc4_modeset_init(struct drm_device *dev);
 /* vc4_gem.c */
 int vc4_submit_cl_ioctl(struct drm_device *dev, void *data,
 			struct drm_file *file_priv);
+
+/* vc4_validate.c */
+int
+vc4_validate_cl(struct drm_device *dev,
+		void *validated,
+		void *unvalidated,
+		uint32_t len,
+		bool is_bin,
+		struct exec_info *exec);
+
+int
+vc4_validate_shader_recs(struct drm_device *dev,
+			 void *validated,
+			 void *unvalidated,
+			 uint32_t len,
+			 struct exec_info *exec);
