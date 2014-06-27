9c7eaf3d62fddd15beed004623b05282f9a6f23b
Thomas Gleixner <tglx@linutronix.de>
2014-06-27 16:24:52 +0200

    workqueue: Prevent deadlock/stall on RT
    Austin reported a XFS deadlock/stall on RT where scheduled work gets
    never exececuted and tasks are waiting for each other for ever.
    
    The underlying problem is the modification of the RT code to the
    handling of workers which are about to go to sleep. In mainline a
    worker thread which goes to sleep wakes an idle worker if there is
    more work to do. This happens from the guts of the schedule()
    function. On RT this must be outside and the accessed data structures
    are not protected against scheduling due to the spinlock to rtmutex
    conversion. So the naive solution to this was to move the code outside
    of the scheduler and protect the data structures by the pool
    lock. That approach turned out to be a little naive as we cannot call
    into that code when the thread blocks on a lock, as it is not allowed
    to block on two locks in parallel. So we dont call into the worker
    wakeup magic when the worker is blocked on a lock, which causes the
    deadlock/stall observed by Austin and Mike.
    
    Looking deeper into that worker code it turns out that the only
    relevant data structure which needs to be protected is the list of
    idle workers which can be woken up.
    
    So the solution is to protect the list manipulation operations with
    preempt_enable/disable pairs on RT and call unconditionally into the
    worker code even when the worker is blocked on a lock. The preemption
    protection is safe as there is nothing which can fiddle with the list
    outside of thread context.
    
    Reported-and_tested-by: Austin Schuh <austin@peloton-tech.com>
    Reported-and_tested-by: Mike Galbraith <umgwanakikbuti@gmail.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: http://vger.kernel.org/r/alpine.DEB.2.10.1406271249510.5170@nanos
    Cc: Richard Weinberger <richard.weinberger@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3568,9 +3568,8 @@ void __noreturn do_task_dead(void)
 
 static inline void sched_submit_work(struct task_struct *tsk)
 {
-	if (!tsk->state || tsk_is_pi_blocked(tsk))
+	if (!tsk->state)
 		return;
-
 	/*
 	 * If a worker went to sleep, notify and ask workqueue whether
 	 * it wants to wake up a task to maintain concurrency.
@@ -3583,6 +3582,9 @@ static inline void sched_submit_work(struct task_struct *tsk)
 		wq_worker_sleeping(tsk);
 		preempt_enable_no_resched();
 	}
+
+	if (tsk_is_pi_blocked(tsk))
+		return;
 
 	/*
 	 * If we are going to sleep and we have plugged IO queued,
