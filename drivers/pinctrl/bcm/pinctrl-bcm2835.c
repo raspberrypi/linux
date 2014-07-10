d3b3bf3252627b5cbd051d241551c4fe8c9f494e
notro <notro@tronnes.org>
2014-07-10 13:59:47 +0200

    pinctrl-bcm2835: Set base to 0 give expected gpio numbering
    Signed-off-by: Noralf Tronnes <notro@tronnes.org>
    
    SQUASH: pinctrl: bcm2835: Set base for bcm2711 GPIO to 0
    
    Without this patch GPIOs don't seem to work properly, primarily
    noticeable as broken LEDs.
    
    Squash with "pinctrl-bcm2835: Set base to 0 give expected gpio numbering"
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/pinctrl/bcm/pinctrl-bcm2835.c
+++ drivers/pinctrl/bcm/pinctrl-bcm2835.c
@@ -369,7 +369,7 @@ static const struct gpio_chip bcm2835_gpio_chip = {
 	.get = bcm2835_gpio_get,
 	.set = bcm2835_gpio_set,
 	.set_config = gpiochip_generic_config,
-	.base = -1,
+	.base = 0,
 	.ngpio = BCM2835_NUM_GPIOS,
 	.can_sleep = false,
 };
@@ -385,7 +385,7 @@ static const struct gpio_chip bcm2711_gpio_chip = {
 	.get = bcm2835_gpio_get,
 	.set = bcm2835_gpio_set,
 	.set_config = gpiochip_generic_config,
-	.base = -1,
+	.base = 0,
 	.ngpio = BCM2711_NUM_GPIOS,
 	.can_sleep = false,
 };
