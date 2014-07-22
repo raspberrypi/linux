284f30fce9ada02e73e24045645f2f46d447dbec
Tim Gover <tgover@broadcom.com>
2014-07-22 15:41:04 +0100

    vcsm: VideoCore shared memory service for BCM2835
    Add experimental support for the VideoCore shared memory service.
    This allows user processes to allocate memory from VideoCore's
    GPU relocatable heap and mmap the buffers. Additionally, the memory
    handles can passed to other VideoCore services such as MMAL, OpenMax
    and DispmanX
    
    TODO
    * This driver was originally released for BCM28155 which has a different
      cache architecture to BCM2835. Consequently, in this release only
      uncached mappings are supported. However, there's no fundamental
      reason which cached mappings cannot be support or BCM2835
    * More refactoring is required to remove the typedefs.
    * Re-enable the some of the commented out debug-fs statistics which were
      disabled when migrating code from proc-fs.
    * There's a lot of code to support sharing of VCSM in order to support
      Android. This could probably done more cleanly or perhaps just
      removed.
    
    Signed-off-by: Tim Gover <timgover@gmail.com>
    
    config: Disable VC_SM for now to fix hang with cutdown kernel
    
    vcsm: Use boolean as it cannot be built as module
    
    On building the bcm_vc_sm as a module we get the following error:
    
    v7_dma_flush_range and do_munmap are undefined in vc-sm.ko.
    
    Fix by making it not an option to build as module
    
    vcsm: Add ioctl for custom cache flushing
    
    vc-sm: Move headers out of arch directory
    
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>

--- drivers/char/broadcom/vc_sm/vmcs_sm.c
+++ drivers/char/broadcom/vc_sm/vmcs_sm.c
@@ -197,14 +197,12 @@ struct SM_STATE_T {
 static struct SM_STATE_T *sm_state;
 static int sm_inited;
 
-#if 0
 static const char *const sm_cache_map_vector[] = {
 	"(null)",
 	"host",
 	"videocore",
 	"host+videocore",
 };
-#endif
 
 /* ---- Private Function Prototypes -------------------------------------- */
 
