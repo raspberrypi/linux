e4c353d25fcefd4be9a85f696a2775426d956523
Semen Protsenko <semen.protsenko@linaro.org>
2014-08-15 16:22:44 +0300

    efi/arm64: Store Runtime Services revision
    commit 6a7519e81321343165f89abb8b616df186d3e57a upstream.
    
    "efi" global data structure contains "runtime_version" field which must
    be assigned in order to use it later in Runtime Services virtual calls
    (virt_efi_* functions).
    
    Before this patch "runtime_version" was unassigned (0), so each
    Runtime Service virtual call that checks revision would fail.
    
    Signed-off-by: Semen Protsenko <semen.protsenko@linaro.org>
    Acked-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/efi.c
+++ arch/arm64/kernel/efi.c
@@ -464,6 +464,8 @@ static int __init arm64_enter_virtual_mode(void)
 
 	set_bit(EFI_RUNTIME_SERVICES, &efi.flags);
 
+	efi.runtime_version = efi.systab->hdr.revision;
+
 	return 0;
 }
 early_initcall(arm64_enter_virtual_mode);
