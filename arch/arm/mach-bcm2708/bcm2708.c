93dd057dac08cca013510fabad88b358efd21277
Serge Schneider <serge@raspberrypi.org>
2014-09-03 14:44:22 +0100

    I2C: Only register the I2C device for the current board revision

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -93,6 +93,7 @@ static unsigned disk_led_active_low = 1;
 static unsigned reboot_part = 0;
 static unsigned w1_gpio_pin = W1_GPIO;
 static unsigned w1_gpio_pullup = W1_PULLUP;
+static bool vc_i2c_override = false;
 
 static unsigned use_dt = 0;
 
@@ -862,8 +863,15 @@ void __init bcm2708_init(void)
 	bcm_register_device(&bcm2835_thermal_device);
 
 	bcm_register_device_dt(&bcm2708_spi_device);
-	bcm_register_device_dt(&bcm2708_bsc0_device);
-	bcm_register_device_dt(&bcm2708_bsc1_device);
+
+	if (vc_i2c_override) {
+		bcm_register_device_dt(&bcm2708_bsc0_device);
+		bcm_register_device_dt(&bcm2708_bsc1_device);
+	} else if ((boardrev & 0xffffff) == 0x2 || (boardrev & 0xffffff) == 0x3) {
+		bcm_register_device_dt(&bcm2708_bsc0_device);
+	} else {
+		bcm_register_device_dt(&bcm2708_bsc1_device);
+	}
 
 #if defined(CONFIG_SND_BCM2708_SOC_I2S) || defined(CONFIG_SND_BCM2708_SOC_I2S_MODULE)
 	bcm_register_device_dt(&bcm2708_i2s_device);
@@ -1093,3 +1101,5 @@ module_param(disk_led_active_low, uint, 0644);
 module_param(reboot_part, uint, 0644);
 module_param(w1_gpio_pin, uint, 0644);
 module_param(w1_gpio_pullup, uint, 0644);
+module_param(vc_i2c_override, bool, 0644);
+MODULE_PARM_DESC(vc_i2c_override, "Allow the use of VC's I2C peripheral.");
