8f91119341c98f7645777985e23e34600957c234
Lan Tianyu <tianyu.lan@intel.com>
2014-09-18 15:03:07 +0800

    cpufreq: fix cpufreq suspend/resume for intel_pstate
    commit 8e30444e153008e8eb3e74cbcb7a865bfcfb04a0 upstream.
    
    Cpufreq core introduces cpufreq_suspended flag to let cpufreq sysfs nodes
    across S2RAM/S2DISK. But the flag is only set in the cpufreq_suspend()
    for cpufreq drivers which have target or target_index callback. This
    skips intel_pstate driver. This patch is to set the flag before checking
    target or target_index callback.
    
    Fixes: 2f0aea936360 (cpufreq: suspend governors on system suspend/hibernate)
    Signed-off-by: Lan Tianyu <tianyu.lan@intel.com>
    [rjw: Subject]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1666,6 +1666,8 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpufreq_suspended = true;
+
 	if (!has_target())
 		return;
 
@@ -1680,8 +1682,6 @@ void cpufreq_suspend(void)
 			pr_err("%s: Failed to suspend driver: %p\n", __func__,
 				policy);
 	}
-
-	cpufreq_suspended = true;
 }
 
 /**
@@ -1697,12 +1697,12 @@ void cpufreq_resume(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpufreq_suspended = false;
+
 	if (!has_target())
 		return;
 
 	pr_debug("%s: Resuming Governors\n", __func__);
-
-	cpufreq_suspended = false;
 
 	list_for_each_entry(policy, &cpufreq_policy_list, policy_list) {
 		if (cpufreq_driver->resume && cpufreq_driver->resume(policy))
