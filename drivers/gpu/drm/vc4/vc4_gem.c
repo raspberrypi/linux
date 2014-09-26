3f57bab50c243821715b8ed538771d9a28e25d56
Eric Anholt <eric@anholt.net>
2014-09-26 14:08:37 -0700

    drm/vc4: Don't forget to flush the other VC4 caches.
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- drivers/gpu/drm/vc4/vc4_gem.c
+++ drivers/gpu/drm/vc4/vc4_gem.c
@@ -164,6 +164,23 @@ wait_for_render_thread(struct drm_device *dev, u32 initial_rfc)
 }
 */
 
+static void
+vc4_flush_caches(struct drm_device *dev)
+{
+	struct vc4_dev *vc4 = to_vc4_dev(dev);
+
+	V3D_WRITE(V3D_L2CACTL,
+		  V3D_L2CACTL_L2CCLR);
+
+	V3D_WRITE(V3D_SLCACTL,
+		  VC4_SET_FIELD(0xf, V3D_SLCACTL_T1CC) |
+		  VC4_SET_FIELD(0xf, V3D_SLCACTL_T0CC) |
+		  VC4_SET_FIELD(0xf, V3D_SLCACTL_UCC) |
+		  VC4_SET_FIELD(0xf, V3D_SLCACTL_ICC));
+
+	barrier();
+}
+
 static int
 vc4_submit(struct drm_device *dev, struct exec_info *exec)
 {
@@ -172,9 +189,7 @@ vc4_submit(struct drm_device *dev, struct exec_info *exec)
 	uint32_t ct1ca = exec->ct1ca, ct1ea = exec->ct1ea;
 	int ret;
 
-	/* flushes caches */
-	V3D_WRITE(V3D_L2CACTL, 1 << 2);
-	barrier();
+	vc4_flush_caches(dev);
 
 	/* Disable the binner's pre-loaded overflow memory address */
 	V3D_WRITE(V3D_BPOA, 0);
