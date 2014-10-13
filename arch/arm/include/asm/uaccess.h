25b5c8e286f3a2b1779c63754b8993107bb416b0
Phil Elwell <phil@raspberrypi.org>
2014-10-13 11:47:53 +0100

    Improve __copy_to_user and __copy_from_user performance
    Provide a __copy_from_user that uses memcpy. On BCM2708, use
    optimised memcpy/memmove/memcmp/memset implementations.

--- arch/arm/include/asm/uaccess.h
+++ arch/arm/include/asm/uaccess.h
@@ -475,6 +475,7 @@ do {									\
 
 #ifdef CONFIG_MMU
 extern unsigned long __must_check __copy_from_user(void *to, const void __user *from, unsigned long n);
+extern unsigned long __must_check __copy_from_user_std(void *to, const void __user *from, unsigned long n);
 extern unsigned long __must_check __copy_to_user(void __user *to, const void *from, unsigned long n);
 extern unsigned long __must_check __copy_to_user_std(void __user *to, const void *from, unsigned long n);
 extern unsigned long __must_check __clear_user(void __user *addr, unsigned long n);
