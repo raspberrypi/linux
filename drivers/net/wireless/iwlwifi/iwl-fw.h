fb14996c2a555322ae2610d783e614430b259592
Luciano Coelho <luciano.coelho@intel.com>
2014-10-21 16:12:18 +0300

    iwlwifi: mvm: check TLV flag before trying to use hotspot firmware commands
    commit 5ac6c72e594471acfa5b00210c51d533a73413ad upstream.
    
    Older firmwares do not provide support for the HOT_SPOT_CMD command.
    Check for the appropriate TLV flag that declares hotspot support in
    the firmware to prevent a firmware assertion failure that can be
    triggered from the userspace,
    
    Signed-off-by: Luciano Coelho <luciano.coelho@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/iwlwifi/iwl-fw.h
+++ drivers/net/wireless/iwlwifi/iwl-fw.h
@@ -138,9 +138,11 @@ enum iwl_ucode_tlv_api {
 /**
  * enum iwl_ucode_tlv_capa - ucode capabilities
  * @IWL_UCODE_TLV_CAPA_D0I3_SUPPORT: supports D0i3
+ * @IWL_UCODE_TLV_CAPA_HOTSPOT_SUPPORT: supports Hot Spot Command
  */
 enum iwl_ucode_tlv_capa {
-	IWL_UCODE_TLV_CAPA_D0I3_SUPPORT		= BIT(0),
+	IWL_UCODE_TLV_CAPA_D0I3_SUPPORT			= BIT(0),
+	IWL_UCODE_TLV_CAPA_HOTSPOT_SUPPORT		= BIT(18),
 };
 
 /* The default calibrate table size if not specified by firmware file */
