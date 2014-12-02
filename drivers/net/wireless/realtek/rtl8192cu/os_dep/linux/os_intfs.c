3ff3507a7a43a0afe2e8b13243b61631b484f8bc
David Lechner <david@lechnology.com>
2014-12-01 19:18:31 -0600

    Set dev_type to wlan
    Fixes #23

--- drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/os_intfs.c
@@ -1062,6 +1062,10 @@ int rtw_init_netdev_name(struct net_device *pnetdev, const char *ifname)
 	return 0;
 }
 
+static const struct device_type wlan_type = {
+	.name = "wlan",
+};
+
 struct net_device *rtw_init_netdev(_adapter *old_padapter)
 {
 	_adapter *padapter;
@@ -1077,6 +1081,7 @@ struct net_device *rtw_init_netdev(_adapter *old_padapter)
 	if (!pnetdev)
 		return NULL;
 
+	pnetdev->dev.type = &wlan_type;
 	padapter = rtw_netdev_priv(pnetdev);
 	padapter->pnetdev = pnetdev;
 
