665c11f53f2b74610818e2840d9585327a407df3
popcornmix <popcornmix@gmail.com>
2015-02-10 11:19:21 +0000

    rtl8192cu: Add PID for D-Link DWA 131

--- drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/usb_intf.c
+++ drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/usb_intf.c
@@ -138,6 +138,7 @@ static void rtw_dev_remove(struct usb_interface *pusb_intf);
 	{USB_DEVICE(0x2001, 0x3307)},/* D-Link - Cameo */ \
 	{USB_DEVICE(0x2001, 0x330A)},/* D-Link - Alpha */ \
 	{USB_DEVICE(0x2001, 0x3309)},/* D-Link - Alpha */ \
+	{USB_DEVICE(0x2001, 0x330D)},/* D-Link - DWA 131 */ \
 	{USB_DEVICE(0x0586, 0x341F)},/* Zyxel - Abocom */ \
 	{USB_DEVICE(0x7392, 0x7822)},/* Edimax - Edimax */ \
 	{USB_DEVICE(0x2019, 0xAB2B)},/* Planex - Abocom */ \
