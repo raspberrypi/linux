51f3eddd039a053a3ae578d85c46662fff625b54
Aaro Koskinen <aaro.koskinen@iki.fi>
2015-03-04 23:08:49 +0200

    MIPS: OCTEON: dma-octeon: fix OHCI USB config check
    [ Upstream commit a8667d706dfa394ef9fe5f9013dee92d40a096e8 ]
    
    CONFIG_USB_OCTEON_OHCI is deprecated and no longer needed to use OHCI
    on OCTEON II. Instead, CONFIG_USB_OHCI_HCD_PLATFORM should be used.
    
    Signed-off-by: Aaro Koskinen <aaro.koskinen@iki.fi>
    Cc: Aleksey Makarov <aleksey.makarov@auriga.com>
    Cc: David Daney <david.daney@cavium.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/9421/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- arch/mips/cavium-octeon/dma-octeon.c
+++ arch/mips/cavium-octeon/dma-octeon.c
@@ -306,7 +306,7 @@ void __init plat_swiotlb_setup(void)
 		swiotlbsize = 64 * (1<<20);
 	}
 #endif
-#ifdef CONFIG_USB_OCTEON_OHCI
+#ifdef CONFIG_USB_OHCI_HCD_PLATFORM
 	/* OCTEON II ohci is only 32-bit. */
 	if (OCTEON_IS_MODEL(OCTEON_CN6XXX) && max_addr >= 0x100000000ul)
 		swiotlbsize = 64 * (1<<20);
