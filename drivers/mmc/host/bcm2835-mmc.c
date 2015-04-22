7d253ca5b2a75bfc541b6f8549c36f1cf96e2911
popcornmix <popcornmix@gmail.com>
2015-04-22 20:31:12 +0100

    bcm2835-mmc: Add option to disable some delays

--- drivers/mmc/host/bcm2835-mmc.c
+++ drivers/mmc/host/bcm2835-mmc.c
@@ -72,6 +72,7 @@ pr_debug(DRIVER_NAME " [%s()]: " f, __func__, ## x)
 
 
 /*static */unsigned mmc_debug;
+/*static */unsigned mmc_debug2;
 
 struct bcm2835_host {
 	spinlock_t				lock;
@@ -153,7 +154,7 @@ static inline u32 bcm2835_mmc_axi_outstanding_writes(void)
 	return (r >> 16) & 0x3ff;
 }
 
-static inline void bcm2835_mmc_writel(struct bcm2835_host *host, u32 val, int reg)
+static inline void bcm2835_mmc_writel(struct bcm2835_host *host, u32 val, int reg, int from)
 {
 	u32 delay;
 	if (mmc_debug & (1<<0))
@@ -167,7 +168,7 @@ static inline void bcm2835_mmc_writel(struct bcm2835_host *host, u32 val, int re
 	udelay(BCM2835_SDHCI_WRITE_DELAY(max(host->clock, MIN_FREQ)));
 
 	delay = ((mmc_debug >> 16) & 0xf) << ((mmc_debug >> 20) & 0xf);
-	if (delay)
+	if (delay && !((1<<from) & mmc_debug2))
 		udelay(delay);
 
 	if (mmc_debug & (1<<2))
@@ -235,7 +236,7 @@ static inline void bcm2835_mmc_writew(struct bcm2835_host *host, u16 val, int re
 	if (reg == SDHCI_TRANSFER_MODE)
 		host->shadow = newval;
 	else
-		bcm2835_mmc_writel(host, newval, reg & ~3);
+		bcm2835_mmc_writel(host, newval, reg & ~3, 0);
 
 }
 
@@ -247,7 +248,7 @@ static inline void bcm2835_mmc_writeb(struct bcm2835_host *host, u8 val, int reg
 	u32 mask = 0xff << byte_shift;
 	u32 newval = (oldval & ~mask) | (val << byte_shift);
 
-	bcm2835_mmc_writel(host, newval, reg & ~3);
+	bcm2835_mmc_writel(host, newval, reg & ~3, 1);
 }
 
 
@@ -279,7 +280,7 @@ static void bcm2835_mmc_unsignal_irqs(struct bcm2835_host *host, u32 clear)
 	ier &= ~clear;
 	/* change which requests generate IRQs - makes no difference to
 	   the content of SDHCI_INT_STATUS, or the need to acknowledge IRQs */
-	bcm2835_mmc_writel(host, ier, SDHCI_SIGNAL_ENABLE);
+	bcm2835_mmc_writel(host, ier, SDHCI_SIGNAL_ENABLE, 2);
 }
 
 
@@ -373,8 +374,8 @@ static void bcm2835_mmc_init(struct bcm2835_host *host, int soft)
 		    SDHCI_INT_TIMEOUT | SDHCI_INT_DATA_END |
 		    SDHCI_INT_RESPONSE;
 
-	bcm2835_mmc_writel(host, host->ier, SDHCI_INT_ENABLE);
-	bcm2835_mmc_writel(host, host->ier, SDHCI_SIGNAL_ENABLE);
+	bcm2835_mmc_writel(host, host->ier, SDHCI_INT_ENABLE, 3);
+	bcm2835_mmc_writel(host, host->ier, SDHCI_SIGNAL_ENABLE, 3);
 
 	if (soft) {
 		/* force clock reconfiguration */
@@ -596,8 +597,8 @@ static void bcm2835_mmc_set_transfer_irqs(struct bcm2835_host *host)
 	else
 		host->ier = (host->ier & ~dma_irqs) | pio_irqs;
 
-	bcm2835_mmc_writel(host, host->ier, SDHCI_INT_ENABLE);
-	bcm2835_mmc_writel(host, host->ier, SDHCI_SIGNAL_ENABLE);
+	bcm2835_mmc_writel(host, host->ier, SDHCI_INT_ENABLE, 4);
+	bcm2835_mmc_writel(host, host->ier, SDHCI_SIGNAL_ENABLE, 4);
 }
 
 
@@ -679,7 +680,7 @@ static void bcm2835_mmc_set_transfer_mode(struct bcm2835_host *host,
 			mode |= SDHCI_TRNS_AUTO_CMD12;
 		else if (host->mrq->sbc && (host->flags & SDHCI_AUTO_CMD23)) {
 			mode |= SDHCI_TRNS_AUTO_CMD23;
-			bcm2835_mmc_writel(host, host->mrq->sbc->arg, SDHCI_ARGUMENT2);
+			bcm2835_mmc_writel(host, host->mrq->sbc->arg, SDHCI_ARGUMENT2, 5);
 		}
 	}
 
@@ -742,7 +743,7 @@ void bcm2835_mmc_send_command(struct bcm2835_host *host, struct mmc_command *cmd
 
 	bcm2835_mmc_prepare_data(host, cmd);
 
-	bcm2835_mmc_writel(host, cmd->arg, SDHCI_ARGUMENT);
+	bcm2835_mmc_writel(host, cmd->arg, SDHCI_ARGUMENT, 6);
 
 	bcm2835_mmc_set_transfer_mode(host, cmd);
 
@@ -899,8 +900,8 @@ static void bcm2835_mmc_enable_sdio_irq_nolock(struct bcm2835_host *host, int en
 		else
 			host->ier &= ~SDHCI_INT_CARD_INT;
 
-		bcm2835_mmc_writel(host, host->ier, SDHCI_INT_ENABLE);
-		bcm2835_mmc_writel(host, host->ier, SDHCI_SIGNAL_ENABLE);
+		bcm2835_mmc_writel(host, host->ier, SDHCI_INT_ENABLE, 7);
+		bcm2835_mmc_writel(host, host->ier, SDHCI_SIGNAL_ENABLE, 7);
 		mmiowb();
 	}
 }
@@ -1047,7 +1048,7 @@ static irqreturn_t bcm2835_mmc_irq(int irq, void *dev_id)
 		/* Clear selected interrupts. */
 		mask = intmask & (SDHCI_INT_CMD_MASK | SDHCI_INT_DATA_MASK |
 				  SDHCI_INT_BUS_POWER);
-		bcm2835_mmc_writel(host, mask, SDHCI_INT_STATUS);
+		bcm2835_mmc_writel(host, mask, SDHCI_INT_STATUS, 8);
 
 
 		if (intmask & SDHCI_INT_CMD_MASK)
@@ -1077,7 +1078,7 @@ static irqreturn_t bcm2835_mmc_irq(int irq, void *dev_id)
 
 		if (intmask) {
 			unexpected |= intmask;
-			bcm2835_mmc_writel(host, intmask, SDHCI_INT_STATUS);
+			bcm2835_mmc_writel(host, intmask, SDHCI_INT_STATUS, 9);
 		}
 
 		if (result == IRQ_NONE)
@@ -1363,7 +1364,7 @@ int bcm2835_mmc_add_host(struct bcm2835_host *host)
 
 	spin_lock_init(&host->lock);
 
-dev_info(dev, "mmc_debug:%x\n", mmc_debug);
+dev_info(dev, "mmc_debug:%x mmc_debug2:%x\n", mmc_debug, mmc_debug2);
 if (mmc_debug & (1<<12)) {
 	dev_info(dev, "Forcing PIO mode\n");
 	host->have_dma = false;
@@ -1576,6 +1577,7 @@ static struct platform_driver bcm2835_mmc_driver = {
 module_platform_driver(bcm2835_mmc_driver);
 
 module_param(mmc_debug, uint, 0644);
+module_param(mmc_debug2, uint, 0644);
 MODULE_ALIAS("platform:mmc-bcm2835");
 MODULE_DESCRIPTION("BCM2835 SDHCI driver");
 MODULE_LICENSE("GPL v2");
