3268698b51766a1995b686da14161fbfeeb5cc1f
popcornmix <popcornmix@gmail.com>
2015-04-28 19:29:54 +0100

    bcm2835-mmc: Default to disabling MMC_QUIRK_BLK_NO_CMD23

--- drivers/mmc/core/quirks.c
+++ drivers/mmc/core/quirks.c
@@ -71,6 +71,7 @@ static const struct mmc_fixup mmc_fixup_methods[] = {
 
 void mmc_fixup_device(struct mmc_card *card, const struct mmc_fixup *table)
 {
+	extern unsigned mmc_debug;
 	const struct mmc_fixup *f;
 	u64 rev = cid_rev_card(card);
 
@@ -98,8 +99,7 @@ void mmc_fixup_device(struct mmc_card *card, const struct mmc_fixup *table)
 	/* SDHCI on BCM2708 - bug causes a certain sequence of CMD23 operations to fail.
 	 * Disable this flag for all cards (fall-back to CMD25/CMD18 multi-block transfers).
 	 */
-	extern unsigned mmc_debug;
-	if (!(mmc_debug & (1<<13)))
+	if (mmc_debug & (1<<13))
 	card->quirks |= MMC_QUIRK_BLK_NO_CMD23;
 }
 EXPORT_SYMBOL(mmc_fixup_device);
