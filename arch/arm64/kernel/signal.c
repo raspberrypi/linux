9b46d91f49786266c66f5c8d81e8428f5916871c
Anders Roxell <anders.roxell@linaro.org>
2015-05-14 17:52:17 +0200

    arch/arm64: Add lazy preempt support
    arm64 is missing support for PREEMPT_RT. The main feature which is
    lacking is support for lazy preemption. The arch-specific entry code,
    thread information structure definitions, and associated data tables
    have to be extended to provide this support. Then the Kconfig file has
    to be extended to indicate the support is available, and also to
    indicate that support for full RT preemption is now available.
    
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -756,7 +756,7 @@ asmlinkage void do_notify_resume(struct pt_regs *regs,
 		/* Check valid user FS if needed */
 		addr_limit_user_check();
 
-		if (thread_flags & _TIF_NEED_RESCHED) {
+		if (thread_flags & _TIF_NEED_RESCHED_MASK) {
 			schedule();
 		} else {
 			local_irq_enable();
