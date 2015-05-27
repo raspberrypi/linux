678a7805a625963cd4f1ef8ba869e62962ccbd1d
Phil Elwell <phil@raspberrypi.org>
2015-05-27 17:22:15 +0100

    bcm2835-audio: Create the platform device if the DT node is disabled
    For backwards compatibility, allow the built-in ALSA driver to be enabled
    either by loading the module from /etc/modules or by enabling the "/audio"
    node in DT.

--- arch/arm/mach-bcm2709/bcm2709.c
+++ arch/arm/mach-bcm2709/bcm2709.c
@@ -918,8 +918,14 @@ void __init bcm2709_init(void)
 #endif
 	bcm2709_init_led();
 	bcm2709_init_uart1();
-	for (i = 0; i < ARRAY_SIZE(bcm2708_alsa_devices); i++)
-		bcm_register_device_dt(&bcm2708_alsa_devices[i]);
+
+	/* Only create the platform devices for the ALSA driver in the
+	   absence of an enabled "audio" DT node */
+	if (!use_dt ||
+	    !of_device_is_available(of_find_node_by_path("/audio"))) {
+		for (i = 0; i < ARRAY_SIZE(bcm2708_alsa_devices); i++)
+			bcm_register_device(&bcm2708_alsa_devices[i]);
+	}
 
 	bcm_register_device_dt(&bcm2708_spi_device);
 
