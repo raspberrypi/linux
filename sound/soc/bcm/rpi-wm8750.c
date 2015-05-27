dd8b34f30544231b9b4ecdd70f0fcd05b57c62c6
GTrannoy <guillaume.trannoy@gmail.com>
2015-05-27 22:56:56 +0200

    Update rpi-wm8750.c

--- sound/soc/bcm/rpi-wm8750.c
+++ sound/soc/bcm/rpi-wm8750.c
@@ -32,7 +32,7 @@ static struct snd_pcm_hw_constraint_list wm8750_constraints_12288000 = {
 	.count = ARRAY_SIZE(wm8750_rates_12288000),
 };
 
-static int snd_rpi_proto_startup(struct snd_pcm_substream *substream)
+static int snd_rpi_wm8750_startup(struct snd_pcm_substream *substream)
 {
 	/* Setup constraints, because there is a 12.288 MHz XTAL on the board */
 	snd_pcm_hw_constraint_list(substream->runtime, 0,
@@ -41,7 +41,7 @@ static int snd_rpi_proto_startup(struct snd_pcm_substream *substream)
 	return 0;
 }
 
-static int snd_rpi_proto_hw_params(struct snd_pcm_substream *substream,
+static int snd_rpi_wm8750_hw_params(struct snd_pcm_substream *substream,
 				       struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
@@ -70,12 +70,12 @@ static int snd_rpi_proto_hw_params(struct snd_pcm_substream *substream,
 }
 
 /* machine stream operations */
-static struct snd_soc_ops snd_rpi_proto_ops = {
-	.startup = snd_rpi_proto_startup,
-	.hw_params = snd_rpi_proto_hw_params,
+static struct snd_soc_ops snd_rpi_wm8750_ops = {
+	.startup = snd_rpi_wm8750_startup,
+	.hw_params = snd_rpi_wm8750_hw_params,
 };
 
-static struct snd_soc_dai_link snd_rpi_proto_dai[] = {
+static struct snd_soc_dai_link snd_rpi_wm8750_dai[] = {
 {
 	.name		= "WM8750",
 	.stream_name	= "WM8750 HiFi",
@@ -86,26 +86,26 @@ static struct snd_soc_dai_link snd_rpi_proto_dai[] = {
 	.dai_fmt	= SND_SOC_DAIFMT_I2S
 				| SND_SOC_DAIFMT_NB_NF
 				| SND_SOC_DAIFMT_CBM_CFM,
-	.ops		= &snd_rpi_proto_ops,
+	.ops		= &snd_rpi_wm8750_ops,
 },
 };
 
 /* audio machine driver */
-static struct snd_soc_card snd_rpi_proto = {
+static struct snd_soc_card snd_rpi_wm8750 = {
 	.name		= "snd_rpi_wm8750",
-	.dai_link	= snd_rpi_proto_dai,
-	.num_links	= ARRAY_SIZE(snd_rpi_proto_dai),
+	.dai_link	= snd_rpi_wm8750_dai,
+	.num_links	= ARRAY_SIZE(snd_rpi_wm8750_dai),
 };
 
-static int snd_rpi_proto_probe(struct platform_device *pdev)
+static int snd_rpi_wm8750_probe(struct platform_device *pdev)
 {
 	int ret = 0;
 
-	snd_rpi_proto.dev = &pdev->dev;
+	snd_rpi_wm8750.dev = &pdev->dev;
 
 	if (pdev->dev.of_node) {
 		struct device_node *i2s_node;
-		struct snd_soc_dai_link *dai = &snd_rpi_proto_dai[0];
+		struct snd_soc_dai_link *dai = &snd_rpi_wm8750_dai[0];
 		i2s_node = of_parse_phandle(pdev->dev.of_node,
 				            "i2s-controller", 0);
 
@@ -117,7 +117,7 @@ static int snd_rpi_proto_probe(struct platform_device *pdev)
 		}
 	}
 
-	ret = snd_soc_register_card(&snd_rpi_proto);
+	ret = snd_soc_register_card(&snd_rpi_wm8750);
 	if (ret) {
 		dev_err(&pdev->dev,
 				"snd_soc_register_card() failed: %d\n", ret);
@@ -127,28 +127,28 @@ static int snd_rpi_proto_probe(struct platform_device *pdev)
 }
 
 
-static int snd_rpi_proto_remove(struct platform_device *pdev)
+static int snd_rpi_wm8750_remove(struct platform_device *pdev)
 {
-	return snd_soc_unregister_card(&snd_rpi_proto);
+	return snd_soc_unregister_card(&snd_rpi_wm8750);
 }
 
-static const struct of_device_id snd_rpi_proto_of_match[] = {
-	{ .compatible = "rpi,rpi-proto", },
+static const struct of_device_id snd_rpi_wm8750_of_match[] = {
+	{ .compatible = "rpi,rpi-wm8750", },
 	{},
 };
-MODULE_DEVICE_TABLE(of, snd_rpi_proto_of_match);
+MODULE_DEVICE_TABLE(of, snd_rpi_wm8750_of_match);
 
-static struct platform_driver snd_rpi_proto_driver = {
+static struct platform_driver snd_rpi_wm8750_driver = {
 	.driver = {
-		.name   = "snd-rpi-proto",
+		.name   = "snd-rpi-wm8750",
 		.owner  = THIS_MODULE,
-		.of_match_table = snd_rpi_proto_of_match,
+		.of_match_table = snd_rpi_wm8750_of_match,
 	},
-	.probe	  = snd_rpi_proto_probe,
-	.remove	 = snd_rpi_proto_remove,
+	.probe	  = snd_rpi_wm8750_probe,
+	.remove	 = snd_rpi_wm8750_remove,
 };
 
-module_platform_driver(snd_rpi_proto_driver);
+module_platform_driver(snd_rpi_wm8750_driver);
 
 MODULE_AUTHOR("Guillaume Trannoy");
 MODULE_DESCRIPTION("ASoC Driver for Raspberry Pi connected to WM8750");
