392df2a9ee33131241755d6f06375868cf39c412
Noralf Trønnes <noralf@tronnes.org>
2015-06-01 20:40:11 +0200

    BCM270x: Add USB controller to Device Tree
    Add Device Tree support to dwc_otg driver.
    Add device to Device Tree.
    Don't add platform devices when booting in DT mode.
    
    Tested on Pi1 and Pi2 with and without DT.
    
    Signed-off-by: Noralf Trønnes <noralf@tronnes.org>

--- arch/arm/mach-bcm2708/bcm2708.c
+++ arch/arm/mach-bcm2708/bcm2708.c
@@ -955,7 +955,7 @@ void __init bcm2708_init(void)
 #endif
 	bcm_register_device(&bcm2708_systemtimer_device);
 	bcm_register_device_dt(&bcm2708_fb_device);
-	bcm_register_device(&bcm2708_usb_device);
+	bcm_register_device_dt(&bcm2708_usb_device);
 	bcm_register_device(&bcm2708_uart1_device);
 	bcm_register_device(&bcm2708_powerman_device);
 
--- arch/arm/mach-bcm2709/bcm2709.c
+++ arch/arm/mach-bcm2709/bcm2709.c
@@ -978,7 +978,7 @@ void __init bcm2709_init(void)
 	bcm_register_device(&bcm2708_systemtimer_device);
 #endif
 	bcm_register_device_dt(&bcm2708_fb_device);
-	bcm_register_device(&bcm2708_usb_device);
+	bcm_register_device_dt(&bcm2708_usb_device);
 	bcm_register_device(&bcm2708_uart1_device);
 	bcm_register_device(&bcm2708_powerman_device);
 
