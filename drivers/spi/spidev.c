75fa46759ed261089bed0b1786b8dbc086785051
Phil Elwell <phil@raspberrypi.org>
2015-07-14 10:26:09 +0100

    spidev: Add "spidev" compatible string to silence warning
    See: https://github.com/raspberrypi/linux/issues/1054

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -670,6 +670,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "ge,achc" },
 	{ .compatible = "semtech,sx1301" },
 	{ .compatible = "lwn,bk4" },
+	{ .compatible = "spidev" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, spidev_dt_ids);
