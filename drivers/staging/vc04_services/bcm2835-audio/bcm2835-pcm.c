5d29a32988e63374bb97097f4a7101826430f08d
wm4 <wm4@nowhere>
2016-01-13 19:44:47 +0100

    bcm2835: interpolate audio delay
    It appears the GPU only sends us a message all 10ms to update
    the playback progress. Other than this, the playback position
    (what SNDRV_PCM_IOCTL_DELAY will return) is not updated at all.
    Userspace will see jitter up to 10ms in the audio position.
    
    Make this a bit nicer for userspace by interpolating the
    position using the CPU clock.
    
    I'm not sure if setting snd_pcm_runtime.delay is the right
    approach for this. Or if there is maybe an already existing
    mechanism for position interpolation in the ALSA core.
    
    I only set SNDRV_PCM_INFO_BATCH because this appears to remove
    at least one situation snd_pcm_runtime.delay is used, so I have
    to worry less in which place I have to update this field, or
    how it interacts with the rest of ALSA.
    
    In the future, it might be nice to use VC_AUDIO_MSG_TYPE_LATENCY.
    One problem is that it requires sending a videocore message, and
    waiting for a reply, which could make the implementation much
    harder due to locking and synchronization requirements.

--- drivers/staging/vc04_services/bcm2835-audio/bcm2835-pcm.c
+++ drivers/staging/vc04_services/bcm2835-audio/bcm2835-pcm.c
@@ -11,7 +11,7 @@
 /* hardware definition */
 static const struct snd_pcm_hardware snd_bcm2835_playback_hw = {
 	.info = (SNDRV_PCM_INFO_INTERLEAVED | SNDRV_PCM_INFO_BLOCK_TRANSFER |
-	SNDRV_PCM_INFO_MMAP | SNDRV_PCM_INFO_MMAP_VALID),
+	SNDRV_PCM_INFO_MMAP | SNDRV_PCM_INFO_MMAP_VALID | SNDRV_PCM_INFO_BATCH),
 	.formats = SNDRV_PCM_FMTBIT_U8 | SNDRV_PCM_FMTBIT_S16_LE,
 	.rates = SNDRV_PCM_RATE_CONTINUOUS | SNDRV_PCM_RATE_8000_48000,
 	.rate_min = 8000,
@@ -80,6 +80,8 @@ void bcm2835_playback_fifo(struct bcm2835_alsa_stream *alsa_stream)
 		alsa_stream->pos += consumed & ~(1 << 30);
 		alsa_stream->pos %= alsa_stream->buffer_size;
 	}
+
+	alsa_stream->interpolate_start = ktime_get_ns();
 
 	if (alsa_stream->substream) {
 		if (new_period)
@@ -306,6 +308,7 @@ static int snd_bcm2835_pcm_prepare(struct snd_pcm_substream *substream)
 	alsa_stream->buffer_size = snd_pcm_lib_buffer_bytes(substream);
 	alsa_stream->period_size = snd_pcm_lib_period_bytes(substream);
 	alsa_stream->pos = 0;
+	alsa_stream->interpolate_start = ktime_get_ns();
 
 	audio_debug("buffer_size=%d, period_size=%d pos=%d frame_bits=%d\n",
 		alsa_stream->buffer_size, alsa_stream->period_size,
@@ -397,11 +400,18 @@ snd_bcm2835_pcm_pointer(struct snd_pcm_substream *substream)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
 	struct bcm2835_alsa_stream *alsa_stream = runtime->private_data;
+	u64 now = ktime_get_ns();
 
 	audio_debug("pcm_pointer... (%d) hwptr=%d appl=%d pos=%d\n", 0,
 		frames_to_bytes(runtime, runtime->status->hw_ptr),
 		frames_to_bytes(runtime, runtime->control->appl_ptr),
 		alsa_stream->pos);
+
+	/* Give userspace better delay reporting by interpolating between GPU
+	 * notifications, assuming audio speed is close enough to the clock
+	 * used for ktime */
+	if (alsa_stream->interpolate_start && alsa_stream->interpolate_start < now)
+		runtime->delay = -(int)div_u64((now - alsa_stream->interpolate_start) * runtime->rate,  1000000000);
 
 	return snd_pcm_indirect_playback_pointer(substream,
 		&alsa_stream->pcm_indirect,
