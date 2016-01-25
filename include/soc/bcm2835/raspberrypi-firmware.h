c2708acb90f57ffb039a6c3ca3107184f6d53190
popcornmix <popcornmix@gmail.com>
2016-01-25 17:25:12 +0000

    firmware: Updated mailbox header

--- include/soc/bcm2835/raspberrypi-firmware.h
+++ include/soc/bcm2835/raspberrypi-firmware.h
@@ -9,6 +9,8 @@
 #include <linux/types.h>
 #include <linux/of_device.h>
 
+#define RPI_FIRMWARE_CHAN_FB		1
+
 struct rpi_firmware;
 
 enum rpi_firmware_property_status {
@@ -159,5 +161,6 @@ static inline struct rpi_firmware *rpi_firmware_get(struct device_node *firmware
 	return NULL;
 }
 #endif
+int rpi_firmware_transaction(struct rpi_firmware *fw, u32 chan, u32 data);
 
 #endif /* __SOC_RASPBERRY_FIRMWARE_H__ */
