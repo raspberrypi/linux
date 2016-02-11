edb001849adf57898330ed3040c06ac36af4b889
Lee Jones <lee.jones@linaro.org>
2016-02-11 13:19:09 -0800

    clk: Allow clocks to be marked as CRITICAL
    Critical clocks are those which must not be gated, else undefined
    or catastrophic failure would occur.  Here we have chosen to
    ensure the prepare/enable counts are correctly incremented, so as
    not to confuse users with enabled clocks with no visible users.
    
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Michael Turquette <mturquette@baylibre.com>
    Link: lkml.kernel.org/r/1455225554-13267-2-git-send-email-mturquette@baylibre.com
    (cherry picked from commit 32b9b10961860860268961d9aad0c56a73018c37)

--- include/linux/clk-provider.h
+++ include/linux/clk-provider.h
@@ -31,6 +31,7 @@
 #define CLK_SET_RATE_NO_REPARENT BIT(7) /* don't re-parent on rate change */
 #define CLK_GET_ACCURACY_NOCACHE BIT(8) /* do not use the cached clk accuracy */
 #define CLK_RECALC_NEW_RATES	BIT(9) /* recalc rates after notifications */
+#define CLK_IS_CRITICAL		BIT(11) /* do not gate, ever */
 
 struct clk;
 struct clk_hw;
