e716abf138f2f2b95e817bdb00e9ba767d5390ad
Dhiraj Goel <dhiraj.goel@gmail.com>
2016-03-03 21:10:50 -0800

    bcm2835-camera: fix a bug in computation of frame timestamp
    Fixes #1318

--- drivers/media/platform/bcm2835/bcm2835-camera.c
+++ drivers/media/platform/bcm2835/bcm2835-camera.c
@@ -360,8 +360,7 @@ static void buffer_cb(struct vchiq_mmal_instance *instance,
 				div =
 				    div_u64_rem(runtime_us, USEC_PER_SEC, &rem);
 				buf->vb.timestamp.tv_sec =
-				    dev->capture.kernel_start_ts.tv_sec - 1 +
-				    div;
+				    dev->capture.kernel_start_ts.tv_sec + div;
 				buf->vb.timestamp.tv_usec =
 				    dev->capture.kernel_start_ts.tv_usec + rem;
 
