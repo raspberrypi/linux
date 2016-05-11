268751484e188e6aade41856b663578dc29304ce
Phil Elwell <phil@raspberrypi.org>
2016-05-11 12:50:33 +0100

    mmc: Add MMC_QUIRK_ERASE_BROKEN for some cards
    Some SD cards have been found that corrupt data when small blocks
    are erased. Add a quirk to indicate that ERASE should not be used,
    and set it for cards of that type.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    
    mmc: Apply QUIRK_BROKEN_ERASE to other capacities
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>
    
    mmc: Add card_quirks module parameter, log quirks
    
    Use mmc_block.card_quirks to override the quirks for all SD or MMC
    cards. The value is a bitfield using the bit positions defined in
    include/linux/mmc/card.h. If the module parameter is placed in the
    kernel command line (or bootargs) stored on the card then, assuming the
    device only has one SD card interface, the override effectively becomes
    card-specific.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -281,6 +281,9 @@ struct mmc_card {
 #define MMC_QUIRK_TRIM_BROKEN	(1<<12)		/* Skip trim */
 
 
+#define MMC_QUIRK_ERASE_BROKEN	(1<<31)		/* Skip erase */
+
+
 	unsigned int		erase_size;	/* erase size in sectors */
  	unsigned int		erase_shift;	/* if erase unit is power 2 */
  	unsigned int		pref_erase;	/* in sectors */
