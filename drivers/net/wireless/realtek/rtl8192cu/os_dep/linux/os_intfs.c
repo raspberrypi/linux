2167c0932ab020f0794985c81181c2310b838020
Francisco Facioni <fran6co@gmail.com>
2016-05-28 11:59:55 -0300

    Fixes compatibility with 3.13

--- drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/os_intfs.c
+++ drivers/net/wireless/realtek/rtl8192cu/os_dep/linux/os_intfs.c
@@ -945,13 +945,14 @@ unsigned int rtw_classify8021d(struct sk_buff *skb)
 	return dscp >> 5;
 }
 
-#if (LINUX_VERSION_CODE>=KERNEL_VERSION(3,14,0))
-static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb,
-			    void *accel_priv,
-			    select_queue_fallback_t fallback)
-#else
-static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb)
+static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0)
+				, void *accel_priv
 #endif
+#if (LINUX_VERSION_CODE>=KERNEL_VERSION(3,14,0))
+			    , select_queue_fallback_t fallback
+#endif
+)
 {
 	_adapter	*padapter = rtw_netdev_priv(dev);
 	struct mlme_priv *pmlmepriv = &padapter->mlmepriv;
