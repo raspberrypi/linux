d47487d34c0ccca5d48acd9a65c0eefa3c7dd142
David Weinehall <david.weinehall@linux.intel.com>
2016-08-17 15:47:48 +0300

    Revert "drm/i915: Check live status before reading edid"
    commit 23f889bdf6ee5cfff012d8b09f6bec920c691696 upstream.
    
    This reverts commit 237ed86c693d8a8e4db476976aeb30df4deac74b.
    
    Our current implementation of live status check (repeat 9 times
    with 10ms delays between each attempt as a workaround for
    buggy displays) imposes a rather serious penalty, time wise,
    on intel_hdmi_detect().  Since we we already skip live status
    checks on platforms before gen 7, and since we seem to have
    coped quite well before the live status check was introduced
    for newer platforms too, the previous behaviour is probably
    preferable, at least unless someone can point to a use-case
    that the live status check improves (apart from "Bspec says so".)
    
    Signed-off-by: David Weinehall <david.weinehall@linux.intel.com>
    Fixes: 237ed86c693d ("drm/i915: Check live status before reading edid")
    Fixes: f8d03ea0053b ("drm/i915: increase the tries for HDMI hotplug live status checking")
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=97139
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=94014
    Acked-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20160817124748.31208-1-david.weinehall@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1388,8 +1388,6 @@ void intel_edp_drrs_disable(struct intel_dp *intel_dp);
 void intel_edp_drrs_invalidate(struct drm_device *dev,
 		unsigned frontbuffer_bits);
 void intel_edp_drrs_flush(struct drm_device *dev, unsigned frontbuffer_bits);
-bool intel_digital_port_connected(struct drm_i915_private *dev_priv,
-					 struct intel_digital_port *port);
 
 void
 intel_dp_program_link_training_pattern(struct intel_dp *intel_dp,
