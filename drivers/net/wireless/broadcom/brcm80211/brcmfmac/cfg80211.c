f263ce844727c06fcd06242745e72ca385ccf90d
Arend Van Spriel <arend.vanspriel@broadcom.com>
2016-11-23 10:25:31 +0000

    brcmfmac: fix scheduled scan result handling for newer chips
    commit 53e3a80d80c80bf50ab64cf6c44fb0fa41aa22d8 upstream.
    
    The scan results for scheduled scan as retrieved from the device
    have changed. A field has been added which is not needed. However,
    the appended info is. Luckily they are versioned so check that to
    find out the location of the appended data.
    
    Reviewed-by: Hante Meuleman <hante.meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieter-paul.giesberts@broadcom.com>
    Reviewed-by: Franky Lin <franky.lin@broadcom.com>
    Signed-off-by: Arend van Spriel <arend.vanspriel@broadcom.com>
    Signed-off-by: Kalle Valo <kvalo@codeaurora.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/cfg80211.c
@@ -3159,6 +3159,28 @@ static void brcmf_init_escan(struct brcmf_cfg80211_info *cfg)
 		  brcmf_cfg80211_escan_timeout_worker);
 }
 
+static struct brcmf_pno_net_info_le *
+brcmf_get_netinfo_array(struct brcmf_pno_scanresults_le *pfn_v1)
+{
+	struct brcmf_pno_scanresults_v2_le *pfn_v2;
+	struct brcmf_pno_net_info_le *netinfo;
+
+	switch (pfn_v1->version) {
+	default:
+		WARN_ON(1);
+		/* fall-thru */
+	case cpu_to_le32(1):
+		netinfo = (struct brcmf_pno_net_info_le *)(pfn_v1 + 1);
+		break;
+	case cpu_to_le32(2):
+		pfn_v2 = (struct brcmf_pno_scanresults_v2_le *)pfn_v1;
+		netinfo = (struct brcmf_pno_net_info_le *)(pfn_v2 + 1);
+		break;
+	}
+
+	return netinfo;
+}
+
 /* PFN result doesn't have all the info which are required by the supplicant
  * (For e.g IEs) Do a target Escan so that sched scan results are reported
  * via wl_inform_single_bss in the required format. Escan does require the
@@ -3215,8 +3237,7 @@ brcmf_notify_sched_scan_results(struct brcmf_if *ifp,
 		}
 
 		request->wiphy = wiphy;
-		data += sizeof(struct brcmf_pno_scanresults_le);
-		netinfo_start = (struct brcmf_pno_net_info_le *)data;
+		netinfo_start = brcmf_get_netinfo_array(pfn_result);
 
 		for (i = 0; i < result_count; i++) {
 			netinfo = &netinfo_start[i];
