317200f043c63221709efbbb6d7d468ecddbb530
popcornmix <popcornmix@gmail.com>
2016-11-28 16:50:04 +0000

    Improve __copy_to_user and __copy_from_user performance
    Provide a __copy_from_user that uses memcpy. On BCM2708, use
    optimised memcpy/memmove/memcmp/memset implementations.
    
    arch/arm: Add mmiocpy/set aliases for memcpy/set
    
    See: https://github.com/raspberrypi/linux/issues/1082
    
    copy_from_user: CPU_SW_DOMAIN_PAN compatibility
    
    The downstream copy_from_user acceleration must also play nice with
    CONFIG_CPU_SW_DOMAIN_PAN.
    
    See: https://github.com/raspberrypi/linux/issues/1381
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- arch/arm/include/asm/string.h
+++ arch/arm/include/asm/string.h
@@ -39,6 +39,11 @@ static inline void *memset64(uint64_t *p, uint64_t v, __kernel_size_t n)
 	return __memset64(p, v, n * 8, v >> 32);
 }
 
+#ifdef CONFIG_BCM2835_FAST_MEMCPY
+#define __HAVE_ARCH_MEMCMP
+extern int memcmp(const void *, const void *, size_t);
+#endif
+
 extern void __memzero(void *ptr, __kernel_size_t n);
 
 #define memset(p,v,n)							\
