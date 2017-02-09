4414cd5bd0b288b4d6ff56372a08c72ac41404e2
Phil Elwell <phil@raspberrypi.org>
2017-02-09 14:36:44 +0000

    sound: Demote deferral errors to INFO level
    At present there is no mechanism to specify driver load order,
    which can lead to deferrals and repeated retries until successful.
    Since this situation is expected, reduce the dmesg level to
    INFO and mention that the operation will be retried.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -869,8 +869,8 @@ static int soc_bind_dai_link(struct snd_soc_card *card,
 	cpu_dai_component.dai_name = dai_link->cpu_dai_name;
 	rtd->cpu_dai = snd_soc_find_dai(&cpu_dai_component);
 	if (!rtd->cpu_dai) {
-		dev_info(card->dev, "ASoC: CPU DAI %s not registered\n",
-			 dai_link->cpu_dai_name);
+		dev_info(card->dev, "ASoC: CPU DAI %s not registered - will retry\n",
+			dai_link->cpu_dai_name);
 		goto _err_defer;
 	}
 	snd_soc_rtdcom_add(rtd, rtd->cpu_dai->component);
@@ -882,7 +882,7 @@ static int soc_bind_dai_link(struct snd_soc_card *card,
 	for (i = 0; i < rtd->num_codecs; i++) {
 		codec_dais[i] = snd_soc_find_dai(&codecs[i]);
 		if (!codec_dais[i]) {
-			dev_err(card->dev, "ASoC: CODEC DAI %s not registered\n",
+			dev_info(card->dev, "ASoC: CODEC DAI %s not registered - will retry\n",
 				codecs[i].dai_name);
 			goto _err_defer;
 		}
