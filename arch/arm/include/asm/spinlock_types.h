86a533488648a2de4592cd00516722d47696a9ef
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-08-04 17:40:42 +0200

    locking: don't check for __LINUX_SPINLOCK_TYPES_H on -RT archs
    Upstream uses arch_spinlock_t within spinlock_t and requests that
    spinlock_types.h header file is included first.
    On -RT we have the rt_mutex with its raw_lock wait_lock which needs
    architectures' spinlock_types.h header file for its definition. However
    we need rt_mutex first because it is used to build the spinlock_t so
    that check does not work for us.
    Therefore I am dropping that check.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- arch/arm/include/asm/spinlock_types.h
+++ arch/arm/include/asm/spinlock_types.h
@@ -2,10 +2,6 @@
 #ifndef __ASM_SPINLOCK_TYPES_H
 #define __ASM_SPINLOCK_TYPES_H
 
-#ifndef __LINUX_SPINLOCK_TYPES_H
-# error "please don't include this file directly"
-#endif
-
 #define TICKET_SHIFT	16
 
 typedef struct {
