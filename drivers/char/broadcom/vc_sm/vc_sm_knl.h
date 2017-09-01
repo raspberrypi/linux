11a15ea32566e9f507b691023d635887ac3e339d
Dave Stevenson <dave.stevenson@raspberrypi.org>
2017-09-01 17:36:11 +0100

    vcsm: Correct "long unsigned int" to "unsigned long"
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.org>

--- drivers/char/broadcom/vc_sm/vc_sm_knl.h
+++ drivers/char/broadcom/vc_sm/vc_sm_knl.h
@@ -36,7 +36,7 @@ int vc_sm_free(int handle);
 
 /* Lock a memory handle for use by kernel. */
 int vc_sm_lock(int handle, VC_SM_LOCK_CACHE_MODE_T mode,
-	       long unsigned int *data);
+	       unsigned long *data);
 
 /* Unlock a memory handle in use by kernel. */
 int vc_sm_unlock(int handle, int flush, int no_vc_unlock);
@@ -46,7 +46,7 @@ int vc_sm_int_handle(int handle);
 
 /* Map a shared memory region for use by kernel. */
 int vc_sm_map(int handle, unsigned int sm_addr, VC_SM_LOCK_CACHE_MODE_T mode,
-	      long unsigned int *data);
+	      unsigned long *data);
 
 /* Import a block of memory into the GPU space. */
 int vc_sm_import_dmabuf(struct dma_buf *dmabuf, int *handle);
