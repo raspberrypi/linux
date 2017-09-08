46791eb9f13e0a092f4814bf23438d79b4259098
David Rientjes <rientjes@google.com>
2017-09-08 16:13:29 -0700

    mm/swapfile.c: fix swapon frontswap_map memory leak on error
    commit b6b1fd2a6bedd533aeed83924d7be0e944fded9f upstream.
    
    Free frontswap_map if an error is encountered before enable_swap_info().
    
    Signed-off-by: David Rientjes <rientjes@google.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Cc: Darrick J. Wong <darrick.wong@oracle.com>
    Cc: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2904,6 +2904,7 @@ bad_swap:
 	spin_unlock(&swap_lock);
 	vfree(swap_map);
 	kvfree(cluster_info);
+	kvfree(frontswap_map);
 	if (swap_file) {
 		if (inode && S_ISREG(inode->i_mode)) {
 			inode_unlock(inode);
