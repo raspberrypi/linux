01bc132048cf9505ed49152cc82e583b18c5538d
Darrick J. Wong <darrick.wong@oracle.com>
2017-09-17 14:06:38 -0700

    xfs: check _alloc_read_agf buffer pointer before using
    commit 10479e2dea83d4c421ad05dfc55d918aa8dfc0cd upstream.
    
    In some circumstances, _alloc_read_agf can return an error code of zero
    but also a null AGF buffer pointer.  Check for this and jump out.
    
    Fixes-coverity-id: 1415250
    Fixes-coverity-id: 1415320
    Signed-off-by: Darrick J. Wong <darrick.wong@oracle.com>
    Reviewed-by: Brian Foster <bfoster@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/xfs/xfs_reflink.c
+++ fs/xfs/xfs_reflink.c
@@ -169,6 +169,8 @@ xfs_reflink_find_shared(
 	error = xfs_alloc_read_agf(mp, NULL, agno, 0, &agbp);
 	if (error)
 		return error;
+	if (!agbp)
+		return -ENOMEM;
 
 	cur = xfs_refcountbt_init_cursor(mp, NULL, agbp, agno, NULL);
 
