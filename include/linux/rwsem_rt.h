130569f05eeda34cef7dfbbab76aa126e96d58f4
Thomas Gleixner <tglx@linutronix.de>
2017-10-12 17:28:34 +0200

    rtmutex: add rwsem implementation based on rtmutex
    The RT specific R/W semaphore implementation restricts the number of readers
    to one because a writer cannot block on multiple readers and inherit its
    priority or budget.
    
    The single reader restricting is painful in various ways:
    
     - Performance bottleneck for multi-threaded applications in the page fault
       path (mmap sem)
    
     - Progress blocker for drivers which are carefully crafted to avoid the
       potential reader/writer deadlock in mainline.
    
    The analysis of the writer code pathes shows, that properly written RT tasks
    should not take them. Syscalls like mmap(), file access which take mmap sem
    write locked have unbound latencies which are completely unrelated to mmap
    sem. Other R/W sem users like graphics drivers are not suitable for RT tasks
    either.
    
    So there is little risk to hurt RT tasks when the RT rwsem implementation is
    changed in the following way:
    
     - Allow concurrent readers
    
     - Make writers block until the last reader left the critical section. This
       blocking is not subject to priority/budget inheritance.
    
     - Readers blocked on a writer inherit their priority/budget in the normal
       way.
    
    There is a drawback with this scheme. R/W semaphores become writer unfair
    though the applications which have triggered writer starvation (mostly on
    mmap_sem) in the past are not really the typical workloads running on a RT
    system. So while it's unlikely to hit writer starvation, it's possible. If
    there are unexpected workloads on RT systems triggering it, we need to rethink
    the approach.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

---
+++ include/linux/rwsem_rt.h
@@ -0,0 +1,67 @@
+#ifndef _LINUX_RWSEM_RT_H
+#define _LINUX_RWSEM_RT_H
+
+#ifndef _LINUX_RWSEM_H
+#error "Include rwsem.h"
+#endif
+
+#include <linux/rtmutex.h>
+#include <linux/swait.h>
+
+#define READER_BIAS		(1U << 31)
+#define WRITER_BIAS		(1U << 30)
+
+struct rw_semaphore {
+	atomic_t		readers;
+	struct rt_mutex		rtmutex;
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	struct lockdep_map	dep_map;
+#endif
+};
+
+#define __RWSEM_INITIALIZER(name)				\
+{								\
+	.readers = ATOMIC_INIT(READER_BIAS),			\
+	.rtmutex = __RT_MUTEX_INITIALIZER(name.rtmutex),	\
+	RW_DEP_MAP_INIT(name)					\
+}
+
+#define DECLARE_RWSEM(lockname) \
+	struct rw_semaphore lockname = __RWSEM_INITIALIZER(lockname)
+
+extern void  __rwsem_init(struct rw_semaphore *rwsem, const char *name,
+			  struct lock_class_key *key);
+
+#define __init_rwsem(sem, name, key)			\
+do {							\
+		rt_mutex_init(&(sem)->rtmutex);		\
+		__rwsem_init((sem), (name), (key));	\
+} while (0)
+
+#define init_rwsem(sem)					\
+do {							\
+	static struct lock_class_key __key;		\
+							\
+	__init_rwsem((sem), #sem, &__key);		\
+} while (0)
+
+static inline int rwsem_is_locked(struct rw_semaphore *sem)
+{
+	return atomic_read(&sem->readers) != READER_BIAS;
+}
+
+static inline int rwsem_is_contended(struct rw_semaphore *sem)
+{
+	return atomic_read(&sem->readers) > 0;
+}
+
+extern void __down_read(struct rw_semaphore *sem);
+extern int __down_read_trylock(struct rw_semaphore *sem);
+extern void __down_write(struct rw_semaphore *sem);
+extern int __must_check __down_write_killable(struct rw_semaphore *sem);
+extern int __down_write_trylock(struct rw_semaphore *sem);
+extern void __up_read(struct rw_semaphore *sem);
+extern void __up_write(struct rw_semaphore *sem);
+extern void __downgrade_write(struct rw_semaphore *sem);
+
+#endif
