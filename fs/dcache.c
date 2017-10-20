0ad8360ebe8c03b6fee08317545dbadf3563dab5
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2017-10-20 11:29:53 +0200

    fs/dcache: disable preemption on i_dir_seq's write side
    i_dir_seq is an opencoded seqcounter. Based on the code it looks like we
    could have two writers in parallel despite the fact that the d_lock is
    held. The problem is that during the write process on RT the preemption
    is still enabled and if this process is interrupted by a reader with RT
    priority then we lock up.
    To avoid that lock up I am disabling the preemption during the update.
    The rename of i_dir_seq is here to ensure to catch new write sides in
    future.
    
    Cc: stable-rt@vger.kernel.org
    Reported-by: Oleg.Karfich@wago.com
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2410,9 +2410,10 @@ EXPORT_SYMBOL(d_rehash);
 static inline unsigned start_dir_add(struct inode *dir)
 {
 
+	preempt_disable_rt();
 	for (;;) {
-		unsigned n = dir->i_dir_seq;
-		if (!(n & 1) && cmpxchg(&dir->i_dir_seq, n, n + 1) == n)
+		unsigned n = dir->__i_dir_seq;
+		if (!(n & 1) && cmpxchg(&dir->__i_dir_seq, n, n + 1) == n)
 			return n;
 		cpu_relax();
 	}
@@ -2420,7 +2421,8 @@ static inline unsigned start_dir_add(struct inode *dir)
 
 static inline void end_dir_add(struct inode *dir, unsigned n)
 {
-	smp_store_release(&dir->i_dir_seq, n + 2);
+	smp_store_release(&dir->__i_dir_seq, n + 2);
+	preempt_enable_rt();
 }
 
 static void d_wait_lookup(struct dentry *dentry)
@@ -2453,7 +2455,7 @@ struct dentry *d_alloc_parallel(struct dentry *parent,
 
 retry:
 	rcu_read_lock();
-	seq = smp_load_acquire(&parent->d_inode->i_dir_seq) & ~1;
+	seq = smp_load_acquire(&parent->d_inode->__i_dir_seq) & ~1;
 	r_seq = read_seqbegin(&rename_lock);
 	dentry = __d_lookup_rcu(parent, name, &d_seq);
 	if (unlikely(dentry)) {
@@ -2475,7 +2477,7 @@ retry:
 		goto retry;
 	}
 	hlist_bl_lock(b);
-	if (unlikely(parent->d_inode->i_dir_seq != seq)) {
+	if (unlikely(parent->d_inode->__i_dir_seq != seq)) {
 		hlist_bl_unlock(b);
 		rcu_read_unlock();
 		goto retry;
