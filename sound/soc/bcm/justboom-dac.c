c4cf05a5ced42a09d5972cb6a8cce44576cb3edd
Matthias Reichl <hias@horus.com>
2018-02-02 20:30:43 +0100

    ASoC: justboom-dac: fix S24_LE format
    Remove set_bclk_ratio call so 24-bit data is transmitted in
    24 bclk cycles.
    
    Also remove hw_params as it's no longer needed.
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- sound/soc/bcm/justboom-dac.c
+++ sound/soc/bcm/justboom-dac.c
@@ -49,17 +49,6 @@ static int snd_rpi_justboom_dac_init(struct snd_soc_pcm_runtime *rtd)
 	return 0;
 }
 
-static int snd_rpi_justboom_dac_hw_params(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-	/*return snd_soc_dai_set_bclk_ratio(cpu_dai, 64);*/
-	unsigned int sample_bits =
-		snd_pcm_format_physical_width(params_format(params));
-	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2);
-}
-
 static int snd_rpi_justboom_dac_startup(struct snd_pcm_substream *substream) {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_codec *codec = rtd->codec;
@@ -75,7 +64,6 @@ static void snd_rpi_justboom_dac_shutdown(struct snd_pcm_substream *substream) {
 
 /* machine stream operations */
 static struct snd_soc_ops snd_rpi_justboom_dac_ops = {
-	.hw_params = snd_rpi_justboom_dac_hw_params,
 	.startup = snd_rpi_justboom_dac_startup,
 	.shutdown = snd_rpi_justboom_dac_shutdown,
 };
