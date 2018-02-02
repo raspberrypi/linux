14a8264f56b683f761e7b22b84d0759ea7089c19
Matthias Reichl <hias@horus.com>
2018-02-02 12:00:00 +0100

    Revert downstream wm8804 changes
    The format change from S24_LE to S24_3LE effectively disables 24-bit
    mode as S24_3LE isn't supported by bcm2835-i2s. This causes issues
    with drivers that want to use wm8804 in 24-bit mode.
    
    Adding the S32_LE format is also incorrect, according to the datasheet
    only 16-24 bit formats are supported.
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- sound/soc/codecs/wm8804.c
+++ sound/soc/codecs/wm8804.c
@@ -304,7 +304,6 @@ static int wm8804_hw_params(struct snd_pcm_substream *substream,
 		blen = 0x1;
 		break;
 	case 24:
-	case 32:
 		blen = 0x2;
 		break;
 	default:
@@ -516,7 +515,7 @@ static const struct snd_soc_dai_ops wm8804_dai_ops = {
 };
 
 #define WM8804_FORMATS (SNDRV_PCM_FMTBIT_S16_LE | SNDRV_PCM_FMTBIT_S20_3LE | \
-			SNDRV_PCM_FMTBIT_S24_3LE | SNDRV_PCM_FMTBIT_S32_LE)
+			SNDRV_PCM_FMTBIT_S24_LE)
 
 #define WM8804_RATES (SNDRV_PCM_RATE_32000 | SNDRV_PCM_RATE_44100 | \
 		      SNDRV_PCM_RATE_48000 | SNDRV_PCM_RATE_64000 | \
@@ -544,7 +543,7 @@ static struct snd_soc_dai_driver wm8804_dai = {
 };
 
 static const struct snd_soc_codec_driver soc_codec_dev_wm8804 = {
-	.idle_bias_off = false,
+	.idle_bias_off = true,
 
 	.component_driver = {
 		.dapm_widgets		= wm8804_dapm_widgets,
