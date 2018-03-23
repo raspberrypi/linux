ae5add3aef2583f36a068347af08ba65e5ce3abc
bigeasy@linutronix.de <bigeasy@linutronix.de>
2018-03-23 18:17:36 +0100

    target: drop spin_lock_assert() + irqs_disabled() combo checks
    There are a few functions which check for if the lock is held
    (spin_lock_assert()) and the interrupts are disabled (irqs_disabled()).
    >From looking at the code, each function is static, the caller is near by
    and does spin_lock_irq|safe(). As Linus puts it:
    
    |It's not like this is some function that is exported to random users,
    |and we should check that the calling convention is right.
    |
    |This looks like "it may have been useful during coding to document
    |things, but it's not useful long-term".
    
    Remove those checks.
    
    Reviewed-by: Bart Van Assche <bart.vanassche@wdc.com>
    Reported-by: Arnaldo Carvalho de Melo <acme@kernel.org>
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/target/target_core_transport.c
+++ drivers/target/target_core_transport.c
@@ -2967,9 +2967,6 @@ __transport_wait_for_tasks(struct se_cmd *cmd, bool fabric_stop,
 	__acquires(&cmd->t_state_lock)
 {
 
-	assert_spin_locked(&cmd->t_state_lock);
-	WARN_ON_ONCE(!irqs_disabled());
-
 	if (fabric_stop)
 		cmd->transport_state |= CMD_T_FABRIC_STOP;
 
@@ -3238,9 +3235,6 @@ static int __transport_check_aborted_status(struct se_cmd *cmd, int send_status)
 	__acquires(&cmd->t_state_lock)
 {
 	int ret;
-
-	assert_spin_locked(&cmd->t_state_lock);
-	WARN_ON_ONCE(!irqs_disabled());
 
 	if (!(cmd->transport_state & CMD_T_ABORTED))
 		return 0;
