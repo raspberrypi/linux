413200f00f89f4f99291278d14468d5abb8f2828
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2018-05-03 17:16:26 +0200

    rtmutex: annotate sleeping lock context
    The RCU code complains on schedule() within a rcu_readlock() section.
    The valid scenario on -RT is if a sleeping is held. In order to suppress
    the warning the mirgrate_disable counter was used to identify the
    invocation of schedule() due to lock contention.
    
    Grygorii Strashko report that during CPU hotplug we might see the
    warning via
     rt_spin_lock() -> migrate_disable() -> pin_current_cpu() -> __read_rt_lock()
    
    because the counter is not yet set.
    It is also possible to trigger the warning from cpu_chill()
    (seen on a kblockd_mod_delayed_work_on() caller).
    
    To address this RCU warning I annotate the sleeping lock context. The
    counter is incremented before migrate_disable() so the warning Grygorii
    should not trigger anymore. Additionally I use that counter in
    cpu_chill() to avoid the RCU warning from there.
    
    Reported-by: Grygorii Strashko <grygorii.strashko@ti.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -598,6 +598,9 @@ struct task_struct {
 	int				migrate_disable_atomic;
 # endif
 #endif
+#ifdef CONFIG_PREEMPT_RT_FULL
+	int				sleeping_lock;
+#endif
 
 #ifdef CONFIG_PREEMPT_RCU
 	int				rcu_read_lock_nesting;
@@ -1707,6 +1710,23 @@ static __always_inline bool need_resched(void)
 {
 	return unlikely(tif_need_resched());
 }
+
+#ifdef CONFIG_PREEMPT_RT_FULL
+static inline void sleeping_lock_inc(void)
+{
+	current->sleeping_lock++;
+}
+
+static inline void sleeping_lock_dec(void)
+{
+	current->sleeping_lock--;
+}
+
+#else
+
+static inline void sleeping_lock_inc(void) { }
+static inline void sleeping_lock_dec(void) { }
+#endif
 
 /*
  * Wrappers for p->thread_info->cpu access. No-op on UP.
