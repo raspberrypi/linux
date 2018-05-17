7dad6e35ee735dcbec3c3f31383f36da9a74fe18
Coly Li <colyli@suse.de>
2018-05-17 23:33:26 +0800

    bcache: return 0 from bch_debug_init() if CONFIG_DEBUG_FS=n
    commit 1c1a2ee1b53b006754073eefc65d2b2cedb5264b upstream.
    
    Commit 539d39eb2708 ("bcache: fix wrong return value in bch_debug_init()")
    returns the return value of debugfs_create_dir() to bcache_init(). When
    CONFIG_DEBUG_FS=n, bch_debug_init() always returns 1 and makes
    bcache_init() failedi.
    
    This patch makes bch_debug_init() always returns 0 if CONFIG_DEBUG_FS=n,
    so bcache can continue to work for the kernels which don't have debugfs
    enanbled.
    
    Changelog:
    v4: Add Acked-by from Kent Overstreet.
    v3: Use IS_ENABLED(CONFIG_DEBUG_FS) to replace #ifdef DEBUG_FS.
    v2: Remove a warning information
    v1: Initial version.
    
    Fixes: Commit 539d39eb2708 ("bcache: fix wrong return value in bch_debug_init()")
    Cc: stable@vger.kernel.org
    Signed-off-by: Coly Li <colyli@suse.de>
    Reported-by: Massimo B. <massimo.b@gmx.net>
    Reported-by: Kai Krakow <kai@kaishome.de>
    Tested-by: Kai Krakow <kai@kaishome.de>
    Acked-by: Kent Overstreet <kent.overstreet@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Kai Krakow <kai@kaishome.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/md/bcache/debug.c
+++ drivers/md/bcache/debug.c
@@ -251,6 +251,9 @@ void bch_debug_exit(void)
 
 int __init bch_debug_init(struct kobject *kobj)
 {
+	if (!IS_ENABLED(CONFIG_DEBUG_FS))
+		return 0;
+
 	debug = debugfs_create_dir("bcache", NULL);
 
 	return IS_ERR_OR_NULL(debug);
