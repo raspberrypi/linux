3438b1147bd5f354138a32ee281b45010ec9fb42
Li RongQing <lirongqing@baidu.com>
2018-06-19 17:23:17 +0800

    net: propagate dev_get_valid_name return code
    [ Upstream commit 7892bd081045222b9e4027fec279a28d6fe7aa66 ]
    
    if dev_get_valid_name failed, propagate its return code
    
    and remove the setting err to ENODEV, it will be set to
    0 again before dev_change_net_namespace exits.
    
    Signed-off-by: Li RongQing <lirongqing@baidu.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/core/dev.c
+++ net/core/dev.c
@@ -8608,7 +8608,8 @@ int dev_change_net_namespace(struct net_device *dev, struct net *net, const char
 		/* We get here if we can't use the current device name */
 		if (!pat)
 			goto out;
-		if (dev_get_valid_name(net, dev, pat) < 0)
+		err = dev_get_valid_name(net, dev, pat);
+		if (err < 0)
 			goto out;
 	}
 
@@ -8620,7 +8621,6 @@ int dev_change_net_namespace(struct net_device *dev, struct net *net, const char
 	dev_close(dev);
 
 	/* And unlink it from device chain */
-	err = -ENODEV;
 	unlist_netdevice(dev);
 
 	synchronize_net();
