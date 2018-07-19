e4cecd1c061143d090ad39cdf06784c36bafd69d
Or Gerlitz <ogerlitz@mellanox.com>
2018-07-19 16:17:00 +0000

    net/mlx5e: Set port trust mode to PCP as default
    [ Upstream commit 2e8e70d249e8c5c79bf88bbb36bb68154ab15471 ]
    
    The hairpin offload code has dependency on the trust mode being PCP.
    
    Hence we should set PCP as the default for handling cases where we are
    disallowed to read the trust mode from the FW, or failed to initialize it.
    
    Fixes: 106be53b6b0a ('net/mlx5e: Set per priority hairpin pairs')
    Signed-off-by: Or Gerlitz <ogerlitz@mellanox.com>
    Reviewed-by: Parav Pandit <parav@mellanox.com>
    Signed-off-by: Saeed Mahameed <saeedm@mellanox.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/mellanox/mlx5/core/en_dcbnl.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_dcbnl.c
@@ -1057,6 +1057,8 @@ static int mlx5e_trust_initialize(struct mlx5e_priv *priv)
 	struct mlx5_core_dev *mdev = priv->mdev;
 	int err;
 
+	priv->dcbx_dp.trust_state = MLX5_QPTS_TRUST_PCP;
+
 	if (!MLX5_DSCP_SUPPORTED(mdev))
 		return 0;
 
