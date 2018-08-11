43709d74fbf3d7217a481fa00bb4b0c7cfa5d792
Chao Yu <yuchao0@huawei.com>
2018-08-11 23:42:09 +0800

    f2fs: fix to skip verifying block address for non-regular inode
    commit dda9f4b9cac6bdd2a96253b4444d7a6ce5132edb upstream.
    
    generic/184 1s ... [failed, exit status 1]- output mismatch
        --- tests/generic/184.out	2015-01-11 16:52:27.643681072 +0800
         QA output created by 184 - silence is golden
        +rm: cannot remove '/mnt/f2fs/null': Bad address
        +mknod: '/mnt/f2fs/null': Bad address
        +chmod: cannot access '/mnt/f2fs/null': Bad address
        +./tests/generic/184: line 36: /mnt/f2fs/null: Bad address
        ...
    
    F2FS-fs (zram0): access invalid blkaddr:259
    EIP: f2fs_is_valid_blkaddr+0x14b/0x1b0 [f2fs]
     f2fs_iget+0x927/0x1010 [f2fs]
     f2fs_lookup+0x26e/0x630 [f2fs]
     __lookup_slow+0xb3/0x140
     lookup_slow+0x31/0x50
     walk_component+0x185/0x1f0
     path_lookupat+0x51/0x190
     filename_lookup+0x7f/0x140
     user_path_at_empty+0x36/0x40
     vfs_statx+0x61/0xc0
     __do_sys_stat64+0x29/0x40
     sys_stat64+0x13/0x20
     do_fast_syscall_32+0xaa/0x22c
     entry_SYSENTER_32+0x53/0x86
    
    In f2fs_iget(), we will check inode's first block address, if it is valid,
    we will set FI_FIRST_BLOCK_WRITTEN flag in inode.
    
    But we should only do this for regular inode, otherwise, like special
    inode, i_addr[0] is used for storing device info instead of block address,
    it will fail checking flow obviously.
    
    So for non-regular inode, let's skip verifying address and setting flag.
    
    Signed-off-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -310,13 +310,15 @@ static int do_read_inode(struct inode *inode)
 	/* get rdev by using inline_info */
 	__get_inode_rdev(inode, ri);
 
-	err = __written_first_block(sbi, ri);
-	if (err < 0) {
-		f2fs_put_page(node_page, 1);
-		return err;
+	if (S_ISREG(inode->i_mode)) {
+		err = __written_first_block(sbi, ri);
+		if (err < 0) {
+			f2fs_put_page(node_page, 1);
+			return err;
+		}
+		if (!err)
+			set_inode_flag(inode, FI_FIRST_BLOCK_WRITTEN);
 	}
-	if (!err)
-		set_inode_flag(inode, FI_FIRST_BLOCK_WRITTEN);
 
 	if (!need_inode_block_update(sbi, inode->i_ino))
 		fi->last_disk_size = inode->i_size;
