23f64bad173ccafd585fe2f215c1d3fda846af0a
Ahmad Fatoum <a.fatoum@pengutronix.de>
2018-08-21 17:35:48 +0200

    net: macb: Fix regression breaking non-MDIO fixed-link PHYs
    [ Upstream commit ab5f11055fdf8dfc3ddbd89e8e3cc550de41d1d3 ]
    
    commit 739de9a1563a ("net: macb: Reorganize macb_mii bringup") broke
    initializing macb on the EVB-KSZ9477 eval board.
    There, of_mdiobus_register was called even for the fixed-link representing
    the RGMII-link to the switch with the result that the driver attempts to
    enumerate PHYs on a non-existent MDIO bus:
    
    	libphy: MACB_mii_bus: probed
    	mdio_bus f0028000.ethernet-ffffffff: fixed-link has invalid PHY address
    	mdio_bus f0028000.ethernet-ffffffff: scan phy fixed-link at address 0
            [snip]
    	mdio_bus f0028000.ethernet-ffffffff: scan phy fixed-link at address 31
    
    The "MDIO" bus registration succeeds regardless, having claimed the reset GPIO,
    and calling of_phy_register_fixed_link later on fails because it tries
    to claim the same GPIO:
    
    	macb f0028000.ethernet: broken fixed-link specification
    
    Fix this by registering the fixed-link before calling mdiobus_register.
    
    Fixes: 739de9a1563a ("net: macb: Reorganize macb_mii bringup")
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/cadence/macb_main.c
+++ drivers/net/ethernet/cadence/macb_main.c
@@ -481,11 +481,6 @@ static int macb_mii_probe(struct net_device *dev)
 
 	if (np) {
 		if (of_phy_is_fixed_link(np)) {
-			if (of_phy_register_fixed_link(np) < 0) {
-				dev_err(&bp->pdev->dev,
-					"broken fixed-link specification\n");
-				return -ENODEV;
-			}
 			bp->phy_node = of_node_get(np);
 		} else {
 			bp->phy_node = of_parse_phandle(np, "phy-handle", 0);
@@ -568,7 +563,7 @@ static int macb_mii_init(struct macb *bp)
 {
 	struct macb_platform_data *pdata;
 	struct device_node *np;
-	int err;
+	int err = -ENXIO;
 
 	/* Enable management port */
 	macb_writel(bp, NCR, MACB_BIT(MPE));
@@ -591,12 +586,23 @@ static int macb_mii_init(struct macb *bp)
 	dev_set_drvdata(&bp->dev->dev, bp->mii_bus);
 
 	np = bp->pdev->dev.of_node;
-	if (pdata)
-		bp->mii_bus->phy_mask = pdata->phy_mask;
+	if (np && of_phy_is_fixed_link(np)) {
+		if (of_phy_register_fixed_link(np) < 0) {
+			dev_err(&bp->pdev->dev,
+				"broken fixed-link specification %pOF\n", np);
+			goto err_out_free_mdiobus;
+		}
 
-	err = of_mdiobus_register(bp->mii_bus, np);
+		err = mdiobus_register(bp->mii_bus);
+	} else {
+		if (pdata)
+			bp->mii_bus->phy_mask = pdata->phy_mask;
+
+		err = of_mdiobus_register(bp->mii_bus, np);
+	}
+
 	if (err)
-		goto err_out_free_mdiobus;
+		goto err_out_free_fixed_link;
 
 	err = macb_mii_probe(bp->dev);
 	if (err)
@@ -606,6 +612,7 @@ static int macb_mii_init(struct macb *bp)
 
 err_out_unregister_bus:
 	mdiobus_unregister(bp->mii_bus);
+err_out_free_fixed_link:
 	if (np && of_phy_is_fixed_link(np))
 		of_phy_deregister_fixed_link(np);
 err_out_free_mdiobus:
