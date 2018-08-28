cd96cb49b406df547236c6f722a2e92f7c68f275
Serge Schneider <serge@raspberrypi.org>
2018-08-28 09:23:32 +0100

    rpi-poe-fan: Add SPDX-License-Identifier
    Signed-off-by: Serge Schneider <serge@raspberrypi.org>

--- drivers/hwmon/rpi-poe-fan.c
+++ drivers/hwmon/rpi-poe-fan.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * rpi-poe-fan.c - Hwmon driver for Raspberry Pi POE HAT fan.
  *
@@ -5,16 +6,6 @@
  * Based on pwm-fan.c by Kamil Debski <k.debski@samsung.com>
  *
  * Author: Serge Schneider <serge@raspberrypi.org>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  */
 
 #include <linux/hwmon.h>
