cd9320a1954642117f572891a8b45b177e6b0ebf
Julia Cartwright <julia@ni.com>
2018-09-28 21:03:51 +0000

    kthread: convert worker lock to raw spinlock
    In order to enable the queuing of kthread work items from hardirq
    context even when PREEMPT_RT_FULL is enabled, convert the worker
    spin_lock to a raw_spin_lock.
    
    This is only acceptable to do because the work performed under the lock
    is well-bounded and minimal.
    
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Guenter Roeck <linux@roeck-us.net>
    Reported-and-tested-by: Steffen Trumtrar <s.trumtrar@pengutronix.de>
    Reported-by: Tim Sander <tim@krieglstein.org>
    Signed-off-by: Julia Cartwright <julia@ni.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- include/linux/kthread.h
+++ include/linux/kthread.h
@@ -85,7 +85,7 @@ enum {
 
 struct kthread_worker {
 	unsigned int		flags;
-	spinlock_t		lock;
+	raw_spinlock_t		lock;
 	struct list_head	work_list;
 	struct list_head	delayed_work_list;
 	struct task_struct	*task;
