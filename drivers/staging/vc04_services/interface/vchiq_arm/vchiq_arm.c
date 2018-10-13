1ba9ed60ceb53f4d0ac2c908f4c19d0ed3a17e0d
Stefan Wahren <stefan.wahren@i2se.com>
2018-10-13 20:51:23 +0200

    staging: vchiq_arm: Fix platform device unregistration
    In error case platform_device_register_data would return an ERR_PTR
    instead of NULL. So we better check this before unregistration.
    
    Fixes: 37b7b3087a2f ("staging/vc04_services: Register a platform device for the camera driver.")
    Signed-off-by: Stefan Wahren <stefan.wahren@i2se.com>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
@@ -3259,7 +3259,8 @@ failed_platform_init:
 
 static int vchiq_remove(struct platform_device *pdev)
 {
-	platform_device_unregister(bcm2835_camera);
+	if (!IS_ERR(bcm2835_camera))
+		platform_device_unregister(bcm2835_camera);
 	vchiq_debugfs_deinit();
 	device_destroy(vchiq_class, vchiq_devid);
 	cdev_del(&vchiq_cdev);
