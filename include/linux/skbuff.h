8db1aafae11a38cd8375d02916266034044b5185
Dave Stevenson <dave.stevenson@raspberrypi.org>
2018-10-17 14:01:02 +0100

    Revert "Revert "net: pskb_trim_rcsum() and CHECKSUM_COMPLETE are friends""
    This reverts commit 2a2fbe18065af8537c9f6d2cf85f3d79fa35601f.

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -3134,6 +3134,7 @@ static inline void *skb_push_rcsum(struct sk_buff *skb, unsigned int len)
 	return skb->data;
 }
 
+int pskb_trim_rcsum_slow(struct sk_buff *skb, unsigned int len);
 /**
  *	pskb_trim_rcsum - trim received skb and update checksum
  *	@skb: buffer to trim
@@ -3147,9 +3148,7 @@ static inline int pskb_trim_rcsum(struct sk_buff *skb, unsigned int len)
 {
 	if (likely(len >= skb->len))
 		return 0;
-	if (skb->ip_summed == CHECKSUM_COMPLETE)
-		skb->ip_summed = CHECKSUM_NONE;
-	return __pskb_trim(skb, len);
+	return pskb_trim_rcsum_slow(skb, len);
 }
 
 static inline int __skb_trim_rcsum(struct sk_buff *skb, unsigned int len)
