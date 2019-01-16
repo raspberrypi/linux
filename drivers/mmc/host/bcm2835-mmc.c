953d790e68dcfdf7c3e0d68bc2529e0909b59a02
Lukas Wunner <lukas@wunner.de>
2019-01-16 12:22:32 +0100

    bcm2835-mmc: Fix DMA channel leak
    The BCM2835 MMC host driver requests a DMA channel on probe but neglects
    to release the channel in the probe error path and on driver unbind.
    
    I'm seeing this happen on every boot of the Compute Module 3: On first
    driver probe, DMA channel 2 is allocated and then leaked with a "could
    not get clk, deferring probe" message. On second driver probe, channel 4
    is allocated.
    
    Fix it.
    
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Cc: Frank Pavlic <f.pavlic@kunbus.de>

--- drivers/mmc/host/bcm2835-mmc.c
+++ drivers/mmc/host/bcm2835-mmc.c
@@ -1503,6 +1503,8 @@ static int bcm2835_mmc_probe(struct platform_device *pdev)
 
 	return 0;
 err:
+	if (host->dma_chan_rxtx)
+		dma_release_channel(host->dma_chan_rxtx);
 	mmc_free_host(mmc);
 
 	return ret;
@@ -1547,6 +1549,9 @@ static int bcm2835_mmc_remove(struct platform_device *pdev)
 	del_timer_sync(&host->timer);
 
 	tasklet_kill(&host->finish_tasklet);
+
+	if (host->dma_chan_rxtx)
+		dma_release_channel(host->dma_chan_rxtx);
 
 	mmc_free_host(host->mmc);
 	platform_set_drvdata(pdev, NULL);
