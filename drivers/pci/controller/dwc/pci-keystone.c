02cb726bfabbe72fe12c15b26eba0fa949399d0a
Kishon Vijay Abraham I <kishon@ti.com>
2019-03-25 15:09:33 +0530

    PCI: keystone: Prevent ARM32 specific code to be compiled for ARM64
    [ Upstream commit f316a2b53cd7f37963ae20ec7072eb27a349a4ce ]
    
    hook_fault_code() is an ARM32 specific API for hooking into data abort.
    
    AM65X platforms (that integrate ARM v8 cores and select CONFIG_ARM64 as
    arch) rely on pci-keystone.c but on them the enumeration of a
    non-present BDF does not trigger a bus error, so the fixup exception
    provided by calling hook_fault_code() is not needed and can be guarded
    with CONFIG_ARM.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    [lorenzo.pieralisi@arm.com: commit log]
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/controller/dwc/pci-keystone.c
+++ drivers/pci/controller/dwc/pci-keystone.c
@@ -705,6 +705,7 @@ static void ks_pcie_setup_interrupts(struct keystone_pcie *ks_pcie)
 		ks_pcie_enable_error_irq(ks_pcie);
 }
 
+#ifdef CONFIG_ARM
 /*
  * When a PCI device does not exist during config cycles, keystone host gets a
  * bus error instead of returning 0xffffffff. This handler always returns 0
@@ -724,6 +725,7 @@ static int ks_pcie_fault(unsigned long addr, unsigned int fsr,
 
 	return 0;
 }
+#endif
 
 static int __init ks_pcie_init_id(struct keystone_pcie *ks_pcie)
 {
@@ -766,12 +768,14 @@ static int __init ks_pcie_host_init(struct pcie_port *pp)
 	if (ret < 0)
 		return ret;
 
+#ifdef CONFIG_ARM
 	/*
 	 * PCIe access errors that result into OCP errors are caught by ARM as
 	 * "External aborts"
 	 */
 	hook_fault_code(17, ks_pcie_fault, SIGBUS, 0,
 			"Asynchronous external abort");
+#endif
 
 	return 0;
 }
