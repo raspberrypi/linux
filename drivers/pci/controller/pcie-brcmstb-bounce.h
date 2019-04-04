845d0322ef8c459c2a70899abe17da9b42b5eb7e
Phil Elwell <phil@raspberrypi.org>
2019-04-04 13:33:47 +0100

    bcm2835-dma: Add proper 40-bit DMA support
    The 40-bit additions are not fully tested, but it should be
    capable of supporting both 40-bit memcpy on BCM2711 and regular
    Lite channels on BCM2835.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- drivers/pci/controller/pcie-brcmstb-bounce.h
+++ drivers/pci/controller/pcie-brcmstb-bounce.h
@@ -8,21 +8,26 @@
 
 #ifdef CONFIG_ARM
 
-int brcm_pcie_bounce_register_dev(struct device *dev, unsigned long buffer_size,
-				  dma_addr_t threshold);
-
-int brcm_pcie_bounce_unregister_dev(struct device *dev);
+int brcm_pcie_bounce_init(struct device *dev, unsigned long buffer_size,
+			  dma_addr_t threshold);
+int brcm_pcie_bounce_uninit(struct device *dev);
+int brcm_pcie_bounce_register_dev(struct device *dev);
 
 #else
 
-static inline int brcm_pcie_bounce_register_dev(struct device *dev,
-						unsigned long buffer_size,
-						dma_addr_t threshold)
+static inline int brcm_pcie_bounce_init(struct device *dev,
+					unsigned long buffer_size,
+					dma_addr_t threshold)
 {
 	return 0;
 }
 
-static inline int brcm_pcie_bounce_unregister_dev(struct device *dev)
+static inline int brcm_pcie_bounce_uninit(struct device *dev)
+{
+	return 0;
+}
+
+static inline int brcm_pcie_bounce_register_dev(struct device *dev)
 {
 	return 0;
 }
