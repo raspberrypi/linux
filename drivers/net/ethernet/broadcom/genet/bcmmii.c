8ebde79afb6bec8a1a10870a686d6493081a4960
Jonathan Bell <jonathan@raspberrypi.org>
2019-05-14 17:17:59 +0100

    net: genet: enable link energy detect powerdown for external PHYs
    There are several warts surrounding bcmgenet_mii_probe() as this
    function is called from ndo_open, but it's calling registration-type
    functions. The probe should be called at probe time and refactored
    such that the PHY device data can be extracted to limit the scope
    of this flag to Broadcom PHYs.
    
    For now, pass this flag in as it puts our attached PHY into a low-power
    state when disconnected.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.org>

--- drivers/net/ethernet/broadcom/genet/bcmmii.c
+++ drivers/net/ethernet/broadcom/genet/bcmmii.c
@@ -300,6 +300,8 @@ int bcmgenet_mii_probe(struct net_device *dev)
 	/* Communicate the integrated PHY revision */
 	if (priv->internal_phy)
 		phy_flags = priv->gphy_rev;
+	else
+		phy_flags = PHY_BRCM_AUTO_PWRDWN_ENABLE;
 
 	/* This is an ugly quirk but we have not been correctly interpreting
 	 * the phy_interface values and we have done that across different
