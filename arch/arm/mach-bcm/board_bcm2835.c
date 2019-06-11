ef6d535c2173f34bdba5bf9f1e6c9dd5c01321e5
Phil Elwell <phil@raspberrypi.org>
2019-06-11 17:38:28 +0100

    ARM: bcm: Backport BCM2711 support from upstream
    Make the BCM2711 a different machine, but keep it in board_bcm2835.
    
    Signed-off-by: Stefan Wahren <wahrenst@gmx.net>

--- arch/arm/mach-bcm/board_bcm2835.c
+++ arch/arm/mach-bcm/board_bcm2835.c
@@ -114,11 +114,25 @@ static const char * const bcm2835_compat[] = {
 };
 
 DT_MACHINE_START(BCM2835, "BCM2835")
+	.map_io = bcm2835_map_io,
+	.init_machine = bcm2835_init,
+	.dt_compat = bcm2835_compat,
+	.smp = smp_ops(bcm2836_smp_ops),
+MACHINE_END
+
+static const char * const bcm2711_compat[] = {
+#ifdef CONFIG_ARCH_MULTI_V7
+	"brcm,bcm2711",
+#endif
+	NULL
+};
+
+DT_MACHINE_START(BCM2711, "BCM2711")
 #if defined(CONFIG_ZONE_DMA) && defined(CONFIG_ARM_LPAE)
 	.dma_zone_size	= SZ_1G,
 #endif
 	.map_io = bcm2835_map_io,
 	.init_machine = bcm2835_init,
-	.dt_compat = bcm2835_compat,
+	.dt_compat = bcm2711_compat,
 	.smp = smp_ops(bcm2836_smp_ops),
 MACHINE_END
