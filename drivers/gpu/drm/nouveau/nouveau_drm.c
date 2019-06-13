e73b09c75adaaea1fb446d81e31fa0cc95d571eb
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2019-06-13 09:36:32 +0200

    Revert "drm/nouveau: add kconfig option to turn off nouveau legacy contexts. (v3)"
    This reverts commit 1e07d637497788971018d9e9e5a184940b1ade0f which is
    commit b30a43ac7132cdda833ac4b13dd1ebd35ace14b7 upstream.
    
    Sven reports:
    	Commit 1e07d63749 ("drm/nouveau: add kconfig option to turn off nouveau
    	legacy contexts. (v3)") has caused a build failure for me when I
    	actually tried that option (CONFIG_NOUVEAU_LEGACY_CTX_SUPPORT=n):
    
    	,----
    	| Kernel: arch/x86/boot/bzImage is ready  (#1)
    	|   Building modules, stage 2.
    	|   MODPOST 290 modules
    	| ERROR: "drm_legacy_mmap" [drivers/gpu/drm/nouveau/nouveau.ko] undefined!
    	| scripts/Makefile.modpost:91: recipe for target '__modpost' failed
    	`----
    
    	Upstream does not have that problem, as commit bed2dd8421 ("drm/ttm:
    	Quick-test mmap offset in ttm_bo_mmap()") has removed the use of
    	drm_legacy_mmap from nouveau_ttm.c.  Unfortunately that commit does not
    	apply in 5.1.9.
    
    The ensuing discussion proposed a number of one-off patches, but no
    solid agreement was made, so just revert the commit for now to get
    people's systems building again.
    
    Reported-by: Sven Joachim <svenjoac@gmx.de>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: Dave Airlie <airlied@redhat.com>
    Cc: Thomas Backlund <tmb@mageia.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -1094,11 +1094,8 @@ nouveau_driver_fops = {
 static struct drm_driver
 driver_stub = {
 	.driver_features =
-		DRIVER_GEM | DRIVER_MODESET | DRIVER_PRIME | DRIVER_RENDER
-#if defined(CONFIG_NOUVEAU_LEGACY_CTX_SUPPORT)
-		| DRIVER_KMS_LEGACY_CONTEXT
-#endif
-		,
+		DRIVER_GEM | DRIVER_MODESET | DRIVER_PRIME | DRIVER_RENDER |
+		DRIVER_KMS_LEGACY_CONTEXT,
 
 	.open = nouveau_drm_open,
 	.postclose = nouveau_drm_postclose,
