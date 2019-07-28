de82d884745dda6fc6e2316a65bf367eb1174f4f
Phil Elwell <phil@raspberrypi.org>
2019-07-28 22:22:36 +0100

    drivers: char: Use correct name for the Raspberry Pi video decoder
    Replace the old code name with a more appropriate name - RPiVid.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.org>

--- drivers/char/broadcom/rpivid-mem.c
+++ drivers/char/broadcom/rpivid-mem.c
@@ -1,5 +1,5 @@
 /**
- * argon-mem.c - character device access to the Argon decoder registers
+ * rpivid-mem.c - character device access to the RPiVid decoder registers
  *
  * Based on bcm2835-gpiomem.c. Provides IO memory access to the decoder
  * register blocks such that ffmpeg plugins can access the hardware.
@@ -48,36 +48,36 @@
 #include <linux/pagemap.h>
 #include <linux/io.h>
 
-#define DRIVER_NAME "argon-mem"
+#define DRIVER_NAME "rpivid-mem"
 #define DEVICE_MINOR 0
 
-struct argon_mem_priv {
+struct rpivid_mem_priv {
 	dev_t devid;
 	struct class *class;
-	struct cdev argon_mem_cdev;
+	struct cdev rpivid_mem_cdev;
 	unsigned long regs_phys;
 	unsigned long mem_window_len;
 	struct device *dev;
 	const char *name;
 };
 
-static int argon_mem_open(struct inode *inode, struct file *file)
+static int rpivid_mem_open(struct inode *inode, struct file *file)
 {
 	int dev = iminor(inode);
 	int ret = 0;
-	struct argon_mem_priv *priv;
+	struct rpivid_mem_priv *priv;
 	if (dev != DEVICE_MINOR)
 		ret = -ENXIO;
 
-	priv = container_of(inode->i_cdev, struct argon_mem_priv,
-				argon_mem_cdev);
+	priv = container_of(inode->i_cdev, struct rpivid_mem_priv,
+				rpivid_mem_cdev);
 	if (!priv)
 		return -EINVAL;
 	file->private_data = priv;
 	return ret;
 }
 
-static int argon_mem_release(struct inode *inode, struct file *file)
+static int rpivid_mem_release(struct inode *inode, struct file *file)
 {
 	int dev = iminor(inode);
 	int ret = 0;
@@ -88,15 +88,15 @@ static int argon_mem_release(struct inode *inode, struct file *file)
 	return ret;
 }
 
-static const struct vm_operations_struct argon_mem_vm_ops = {
+static const struct vm_operations_struct rpivid_mem_vm_ops = {
 #ifdef CONFIG_HAVE_IOREMAP_PROT
 	.access = generic_access_phys
 #endif
 };
 
-static int argon_mem_mmap(struct file *file, struct vm_area_struct *vma)
+static int rpivid_mem_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	struct argon_mem_priv *priv;
+	struct rpivid_mem_priv *priv;
 	unsigned long pages;
 
 	priv = file->private_data;
@@ -108,7 +108,7 @@ static int argon_mem_mmap(struct file *file, struct vm_area_struct *vma)
 	vma->vm_page_prot = phys_mem_access_prot(file, pages,
 						 priv->mem_window_len,
 						 vma->vm_page_prot);
-	vma->vm_ops = &argon_mem_vm_ops;
+	vma->vm_ops = &rpivid_mem_vm_ops;
 	if (remap_pfn_range(vma, vma->vm_start,
 			pages,
 			priv->mem_window_len,
@@ -119,28 +119,28 @@ static int argon_mem_mmap(struct file *file, struct vm_area_struct *vma)
 }
 
 static const struct file_operations
-argon_mem_fops = {
+rpivid_mem_fops = {
 	.owner = THIS_MODULE,
-	.open = argon_mem_open,
-	.release = argon_mem_release,
-	.mmap = argon_mem_mmap,
+	.open = rpivid_mem_open,
+	.release = rpivid_mem_release,
+	.mmap = rpivid_mem_mmap,
 };
 
-static const struct of_device_id argon_mem_of_match[];
-static int argon_mem_probe(struct platform_device *pdev)
+static const struct of_device_id rpivid_mem_of_match[];
+static int rpivid_mem_probe(struct platform_device *pdev)
 {
 	int err;
 	void *ptr_err;
 	const struct of_device_id *id;
 	struct device *dev = &pdev->dev;
-	struct device *argon_mem_dev;
+	struct device *rpivid_mem_dev;
 	struct resource *ioresource;
-	struct argon_mem_priv *priv;
+	struct rpivid_mem_priv *priv;
 
 
 	/* Allocate buffers and instance data */
 
-	priv = kzalloc(sizeof(struct argon_mem_priv), GFP_KERNEL);
+	priv = kzalloc(sizeof(struct rpivid_mem_priv), GFP_KERNEL);
 
 	if (!priv) {
 		err = -ENOMEM;
@@ -149,7 +149,7 @@ static int argon_mem_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, priv);
 
 	priv->dev = dev;
-	id = of_match_device(argon_mem_of_match, dev);
+	id = of_match_device(rpivid_mem_of_match, dev);
 	if (!id)
 		return -EINVAL;
 	priv->name = id->data;
@@ -172,9 +172,9 @@ static int argon_mem_probe(struct platform_device *pdev)
 		dev_err(priv->dev, "unable to allocate device number");
 		goto failed_alloc_chrdev;
 	}
-	cdev_init(&priv->argon_mem_cdev, &argon_mem_fops);
-	priv->argon_mem_cdev.owner = THIS_MODULE;
-	err = cdev_add(&priv->argon_mem_cdev, priv->devid, 1);
+	cdev_init(&priv->rpivid_mem_cdev, &rpivid_mem_fops);
+	priv->rpivid_mem_cdev.owner = THIS_MODULE;
+	err = cdev_add(&priv->rpivid_mem_cdev, priv->devid, 1);
 	if (err != 0) {
 		dev_err(priv->dev, "unable to register device");
 		goto failed_cdev_add;
@@ -187,10 +187,10 @@ static int argon_mem_probe(struct platform_device *pdev)
 	if (IS_ERR(ptr_err))
 		goto failed_class_create;
 
-	argon_mem_dev = device_create(priv->class, NULL,
+	rpivid_mem_dev = device_create(priv->class, NULL,
 					priv->devid, NULL,
 					priv->name);
-	ptr_err = argon_mem_dev;
+	ptr_err = rpivid_mem_dev;
 	if (IS_ERR(ptr_err))
 		goto failed_device_create;
 
@@ -202,7 +202,7 @@ static int argon_mem_probe(struct platform_device *pdev)
 failed_device_create:
 	class_destroy(priv->class);
 failed_class_create:
-	cdev_del(&priv->argon_mem_cdev);
+	cdev_del(&priv->rpivid_mem_cdev);
 	err = PTR_ERR(ptr_err);
 failed_cdev_add:
 	unregister_chrdev_region(priv->devid, 1);
@@ -210,18 +210,18 @@ failed_alloc_chrdev:
 failed_get_resource:
 	kfree(priv);
 failed_inst_alloc:
-	dev_err(priv->dev, "could not load argon_mem");
+	dev_err(priv->dev, "could not load rpivid_mem");
 	return err;
 }
 
-static int argon_mem_remove(struct platform_device *pdev)
+static int rpivid_mem_remove(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	struct argon_mem_priv *priv = platform_get_drvdata(pdev);
+	struct rpivid_mem_priv *priv = platform_get_drvdata(pdev);
 
 	device_destroy(priv->class, priv->devid);
 	class_destroy(priv->class);
-	cdev_del(&priv->argon_mem_cdev);
+	cdev_del(&priv->rpivid_mem_cdev);
 	unregister_chrdev_region(priv->devid, 1);
 	kfree(priv);
 
@@ -229,49 +229,44 @@ static int argon_mem_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static const char argon_hevc_name[] = "argon-hevcmem";
-static const char argon_h264_name[] = "argon-h264mem";
-static const char argon_vp9_name[] = "argon-vp9mem";
-static const char argon_intc_name[] = "argon-intcmem";
-
-static const struct of_device_id argon_mem_of_match[] = {
+static const struct of_device_id rpivid_mem_of_match[] = {
 	{
-		.compatible = "raspberrypi,argon-hevc-decoder",
-		.data = &argon_hevc_name,
+		.compatible = "raspberrypi,rpivid-hevc-decoder",
+		.data = "rpivid-hevcmem",
 	},
 	{
-		.compatible = "raspberrypi,argon-h264-decoder",
-		.data = &argon_h264_name,
+		.compatible = "raspberrypi,rpivid-h264-decoder",
+		.data = "rpivid-h264mem",
 	},
 	{
-		.compatible = "raspberrypi,argon-vp9-decoder",
-		.data = &argon_vp9_name,
+		.compatible = "raspberrypi,rpivid-vp9-decoder",
+		.data = "rpivid-vp9mem",
 	},
 	/* The "intc" is included as this block of hardware contains the
 	 * "frame done" status flags.
 	 */
 	{
-		.compatible = "raspberrypi,argon-local-intc",
-		.data = &argon_intc_name,
+		.compatible = "raspberrypi,rpivid-local-intc",
+		.data = "rpivid-intcmem",
 	},
 	{ /* sentinel */ },
 };
 
-MODULE_DEVICE_TABLE(of, argon_mem_of_match);
+MODULE_DEVICE_TABLE(of, rpivid_mem_of_match);
 
-static struct platform_driver argon_mem_driver = {
-	.probe = argon_mem_probe,
-	.remove = argon_mem_remove,
+static struct platform_driver rpivid_mem_driver = {
+	.probe = rpivid_mem_probe,
+	.remove = rpivid_mem_remove,
 	.driver = {
 		   .name = DRIVER_NAME,
 		   .owner = THIS_MODULE,
-		   .of_match_table = argon_mem_of_match,
+		   .of_match_table = rpivid_mem_of_match,
 		   },
 };
 
-module_platform_driver(argon_mem_driver);
+module_platform_driver(rpivid_mem_driver);
 
-MODULE_ALIAS("platform:argon-mem");
+MODULE_ALIAS("platform:rpivid-mem");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("Driver for accessing Argon decoder registers from userspace");
+MODULE_DESCRIPTION("Driver for accessing RPiVid decoder registers from userspace");
 MODULE_AUTHOR("Jonathan Bell <jonathan@raspberrypi.org>");
