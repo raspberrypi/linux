bdff55b2d948d44f555bae75d7000636dc83f5ca
Jonathan Bell <jonathan@raspberrypi.org>
2019-08-13 15:53:29 +0100

    xhci: Use more event ring segment table entries
    Users have reported log spam created by "Event Ring Full" xHC event
    TRBs. These are caused by interrupt latency in conjunction with a very
    busy set of devices on the bus. The errors are benign, but throughput
    will suffer as the xHC will pause processing of transfers until the
    event ring is drained by the kernel. Expand the number of event TRB slots
    available by increasing the number of event ring segments in the ERST.
    
    Controllers have a hardware-defined limit as to the number of ERST
    entries they can process, so make the actual number in use
    min(ERST_MAX_SEGS, hw_max).
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.org>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1673,8 +1673,8 @@ struct urb_priv {
  * Each segment table entry is 4*32bits long.  1K seems like an ok size:
  * (1K bytes * 8bytes/bit) / (4*32 bits) = 64 segment entries in the table,
  * meaning 64 ring segments.
- * Initial allocated size of the ERST, in number of entries */
-#define	ERST_NUM_SEGS	1
+ * Maximum number of segments in the ERST */
+#define	ERST_MAX_SEGS	8
 /* Poll every 60 seconds */
 #define	POLL_TIMEOUT	60
 /* Stop endpoint command timeout (secs) for URB cancellation watchdog timer */
