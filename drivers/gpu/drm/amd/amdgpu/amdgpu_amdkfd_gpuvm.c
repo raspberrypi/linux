e8d6bbfe5dfc7b28e8cab2c1d322a0d47700019e
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-08-14 16:38:43 +0200

    dma-buf: Use seqlock_t instread disabling preemption
    [ Upstream commit 240610aa31094f51f299f06eb8dae8d4cd8d4500 ]
    
    "dma reservation" disables preemption while acquiring the write access
    for "seqcount" and then may acquire a spinlock_t.
    
    Replace the seqcount with a seqlock_t which provides seqcount like
    semantic and lock for writer.
    
    Link: https://lkml.kernel.org/r/f410b429-db86-f81c-7c67-f563fa808b62@free.fr
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_amdkfd_gpuvm.c
@@ -261,11 +261,9 @@ static int amdgpu_amdkfd_remove_eviction_fence(struct amdgpu_bo *bo,
 	}
 
 	/* Install the new fence list, seqcount provides the barriers */
-	preempt_disable();
-	write_seqcount_begin(&resv->seq);
+	write_seqlock(&resv->seq);
 	RCU_INIT_POINTER(resv->fence, new);
-	write_seqcount_end(&resv->seq);
-	preempt_enable();
+	write_sequnlock(&resv->seq);
 
 	/* Drop the references to the removed fences or move them to ef_list */
 	for (i = j, k = 0; i < old->shared_count; ++i) {
