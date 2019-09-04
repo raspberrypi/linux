40aae5708e7ac10ad9faa0168aba306a3487656d
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-09-04 16:55:27 +0200

    hrtimer: Add a missing bracket and hide `migration_base' on !SMP
    [ Upstream commit 47b6de0b7f22c28b40275aeede3993d807668c3b ]
    
    [ Upstream commit 5d2295f3a93b04986d069ebeaf5b07725f9096c1 ]
    
    The recent change to avoid taking the expiry lock when a timer is currently
    migrated missed to add a bracket at the end of the if statement leading to
    compile errors.  Since that commit the variable `migration_base' is always
    used but it is only available on SMP configuration thus leading to another
    compile error.  The changelog says "The timer base and base->cpu_base
    cannot be NULL in the code path", so it is safe to limit this check to SMP
    configurations only.
    
    Add the missing bracket to the if statement and hide `migration_base'
    behind CONFIG_SMP bars.
    
    [ tglx: Mark the functions inline ... ]
    
    Fixes: 68b2c8c1e4210 ("hrtimer: Don't take expiry_lock when timer is currently migrated")
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20190904145527.eah7z56ntwobqm6j@linutronix.de
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>
    [bigeasy: port back to RT]
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -150,6 +150,11 @@ static struct hrtimer_cpu_base migration_cpu_base = {
 
 #define migration_base	migration_cpu_base.clock_base[0]
 
+static inline bool is_migration_base(struct hrtimer_clock_base *base)
+{
+	return base == &migration_base;
+}
+
 /*
  * We are using hashed locking: holding per_cpu(hrtimer_bases)[n].lock
  * means that all timers which are tied to this base via timer->base are
@@ -273,6 +278,11 @@ again:
 }
 
 #else /* CONFIG_SMP */
+
+static inline bool is_migration_base(struct hrtimer_clock_base *base)
+{
+	return false;
+}
 
 static inline struct hrtimer_clock_base *
 lock_hrtimer_base(const struct hrtimer *timer, unsigned long *flags)
@@ -943,7 +953,7 @@ void hrtimer_grab_expiry_lock(const struct hrtimer *timer)
 {
 	struct hrtimer_clock_base *base = READ_ONCE(timer->base);
 
-	if (timer->is_soft && base != &migration_base) {
+	if (timer->is_soft && is_migration_base(base)) {
 		spin_lock(&base->cpu_base->softirq_expiry_lock);
 		spin_unlock(&base->cpu_base->softirq_expiry_lock);
 	}
