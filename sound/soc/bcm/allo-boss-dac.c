a744810f7ed771f0d656ae1af11fb4a74a33cc9d
Hui Wang <hui.wang@canonical.com>
2019-09-05 20:37:19 +0800

    ASoC: allo-boss-dac: use modern dai_link style
    Signed-off-by: Hui Wang <hui.wang@canonical.com>

--- sound/soc/bcm/allo-boss-dac.c
+++ sound/soc/bcm/allo-boss-dac.c
@@ -346,19 +346,21 @@ static struct snd_soc_ops snd_allo_boss_ops = {
 	.prepare = snd_allo_boss_prepare,
 };
 
+SND_SOC_DAILINK_DEFS(allo_boss,
+	DAILINK_COMP_ARRAY(COMP_CPU("bcm2708-i2s.0")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("pcm512x.1-004d", "pcm512x-hifi")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("bcm2708-i2s.0")));
+
 static struct snd_soc_dai_link snd_allo_boss_dai[] = {
 {
 	.name		= "Boss DAC",
 	.stream_name	= "Boss DAC HiFi",
-	.cpu_dai_name	= "bcm2708-i2s.0",
-	.codec_dai_name	= "pcm512x-hifi",
-	.platform_name	= "bcm2708-i2s.0",
-	.codec_name	= "pcm512x.1-004d",
 	.dai_fmt	= SND_SOC_DAIFMT_I2S |
 			  SND_SOC_DAIFMT_NB_NF |
 			  SND_SOC_DAIFMT_CBS_CFS,
 	.ops		= &snd_allo_boss_ops,
 	.init		= snd_allo_boss_init,
+	SND_SOC_DAILINK_REG(allo_boss),
 },
 };
 
@@ -385,10 +387,10 @@ static int snd_allo_boss_probe(struct platform_device *pdev)
 					    "i2s-controller", 0);
 
 		if (i2s_node) {
-			dai->cpu_dai_name = NULL;
-			dai->cpu_of_node = i2s_node;
-			dai->platform_name = NULL;
-			dai->platform_of_node = i2s_node;
+			dai->cpus->dai_name = NULL;
+			dai->cpus->of_node = i2s_node;
+			dai->platforms->name = NULL;
+			dai->platforms->of_node = i2s_node;
 		}
 
 		digital_gain_0db_limit = !of_property_read_bool(
