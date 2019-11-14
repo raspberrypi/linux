adeec3de92221e6f32935b95e96b7418f14ea457
Michael Kelley <mikelley@microsoft.com>
2019-11-14 06:32:01 +0000

    Drivers: hv: vmbus: Fix crash handler reset of Hyper-V synic
    [ Upstream commit 7a1323b5dfe44a9013a2cc56ef2973034a00bf88 ]
    
    The crash handler calls hv_synic_cleanup() to shutdown the
    Hyper-V synthetic interrupt controller.  But if the CPU
    that calls hv_synic_cleanup() has a VMbus channel interrupt
    assigned to it (which is likely the case in smaller VM sizes),
    hv_synic_cleanup() returns an error and the synthetic
    interrupt controller isn't shutdown.  While the lack of
    being shutdown hasn't caused a known problem, it still
    should be fixed for highest reliability.
    
    So directly call hv_synic_disable_regs() instead of
    hv_synic_cleanup(), which ensures that the synic is always
    shutdown.
    
    Signed-off-by: Michael Kelley <mikelley@microsoft.com>
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Reviewed-by: Dexuan Cui <decui@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/hv/vmbus_drv.c
+++ drivers/hv/vmbus_drv.c
@@ -2308,7 +2308,7 @@ static void hv_crash_handler(struct pt_regs *regs)
 	vmbus_connection.conn_state = DISCONNECTED;
 	cpu = smp_processor_id();
 	hv_stimer_cleanup(cpu);
-	hv_synic_cleanup(cpu);
+	hv_synic_disable_regs(cpu);
 	hyperv_cleanup();
 };
 
