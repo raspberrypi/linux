afc13aa9400b5d3d54efa4b4183168a9a3e03c12
Michael Chan <michael.chan@broadcom.com>
2019-12-10 02:49:08 -0500

    bnxt_en: Free context memory in the open path if firmware has been reset.
    [ Upstream commit 325f85f37e5b35807d86185bdf2c64d2980c44ba ]
    
    This will trigger new context memory to be rediscovered and allocated
    during the re-probe process after a firmware reset.  Without this, the
    newly reset firmware does not have valid context memory and the driver
    will eventually fail to allocate some resources.
    
    Fixes: ec5d31e3c15d ("bnxt_en: Handle firmware reset status during IF_UP.")
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -8766,6 +8766,9 @@ static int bnxt_hwrm_if_change(struct bnxt *bp, bool up)
 	}
 	if (resc_reinit || fw_reset) {
 		if (fw_reset) {
+			bnxt_free_ctx_mem(bp);
+			kfree(bp->ctx);
+			bp->ctx = NULL;
 			rc = bnxt_fw_init_one(bp);
 			if (rc) {
 				set_bit(BNXT_STATE_ABORT_ERR, &bp->state);
