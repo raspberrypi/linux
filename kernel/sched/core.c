5ae2243ef4ce15846dcac189c4ea95adbf108b07
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-12-12 10:53:59 +0100

    sched: migrate_enable: Busy loop until the migration request is completed
    [ Upstream commit 140d7f54a5fff02898d2ca9802b39548bf7455f1 ]
    
    If user task changes the CPU affinity mask of a running task it will
    dispatch migration request if the current CPU is no longer allowed. This
    might happen shortly before a task enters a migrate_disable() section.
    Upon leaving the migrate_disable() section, the task will notice that
    the current CPU is no longer allowed and will will dispatch its own
    migration request to move it off the current CPU.
    While invoking __schedule() the first migration request will be
    processed and the task returns on the "new" CPU with "arg.done = 0". Its
    own migration request will be processed shortly after and will result in
    memory corruption if the stack memory, designed for request, was used
    otherwise in the meantime.
    
    Spin until the migration request has been processed if it was accepted.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7329,7 +7329,7 @@ void migrate_enable(void)
 
 	WARN_ON(smp_processor_id() != cpu);
 	if (!is_cpu_allowed(p, cpu)) {
-		struct migration_arg arg = { p };
+		struct migration_arg arg = { .task = p };
 		struct cpu_stop_work work;
 		struct rq_flags rf;
 
@@ -7342,7 +7342,10 @@ void migrate_enable(void)
 				    &arg, &work);
 		tlb_migrate_finish(p->mm);
 		__schedule(true);
-		WARN_ON_ONCE(!arg.done && !work.disabled);
+		if (!work.disabled) {
+			while (!arg.done)
+				cpu_relax();
+		}
 	}
 
 out:
