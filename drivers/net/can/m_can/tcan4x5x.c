d527bb8bd577f511a6096036cd3f3840a607efff
Marc Kleine-Budde <mkl@pengutronix.de>
2020-01-03 11:30:34 +0100

    can: tcan4x5x: tcan4x5x_can_probe(): add missing error checking for devm_regmap_init()
    [ Upstream commit 1ff203badbbf1738027c8395d5b40b0d462b6e4d ]
    
    This patch adds the missing error checking when initializing the regmap
    interface fails.
    
    Fixes: 5443c226ba91 ("can: tcan4x5x: Add tcan4x5x driver to the kernel")
    Cc: Dan Murphy <dmurphy@ti.com>
    Link: http://lore.kernel.org/r/20201019154233.1262589-7-mkl@pengutronix.de
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/can/m_can/tcan4x5x.c
+++ drivers/net/can/m_can/tcan4x5x.c
@@ -467,6 +467,10 @@ static int tcan4x5x_can_probe(struct spi_device *spi)
 
 	priv->regmap = devm_regmap_init(&spi->dev, &tcan4x5x_bus,
 					&spi->dev, &tcan4x5x_regmap);
+	if (IS_ERR(priv->regmap)) {
+		ret = PTR_ERR(priv->regmap);
+		goto out_clk;
+	}
 
 	tcan4x5x_power_enable(priv->power, 1);
 
