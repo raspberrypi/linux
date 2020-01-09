879043dc5c25338d6adfab910c5801013b537eeb
Mario Kleiner <mario.kleiner.de@gmail.com>
2020-01-09 16:20:27 +0100

    drm/amd/display: Reorder detect_edp_sink_caps before link settings read.
    [ Upstream commit 3b7c59754cc22760760a84ebddb8e0b1e8dd871b ]
    
    read_current_link_settings_on_detect() on eDP 1.4+ may use the
    edp_supported_link_rates table which is set up by
    detect_edp_sink_caps(), so that function needs to be called first.
    
    Reviewed-by: Harry Wentland <harry.wentland@amd.com>
    Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
    Cc: Martin Leung <martin.leung@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -809,8 +809,8 @@ bool dc_link_detect(struct dc_link *link, enum dc_detect_reason reason)
 		}
 
 		case SIGNAL_TYPE_EDP: {
-			read_edp_current_link_settings_on_detect(link);
 			detect_edp_sink_caps(link);
+			read_edp_current_link_settings_on_detect(link);
 			sink_caps.transaction_type =
 				DDC_TRANSACTION_TYPE_I2C_OVER_AUX;
 			sink_caps.signal = SIGNAL_TYPE_EDP;
