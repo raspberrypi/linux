5222ded5c72ce7b173c66d3e3fb2af6bea511367
Mark Rutland <mark.rutland@arm.com>
2020-01-22 12:45:46 +0000

    arm64: acpi: fix DAIF manipulation with pNMI
    commit e533dbe9dcb199bb637a2c465f3a6e70564994fe upstream.
    
    Since commit:
    
      d44f1b8dd7e66d80 ("arm64: KVM/mm: Move SEA handling behind a single 'claim' interface")
    
    ... the top-level APEI SEA handler has the shape:
    
    1. current_flags = arch_local_save_flags()
    2. local_daif_restore(DAIF_ERRCTX)
    3. <GHES handler>
    4. local_daif_restore(current_flags)
    
    However, since commit:
    
      4a503217ce37e1f4 ("arm64: irqflags: Use ICC_PMR_EL1 for interrupt masking")
    
    ... when pseudo-NMIs (pNMIs) are in use, arch_local_save_flags() will save
    the PMR value rather than the DAIF flags.
    
    The combination of these two commits means that the APEI SEA handler will
    erroneously attempt to restore the PMR value into DAIF. Fix this by
    factoring local_daif_save_flags() out of local_daif_save(), so that we
    can consistently save DAIF in step #1, regardless of whether pNMIs are in
    use.
    
    Both commits were introduced concurrently in v5.0.
    
    Cc: <stable@vger.kernel.org>
    Fixes: 4a503217ce37e1f4 ("arm64: irqflags: Use ICC_PMR_EL1 for interrupt masking")
    Fixes: d44f1b8dd7e66d80 ("arm64: KVM/mm: Move SEA handling behind a single 'claim' interface")
    Signed-off-by: Mark Rutland <mark.rutland@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: James Morse <james.morse@arm.com>
    Cc: Julien Thierry <julien.thierry.kdev@gmail.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/include/asm/daifflags.h
+++ arch/arm64/include/asm/daifflags.h
@@ -36,7 +36,7 @@ static inline void local_daif_mask(void)
 	trace_hardirqs_off();
 }
 
-static inline unsigned long local_daif_save(void)
+static inline unsigned long local_daif_save_flags(void)
 {
 	unsigned long flags;
 
@@ -47,6 +47,15 @@ static inline unsigned long local_daif_save(void)
 		if (read_sysreg_s(SYS_ICC_PMR_EL1) != GIC_PRIO_IRQON)
 			flags |= PSR_I_BIT;
 	}
+
+	return flags;
+}
+
+static inline unsigned long local_daif_save(void)
+{
+	unsigned long flags;
+
+	flags = local_daif_save_flags();
 
 	local_daif_mask();
 
