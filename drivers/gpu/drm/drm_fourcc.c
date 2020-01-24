9491378661906db1baab1d488ce40540ecebd89f
Dave Stevenson <dave.stevenson@raspberrypi.com>
2020-01-24 14:22:06 +0000

    drm/fourcc: Add packed 10bit YUV 4:2:0 format
    Adds a format that is 3 10bit YUV 4:2:0 samples packed into
    a 32bit work (with 2 spare bits).
    
    Supported on Broadcom BCM2711 chips.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/gpu/drm/drm_fourcc.c
+++ drivers/gpu/drm/drm_fourcc.c
@@ -274,6 +274,9 @@ const struct drm_format_info *__drm_format_info(u32 format)
 		{ .format = DRM_FORMAT_YUV420_10BIT,    .depth = 0,
 		  .num_planes = 1, .cpp = { 0, 0, 0 }, .hsub = 2, .vsub = 2,
 		  .is_yuv = true },
+		{ .format = DRM_FORMAT_P030,            .depth = 0,  .num_planes = 2,
+		  .char_per_block = { 4, 4, 0 }, .block_w = { 3, 0, 0 }, .block_h = { 1, 0, 0 },
+		  .hsub = 2, .vsub = 2, .is_yuv = true},
 	};
 
 	unsigned int i;
