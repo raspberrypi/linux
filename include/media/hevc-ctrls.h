bbd875e8e6bee14c973a078a884d098c2986e068
Dave Stevenson <dave.stevenson@raspberrypi.com>
2020-03-23 19:00:17 +0000

    media: uapi: Add hevc ctrls for WPP decoding
    WPP can allow greater parallelism within the decode, but needs
    offset information to be passed in.
    
    Adds num_entry_point_offsets and entry_point_offset_minus1 to
    v4l2_ctrl_hevc_slice_params.
    
    This is based on Jernej Skrabec's patches for cedrus which
    implement the same feature.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- include/media/hevc-ctrls.h
+++ include/media/hevc-ctrls.h
@@ -170,6 +170,7 @@ struct v4l2_ctrl_hevc_slice_params {
 
 	/* ISO/IEC 23008-2, ITU-T Rec. H.265: General slice segment header */
 	__u32	slice_segment_addr;
+	__u32	num_entry_point_offsets;
 
 	/* ISO/IEC 23008-2, ITU-T Rec. H.265: NAL unit header */
 	__u8	nal_unit_type;
@@ -204,7 +205,9 @@ struct v4l2_ctrl_hevc_slice_params {
 	__u8	num_rps_poc_st_curr_after;
 	__u8	num_rps_poc_lt_curr;
 
-	__u8	padding[5];
+	__u8	padding;
+
+	__u32	entry_point_offset_minus1[256];
 
 	/* ISO/IEC 23008-2, ITU-T Rec. H.265: General slice segment header */
 	struct v4l2_hevc_dpb_entry dpb[V4L2_HEVC_DPB_ENTRIES_NUM_MAX];
