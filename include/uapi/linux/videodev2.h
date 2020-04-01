c55d1e3ce2f4d833fe2acfcce9945270c08a9982
Naushir Patuck <naush@raspberrypi.com>
2020-04-01 08:46:29 +0100

    media: uapi: v4l2-core: Add sensor ancillary data V4L2 foucc type.
    Add V4L2_META_FMT_SENSOR_DATA format 4CC.
    
    This new format will be used by the BCM2835 Unicam device to return
    out camera sensor embedded data.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -845,6 +845,7 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_UVC         v4l2_fourcc('U', 'V', 'C', 'H') /* UVC Payload Header metadata */
 #define V4L2_META_FMT_D4XX        v4l2_fourcc('D', '4', 'X', 'X') /* D4XX Payload Header metadata */
 #define V4L2_META_FMT_VIVID	  v4l2_fourcc('V', 'I', 'V', 'D') /* Vivid Metadata */
+#define V4L2_META_FMT_SENSOR_DATA v4l2_fourcc('S', 'E', 'N', 'S') /* Sensor Ancillary metadata */
 
 /* Vendor specific - used for RK_ISP1 camera sub-system */
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
