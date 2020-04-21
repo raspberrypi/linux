52a098db96b3e88340eb131d3ff285ef331d3ac5
Naushir Patuck <naush@raspberrypi.com>
2020-04-21 15:06:19 +0100

    media: uapi: v4l-ctrls: Add CID base for the bcm2835-isp driver
    We are reserving controls for the new bcm2835-isp driver.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- include/uapi/linux/v4l2-controls.h
+++ include/uapi/linux/v4l2-controls.h
@@ -212,6 +212,10 @@ enum v4l2_colorfx {
  */
 #define V4L2_CID_USER_CCS_BASE			(V4L2_CID_USER_BASE + 0x10f0)
 
+/* The base for the bcm2835-isp driver controls.
+ * We reserve 16 controls for this driver. */
+#define V4L2_CID_USER_BCM2835_ISP_BASE		(V4L2_CID_USER_BASE + 0x10f0)
+
 /* MPEG-class control IDs */
 /* The MPEG controls are applicable to all codec controls
  * and the 'MPEG' part of the define is historical */
