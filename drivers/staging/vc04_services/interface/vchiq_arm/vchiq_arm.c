7cbfecd456b793e0c64afc639619e0a0437abced
Naushir Patuck <naush@raspberrypi.com>
2020-04-22 08:32:32 +0100

    staging: vchiq: Load bcm2835_isp driver from vchiq
    bcmn2835_isp is a platform driver dependent on vchiq,
    therefore add the load/unload functions for it to vchiq.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
+++ drivers/staging/vc04_services/interface/vchiq_arm/vchiq_arm.c
@@ -71,6 +71,7 @@
 static struct vchiq_device *bcm2835_audio;
 static struct vchiq_device *bcm2835_camera;
 static struct vchiq_device *bcm2835_codec;
+static struct vchiq_device *bcm2835_isp;
 static struct vchiq_device *vcsm_cma;
 
 static const struct vchiq_platform_info bcm2835_info = {
@@ -1876,6 +1877,7 @@ static int vchiq_probe(struct platform_device *pdev)
 	bcm2835_codec = vchiq_device_register(&pdev->dev, "bcm2835-codec");
 	bcm2835_audio = vchiq_device_register(&pdev->dev, "bcm2835-audio");
 	bcm2835_camera = vchiq_device_register(&pdev->dev, "bcm2835-camera");
+	bcm2835_isp = vchiq_device_register(&pdev->dev, "bcm2835-isp");
 
 	return 0;
 
@@ -1889,6 +1891,7 @@ static void vchiq_remove(struct platform_device *pdev)
 {
 	struct vchiq_drv_mgmt *mgmt = dev_get_drvdata(&pdev->dev);
 
+	vchiq_device_unregister(bcm2835_isp);
 	vchiq_device_unregister(bcm2835_audio);
 	vchiq_device_unregister(bcm2835_camera);
 	vchiq_device_unregister(bcm2835_codec);
