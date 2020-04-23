34a94277e1b5dbfaa8a9700b6560a655abb32aff
Naushir Patuck <naush@raspberrypi.com>
2020-04-23 10:20:26 +0100

    media: uapi: v4l2-core: Add ISP statistics output V4L2 fourcc type
    Add V4L2_META_FMT_BCM2835_ISP_STATS V4L2 format type.
    
    This new format will be used by the BCM2835 ISP device to return
    out ISP statistics for 3A.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1370,6 +1370,7 @@ static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 	case V4L2_META_FMT_RK_ISP1_PARAMS:	descr = "Rockchip ISP1 3A Parameters"; break;
 	case V4L2_META_FMT_RK_ISP1_STAT_3A:	descr = "Rockchip ISP1 3A Statistics"; break;
 	case V4L2_META_FMT_SENSOR_DATA:	descr = "Sensor Ancillary Metadata"; break;
+	case V4L2_META_FMT_BCM2835_ISP_STATS: descr = "BCM2835 ISP Image Statistics"; break;
 
 	default:
 		/* Compressed formats */
