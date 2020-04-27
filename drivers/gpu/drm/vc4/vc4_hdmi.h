b961e817abcf0121e648a5308d843ce166086da3
popcornmix <popcornmix@gmail.com>
2020-04-27 19:07:50 +0100

    vc4_hdmi: BCM2835 requires a fixed hsm clock for CEC to work
    Signed-off-by: popcornmix <popcornmix@gmail.com>

--- drivers/gpu/drm/vc4/vc4_hdmi.h
+++ drivers/gpu/drm/vc4/vc4_hdmi.h
@@ -92,6 +92,9 @@ struct vc4_hdmi_variant {
 	/* Callback to get hsm clock */
 	u32 (*get_hsm_clock)(struct vc4_hdmi *vc4_hdmi);
 
+	/* Callback to get hsm clock */
+	u32 (*calc_hsm_clock)(struct vc4_hdmi *vc4_hdmi, unsigned long pixel_rate);
+
 	/* Callback to get channel map */
 	u32 (*channel_map)(struct vc4_hdmi *vc4_hdmi, u32 channel_mask);
 };
