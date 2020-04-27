79ae4a658a4a0a6f4a0562eb52973c2850202b99
Jérôme Pouiller <jerome.pouiller@silabs.com>
2020-04-27 15:40:23 +0200

    staging: wfx: fix overflow in frame counters
    [ Upstream commit 87066173e34b0ca5d041d5519e6bb030b1958184 ]
    
    It has been reported that trying to send small packets of data could
    produce a "inconsistent notification" warning.
    
    It seems that in some circumstances, the number of frame queued in the
    driver could greatly increase and exceed UCHAR_MAX. So the field
    "buffered" from struct sta_priv can overflow.
    
    Just increase the size of "bueffered" to fix the problem.
    
    Fixes: 7d2d2bfdeb82 ("staging: wfx: relocate "buffered" information to sta_priv")
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Link: https://lore.kernel.org/r/20200427134031.323403-10-Jerome.Pouiller@silabs.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/staging/wfx/sta.h
+++ drivers/staging/wfx/sta.h
@@ -37,7 +37,7 @@ struct wfx_grp_addr_table {
 struct wfx_sta_priv {
 	int link_id;
 	int vif_id;
-	u8 buffered[IEEE80211_NUM_TIDS];
+	int buffered[IEEE80211_NUM_TIDS];
 	// Ensure atomicity of "buffered" and calls to ieee80211_sta_set_buffered()
 	spinlock_t lock;
 };
