6b292d7806a19781ea2ece291e92b5ab7b6bb481
Jeff Layton <jlayton@kernel.org>
2020-05-20 10:36:07 -0400

    ceph: flush release queue when handling caps for unknown inode
    [ Upstream commit fb33c114d3ed5bdac230716f5b0a93b56b92a90d ]
    
    It's possible for the VFS to completely forget about an inode, but for
    it to still be sitting on the cap release queue. If the MDS sends the
    client a cap message for such an inode, it just ignores it today, which
    can lead to a stall of up to 5s until the cap release queue is flushed.
    
    If we get a cap message for an inode that can't be located, then go
    ahead and flush the cap release queue.
    
    Cc: stable@vger.kernel.org
    URL: https://tracker.ceph.com/issues/45532
    Fixes: 1e9c2eb6811e ("ceph: delete stale dentry when last reference is dropped")
    Reported-and-Tested-by: Andrej Filipčič <andrej.filipcic@ijs.si>
    Suggested-by: Yan, Zheng <zyan@redhat.com>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -3938,7 +3938,7 @@ void ceph_handle_caps(struct ceph_mds_session *session,
 			__ceph_queue_cap_release(session, cap);
 			spin_unlock(&session->s_cap_lock);
 		}
-		goto done;
+		goto flush_cap_releases;
 	}
 
 	/* these will work even if we don't have a cap yet */
