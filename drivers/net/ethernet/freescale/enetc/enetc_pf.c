47ea457d8f565975ca10a4298960b2d58849c6e0
Claudiu Manoil <claudiu.manoil@nxp.com>
2020-06-18 12:16:52 +0300

    enetc: Fix HW_VLAN_CTAG_TX|RX toggling
    [ Upstream commit 9deba33f1b7266a3870c9da31f787b605748fc0c ]
    
    VLAN tag insertion/extraction offload is correctly
    activated at probe time but deactivation of this feature
    (i.e. via ethtool) is broken.  Toggling works only for
    Tx/Rx ring 0 of a PF, and is ignored for the other rings,
    including the VF rings.
    To fix this, the existing VLAN offload toggling code
    was extended to all the rings assigned to a netdevice,
    instead of the default ring 0 (likely a leftover from the
    early validation days of this feature).  And the code was
    moved to the common set_features() function to fix toggling
    for the VF driver too.
    
    Fixes: d4fd0404c1c9 ("enetc: Introduce basic PF and VF ENETC ethernet drivers")
    Signed-off-by: Claudiu Manoil <claudiu.manoil@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/freescale/enetc/enetc_pf.c
+++ drivers/net/ethernet/freescale/enetc/enetc_pf.c
@@ -667,15 +667,6 @@ static int enetc_pf_set_features(struct net_device *ndev,
 				 netdev_features_t features)
 {
 	netdev_features_t changed = ndev->features ^ features;
-	struct enetc_ndev_priv *priv = netdev_priv(ndev);
-
-	if (changed & NETIF_F_HW_VLAN_CTAG_RX)
-		enetc_enable_rxvlan(&priv->si->hw, 0,
-				    !!(features & NETIF_F_HW_VLAN_CTAG_RX));
-
-	if (changed & NETIF_F_HW_VLAN_CTAG_TX)
-		enetc_enable_txvlan(&priv->si->hw, 0,
-				    !!(features & NETIF_F_HW_VLAN_CTAG_TX));
 
 	if (changed & NETIF_F_LOOPBACK)
 		enetc_set_loopback(ndev, !!(features & NETIF_F_LOOPBACK));
