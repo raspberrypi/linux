32779e12681d95acdc87e2bc646e0485401ccfb7
Dave Stevenson <dave.stevenson@raspberrypi.com>
2020-07-07 14:52:43 +0100

    media: bcm2835-unicam: Ensure type is VIDEO_CAPTURE in [g|s]_selection
    [g|s]_selection pass in a buffer type that needs to be validated
    before passing on to the sensor subdev.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/media/platform/bcm2835/bcm2835-unicam.c
+++ drivers/media/platform/bcm2835/bcm2835-unicam.c
@@ -1877,6 +1877,9 @@ static int unicam_s_selection(struct file *file, void *priv,
 		.r = sel->r,
 	};
 
+	if (sel->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
+		return -EINVAL;
+
 	return v4l2_subdev_call(dev->sensor, pad, set_selection, NULL, &sdsel);
 }
 
@@ -1890,6 +1893,9 @@ static int unicam_g_selection(struct file *file, void *priv,
 		.target = sel->target,
 	};
 	int ret;
+
+	if (sel->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
+		return -EINVAL;
 
 	ret = v4l2_subdev_call(dev->sensor, pad, get_selection, NULL, &sdsel);
 	if (!ret)
