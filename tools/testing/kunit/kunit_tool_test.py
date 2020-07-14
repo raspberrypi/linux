ee9db7fc3a2ba7bcdd634d46f111923dd4b88790
Brendan Higgins <brendanhiggins@google.com>
2020-07-14 13:41:30 -0700

    kunit: tool: fix improper treatment of file location
    [ Upstream commit d43c7fb05765152d4d4a39a8ef957c4ea14d8847 ]
    
    Commit 01397e822af4 ("kunit: Fix TabError, remove defconfig code and
    handle when there is no kunitconfig") and commit 45ba7a893ad8 ("kunit:
    kunit_tool: Separate out config/build/exec/parse") introduced two
    closely related issues which built off of each other: they excessively
    created the build directory when not present and modified a constant
    (constants in Python only exist by convention).
    
    Together these issues broken a number of unit tests for KUnit tool, so
    fix them.
    
    Fixed up commit log to fic checkpatch commit description style error.
    Shuah Khan <skhan@linuxfoundation.org>
    
    Fixes: 01397e822af4 ("kunit: Fix TabError, remove defconfig code and handle when there is no kunitconfig")
    Fixes: 45ba7a893ad8 ("kunit: kunit_tool: Separate out config/build/exec/parse")
    Signed-off-by: Brendan Higgins <brendanhiggins@google.com>
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/kunit/kunit_tool_test.py
+++ tools/testing/kunit/kunit_tool_test.py
@@ -251,7 +251,7 @@ class KUnitMainTest(unittest.TestCase):
 		pass
 
 	def test_config_passes_args_pass(self):
-		kunit.main(['config'], self.linux_source_mock)
+		kunit.main(['config', '--build_dir=.kunit'], self.linux_source_mock)
 		assert self.linux_source_mock.build_reconfig.call_count == 1
 		assert self.linux_source_mock.run_kernel.call_count == 0
 
@@ -326,7 +326,7 @@ class KUnitMainTest(unittest.TestCase):
 
 	def test_run_builddir(self):
 		build_dir = '.kunit'
-		kunit.main(['run', '--build_dir', build_dir], self.linux_source_mock)
+		kunit.main(['run', '--build_dir=.kunit'], self.linux_source_mock)
 		assert self.linux_source_mock.build_reconfig.call_count == 1
 		self.linux_source_mock.run_kernel.assert_called_once_with(
 			build_dir=build_dir, timeout=300)
