f6b2fb80ffdc1509686eec9914a7a7d212bf1069
Jaegeuk Kim <jaegeuk@kernel.org>
2020-07-16 09:57:03 -0700

    f2fs: should avoid inode eviction in synchronous path
    [ Upstream commit b0f3b87fb3abc42c81d76c6c5795f26dbdb2f04b ]
    
    https://bugzilla.kernel.org/show_bug.cgi?id=208565
    
    PID: 257    TASK: ecdd0000  CPU: 0   COMMAND: "init"
      #0 [<c0b420ec>] (__schedule) from [<c0b423c8>]
      #1 [<c0b423c8>] (schedule) from [<c0b459d4>]
      #2 [<c0b459d4>] (rwsem_down_read_failed) from [<c0b44fa0>]
      #3 [<c0b44fa0>] (down_read) from [<c044233c>]
      #4 [<c044233c>] (f2fs_truncate_blocks) from [<c0442890>]
      #5 [<c0442890>] (f2fs_truncate) from [<c044d408>]
      #6 [<c044d408>] (f2fs_evict_inode) from [<c030be18>]
      #7 [<c030be18>] (evict) from [<c030a558>]
      #8 [<c030a558>] (iput) from [<c047c600>]
      #9 [<c047c600>] (f2fs_sync_node_pages) from [<c0465414>]
     #10 [<c0465414>] (f2fs_write_checkpoint) from [<c04575f4>]
     #11 [<c04575f4>] (f2fs_sync_fs) from [<c0441918>]
     #12 [<c0441918>] (f2fs_do_sync_file) from [<c0441098>]
     #13 [<c0441098>] (f2fs_sync_file) from [<c0323fa0>]
     #14 [<c0323fa0>] (vfs_fsync_range) from [<c0324294>]
     #15 [<c0324294>] (do_fsync) from [<c0324014>]
     #16 [<c0324014>] (sys_fsync) from [<c0108bc0>]
    
    This can be caused by flush_dirty_inode() in f2fs_sync_node_pages() where
    iput() requires f2fs_lock_op() again resulting in livelock.
    
    Reported-by: Zhiguo Niu <Zhiguo.Niu@unisoc.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1924,8 +1924,12 @@ continue_unlock:
 				goto continue_unlock;
 			}
 
-			/* flush inline_data, if it's async context. */
-			if (do_balance && is_inline_node(page)) {
+			/* flush inline_data/inode, if it's async context. */
+			if (!do_balance)
+				goto write_node;
+
+			/* flush inline_data */
+			if (is_inline_node(page)) {
 				clear_inline_node(page);
 				unlock_page(page);
 				flush_inline_data(sbi, ino_of_node(page));
@@ -1938,7 +1942,7 @@ continue_unlock:
 				if (flush_dirty_inode(page))
 					goto lock_node;
 			}
-
+write_node:
 			f2fs_wait_on_page_writeback(page, NODE, true, true);
 
 			if (!clear_page_dirty_for_io(page))
