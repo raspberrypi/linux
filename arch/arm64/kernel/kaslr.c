c986f31a942017b63aa8733e34e353607c5b22e1
Linus Torvalds <torvalds@linux-foundation.org>
2020-08-05 12:39:48 -0700

    random: random.h should include archrandom.h, not the other way around
    commit 585524081ecdcde1c719e63916c514866d898217 upstream.
    
    This is hopefully the final piece of the crazy puzzle with random.h
    dependencies.
    
    And by "hopefully" I obviously mean "Linus is a hopeless optimist".
    
    Reported-and-tested-by: Daniel Díaz <daniel.diaz@linaro.org>
    Acked-by: Guenter Roeck <linux@roeck-us.net>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kernel/kaslr.c
+++ arch/arm64/kernel/kaslr.c
@@ -10,8 +10,8 @@
 #include <linux/mm_types.h>
 #include <linux/sched.h>
 #include <linux/types.h>
+#include <linux/random.h>
 
-#include <asm/archrandom.h>
 #include <asm/cacheflush.h>
 #include <asm/fixmap.h>
 #include <asm/kernel-pgtable.h>
