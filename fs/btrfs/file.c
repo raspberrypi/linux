bb77dd02ba169cf10a36de65e9828ea834ec8bbc
Boris Burkov <boris@bur.io>
2020-08-18 11:00:05 -0700

    btrfs: detect nocow for swap after snapshot delete
    commit a84d5d429f9eb56f81b388609841ed993f0ddfca upstream.
    
    can_nocow_extent and btrfs_cross_ref_exist both rely on a heuristic for
    detecting a must cow condition which is not exactly accurate, but saves
    unnecessary tree traversal. The incorrect assumption is that if the
    extent was created in a generation smaller than the last snapshot
    generation, it must be referenced by that snapshot. That is true, except
    the snapshot could have since been deleted, without affecting the last
    snapshot generation.
    
    The original patch claimed a performance win from this check, but it
    also leads to a bug where you are unable to use a swapfile if you ever
    snapshotted the subvolume it's in. Make the check slower and more strict
    for the swapon case, without modifying the general cow checks as a
    compromise. Turning swap on does not seem to be a particularly
    performance sensitive operation, so incurring a possibly unnecessary
    btrfs_search_slot seems worthwhile for the added usability.
    
    Note: Until the snapshot is competely cleaned after deletion,
    check_committed_refs will still cause the logic to think that cow is
    necessary, so the user must until 'btrfs subvolu sync' finished before
    activating the swapfile swapon.
    
    CC: stable@vger.kernel.org # 5.4+
    Suggested-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Boris Burkov <boris@bur.io>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -1568,7 +1568,7 @@ int btrfs_check_can_nocow(struct btrfs_inode *inode, loff_t pos,
 	}
 
 	ret = can_nocow_extent(&inode->vfs_inode, lockstart, &num_bytes,
-			NULL, NULL, NULL);
+			NULL, NULL, NULL, false);
 	if (ret <= 0) {
 		ret = 0;
 		if (!nowait)
