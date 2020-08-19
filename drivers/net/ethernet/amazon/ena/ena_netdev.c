da995e0a6dacf5ce59c27ea4302f94232ec8b797
Shay Agroskin <shayagr@amazon.com>
2020-08-19 20:28:38 +0300

    net: ena: Make missed_tx stat incremental
    [ Upstream commit ccd143e5150f24b9ba15145c7221b61dd9e41021 ]
    
    Most statistics in ena driver are incremented, meaning that a stat's
    value is a sum of all increases done to it since driver/queue
    initialization.
    
    This patch makes all statistics this way, effectively making missed_tx
    statistic incremental.
    Also added a comment regarding rx_drops and tx_drops to make it
    clearer how these counters are calculated.
    
    Fixes: 11095fdb712b ("net: ena: add statistics for missed tx packets")
    Signed-off-by: Shay Agroskin <shayagr@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/amazon/ena/ena_netdev.c
+++ drivers/net/ethernet/amazon/ena/ena_netdev.c
@@ -3609,7 +3609,7 @@ static int check_missing_comp_in_tx_queue(struct ena_adapter *adapter,
 	}
 
 	u64_stats_update_begin(&tx_ring->syncp);
-	tx_ring->tx_stats.missed_tx = missed_tx;
+	tx_ring->tx_stats.missed_tx += missed_tx;
 	u64_stats_update_end(&tx_ring->syncp);
 
 	return rc;
@@ -4537,6 +4537,9 @@ static void ena_keep_alive_wd(void *adapter_data,
 	tx_drops = ((u64)desc->tx_drops_high << 32) | desc->tx_drops_low;
 
 	u64_stats_update_begin(&adapter->syncp);
+	/* These stats are accumulated by the device, so the counters indicate
+	 * all drops since last reset.
+	 */
 	adapter->dev_stats.rx_drops = rx_drops;
 	adapter->dev_stats.tx_drops = tx_drops;
 	u64_stats_update_end(&adapter->syncp);
