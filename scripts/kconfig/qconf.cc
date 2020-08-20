f09a790a6e463b1bdd3d76f77111a11637d65a3e
Masahiro Yamada <masahiroy@kernel.org>
2020-08-21 00:16:46 +0900

    kconfig: qconf: remove qInfo() to get back Qt4 support
    [ Upstream commit 53efe2e76ca2bfad7f35e0b5330e2ccd44a643e3 ]
    
    qconf is supposed to work with Qt4 and Qt5, but since commit
    c4f7398bee9c ("kconfig: qconf: make debug links work again"),
    building with Qt4 fails as follows:
    
      HOSTCXX scripts/kconfig/qconf.o
    scripts/kconfig/qconf.cc: In member function ‘void ConfigInfoView::clicked(const QUrl&)’:
    scripts/kconfig/qconf.cc:1241:3: error: ‘qInfo’ was not declared in this scope; did you mean ‘setInfo’?
     1241 |   qInfo() << "Clicked link is empty";
          |   ^~~~~
          |   setInfo
    scripts/kconfig/qconf.cc:1254:3: error: ‘qInfo’ was not declared in this scope; did you mean ‘setInfo’?
     1254 |   qInfo() << "Clicked symbol is invalid:" << data;
          |   ^~~~~
          |   setInfo
    make[1]: *** [scripts/Makefile.host:129: scripts/kconfig/qconf.o] Error 1
    make: *** [Makefile:606: xconfig] Error 2
    
    qInfo() does not exist in Qt4. In my understanding, these call-sites
    should be unreachable. Perhaps, qWarning(), assertion, or something
    is better, but qInfo() is not the right one to use here, I think.
    
    Fixes: c4f7398bee9c ("kconfig: qconf: make debug links work again")
    Reported-by: Ronald Warsow <rwarsow@gmx.de>
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- scripts/kconfig/qconf.cc
+++ scripts/kconfig/qconf.cc
@@ -1228,7 +1228,6 @@ void ConfigInfoView::clicked(const QUrl &url)
 	struct menu *m = NULL;
 
 	if (count < 1) {
-		qInfo() << "Clicked link is empty";
 		delete[] data;
 		return;
 	}
@@ -1241,7 +1240,6 @@ void ConfigInfoView::clicked(const QUrl &url)
 	strcat(data, "$");
 	result = sym_re_search(data);
 	if (!result) {
-		qInfo() << "Clicked symbol is invalid:" << data;
 		delete[] data;
 		return;
 	}
