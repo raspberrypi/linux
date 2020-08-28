a0cfda9cb3a135d78cdda63b390adfa9d86b7dfa
Sasha Levin <sashal@kernel.org>
2020-08-28 14:20:57 -0400

    s390/numa: set node distance to LOCAL_DISTANCE
    [ Upstream commit 535e4fc623fab2e09a0653fc3a3e17f382ad0251 ]
    
    The node distance is hardcoded to 0, which causes a trouble
    for some user-level applications. In particular, "libnuma"
    expects the distance of a node to itself as LOCAL_DISTANCE.
    This update removes the offending node distance override.
    
    Cc: <stable@vger.kernel.org> # 4.4
    Fixes: 3a368f742da1 ("s390/numa: add core infrastructure")
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/s390/include/asm/topology.h
+++ arch/s390/include/asm/topology.h
@@ -83,8 +83,6 @@ static inline const struct cpumask *cpumask_of_node(int node)
 
 #define pcibus_to_node(bus) __pcibus_to_node(bus)
 
-#define node_distance(a, b) __node_distance(a, b)
-
 #else /* !CONFIG_NUMA */
 
 #define numa_node_id numa_node_id
