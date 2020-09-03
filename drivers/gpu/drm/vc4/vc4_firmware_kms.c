ed9bea35e5b47fb767d28d658c276be774eb3308
Dave Stevenson <dave.stevenson@raspberrypi.com>
2020-09-03 16:40:06 +0100

    Revert "drm/vc4: Fix warning about vblank interrupts before DRM core is ready."
    This reverts commit e35e860be1c7bf753413318c5ab89b0ab9258ef9.

--- drivers/gpu/drm/vc4/vc4_firmware_kms.c
+++ drivers/gpu/drm/vc4/vc4_firmware_kms.c
@@ -43,7 +43,6 @@ struct vc4_crtc {
 
 	struct drm_pending_vblank_event *event;
 	u32 overscan[4];
-	bool vblank_enabled;
 };
 
 static inline struct vc4_crtc *to_vc4_crtc(struct drm_crtc *crtc)
@@ -421,8 +420,7 @@ static irqreturn_t vc4_crtc_irq_handler(int irq, void *data)
 
 	if (stat & SMICS_INTERRUPTS) {
 		writel(0, vc4_crtc->regs + SMICS);
-		if (vc4_crtc->vblank_enabled)
-			drm_crtc_handle_vblank(&vc4_crtc->base);
+		drm_crtc_handle_vblank(&vc4_crtc->base);
 		vc4_crtc_handle_page_flip(vc4_crtc);
 		ret = IRQ_HANDLED;
 	}
@@ -445,9 +443,9 @@ static int vc4_page_flip(struct drm_crtc *crtc,
 
 static int vc4_fkms_enable_vblank(struct drm_crtc *crtc)
 {
-	struct vc4_crtc *vc4_crtc = to_vc4_crtc(crtc);
-
-	vc4_crtc->vblank_enabled = true;
+	/* XXX: Need a way to enable/disable the interrupt, to avoid
+	 * DRM warnings at boot time.
+	 */
 
 	return 0;
 }
