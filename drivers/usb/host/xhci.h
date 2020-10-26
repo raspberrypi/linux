e404d4b05fd564e6dd12e6c40e0d3b5b899050e3
Jonathan Bell <jonathan@raspberrypi.org>
2020-10-26 14:03:35 +0000

    xhci: quirks: add link TRB quirk for VL805
    The VL805 controller can't cope with the TR Dequeue Pointer for an endpoint
    being set to a Link TRB. The hardware-maintained endpoint context ends up
    stuck at the address of the Link TRB, leading to erroneous ring expansion
    events whenever the enqueue pointer wraps to the dequeue position.
    
    If the search for the end of the current TD and ring cycle state lands on
    a Link TRB, move to the next segment.
    
    See: https://github.com/raspberrypi/linux/issues/3919
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1876,6 +1876,7 @@ struct xhci_hcd {
 #define XHCI_RENESAS_FW_QUIRK	BIT_ULL(36)
 #define XHCI_EP_CTX_BROKEN_DCS	BIT_ULL(37)
 #define XHCI_DISABLE_SPARSE	BIT_ULL(38)
+#define XHCI_AVOID_DQ_ON_LINK	BIT_ULL(39)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
