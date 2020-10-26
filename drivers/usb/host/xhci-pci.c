191358c2c9d0b07fd778081f3449bb3b9f662619
Jonathan Bell <jonathan@raspberrypi.com>
2020-10-26 14:03:35 +0000

    xhci: quirks: add link TRB quirk for VL805
    The VL805 controller can't cope with the TR Dequeue Pointer for an endpoint
    being set to a Link TRB. The hardware-maintained endpoint context ends up
    stuck at the address of the Link TRB, leading to erroneous ring expansion
    events whenever the enqueue pointer wraps to the dequeue position.
    
    If the search for the end of the current TD and ring cycle state lands on
    a Link TRB, move to the next segment.
    
    Link: https://github.com/raspberrypi/linux/issues/3919
    
    [6.5.y Fixup - move downstream quirk bits further along]
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -430,6 +430,7 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 		xhci->quirks |= XHCI_LPM_SUPPORT;
 		xhci->quirks |= XHCI_TRB_OVERFETCH;
 		xhci->quirks |= XHCI_EP_CTX_BROKEN_DCS;
+		xhci->quirks |= XHCI_AVOID_DQ_ON_LINK;
 	}
 
 	if (pdev->vendor == PCI_VENDOR_ID_ASMEDIA &&
