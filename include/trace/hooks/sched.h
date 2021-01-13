6f0f175829787142257811543d6784486be2f07b
Yun Hsiang <yun.hsiang@mediatek.com>
2021-01-13 17:00:22 +0800

    ANDROID: schedutil: add vendor hook for adjusting util to freq calculation
    Currently, the frequency is calculated by max freq * 1.25 * util / max cap.
    Add a vendor hook to adjust the frequency when the calculation
    overestimate.
    
    android_vh_map_util_freq
    	adjust util to freq calculation
    
    Bug: 177845439
    
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    Change-Id: I9aa9079f00af7d3380b19f2fe21b75cddd107d15
    Signed-off-by: shenshen mao <maoshenshen@oppo.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -378,6 +378,11 @@ DECLARE_HOOK(android_vh_sched_setaffinity_early,
 	TP_PROTO(struct task_struct *p, const struct cpumask *new_mask, bool *retval),
 	TP_ARGS(p, new_mask, retval));
 
+DECLARE_HOOK(android_vh_map_util_freq,
+	TP_PROTO(unsigned long util, unsigned long freq,
+		unsigned long cap, unsigned long *next_freq),
+	TP_ARGS(util, freq, cap, next_freq));
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
