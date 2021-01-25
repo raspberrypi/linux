3ff6be8f95057b46605aec5b626763ad697cf9c7
Josef Bacik <josef@toxicpanda.com>
2021-01-25 16:42:35 -0500

    btrfs: avoid double put of block group when emptying cluster
    commit 95c85fba1f64c3249c67f0078a29f8a125078189 upstream.
    
    It's wrong calling btrfs_put_block_group in
    __btrfs_return_cluster_to_free_space if the block group passed is
    different than the block group the cluster represents. As this means the
    cluster doesn't have a reference to the passed block group. This results
    in double put and a use-after-free bug.
    
    Fix this by simply bailing if the block group we passed in does not
    match the block group on the cluster.
    
    Fixes: fa9c0d795f7b ("Btrfs: rework allocation clustering")
    CC: stable@vger.kernel.org # 4.4+
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    [ update changelog ]
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/btrfs/free-space-cache.c
+++ fs/btrfs/free-space-cache.c
@@ -2708,8 +2708,10 @@ static void __btrfs_return_cluster_to_free_space(
 	struct rb_node *node;
 
 	spin_lock(&cluster->lock);
-	if (cluster->block_group != block_group)
-		goto out;
+	if (cluster->block_group != block_group) {
+		spin_unlock(&cluster->lock);
+		return;
+	}
 
 	cluster->block_group = NULL;
 	cluster->window_start = 0;
@@ -2747,8 +2749,6 @@ static void __btrfs_return_cluster_to_free_space(
 				   entry->offset, &entry->offset_index, bitmap);
 	}
 	cluster->root = RB_ROOT;
-
-out:
 	spin_unlock(&cluster->lock);
 	btrfs_put_block_group(block_group);
 }
