da97c74c328050c04954527945a7122640a426f5
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2021-02-20 15:29:35 +0100

    mmc: uniphier-sd: Fix an error handling path in uniphier_sd_probe()
    commit b03aec1c1f337dfdae44cdb0645ecac34208ae0a upstream.
    
    A 'uniphier_sd_clk_enable()' call should be balanced by a corresponding
    'uniphier_sd_clk_disable()' call.
    This is done in the remove function, but not in the error handling path of
    the probe.
    
    Add the missing call.
    
    Fixes: 3fd784f745dd ("mmc: uniphier-sd: add UniPhier SD/eMMC controller driver")
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Reviewed-by: Masahiro Yamada <masahiroy@kernel.org>
    Link: https://lore.kernel.org/r/20210220142935.918554-1-christophe.jaillet@wanadoo.fr
    Cc: stable@vger.kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/host/uniphier-sd.c
+++ drivers/mmc/host/uniphier-sd.c
@@ -637,7 +637,7 @@ static int uniphier_sd_probe(struct platform_device *pdev)
 
 	ret = tmio_mmc_host_probe(host);
 	if (ret)
-		goto free_host;
+		goto disable_clk;
 
 	ret = devm_request_irq(dev, irq, tmio_mmc_irq, IRQF_SHARED,
 			       dev_name(dev), host);
@@ -648,6 +648,8 @@ static int uniphier_sd_probe(struct platform_device *pdev)
 
 remove_host:
 	tmio_mmc_host_remove(host);
+disable_clk:
+	uniphier_sd_clk_disable(host);
 free_host:
 	tmio_mmc_host_free(host);
 
