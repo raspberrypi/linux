f8588e0b480bbde233ca9dc0000b890783f93750
Ben Avison <bavison@riscosopen.org>
2021-03-08 15:32:25 +0000

    Assign crypto aliases to different AES implementation modules
    The kernel modules aes-neon-blk and aes-neon-bs perform poorly, at least on
    Cortex-A72 without crypto extensions. In fact, aes-arm64 outperforms them
    on benchmarks, despite it being a simpler implementation (only accelerating
    the single-block AES cipher).
    
    For modes of operation where multiple cipher blocks can be processed in
    parallel, aes-neon-bs outperforms aes-neon-blk by around 60-70% and aes-arm64
    is another 10-20% faster still. But the difference is even more marked with
    modes of operation with dependencies between neighbouring blocks, such as
    CBC encryption, which defeat parallelism: in these cases, aes-arm64 is
    typically around 250% faster than either aes-neon-blk or aes-neon-bs.
    
    The key trade-off with aes-arm64 is that the look-up tables are situated in
    RAM. This leaves them potentially open to cache timing attacks. The two other
    modules, by contrast, load the look-up tables into NEON registers and so are
    able to perform in constant time.
    
    This patch aims to load aes-arm64 more often.
    
    If none of the currently-loaded crypto modules implement a given algorithm,
    a new one is typically selected for loading using a platform-neutral alias
    describing the required algorithm. To enable users to still
    load aes-neon-blk or aes-neon-bs if they really want them, while still
    ensuring that aes-arm64 is usually selected, remove the aliases from
    aes-neonbs-glue.c and aes-glue.c and apply them to aes-cipher-glue.c, but
    still build the two NEON modules.
    
    Since aes-glue.c can also be used to build aes-ce-blk, leave them enabled
    if USE_V8_CRYPTO_EXTENSIONS is defined, to ensure they are selected if we
    in future use a CPU which has the crypto extensions enabled.
    
    Note that the algorithm priority specifiers are unchanged, so if
    aes-neon-bs is loaded at the same time as aes-arm64, the former will be
    used in preference. However, aes-neon-blk and aes-arm64 have tied priority,
    so whichever module was loaded first will be used (assuming aes-neon-bs is
    not loaded).
    
    Signed-off-by: Ben Avison <bavison@riscosopen.org>

--- arch/arm64/crypto/aes-glue.c
+++ arch/arm64/crypto/aes-glue.c
@@ -55,17 +55,17 @@ MODULE_DESCRIPTION("AES-ECB/CBC/CTR/XTS using ARMv8 Crypto Extensions");
 #define aes_mac_update		neon_aes_mac_update
 MODULE_DESCRIPTION("AES-ECB/CBC/CTR/XTS using ARMv8 NEON");
 #endif
-#if defined(USE_V8_CRYPTO_EXTENSIONS) || !IS_ENABLED(CONFIG_CRYPTO_AES_ARM64_BS)
+#if defined(USE_V8_CRYPTO_EXTENSIONS)
 MODULE_ALIAS_CRYPTO("ecb(aes)");
 MODULE_ALIAS_CRYPTO("cbc(aes)");
 MODULE_ALIAS_CRYPTO("ctr(aes)");
 MODULE_ALIAS_CRYPTO("xts(aes)");
-#endif
 MODULE_ALIAS_CRYPTO("cts(cbc(aes))");
 MODULE_ALIAS_CRYPTO("essiv(cbc(aes),sha256)");
 MODULE_ALIAS_CRYPTO("cmac(aes)");
 MODULE_ALIAS_CRYPTO("xcbc(aes)");
 MODULE_ALIAS_CRYPTO("cbcmac(aes)");
+#endif
 
 MODULE_AUTHOR("Ard Biesheuvel <ard.biesheuvel@linaro.org>");
 MODULE_LICENSE("GPL v2");
