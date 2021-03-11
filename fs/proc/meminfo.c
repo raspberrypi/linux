dccd72bc17d87eff07a25a297755c3852df06a16
Liujie Xie <xieliujie@oppo.com>
2021-03-11 20:40:06 +0800

    ANDROID: vendor_hooks: Add hooks for memory when debug
    Add vendors hooks for recording memory used
    
    Vendor modules allocate and manages the memory itself.
    
    These memories might not be included in kernel memory
    statistics. Also, detailed references and vendor-specific
    information are managed only inside modules. When
    various problems such as memory leaks occurs, these
    information should be showed in real-time.
    
    Bug: 182443489
    Bug: 234407991
    Bug: 277799025
    Bug: 341802931
    
    Change-Id: I976de52fa514805e42fabb9fcfb23b399990bcc8
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    Signed-off-by: Hyesoo Yu <hyesoo.yu@samsung.com>

--- fs/proc/meminfo.c
+++ fs/proc/meminfo.c
@@ -21,6 +21,7 @@
 #include <trace/hooks/mm.h>
 #include <asm/page.h>
 #include "internal.h"
+#include <trace/hooks/mm.h>
 
 void __attribute__((weak)) arch_report_meminfo(struct seq_file *m)
 {
@@ -160,6 +161,7 @@ static int meminfo_proc_show(struct seq_file *m, void *v)
 	show_val_kb(m, "CmaFree:        ",
 		    global_zone_page_state(NR_FREE_CMA_PAGES));
 #endif
+	trace_android_vh_meminfo_proc_show(m);
 
 #ifdef CONFIG_UNACCEPTED_MEMORY
 	show_val_kb(m, "Unaccepted:     ",
--- include/trace/hooks/mm.h
+++ include/trace/hooks/mm.h
@@ -154,6 +154,22 @@ DECLARE_HOOK(android_vh_page_add_new_anon_rmap,
 	TP_PROTO(struct page *page, struct vm_area_struct *vma,
 		unsigned long address),
 	TP_ARGS(page, vma, address));
+DECLARE_HOOK(android_vh_meminfo_proc_show,
+	TP_PROTO(struct seq_file *m),
+	TP_ARGS(m));
+DECLARE_HOOK(android_vh_exit_mm,
+	TP_PROTO(struct mm_struct *mm),
+	TP_ARGS(mm));
+DECLARE_HOOK(android_vh_show_mem,
+	TP_PROTO(unsigned int filter, nodemask_t *nodemask),
+	TP_ARGS(filter, nodemask));
+DECLARE_HOOK(android_vh_print_slabinfo_header,
+	TP_PROTO(struct seq_file *m),
+	TP_ARGS(m));
+struct slabinfo;
+DECLARE_HOOK(android_vh_cache_show,
+	TP_PROTO(struct seq_file *m, struct slabinfo *sinfo, struct kmem_cache *s),
+	TP_ARGS(m, sinfo, s));
 DECLARE_HOOK(android_vh_customize_alloc_gfp,
 	TP_PROTO(gfp_t *alloc_gfp, unsigned int order),
 	TP_ARGS(alloc_gfp, order));
