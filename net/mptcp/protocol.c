5d3ba18a67b2af9fae07535c8c785cbf91e88fa9
Dan Carpenter <dan.carpenter@oracle.com>
2021-03-12 10:41:12 +0300

    mptcp: fix bit MPTCP_PUSH_PENDING tests
    [ Upstream commit 2e5de7e0c8d2caa860e133ef71fc94671cb8e0bf ]
    
    The MPTCP_PUSH_PENDING define is 6 and these tests should be testing if
    BIT(6) is set.
    
    Fixes: c2e6048fa1cf ("mptcp: fix race in release_cb")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/mptcp/protocol.c
+++ net/mptcp/protocol.c
@@ -2947,7 +2947,7 @@ static void mptcp_release_cb(struct sock *sk)
 	for (;;) {
 		flags = 0;
 		if (test_and_clear_bit(MPTCP_PUSH_PENDING, &mptcp_sk(sk)->flags))
-			flags |= MPTCP_PUSH_PENDING;
+			flags |= BIT(MPTCP_PUSH_PENDING);
 		if (!flags)
 			break;
 
@@ -2960,7 +2960,7 @@ static void mptcp_release_cb(struct sock *sk)
 		 */
 
 		spin_unlock_bh(&sk->sk_lock.slock);
-		if (flags & MPTCP_PUSH_PENDING)
+		if (flags & BIT(MPTCP_PUSH_PENDING))
 			__mptcp_push_pending(sk, 0);
 
 		cond_resched();
