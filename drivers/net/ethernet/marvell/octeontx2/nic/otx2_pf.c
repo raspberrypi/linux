f4e51680456d77b78014a669b31de5b86738e635
Geetha sowjanya <gakula@marvell.com>
2021-03-18 19:45:47 +0530

    octeontx2-pf: Clear RSS enable flag on interace down
    [ Upstream commit f12098ce9b43e1a6fcaa524acbd90f9118a74c0a ]
    
    RSS configuration can not be get/set when interface is in down state
    as they required mbox communication. RSS enable flag status
    is used for set/get configuration. Current code do not clear the
    RSS enable flag on interface down which lead to mbox error while
    trying to set/get RSS configuration.
    
    Fixes: 85069e95e531 ("octeontx2-pf: Receive side scaling support")
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>
    Signed-off-by: Sunil Kovvuri Goutham <sgoutham@marvell.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -1625,6 +1625,7 @@ int otx2_stop(struct net_device *netdev)
 	struct otx2_nic *pf = netdev_priv(netdev);
 	struct otx2_cq_poll *cq_poll = NULL;
 	struct otx2_qset *qset = &pf->qset;
+	struct otx2_rss_info *rss;
 	int qidx, vec, wrk;
 
 	netif_carrier_off(netdev);
@@ -1636,6 +1637,10 @@ int otx2_stop(struct net_device *netdev)
 
 	/* First stop packet Rx/Tx */
 	otx2_rxtx_enable(pf, false);
+
+	/* Clear RSS enable flag */
+	rss = &pf->hw.rss_info;
+	rss->enable = false;
 
 	/* Cleanup Queue IRQ */
 	vec = pci_irq_vector(pf->pdev,
