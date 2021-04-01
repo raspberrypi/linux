97720ce80ae0a177b42c728c73a2c0c4c2592df1
Matthieu Baerts <matthieu.baerts@tessares.net>
2021-04-01 16:19:45 -0700

    selftests: mptcp: launch mptcp_connect with timeout
    [ Upstream commit 5888a61cb4e00695075bbacfd86f3fa73af00413 ]
    
    'mptcp_connect' already has a timeout for poll() but in some cases, it
    is not enough.
    
    With "timeout" tool, we will force the command to fail if it doesn't
    finish on time. Thanks to that, the script will continue and display
    details about the current state before marking the test as failed.
    Displaying this state is very important to be able to understand the
    issue. Best to have our CI reporting the issue than just "the test
    hanged".
    
    Note that in mptcp_connect.sh, we were using a long timeout to validate
    the fact we cannot create a socket if a sysctl is set. We don't need
    this timeout.
    
    In diag.sh, we want to send signals to mptcp_connect instances that have
    been started in the netns. But we cannot send this signal to 'timeout'
    otherwise that will stop the timeout and messages telling us SIGUSR1 has
    been received will be printed. Instead of trying to find the right PID
    and storing them in an array, we can simply use the output of
    'ip netns pids' which is all the PIDs we want to send signal to.
    
    Closes: https://github.com/multipath-tcp/mptcp_net-next/issues/160
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/net/mptcp/mptcp_connect.sh
+++ tools/testing/selftests/net/mptcp/mptcp_connect.sh
@@ -11,7 +11,8 @@ cin=""
 cout=""
 ksft_skip=4
 capture=false
-timeout=30
+timeout_poll=30
+timeout_test=$((timeout_poll * 2 + 1))
 ipv6=true
 ethtool_random_on=true
 tc_delay="$((RANDOM%50))"
@@ -272,7 +273,7 @@ check_mptcp_disabled()
 	ip netns exec ${disabled_ns} sysctl -q net.mptcp.enabled=0
 
 	local err=0
-	LANG=C ip netns exec ${disabled_ns} ./mptcp_connect -t $timeout -p 10000 -s MPTCP 127.0.0.1 < "$cin" 2>&1 | \
+	LANG=C ip netns exec ${disabled_ns} ./mptcp_connect -p 10000 -s MPTCP 127.0.0.1 < "$cin" 2>&1 | \
 		grep -q "^socket: Protocol not available$" && err=1
 	ip netns delete ${disabled_ns}
 
@@ -414,14 +415,20 @@ do_transfer()
 	local stat_cookietx_last=$(ip netns exec ${listener_ns} nstat -z -a TcpExtSyncookiesSent | while read a count c rest ;do  echo $count;done)
 	local stat_cookierx_last=$(ip netns exec ${listener_ns} nstat -z -a TcpExtSyncookiesRecv | while read a count c rest ;do  echo $count;done)
 
-	ip netns exec ${listener_ns} ./mptcp_connect -t $timeout -l -p $port -s ${srv_proto} $extra_args $local_addr < "$sin" > "$sout" &
+	timeout ${timeout_test} \
+		ip netns exec ${listener_ns} \
+			./mptcp_connect -t ${timeout_poll} -l -p $port -s ${srv_proto} \
+				$extra_args $local_addr < "$sin" > "$sout" &
 	local spid=$!
 
 	wait_local_port_listen "${listener_ns}" "${port}"
 
 	local start
 	start=$(date +%s%3N)
-	ip netns exec ${connector_ns} ./mptcp_connect -t $timeout -p $port -s ${cl_proto} $extra_args $connect_addr < "$cin" > "$cout" &
+	timeout ${timeout_test} \
+		ip netns exec ${connector_ns} \
+			./mptcp_connect -t ${timeout_poll} -p $port -s ${cl_proto} \
+				$extra_args $connect_addr < "$cin" > "$cout" &
 	local cpid=$!
 
 	wait $cpid
