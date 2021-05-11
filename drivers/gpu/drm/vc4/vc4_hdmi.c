f4343b6be9adcb9a06349c05463f9002b0e5f325
Maxime Ripard <maxime@cerno.tech>
2021-05-11 17:05:11 +0200

    drm/vc4: hdmi: Remove the DDC probing for status detection
    Commit 9d44abbbb8d5 ("drm/vc4: Fall back to using an EDID probe in the
    absence of a GPIO.") added some code to read the EDID through DDC in the
    HDMI driver detect hook since the Pi3 had no HPD GPIO back then.
    However, commit b1b8f45b3130 ("ARM: dts: bcm2837: Add missing GPIOs of
    Expander") changed that a couple of years later.
    
    This causes an issue though since some TV (like the LG 55C8) when it
    comes out of standy will deassert the HPD line, but the EDID will
    remain readable.
    
    It causes an issues nn platforms without an HPD GPIO, like the Pi4,
    where the DDC probing will be our primary mean to detect a display, and
    thus we will never detect the HPD pulse. This was fine before since the
    pulse was small enough that we would never detect it, and we also didn't
    have anything (like the scrambler) that needed to be set up in the
    display.
    
    However, now that we have both, the display during the HPD pulse will
    clear its scrambler status, and since we won't detect the
    disconnect/reconnect cycle we will never enable the scrambler back.
    
    As our main reason for that DDC probing is gone, let's just remove it.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -193,8 +193,6 @@ vc4_hdmi_connector_detect(struct drm_connector *connector, bool force)
 	if (vc4_hdmi->hpd_gpio &&
 	    gpiod_get_value_cansleep(vc4_hdmi->hpd_gpio)) {
 		connected = true;
-	} else if (drm_probe_ddc(vc4_hdmi->ddc)) {
-		connected = true;
 	} else {
 		unsigned long flags;
 		u32 hotplug;
