159c493606c60dfa0e573b4d1c28179ec59626da
Maxime Ripard <maxime@cerno.tech>
2021-05-26 16:13:02 +0200

    drm/vc4: Increase the core clock based on HVS load
    Depending on a given HVS output (HVS to PixelValves) and input (planes
    attached to a channel) load, the HVS needs for the core clock to be
    raised above its boot time default.
    
    Failing to do so will result in a vblank timeout and a stalled display
    pipeline.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -319,6 +319,7 @@ struct vc4_hvs {
 	u32 __iomem *dlist;
 
 	struct clk *core_clk;
+	struct clk_request *core_req;
 
 	/* Memory manager for CRTCs to allocate space in the display
 	 * list.  Units are dwords.
@@ -529,6 +530,8 @@ struct vc4_crtc_state {
 		unsigned int top;
 		unsigned int bottom;
 	} margins;
+
+	unsigned long hvs_load;
 
 	/* Transitional state below, only valid during atomic commits */
 	bool update_muxing;
