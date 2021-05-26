2cac9bd43db671ec65d4c22d9f396a48b6940933
Maxime Ripard <maxime@cerno.tech>
2021-05-26 16:13:02 +0200

    drm/vc4: Increase the core clock based on HVS load
    Depending on a given HVS output (HVS to PixelValves) and input (planes
    attached to a channel) load, the HVS needs for the core clock to be
    raised above its boot time default.
    
    Failing to do so will result in a vblank timeout and a stalled display
    pipeline.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -372,6 +372,12 @@ static void vc4_atomic_commit_tail(struct drm_atomic_state *state)
 						new_hvs_state->core_clock_rate);
 
 		core_req = clk_request_start(hvs->core_clk, core_rate);
+
+		/*
+		 * And remove the previous one based on the HVS
+		 * requirements if any.
+		 */
+		clk_request_done(hvs->core_req);
 	}
 
 	for_each_old_crtc_in_state(state, crtc, old_crtc_state, i) {
@@ -416,6 +422,14 @@ static void vc4_atomic_commit_tail(struct drm_atomic_state *state)
 		drm_dbg(dev, "Running the core clock at %lu Hz\n",
 			new_hvs_state->core_clock_rate);
 
+		/*
+		 * Request a clock rate based on the current HVS
+		 * requirements.
+		 */
+		hvs->core_req = clk_request_start(hvs->core_clk,
+						  new_hvs_state->core_clock_rate);
+
+		/* And drop the temporary request */
 		clk_request_done(core_req);
 	}
 }
