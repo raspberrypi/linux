aa51707b0c284957656811162082c5672968aee5
heshuai1 <heshuai1@xiaomi.com>
2021-06-10 15:46:59 +0800

    ANDROID: power: Add vendor hook to qos for GKI purpose.
    Add vendor hooks in add/update/remove frequency QoS request process to
    ensure that we can access the OEM's "frequency watchdog" logic for
    abnormal frequency monitoring. This is necessary for our power tuning
    policy.
    
    Bug: 187458531
    
    Signed-off-by: shenshen mao <maoshenshen@oppo.com>
    Change-Id: I1fb8fd6134432ecfb44ad242c66ccd8280ab9b43
    Signed-off-by: heshuai1 <heshuai1@xiaomi.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,6 +8,7 @@
 
 #include <linux/iova.h>
 #include <linux/futex.h>
+#include <linux/pm_qos.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
@@ -145,6 +146,9 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpufreq_transition);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_proc_transaction_finish);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_binder_select_special_worklist);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sync_txn_recvd);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freq_qos_add_request);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freq_qos_update_request);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_freq_qos_remove_request);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_balance_anon_file_reclaim);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_show_max_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_shrink_slab_bypass);
