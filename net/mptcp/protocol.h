cd7f1414f17072e9c267ee37013ba38446ee0ab7
Paolo Abeni <pabeni@redhat.com>
2021-06-22 12:25:23 -0700

    mptcp: refine mptcp_cleanup_rbuf
    [ Upstream commit fde56eea01f96b664eb63033990be0fd2a945da5 ]
    
    The current cleanup rbuf tries a bit too hard to avoid acquiring
    the subflow socket lock. We may end-up delaying the needed ack,
    or skip acking a blocked subflow.
    
    Address the above extending the conditions used to trigger the cleanup
    to reflect more closely what TCP does and invoking tcp_cleanup_rbuf()
    on all the active subflows.
    
    Note that we can't replicate the exact tests implemented in
    tcp_cleanup_rbuf(), as MPTCP lacks some of the required info - e.g.
    ping-pong mode.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Mat Martineau <mathew.j.martineau@linux.intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/mptcp/protocol.h
+++ net/mptcp/protocol.h
@@ -236,7 +236,6 @@ struct mptcp_sock {
 	bool		rcv_fastclose;
 	bool		use_64bit_ack; /* Set when we received a 64-bit DSN */
 	spinlock_t	join_list_lock;
-	struct sock	*ack_hint;
 	struct work_struct work;
 	struct sk_buff  *ooo_last_skb;
 	struct rb_root  out_of_order_queue;
