59e5c9ecdeb0919ff8490ff9aeb7f6dc2dbef982
Naoya Horiguchi <naoya.horiguchi@nec.com>
2021-06-28 19:43:17 -0700

    mm,hwpoison: make get_hwpoison_page() call get_any_page()
    [ Upstream commit 0ed950d1f28142ccd9a9453c60df87853530d778 ]
    
    __get_hwpoison_page() could fail to grab refcount by some race condition,
    so it's helpful if we can handle it by retrying.  We already have retry
    logic, so make get_hwpoison_page() call get_any_page() when called from
    memory_failure().
    
    As a result, get_hwpoison_page() can return negative values (i.e.  error
    code), so some callers are also changed to handle error cases.
    soft_offline_page() does nothing for -EBUSY because that's enough and
    users in userspace can easily handle it.  unpoison_memory() is also
    unchanged because it's broken and need thorough fixes (will be done
    later).
    
    Link: https://lkml.kernel.org/r/20210603233632.2964832-3-nao.horiguchi@gmail.com
    Signed-off-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -5930,6 +5930,8 @@ int get_hwpoison_huge_page(struct page *page, bool *hugetlb)
 		*hugetlb = true;
 		if (HPageFreed(page) || HPageMigratable(page))
 			ret = get_page_unless_zero(page);
+		else
+			ret = -EBUSY;
 	}
 	spin_unlock_irq(&hugetlb_lock);
 	return ret;
