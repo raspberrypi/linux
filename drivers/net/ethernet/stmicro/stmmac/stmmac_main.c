2f2b3b953b43d7653310fd0d4e51607d920ef9f3
Mohammad Athari Bin Ismail <mohammad.athari.ismail@intel.com>
2021-06-30 17:59:35 +0800

    net: stmmac: Terminate FPE workqueue in suspend
    [ Upstream commit 6b28a86d6c0bb02119f386ec2f56efde909e9bcb ]
    
    Add stmmac_fpe_stop_wq() in stmmac_suspend() to terminate FPE workqueue
    during suspend. So, in suspend mode, there will be no FPE workqueue
    available. Without this fix, new additional FPE workqueue will be created
    in every suspend->resume cycle.
    
    Fixes: 5a5586112b92 ("net: stmmac: support FPE link partner hand-shaking procedure")
    Signed-off-by: Mohammad Athari Bin Ismail <mohammad.athari.ismail@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7170,6 +7170,7 @@ int stmmac_suspend(struct device *dev)
 				     priv->plat->rx_queues_to_use, false);
 
 		stmmac_fpe_handshake(priv, false);
+		stmmac_fpe_stop_wq(priv);
 	}
 
 	priv->speed = SPEED_UNKNOWN;
