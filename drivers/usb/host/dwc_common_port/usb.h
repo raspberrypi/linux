b28e7600a46fefed11acdef54c40ee2a8e66eb5f
Phil Elwell <phil@raspberrypi.com>
2021-07-05 19:38:21 +0100

    dwc_otg: Update NetBSD usb.h header licence
    NetBSD have changed their licensing requirements such that the 2-clause
    licence is preferred. Update usb.h in the downstream dwc_otg code
    accordingly.
    
    See https://www.netbsd.org/about/redistribution.html for more
    information.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/usb/host/dwc_common_port/usb.h
+++ drivers/usb/host/dwc_common_port/usb.h
@@ -14,13 +14,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *        This product includes software developed by the NetBSD
- *        Foundation, Inc. and its contributors.
- * 4. Neither the name of The NetBSD Foundation nor the names of its
- *    contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
  * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
