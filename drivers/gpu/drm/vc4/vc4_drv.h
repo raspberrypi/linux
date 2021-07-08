9a9ab81437166203a74f53db42a9d322bf44ce05
Dom Cobley <popcornmix@gmail.com>
2021-07-08 19:16:44 +0100

    Revert "drm/vc4: Increase the core clock based on HVS load"
    This reverts commit 69a25f086d2e81d6592f9f18f45f37ebab65297a.

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -326,7 +326,6 @@ struct vc4_hvs {
 	u32 __iomem *dlist;
 
 	struct clk *core_clk;
-	struct clk_request *core_req;
 
 	/* Memory manager for CRTCs to allocate space in the display
 	 * list.  Units are dwords.
@@ -537,8 +536,6 @@ struct vc4_crtc_state {
 		unsigned int top;
 		unsigned int bottom;
 	} margins;
-
-	unsigned long hvs_load;
 
 	/* Transitional state below, only valid during atomic commits */
 	bool update_muxing;
