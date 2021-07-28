3688e05c0ef3e0934284bedc55173e1e48342748
RuffaloLavoisier <jsh970307@daum.net>
2021-07-28 23:40:28 +0900

    fixed : usage comment (enviroment)

--- Build.sh
+++ Build.sh
@@ -1,4 +1,4 @@
-# usage $ ./Build.sh model(zero,3,4) environment(cross,compile)
+# usage $ ./Build.sh model(zero,3,4) environment(cross,local)
 function build_Script(){
 	sudo apt-get update
 	sudo apt-get install git bc bison flex libssl-dev make -y
