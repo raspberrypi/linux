0ca6ac8a2691762307beaa4841255d1cfe6b2684
Rohith Surabattula <rohiths@microsoft.com>
2021-08-09 09:32:46 +0000

    cifs: Call close synchronously during unlink/rename/lease break.
    commit 9e992755be8f2d458a0bcbefd19e493483c1dba2 upstream.
    
    During unlink/rename/lease break, deferred work for close is
    scheduled immediately but in an asynchronous manner which might
    lead to race with actual(unlink/rename) commands.
    
    This change will schedule close synchronously which will avoid
    the race conditions with other commands.
    
    Signed-off-by: Rohith Surabattula <rohiths@microsoft.com>
    Reviewed-by: Shyam Prasad N <sprasad@microsoft.com>
    Cc: stable@vger.kernel.org # 5.13
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/cifs/cifsglob.h
+++ fs/cifs/cifsglob.h
@@ -1615,6 +1615,11 @@ struct dfs_info3_param {
 	int ttl;
 };
 
+struct file_list {
+	struct list_head list;
+	struct cifsFileInfo *cfile;
+};
+
 /*
  * common struct for holding inode info when searching for or updating an
  * inode with new info
