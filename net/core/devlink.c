5eb9d59b73f6ec20f939daa4939bbbe5eaf1e8e3
Leon Romanovsky <leonro@nvidia.com>
2021-08-14 12:57:30 +0300

    devlink: Clear whole devlink_flash_notify struct
    [ Upstream commit ed43fbac717882165a2a4bd64f7b1f56f7467bb7 ]
    
    The { 0 } doesn't clear all fields in the struct, but tells to the
    compiler to set all fields to zero and doesn't touch any sub-fields
    if they exists.
    
    The {} is an empty initialiser that instructs to fully initialize whole
    struct including sub-fields, which is error-prone for future
    devlink_flash_notify extensions.
    
    Fixes: 6700acc5f1fe ("devlink: collect flash notify params into a struct")
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/devlink.c
+++ net/core/devlink.c
@@ -4117,7 +4117,7 @@ out_free_msg:
 
 static void devlink_flash_update_begin_notify(struct devlink *devlink)
 {
-	struct devlink_flash_notify params = { 0 };
+	struct devlink_flash_notify params = {};
 
 	__devlink_flash_update_notify(devlink,
 				      DEVLINK_CMD_FLASH_UPDATE,
@@ -4126,7 +4126,7 @@ static void devlink_flash_update_begin_notify(struct devlink *devlink)
 
 static void devlink_flash_update_end_notify(struct devlink *devlink)
 {
-	struct devlink_flash_notify params = { 0 };
+	struct devlink_flash_notify params = {};
 
 	__devlink_flash_update_notify(devlink,
 				      DEVLINK_CMD_FLASH_UPDATE_END,
