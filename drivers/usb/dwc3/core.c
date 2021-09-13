622fc3f8d6a5eb062c3ee9a102e60e3e00813571
Jonathan Bell <jonathan@raspberrypi.com>
2021-09-13 11:14:32 +0100

    usb: dwc3: Set DMA and coherent masks early
    dwc3 allocates scratch and event buffers in the top-level driver. Hack the
    probe function to set the DMA mask before trying to allocate these.
    
    I think the event buffers are only used in device mode, but the scratch
    buffers may be used if core hibernation is enabled.
    
    usb: dwc3: add support for new DT quirks
    
    Apply the optional axi-pipe-limit and dis-in-autoretry-quirk properties
    during driver probe.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    phy: phy-brcm-usb: Add 2712 support
    
    usb: dwc3: if the host controller instance number is present in DT, use it
    
    If two instances of a dwc3 host controller are specified in devicetree,
    then the probe order may be arbitrary which results in the device names
    swapping on a per-boot basis.
    
    If a "usb" alias with the instance number is specified, then use
    that to construct the device name instead of autogenerating one.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    rp1 dwc3 changes
    
    drivers: usb: dwc3: allow setting GTXTHRCFG on dwc_usb3.0 hardware
    
    Equivalent register fields exist in the SuperSpeed Host version of the
    hardware, so allow the use of TX thresholds if specified in devicetree.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    drivers: usb: dwc3: remove downstream quirk dis-in-autoretry
    
    Upstream have unilaterally disabled the feature.
    
    Partially reverts 6e9142a26ee0fdc3a5adc49ed6cedc0b16ec2ed1 (downstream)
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1323,6 +1323,24 @@ static void dwc3_config_threshold(struct dwc3 *dwc)
 	}
 }
 
+static void dwc3_set_axi_pipe_limit(struct dwc3 *dwc)
+{
+	struct device *dev = dwc->dev;
+	u32 cfg;
+
+	if (!dwc->axi_pipe_limit)
+		return;
+	if (dwc->axi_pipe_limit > 16) {
+		dev_err(dev, "Invalid axi_pipe_limit property\n");
+		return;
+	}
+	cfg = dwc3_readl(dwc->regs, DWC3_GSBUSCFG1);
+	cfg &= ~DWC3_GSBUSCFG1_PIPETRANSLIMIT(15);
+	cfg |= DWC3_GSBUSCFG1_PIPETRANSLIMIT(dwc->axi_pipe_limit - 1);
+
+	dwc3_writel(dwc->regs, DWC3_GSBUSCFG1, cfg);
+}
+
 /**
  * dwc3_core_init - Low-level initialization of DWC3 Core
  * @dwc: Pointer to our controller context structure
@@ -1389,6 +1407,8 @@ static int dwc3_core_init(struct dwc3 *dwc)
 	dwc3_set_incr_burst_type(dwc);
 
 	dwc3_config_soc_bus(dwc);
+
+	dwc3_set_axi_pipe_limit(dwc);
 
 	ret = dwc3_phy_power_on(dwc);
 	if (ret)
@@ -1516,6 +1536,24 @@ static int dwc3_core_init(struct dwc3 *dwc)
 		reg = dwc3_readl(dwc->regs, DWC3_GUCTL3);
 		reg |= DWC3_GUCTL3_USB20_RETRY_DISABLE;
 		dwc3_writel(dwc->regs, DWC3_GUCTL3, reg);
+	}
+
+	if (DWC3_IP_IS(DWC3) && dwc->dr_mode == USB_DR_MODE_HOST) {
+		u8 tx_thr_num = dwc->tx_thr_num_pkt_prd;
+		u8 tx_maxburst = dwc->tx_max_burst_prd;
+
+		if (tx_thr_num && tx_maxburst) {
+			reg = dwc3_readl(dwc->regs, DWC3_GTXTHRCFG);
+			reg |= DWC3_GTXTHRCFG_PKTCNTSEL;
+
+			reg &= ~DWC3_GTXTHRCFG_TXPKTCNT(~0);
+			reg |= DWC3_GTXTHRCFG_TXPKTCNT(tx_thr_num);
+
+			reg &= ~DWC3_GTXTHRCFG_MAXTXBURSTSIZE(~0);
+			reg |= DWC3_GTXTHRCFG_MAXTXBURSTSIZE(tx_maxburst);
+
+			dwc3_writel(dwc->regs, DWC3_GTXTHRCFG, reg);
+		}
 	}
 
 	return 0;
@@ -1704,6 +1742,7 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	u8			tx_max_burst_prd = 0;
 	u8			tx_fifo_resize_max_num;
 	u16			num_hc_interrupters;
+	u8			axi_pipe_limit;
 
 	/* default to highest possible threshold */
 	lpm_nyet_threshold = 0xf;
@@ -1726,6 +1765,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 
 	/* default to a single XHCI interrupter */
 	num_hc_interrupters = 1;
+	/* Default to 0 (don't override hardware defaults) */
+	axi_pipe_limit = 0;
 
 	dwc->maximum_speed = usb_get_maximum_speed(dev);
 	dwc->max_ssp_rate = usb_get_maximum_ssp_rate(dev);
@@ -1847,6 +1888,9 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 	dwc->dis_split_quirk = device_property_read_bool(dev,
 				"snps,dis-split-quirk");
 
+	device_property_read_u8(dev, "snps,axi-pipe-limit",
+				   &axi_pipe_limit);
+
 	dwc->lpm_nyet_threshold = lpm_nyet_threshold;
 	dwc->tx_de_emphasis = tx_de_emphasis;
 
@@ -1863,6 +1907,8 @@ static void dwc3_get_properties(struct dwc3 *dwc)
 
 	dwc->tx_thr_num_pkt_prd = tx_thr_num_pkt_prd;
 	dwc->tx_max_burst_prd = tx_max_burst_prd;
+
+	dwc->axi_pipe_limit = axi_pipe_limit;
 
 	dwc->tx_fifo_resize_max_num = tx_fifo_resize_max_num;
 
@@ -2212,6 +2258,12 @@ int dwc3_core_probe(const struct dwc3_probe_data *data)
 	dwc->usb_psy = dwc3_get_usb_power_supply(dwc);
 	if (IS_ERR(dwc->usb_psy))
 		return dev_err_probe(dev, PTR_ERR(dwc->usb_psy), "couldn't get usb power supply\n");
+
+	if (!dwc->sysdev_is_parent) {
+		ret = dma_set_mask_and_coherent(dwc->sysdev, DMA_BIT_MASK(64));
+		if (ret)
+			return ret;
+	}
 
 	if (!data->ignore_clocks_and_resets) {
 		dwc->reset = devm_reset_control_array_get_optional_shared(dev);
