6eb0c6c8b2727252ffdb719ca053bd40f3762cdb
Jonathan Bell <jonathan@raspberrypi.com>
2021-09-13 11:14:32 +0100

    usb: dwc3: Set DMA and coherent masks early
    dwc3 allocates scratch and event buffers in the top-level driver. Hack the
    probe function to set the DMA mask before trying to allocate these.
    
    I think the event buffers are only used in device mode, but the scratch
    buffers may be used if core hibernation is enabled.
    
    usb: dwc3: add support for new DT quirks
    
    Apply the optional axi-pipe-limit and dis-in-autoretry-quirk properties
    during driver probe.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    phy: phy-brcm-usb: Add 2712 support
    
    usb: dwc3: if the host controller instance number is present in DT, use it
    
    If two instances of a dwc3 host controller are specified in devicetree,
    then the probe order may be arbitrary which results in the device names
    swapping on a per-boot basis.
    
    If a "usb" alias with the instance number is specified, then use
    that to construct the device name instead of autogenerating one.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    rp1 dwc3 changes
    
    drivers: usb: dwc3: allow setting GTXTHRCFG on dwc_usb3.0 hardware
    
    Equivalent register fields exist in the SuperSpeed Host version of the
    hardware, so allow the use of TX thresholds if specified in devicetree.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    drivers: usb: dwc3: remove downstream quirk dis-in-autoretry
    
    Upstream have unilaterally disabled the feature.
    
    Partially reverts 6e9142a26ee0fdc3a5adc49ed6cedc0b16ec2ed1 (downstream)
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/dwc3/host.c
+++ drivers/usb/dwc3/host.c
@@ -82,16 +82,23 @@ out:
 
 int dwc3_host_init(struct dwc3 *dwc)
 {
+	struct platform_device	*pdev = to_platform_device(dwc->dev);
 	struct property_entry	props[5];
 	struct platform_device	*xhci;
 	int			ret, irq;
 	int			prop_idx = 0;
+	int			id;
 
 	irq = dwc3_host_get_irq(dwc);
 	if (irq < 0)
 		return irq;
 
-	xhci = platform_device_alloc("xhci-hcd", PLATFORM_DEVID_AUTO);
+	id = of_alias_get_id(pdev->dev.of_node, "usb");
+	if (id >= 0)
+		xhci = platform_device_alloc("xhci-hcd", id);
+	else
+		xhci = platform_device_alloc("xhci-hcd", PLATFORM_DEVID_AUTO);
+
 	if (!xhci) {
 		dev_err(dwc->dev, "couldn't allocate xHCI device\n");
 		return -ENOMEM;
