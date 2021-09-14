f8bc3c6face170c79e0007fb7b54e045a9e2412f
Maxime Ripard <maxime@cerno.tech>
2021-09-14 15:23:47 +0200

    drm/vc4: hdmi: Remove unnecessary pm_runtime_put
    Unlike pm_runtime_get_sync(), pm_runtime_resume_and_get() doesn't take a
    reference on failure, so we don't need to call pm_runtime_put() on
    failure.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -915,7 +915,6 @@ static void vc4_hdmi_encoder_pre_crtc_configure(struct drm_encoder *encoder,
 	ret = pm_runtime_resume_and_get(&vc4_hdmi->pdev->dev);
 	if (ret < 0) {
 		DRM_ERROR("Failed to retain power domain: %d\n", ret);
-		pm_runtime_put(&vc4_hdmi->pdev->dev);
 		return;
 	}
 
