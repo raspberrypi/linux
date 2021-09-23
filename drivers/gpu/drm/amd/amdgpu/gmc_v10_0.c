18d1c5ea3798ba42cfa0f8b2264d873463facb03
Leslie Shi <Yuliang.Shi@amd.com>
2021-09-23 16:05:31 +0800

    drm/amdgpu: fix gart.bo pin_count leak
    [ Upstream commit 66805763a97f8f7bdf742fc0851d85c02ed9411f ]
    
    gmc_v{9,10}_0_gart_disable() isn't called matched with
    correspoding gart_enbale function in SRIOV case. This will
    lead to gart.bo pin_count leak on driver unload.
    
    Cc: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Leslie Shi <Yuliang.Shi@amd.com>
    Signed-off-by: Guchun Chen <guchun.chen@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/gmc_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/gmc_v10_0.c
@@ -1094,6 +1094,8 @@ static int gmc_v10_0_hw_fini(void *handle)
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 
+	gmc_v10_0_gart_disable(adev);
+
 	if (amdgpu_sriov_vf(adev)) {
 		/* full access mode, so don't touch any GMC register */
 		DRM_DEBUG("For SRIOV client, shouldn't do anything.\n");
@@ -1102,7 +1104,6 @@ static int gmc_v10_0_hw_fini(void *handle)
 
 	amdgpu_irq_put(adev, &adev->gmc.ecc_irq, 0);
 	amdgpu_irq_put(adev, &adev->gmc.vm_fault, 0);
-	gmc_v10_0_gart_disable(adev);
 
 	return 0;
 }
