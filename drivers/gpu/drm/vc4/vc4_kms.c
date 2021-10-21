a7a1e319d6abca7deb52e5973213a7bb2788f8cc
Maxime Ripard <maxime@cerno.tech>
2021-10-21 13:57:44 +0200

    drm/vc4: kms: Don't duplicate pending commit
    Our HVS global state, when duplicated, will also copy the pointer to the
    drm_crtc_commit (and increase the reference count) for each FIFO if the
    pointer is not NULL.
    
    However, our atomic_setup function will overwrite that pointer without
    putting the reference back leading to a memory leak.
    
    Since the commit is only relevant during the atomic commit process, it
    doesn't make sense to duplicate the reference to the commit anyway.
    Let's remove it.
    
    Fixes: 9ec03d7f1ed3 ("drm/vc4: kms: Wait on previous FIFO users before a commit")
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -720,12 +720,6 @@ vc4_hvs_channels_duplicate_state(struct drm_private_obj *obj)
 	for (i = 0; i < HVS_NUM_CHANNELS; i++) {
 		state->fifo_state[i].in_use = old_state->fifo_state[i].in_use;
 		state->fifo_state[i].fifo_load = old_state->fifo_state[i].fifo_load;
-
-		if (!old_state->fifo_state[i].pending_commit)
-			continue;
-
-		state->fifo_state[i].pending_commit =
-			drm_crtc_commit_get(old_state->fifo_state[i].pending_commit);
 	}
 
 	state->core_clock_rate = old_state->core_clock_rate;
