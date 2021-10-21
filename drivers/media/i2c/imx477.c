6ff3bd8688235bbaa2884595002657b06347c02d
David Plowman <david.plowman@raspberrypi.com>
2021-10-21 14:44:43 +0100

    media: i2c: imx477: Sensor should report RAW color space
    Tested on Raspberry Pi running libcamera.
    
    Signed-off-by: David Plowman <david.plowman@raspberrypi.com>

--- drivers/media/i2c/imx477.c
+++ drivers/media/i2c/imx477.c
@@ -1471,7 +1471,7 @@ static int imx477_enum_frame_size(struct v4l2_subdev *sd,
 
 static void imx477_reset_colorspace(struct v4l2_mbus_framefmt *fmt)
 {
-	fmt->colorspace = V4L2_COLORSPACE_SRGB;
+	fmt->colorspace = V4L2_COLORSPACE_RAW;
 	fmt->ycbcr_enc = V4L2_MAP_YCBCR_ENC_DEFAULT(fmt->colorspace);
 	fmt->quantization = V4L2_MAP_QUANTIZATION_DEFAULT(true,
 							  fmt->colorspace,
