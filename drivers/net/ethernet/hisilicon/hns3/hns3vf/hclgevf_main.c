4294c96432c3238eb4287dea197e320291d77cf5
Yufeng Mo <moyufeng@huawei.com>
2021-10-27 20:11:44 +0800

    net: hns3: change hclge/hclgevf workqueue to WQ_UNBOUND mode
    [ Upstream commit f29da4088fb4eeba457219a931327d1d5f45196a ]
    
    Currently, the workqueue of hclge/hclgevf is executed on
    the CPU that initiates scheduling requests by default. In
    stress scenarios, the CPU may be busy and workqueue scheduling
    is completed after a long period of time. To avoid this
    situation and implement proper scheduling, use the WQ_UNBOUND
    mode instead. In this way, the workqueue can be performed on
    a relatively idle CPU.
    
    Signed-off-by: Yufeng Mo <moyufeng@huawei.com>
    Signed-off-by: Guangbin Huang <huangguangbin2@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3vf/hclgevf_main.c
@@ -3890,7 +3890,7 @@ static int hclgevf_init(void)
 {
 	pr_info("%s is initializing\n", HCLGEVF_NAME);
 
-	hclgevf_wq = alloc_workqueue("%s", 0, 0, HCLGEVF_NAME);
+	hclgevf_wq = alloc_workqueue("%s", WQ_UNBOUND, 0, HCLGEVF_NAME);
 	if (!hclgevf_wq) {
 		pr_err("%s: failed to create workqueue\n", HCLGEVF_NAME);
 		return -ENOMEM;
