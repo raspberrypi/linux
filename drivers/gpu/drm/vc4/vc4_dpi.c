f1265511503bc51f091cdcb846387e267640d126
rtomasa <ruben.tomas.alonso@gmail.com>
2021-11-18 18:28:12 +0100

    Enabled Interlaced Mode

--- drivers/gpu/drm/vc4/vc4_dpi.c
+++ drivers/gpu/drm/vc4/vc4_dpi.c
@@ -216,9 +216,6 @@ static void vc4_dpi_encoder_enable(struct drm_encoder *encoder)
 static enum drm_mode_status vc4_dpi_encoder_mode_valid(struct drm_encoder *encoder,
 						       const struct drm_display_mode *mode)
 {
-	if (mode->flags & DRM_MODE_FLAG_INTERLACE)
-		return MODE_NO_INTERLACE;
-
 	return MODE_OK;
 }
 
