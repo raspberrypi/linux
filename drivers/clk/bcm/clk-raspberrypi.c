e5feb9a059e90cf15dd6da89889cdbb479e1c973
Dave Stevenson <dave.stevenson@raspberrypi.com>
2021-12-02 13:53:36 +0000

    clk: bcm: rpi: Add the BCM283x pixel clock.
    The clk-bcm2835 handling of the pixel clock does not function
    correctly when the HDMI power domain is disabled.
    
    The firmware supports it correctly, so add it to the
    firmware clock driver.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/clk/bcm/clk-raspberrypi.c
+++ drivers/clk/bcm/clk-raspberrypi.c
@@ -141,6 +141,7 @@ raspberrypi_clk_variants[RPI_FIRMWARE_NUM_CLK_ID] = {
 	},
 	[RPI_FIRMWARE_PIXEL_CLK_ID] = {
 		.export = true,
+		.minimize = true,
 	},
 	[RPI_FIRMWARE_HEVC_CLK_ID] = {
 		.export = true,
