668e0bcf3849538a257ad0772f078421b1744d2d
Jonathan Bell <jonathan@raspberrypi.com>
2021-12-13 16:04:03 +0000

    usb: xhci: add VLI_TRB_CACHE_BUG quirk
    The VL805 fetches up to 4 transfer TRBs at a time. TRB reads don't cross
    a 64B boundary, and if a TRB is fetched and is not on a 64B boundary,
    the read is sized up to the next 64B boundary.
    
    However the VL805 implements a readahead prefetch for TRBs on a transfer
    ring. This fetches the next 64B after any TRB read has happened. Near
    the end of a ring segment, the prefetcher can read the first 64B of the
    next page in physical memory and this is where the behaviour causes a
    bug.
    
    The controller does not tag reads with which endpoint they are for, so
    if the start of the next page is a ring segment used by a victim
    endpoint, and the victim endpoint is about to fetch TRBs from the start
    of the segment, the victim endpoint will read from the prefetched data
    and not perform a read to main memory. If the data is stale, the ring
    cycle state bit may not be correct and the endpoint will silently halt.
    
    Adjust trbs_per_seg for transfer rings allocated for this controller.
    
    See https://github.com/raspberrypi/linux/issues/4685
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1905,6 +1905,7 @@ struct xhci_hcd {
 #define XHCI_BROKEN_D3COLD	BIT_ULL(41)
 #define XHCI_EP_CTX_BROKEN_DCS	BIT_ULL(42)
 #define XHCI_AVOID_DQ_ON_LINK	BIT_ULL(43)
+#define XHCI_VLI_TRB_CACHE_BUG	BIT_ULL(44)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
