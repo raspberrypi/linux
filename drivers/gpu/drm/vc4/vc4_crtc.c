5ffb1f787640cd32fd8ca69f48b51a1b1329f2da
Maxime Ripard <maxime@cerno.tech>
2021-12-16 14:54:54 +0100

    drm/vc4: hvs: Defer dlist slots deallocation
    During normal operations, the cursor position update is done through an
    asynchronous plane update, which on the vc4 driver basically just
    modifies the right dlist word to move the plane to the new coordinates.
    
    However, when we have the overscan margins setup, we fall back to a
    regular commit when we are next to the edges. And since that commit
    happens to be on a cursor plane, it's considered a legacy cursor update
    by KMS.
    
    The main difference it makes is that it won't wait for its completion
    (ie, next vblank) before returning. This means if we have multiple
    commits happening in rapid succession, we can have several of them
    happening before the next vblank.
    
    In parallel, our dlist allocation is tied to a CRTC state, and each time
    we do a commit we end up with a new CRTC state, with the previous one
    being freed. This means that we free our previous dlist entry (but don't
    clear it though) every time a new one is being committed.
    
    Now, if we were to have two commits happening before the next vblank, we
    could end up freeing reusing the same dlist entries before the next
    vblank.
    
    Indeed, we would start from an initial state taking, for example, the
    dlist entries 10 to 20, then start a commit taking the entries 20 to 30
    and setting the dlist pointer to 20, and freeing the dlist entries 10 to
    20. However, since we haven't reach vblank yet, the HVS is still using
    the entries 10 to 20.
    
    If we were to make a new commit now, chances are the allocator are going
    to give the 10 to 20 entries back, and we would change their content to
    match the new state. If vblank hasn't happened yet, we just corrupted
    the active dlist entries.
    
    A first attempt to solve this was made by creating an intermediate dlist
    buffer to store the current (ie, as of the last commit) dlist content,
    that we would update each time the HVS is done with a frame. However, if
    the interrupt handler missed the vblank window, we would end up copying
    our intermediate dlist to the hardware one during the composition,
    essentially creating the same issue.
    
    Since making sure that our interrupt handler runs within a fixed,
    constrained, time window would require to make Linux a real-time kernel,
    this seems a bit out of scope.
    
    Instead, we can work around our original issue by keeping the dlist
    slots allocation longer. That way, we won't reuse a dlist slot while
    it's still in flight. In order to achieve this, instead of freeing the
    dlist slot when its associated CRTC state is destroyed, we'll queue it
    in a list.
    
    A naive implementation would free the buffers in that queue when we get
    our end of frame interrupt. However, there's still a race since, just
    like in the shadow dlist case, we don't control when the handler for
    that interrupt is going to run. Thus, we can end up with a commit adding
    an old dlist allocation to our queue during the window between our
    actual interrupt and when our handler will run. And since that buffer is
    still being used for the composition of the current frame, we can't free
    it right away, exposing us to the original bug.
    
    Fortunately for us, the hardware provides a frame counter that is
    increased each time the first line of a frame is being generated.
    Associating the frame counter the image is supposed to go away to the
    allocation, and then only deallocate buffers that have a counter below
    or equal to the one we see when the deallocation code should prevent the
    above race from occuring.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_crtc.c
+++ drivers/gpu/drm/vc4/vc4_crtc.c
@@ -1097,14 +1097,8 @@ void vc4_crtc_destroy_state(struct drm_crtc *crtc,
 	struct vc4_dev *vc4 = to_vc4_dev(crtc->dev);
 	struct vc4_crtc_state *vc4_state = to_vc4_crtc_state(state);
 
-	if (drm_mm_node_allocated(&vc4_state->mm)) {
-		unsigned long flags;
-
-		spin_lock_irqsave(&vc4->hvs->mm_lock, flags);
-		drm_mm_remove_node(&vc4_state->mm);
-		spin_unlock_irqrestore(&vc4->hvs->mm_lock, flags);
-
-	}
+	vc4_hvs_mark_dlist_entry_stale(vc4->hvs, vc4_state->mm);
+	vc4_state->mm = NULL;
 
 	drm_atomic_helper_crtc_destroy_state(crtc, state);
 }
