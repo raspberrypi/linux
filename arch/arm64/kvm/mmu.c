68676b576c0e63a33b5b2983a389cdcb65184292
Marc Zyngier <maz@kernel.org>
2022-01-06 10:01:01 +0000

    ANDROID: KVM: arm64: Expose topup_hyp_memcache() to the rest of KVM
    In order to simplify the implementation of an EL2-only version of
    MMIO guard, expose topup_hyp_memcache() and simplify its usage
    by only requiring a vcpu.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 278749606
    Change-Id: I4f54c57a9693cf7a3450f99fedc15ae32af09a31

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1068,12 +1068,14 @@ void free_hyp_memcache(struct kvm_hyp_memcache *mc)
 				    kvm_host_va, NULL);
 }
 
-int topup_hyp_memcache(struct kvm_hyp_memcache *mc, unsigned long min_pages)
+int topup_hyp_memcache(struct kvm_vcpu *vcpu)
 {
 	if (!is_protected_kvm_enabled())
 		return 0;
 
-	return __topup_hyp_memcache(mc, min_pages, hyp_mc_alloc_fn,
+	return __topup_hyp_memcache(&vcpu->arch.pkvm_memcache,
+				    kvm_mmu_cache_min_pages(vcpu->kvm),
+				    hyp_mc_alloc_fn,
 				    kvm_host_pa, NULL);
 }
 
@@ -1431,7 +1433,6 @@ static int pkvm_host_map_guest(u64 pfn, u64 gfn)
 static int pkvm_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 			  unsigned long hva)
 {
-	struct kvm_hyp_memcache *hyp_memcache = &vcpu->arch.pkvm_memcache;
 	struct mm_struct *mm = current->mm;
 	unsigned int flags = FOLL_HWPOISON | FOLL_LONGTERM | FOLL_WRITE;
 	struct kvm_pinned_page *ppage;
@@ -1440,7 +1441,7 @@ static int pkvm_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 	u64 pfn;
 	int ret;
 
-	ret = topup_hyp_memcache(hyp_memcache, kvm_mmu_cache_min_pages(kvm));
+	ret = topup_hyp_memcache(vcpu);
 	if (ret)
 		return -ENOMEM;
 
