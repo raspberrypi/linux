c9ab587133583cc4e1cc1ff5d22600573ffdc211
Marc Zyngier <maz@kernel.org>
2022-01-06 10:29:13 +0000

    ANDROID: KVM: arm64: pkvm: Add MMIO guard infrastructure
    Introduce the infrastructure required to identify an IPA region
    that is expected to be used as an MMIO window.
    
    This include mapping, unmapping and checking the regions. Nothing
    calls into it yet, so no expected functional change.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 278749606
    Change-Id: I227eaa28b98e067e3daae4f9e1071eb37a6761cc

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -75,6 +75,9 @@ int __pkvm_host_share_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu);
 int __pkvm_host_donate_guest(u64 pfn, u64 gfn, struct pkvm_hyp_vcpu *vcpu);
 int __pkvm_guest_share_host(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
 int __pkvm_guest_unshare_host(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
+int __pkvm_install_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
+int __pkvm_remove_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu, u64 ipa);
+bool __pkvm_check_ioguard_page(struct pkvm_hyp_vcpu *hyp_vcpu);
 
 bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
