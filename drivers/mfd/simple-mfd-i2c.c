5f48cba9cb6c1e060e3663f597581cbe98da0c82
Dave Stevenson <dave.stevenson@raspberrypi.com>
2022-01-19 17:22:57 +0000

    mfd: simple-mfd-i2c: Add configuration for RPi POE HAT
    The Raspbery Pi PoE+ HAT exposes a fan controller and power
    supply status reporting via a single I2C address.
    
    Create an MFD device that allows loading of the relevant
    sub-drivers, with a shared I2C regmap.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/mfd/simple-mfd-i2c.c
+++ drivers/mfd/simple-mfd-i2c.c
@@ -29,6 +29,15 @@ static const struct regmap_config regmap_config_8r_8v = {
 	.val_bits = 8,
 };
 
+static const struct regmap_config regmap_config_16r_8v = {
+	.reg_bits = 16,
+	.val_bits = 8,
+};
+
+static const struct simple_mfd_data rpi_poe_core = {
+	.regmap_config = &regmap_config_16r_8v,
+};
+
 static int simple_mfd_i2c_probe(struct i2c_client *i2c)
 {
 	const struct simple_mfd_data *simple_mfd_data;
@@ -88,6 +97,7 @@ static const struct of_device_id simple_mfd_i2c_of_match[] = {
 	{ .compatible = "silergy,sy7636a", .data = &silergy_sy7636a},
 	{ .compatible = "maxim,max5970", .data = &maxim_max5970},
 	{ .compatible = "maxim,max5978", .data = &maxim_max5970},
+	{ .compatible = "raspberrypi,poe-core", &rpi_poe_core },
 	{ .compatible = "raspberrypi,sensehat" },
 	{}
 };
