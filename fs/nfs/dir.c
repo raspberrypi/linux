77bfe650e59d77ae2ec096a466f68a531ce09fde
trondmy@kernel.org <trondmy@kernel.org>
2022-01-18 19:52:16 -0500

    NFS: Don't skip directory entries when doing uncached readdir
    [ Upstream commit ce292d8faf41f62e0fb0c78476c6fce5d629235a ]
    
    Ensure that we initialise desc->cache_entry_index correctly in
    uncached_readdir().
    
    Fixes: d1bacf9eb2fd ("NFS: add readdir cache array")
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Signed-off-by: Anna Schumaker <Anna.Schumaker@Netapp.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfs/dir.c
+++ fs/nfs/dir.c
@@ -1041,6 +1041,7 @@ static int uncached_readdir(struct nfs_readdir_descriptor *desc)
 		goto out;
 
 	desc->page_index = 0;
+	desc->cache_entry_index = 0;
 	desc->last_cookie = desc->dir_cookie;
 	desc->duped = 0;
 
