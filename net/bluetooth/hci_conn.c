aa1ca580e3ffe62a2c5ea1c095b609b2943c5269
Soenke Huster <soenke.huster@eknoes.de>
2022-01-23 15:06:24 +0100

    Bluetooth: hci_event: Ignore multiple conn complete events
    [ Upstream commit d5ebaa7c5f6f688959e8d40840b2249ede63b8ed ]
    
    When one of the three connection complete events is received multiple
    times for the same handle, the device is registered multiple times which
    leads to memory corruptions. Therefore, consequent events for a single
    connection are ignored.
    
    The conn->state can hold different values, therefore HCI_CONN_HANDLE_UNSET
    is introduced to identify new connections. To make sure the events do not
    contain this or another invalid handle HCI_CONN_HANDLE_MAX and checks
    are introduced.
    
    Buglink: https://bugzilla.kernel.org/show_bug.cgi?id=215497
    Signed-off-by: Soenke Huster <soenke.huster@eknoes.de>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -691,6 +691,7 @@ struct hci_conn *hci_conn_add(struct hci_dev *hdev, int type, bdaddr_t *dst,
 
 	bacpy(&conn->dst, dst);
 	bacpy(&conn->src, &hdev->bdaddr);
+	conn->handle = HCI_CONN_HANDLE_UNSET;
 	conn->hdev  = hdev;
 	conn->type  = type;
 	conn->role  = role;
