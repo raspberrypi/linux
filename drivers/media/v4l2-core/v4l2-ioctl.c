70d04fb67329c310f0acb6847fe28aeae2824c7f
Naushir Patuck <naush@raspberrypi.com>
2022-02-07 09:20:49 +0000

    V4L2: Add PiSP opaque formats to V4L2
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1458,7 +1458,9 @@ static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 	case V4L2_PIX_FMT_Y216:		descr = "16-bit YUYV Packed"; break;
 	case V4L2_META_FMT_SENSOR_DATA:	descr = "Sensor Ancillary Metadata"; break;
 	case V4L2_META_FMT_BCM2835_ISP_STATS: descr = "BCM2835 ISP Image Statistics"; break;
-	case V4L2_META_FMT_RPI_BE_CFG: descr = "PiSP Config format"; break;
+	case V4L2_META_FMT_RPI_BE_CFG: descr = "PiSP BE Config format"; break;
+	case V4L2_META_FMT_RPI_FE_CFG: descr = "PiSP FE Config format"; break;
+	case V4L2_META_FMT_RPI_FE_STATS: descr = "PiSP FE Statistics format"; break;
 
 	default:
 		/* Compressed formats */
