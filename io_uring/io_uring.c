e944f1e37b971cf96216fd842e1b0399d541a5d8
Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
2022-02-07 21:05:33 +0700

    io_uring: Clean up a false-positive warning from GCC 9.3.0
    commit 0d7c1153d9291197c1dc473cfaade77acb874b4b upstream.
    
    In io_recv(), if import_single_range() fails, the @flags variable is
    uninitialized, then it will goto out_free.
    
    After the goto, the compiler doesn't know that (ret < min_ret) is
    always true, so it thinks the "if ((flags & MSG_WAITALL) ..."  path
    could be taken.
    
    The complaint comes from gcc-9 (Debian 9.3.0-22) 9.3.0:
    ```
      fs/io_uring.c:5238 io_recvfrom() error: uninitialized symbol 'flags'
    ```
    Fix this by bypassing the @ret and @flags check when
    import_single_range() fails.
    
    Reasons:
     1. import_single_range() only returns -EFAULT when it fails.
     2. At that point, @flags is uninitialized and shouldn't be read.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reported-by: "Chen, Rong A" <rong.a.chen@intel.com>
    Link: https://lore.gnuweeb.org/timl/d33bb5a9-8173-f65b-f653-51fc0681c6d6@intel.com/
    Cc: Pavel Begunkov <asml.silence@gmail.com>
    Suggested-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Fixes: 7297ce3d59449de49d3c9e1f64ae25488750a1fc ("io_uring: improve send/recv error handling")
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gmail.com>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Link: https://lore.kernel.org/r/20220207140533.565411-1-ammarfaizi2@gnuweeb.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -5240,7 +5240,6 @@ static int io_recv(struct io_kiocb *req, unsigned int issue_flags)
 		min_ret = iov_iter_count(&msg.msg_iter);
 
 	ret = sock_recvmsg(sock, &msg, flags);
-out_free:
 	if (ret < min_ret) {
 		if (ret == -EAGAIN && force_nonblock)
 			return -EAGAIN;
@@ -5255,6 +5254,7 @@ out_free:
 		}
 		req_set_fail(req);
 	} else if ((flags & MSG_WAITALL) && (msg.msg_flags & (MSG_TRUNC | MSG_CTRUNC))) {
+out_free:
 		req_set_fail(req);
 	}
 	if (req->flags & REQ_F_BUFFER_SELECTED)
