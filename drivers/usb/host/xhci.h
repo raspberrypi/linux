83b4dc6c84b38b9f7f360f69083e459485d2eade
Jonathan Bell <jonathan@raspberrypi.com>
2022-02-16 14:31:02 +0000

    usb: xhci: add a quirk for Superspeed bulk OUT transfers on VL805
    The VL805 has a bug in its internal FIFO space accounting that results
    in bulk OUT babble if a TRB in a large multi-element TD has a data
    buffer size that is larger than and not a multiple of wMaxPacketSize for
    the endpoint. If the downstream USB3.0 link is congested, or latency is
    increased through an intermediate hub, then the VL805 enters a suspected
    FIFO overflow condition and transmits repeated, garbled data to the
    endpoint.
    
    TDs with TRBs of exact multiples of wMaxPacketSize and smaller than
    wMaxPacketSize appear to be handled correctly, so split buffers at a
    1024-byte length boundary and put the remainder in a separate smaller
    TRB.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1910,6 +1910,7 @@ struct xhci_hcd {
 #define XHCI_ZHAOXIN_HOST	BIT_ULL(46)
 #define XHCI_AVOID_DQ_ON_LINK	BIT_ULL(47)
 #define XHCI_VLI_TRB_CACHE_BUG	BIT_ULL(48)
+#define XHCI_VLI_SS_BULK_OUT_BUG	BIT_ULL(49)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
