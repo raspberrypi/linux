d23a902a9684236527508ecb4970390f1c4ebef9
Dave Stevenson <dave.stevenson@raspberrypi.com>
2022-02-23 15:36:56 +0000

    drm/panel: Add prepare_upstream_first flag to drm_panel
    Mapping to the drm_bridge flag pre_enable_upstream_first,
    add a new flag prepare_upstream_first to drm_panel to allow
    the panel driver to request that the upstream bridge should
    be pre_enabled before the panel prepare.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- include/drm/drm_panel.h
+++ include/drm/drm_panel.h
@@ -179,6 +179,16 @@ struct drm_panel {
 	 * Panel entry in registry.
 	 */
 	struct list_head list;
+
+	/**
+	 * @prepare_upstream_first:
+	 *
+	 * The upstream controller should be prepared first, before the prepare
+	 * for the panel is called. This is largely required for DSI panels
+	 * where the DSI host controller should be initialised to LP-11 before
+	 * the panel is powered up.
+	 */
+	bool prepare_upstream_first;
 };
 
 void drm_panel_init(struct drm_panel *panel, struct device *dev,
