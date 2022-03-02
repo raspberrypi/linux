b34dd3a287f32b920159acd82c894da692875388
Martin K. Petersen <martin.petersen@oracle.com>
2022-03-02 00:35:57 -0500

    scsi: sd: sd_read_cpr() requires VPD pages
    [ Upstream commit 1700714b1ff252b634db21186db4d91e7e006043 ]
    
    As such it should be called inside the scsi_device_supports_vpd()
    conditional.
    
    Link: https://lore.kernel.org/r/20220302053559.32147-13-martin.petersen@oracle.com
    Fixes: e815d36548f0 ("scsi: sd: add concurrent positioning ranges support")
    Cc: Damien Le Moal <damien.lemoal@wdc.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>
    Reviewed-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3320,6 +3320,7 @@ static int sd_revalidate_disk(struct gendisk *disk)
 			sd_read_block_limits(sdkp);
 			sd_read_block_characteristics(sdkp);
 			sd_zbc_read_zones(sdkp, buffer);
+			sd_read_cpr(sdkp);
 		}
 
 		sd_print_capacity(sdkp, old_capacity);
@@ -3329,7 +3330,6 @@ static int sd_revalidate_disk(struct gendisk *disk)
 		sd_read_app_tag_own(sdkp, buffer);
 		sd_read_write_same(sdkp, buffer);
 		sd_read_security(sdkp, buffer);
-		sd_read_cpr(sdkp);
 	}
 
 	/*
