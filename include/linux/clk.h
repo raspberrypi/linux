9fed0f8ed34d1bb43f5a60deff5777f2ff1e5e4e
Maxime Ripard <maxime@cerno.tech>
2022-03-16 09:41:49 +0100

    Revert "clk: Introduce a clock request API"
    This reverts commit 23fbabe348436e250deca7c5f2fd0caf620af174.

--- include/linux/clk.h
+++ include/linux/clk.h
@@ -15,7 +15,6 @@
 
 struct device;
 struct clk;
-struct clk_request;
 struct device_node;
 struct of_phandle_args;
 
@@ -784,9 +783,6 @@ int clk_save_context(void);
  * so locking is not necessary.
  */
 void clk_restore_context(void);
-
-struct clk_request *clk_request_start(struct clk *clk, unsigned long rate);
-void clk_request_done(struct clk_request *req);
 
 #else /* !CONFIG_HAVE_CLK */
 
