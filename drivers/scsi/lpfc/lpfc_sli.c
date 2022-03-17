66cd3f99e7ca2e98edbab030877650bf689e363b
James Smart <jsmart2021@gmail.com>
2022-03-16 20:27:34 -0700

    scsi: lpfc: Improve PCI EEH Error and Recovery Handling
    [ Upstream commit 35ed9613d83f3c1f011877d591fd7d36f2666106 ]
    
    Following EEH errors, the driver can crash or hang when deleting the
    localport or when attempting to unload.
    
    The EEH handlers in the driver did not notify the NVMe-FC transport before
    tearing the driver down. This was delayed until the resume steps. This
    worked for SCSI because lpfc_block_scsi() would notify the
    scsi_fc_transport that the target was not available but it would not clean
    up all the references to the ndlp.
    
    The SLI3 prep for dev reset handler did the lpfc_offline_prep() and
    lpfc_offline() calls to get the port stopped before restarting. The SLI4
    version of the prep for dev reset just destroyed the queues and did not
    stop NVMe from continuing.  Also because the port was not really stopped
    the localport destroy would hang because the transport was still waiting
    for I/O. Additionally, a devloss tmo can fire and post events to a stopped
    worker thread creating another hang condition.
    
    lpfc_sli4_prep_dev_for_reset() is modified to call lpfc_offline_prep() and
    lpfc_offline() rather than just lpfc_scsi_dev_block() to ensure both SCSI
    and NVMe transports are notified to block I/O to the driver.
    
    Logic is added to devloss handler and worker thread to clean up ndlp
    references and quiesce appropriately.
    
    Link: https://lore.kernel.org/r/20220317032737.45308-2-jsmart2021@gmail.com
    Co-developed-by: Justin Tee <justin.tee@broadcom.com>
    Signed-off-by: Justin Tee <justin.tee@broadcom.com>
    Signed-off-by: James Smart <jsmart2021@gmail.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/lpfc/lpfc_sli.c
+++ drivers/scsi/lpfc/lpfc_sli.c
@@ -2833,6 +2833,12 @@ __lpfc_sli_rpi_release(struct lpfc_vport *vport, struct lpfc_nodelist *ndlp)
 	ndlp->nlp_flag &= ~NLP_UNREG_INP;
 }
 
+void
+lpfc_sli_rpi_release(struct lpfc_vport *vport, struct lpfc_nodelist *ndlp)
+{
+	__lpfc_sli_rpi_release(vport, ndlp);
+}
+
 /**
  * lpfc_sli_def_mbox_cmpl - Default mailbox completion handler
  * @phba: Pointer to HBA context object.
@@ -4554,11 +4560,6 @@ lpfc_sli_flush_io_rings(struct lpfc_hba *phba)
 	struct lpfc_iocbq *piocb, *next_iocb;
 
 	spin_lock_irq(&phba->hbalock);
-	if (phba->hba_flag & HBA_IOQ_FLUSH ||
-	    !phba->sli4_hba.hdwq) {
-		spin_unlock_irq(&phba->hbalock);
-		return;
-	}
 	/* Indicate the I/O queues are flushed */
 	phba->hba_flag |= HBA_IOQ_FLUSH;
 	spin_unlock_irq(&phba->hbalock);
@@ -11234,6 +11235,10 @@ lpfc_sli_issue_iocb(struct lpfc_hba *phba, uint32_t ring_number,
 	struct lpfc_queue *eq;
 	unsigned long iflags;
 	int rc;
+
+	/* If the PCI channel is in offline state, do not post iocbs. */
+	if (unlikely(pci_channel_offline(phba->pcidev)))
+		return IOCB_ERROR;
 
 	if (phba->sli_rev == LPFC_SLI_REV4) {
 		eq = phba->sli4_hba.hdwq[piocb->hba_wqidx].hba_eq;
