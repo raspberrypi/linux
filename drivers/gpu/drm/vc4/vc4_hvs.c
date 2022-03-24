78a2f6ecd4dcd5783a11750182616ec762ad4fc3
Maxime Ripard <maxime@cerno.tech>
2022-03-24 11:57:57 +0100

    drm/vc4: hdmi: Rework hdmi_enable_4kp60 detection
    In order to support higher HDMI frequencies, users have to set the
    hdmi_enable_4kp60 parameter in their config.txt file.
    
    We were detecting this so far by calling clk_round_rate on the core
    clock with the frequency we're supposed to run at when one of those
    modes is enabled. Whether or not the parameter was enabled could then be
    inferred by the returned rate since the maximum clock rate reported by
    the firmware was one of the side effect of setting that parameter.
    
    However, the recent clock rework we did changed what clk_round_rate was
    returning to always return the minimum allowed, and thus this test
    wasn't reliable anymore.
    
    Let's instead try to set a minimum on that clock for the rate we'd like
    to reach. If the maximum reported by the firmware is below the minimum
    we're trying to set, the clock framework will return an error which we
    then can use to infer whether the parameter is set or not.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hvs.c
+++ drivers/gpu/drm/vc4/vc4_hvs.c
@@ -897,11 +897,17 @@ static int vc4_hvs_bind(struct device *dev, struct device *master, void *data)
 	hvs->regset.nregs = ARRAY_SIZE(hvs_regs);
 
 	if (hvs->hvs5) {
+		unsigned long max_rate;
+
 		hvs->core_clk = devm_clk_get(&pdev->dev, NULL);
 		if (IS_ERR(hvs->core_clk)) {
 			dev_err(&pdev->dev, "Couldn't get core clock\n");
 			return PTR_ERR(hvs->core_clk);
 		}
+
+		max_rate = clk_get_max_rate(hvs->core_clk);
+		if (max_rate >= 550000000)
+			hvs->vc5_hdmi_enable_scrambling = true;
 
 		ret = clk_prepare_enable(hvs->core_clk);
 		if (ret) {
