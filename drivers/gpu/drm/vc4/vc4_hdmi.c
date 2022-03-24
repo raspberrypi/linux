15acfbaf70d7f4b371f2a3fe85e47d5988264217
Maxime Ripard <maxime@cerno.tech>
2022-03-24 11:57:57 +0100

    drm/vc4: hdmi: Rework hdmi_enable_4kp60 detection
    In order to support higher HDMI frequencies, users have to set the
    hdmi_enable_4kp60 parameter in their config.txt file.
    
    We were detecting this so far by calling clk_round_rate on the core
    clock with the frequency we're supposed to run at when one of those
    modes is enabled. Whether or not the parameter was enabled could then be
    inferred by the returned rate since the maximum clock rate reported by
    the firmware was one of the side effect of setting that parameter.
    
    However, the recent clock rework we did changed what clk_round_rate was
    returning to always return the minimum allowed, and thus this test
    wasn't reliable anymore.
    
    Let's instead try to set a minimum on that clock for the rate we'd like
    to reach. If the maximum reported by the firmware is below the minimum
    we're trying to set, the clock framework will return an error which we
    then can use to infer whether the parameter is set or not.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -291,6 +291,7 @@ static int vc4_hdmi_connector_get_modes(struct drm_connector *connector)
 {
 	struct vc4_hdmi *vc4_hdmi = connector_to_vc4_hdmi(connector);
 	struct vc4_hdmi_encoder *vc4_encoder = &vc4_hdmi->encoder;
+	struct vc4_dev *vc4 = to_vc4_dev(connector->dev);
 	int ret = 0;
 	struct edid *edid;
 
@@ -309,7 +310,7 @@ static int vc4_hdmi_connector_get_modes(struct drm_connector *connector)
 	ret = drm_add_edid_modes(connector, edid);
 	kfree(edid);
 
-	if (vc4_hdmi->disable_4kp60) {
+	if (!vc4->hvs->vc5_hdmi_enable_scrambling) {
 		struct drm_device *drm = connector->dev;
 		struct drm_display_mode *mode;
 
@@ -1671,11 +1672,12 @@ vc4_hdmi_encoder_clock_valid(const struct vc4_hdmi *vc4_hdmi,
 {
 	const struct drm_connector *connector = &vc4_hdmi->connector;
 	const struct drm_display_info *info = &connector->display_info;
+	struct vc4_dev *vc4 = to_vc4_dev(connector->dev);
 
 	if (clock > vc4_hdmi->variant->max_pixel_clock)
 		return MODE_CLOCK_HIGH;
 
-	if (vc4_hdmi->disable_4kp60 && clock > HDMI_14_MAX_TMDS_CLK)
+	if (!vc4->hvs->vc5_hdmi_enable_scrambling && clock > HDMI_14_MAX_TMDS_CLK)
 		return MODE_CLOCK_HIGH;
 
 	if (info->max_tmds_clock && clock > (info->max_tmds_clock * 1000))
@@ -3156,14 +3158,6 @@ static int vc4_hdmi_bind(struct device *dev, struct device *master, void *data)
 
 	vc4_hdmi->disable_wifi_frequencies =
 		of_property_read_bool(dev->of_node, "wifi-2.4ghz-coexistence");
-
-	if (variant->max_pixel_clock == 600000000) {
-		struct vc4_dev *vc4 = to_vc4_dev(drm);
-		long max_rate = clk_round_rate(vc4->hvs->core_clk, 550000000);
-
-		if (max_rate < 550000000)
-			vc4_hdmi->disable_4kp60 = true;
-	}
 
 	/*
 	 * If we boot without any cable connected to the HDMI connector,
