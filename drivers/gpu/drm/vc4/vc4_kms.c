2cecf3ce54e3a7d81f325d1094e4eaf21cc621c5
Maxime Ripard <maxime@cerno.tech>
2022-03-31 16:37:38 +0200

    drm/vc4: kms: Take old state core clock rate into account
    [ Upstream commit 748acfc98adab21a93ae7a1b5bed0f048463e873 ]
    
    During a commit, the core clock, which feeds the HVS, needs to run at
    a minimum of 500MHz.
    
    While doing that commit, we can also change the mode to one that
    requires a higher core clock, so we take the core clock rate associated
    to that new state into account for that boost.
    
    However, the old state also needs to be taken into account if it
    requires a core clock higher that the new one and our 500MHz limit,
    since it's still live in hardware at the beginning of our commit.
    
    Fixes: 16e101051f32 ("drm/vc4: Increase the core clock based on HVS load")
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Link: https://lore.kernel.org/r/20220331143744.777652-2-maxime@cerno.tech
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -385,9 +385,10 @@ static void vc4_atomic_commit_tail(struct drm_atomic_state *state)
 	}
 
 	if (vc4->hvs->hvs5) {
+		unsigned long state_rate = max(old_hvs_state->core_clock_rate,
+					       new_hvs_state->core_clock_rate);
 		unsigned long core_rate = max_t(unsigned long,
-						500000000,
-						new_hvs_state->core_clock_rate);
+						500000000, state_rate);
 
 		clk_set_min_rate(hvs->core_clk, core_rate);
 	}
