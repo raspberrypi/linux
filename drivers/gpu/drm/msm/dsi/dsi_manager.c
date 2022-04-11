c0ab974d50b8c2496208a542b2c35f5ec56bd452
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2022-04-12 02:49:53 +0300

    drm/msm: properly add and remove internal bridges
    [ Upstream commit d28ea556267c4f2ec7264ab49f1b1296834321ec ]
    
    Add calls to drm_bridge_add()/drm_bridge_remove() DRM bridges created by
    the driver. This fixes the following warning.
    
    WARNING: CPU: 0 PID: 1 at kernel/locking/mutex.c:579 __mutex_lock+0x840/0x9f4
    DEBUG_LOCKS_WARN_ON(lock->magic != lock)
    Modules linked in:
    CPU: 0 PID: 1 Comm: swapper/0 Not tainted 5.18.0-rc1-00002-g3054695a0d27-dirty #55
    Hardware name: Generic DT based system
     unwind_backtrace from show_stack+0x10/0x14
     show_stack from dump_stack_lvl+0x58/0x70
     dump_stack_lvl from __warn+0xc8/0x1e8
     __warn from warn_slowpath_fmt+0x78/0xa8
     warn_slowpath_fmt from __mutex_lock+0x840/0x9f4
     __mutex_lock from mutex_lock_nested+0x1c/0x24
     mutex_lock_nested from drm_bridge_hpd_enable+0x2c/0x84
     drm_bridge_hpd_enable from msm_hdmi_modeset_init+0xc0/0x21c
     msm_hdmi_modeset_init from mdp4_kms_init+0x53c/0x90c
     mdp4_kms_init from msm_drm_bind+0x514/0x698
     msm_drm_bind from try_to_bring_up_aggregate_device+0x160/0x1bc
     try_to_bring_up_aggregate_device from component_master_add_with_match+0xc4/0xf8
     component_master_add_with_match from msm_pdev_probe+0x274/0x350
     msm_pdev_probe from platform_probe+0x5c/0xbc
     platform_probe from really_probe.part.0+0x9c/0x290
     really_probe.part.0 from __driver_probe_device+0xa8/0x13c
     __driver_probe_device from driver_probe_device+0x34/0x10c
     driver_probe_device from __driver_attach+0xbc/0x178
     __driver_attach from bus_for_each_dev+0x74/0xc0
     bus_for_each_dev from bus_add_driver+0x160/0x1e4
     bus_add_driver from driver_register+0x88/0x118
     driver_register from do_one_initcall+0x6c/0x334
     do_one_initcall from kernel_init_freeable+0x1bc/0x220
     kernel_init_freeable from kernel_init+0x18/0x12c
     kernel_init from ret_from_fork+0x14/0x2c
    
    Fixes: 3d3f8b1f8b62 ("drm/bridge: make bridge registration independent of drm flow")
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Patchwork: https://patchwork.freedesktop.org/patch/481778/
    Link: https://lore.kernel.org/r/20220411234953.2425280-1-dmitry.baryshkov@linaro.org
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/msm/dsi/dsi_manager.c
+++ drivers/gpu/drm/msm/dsi/dsi_manager.c
@@ -668,6 +668,8 @@ struct drm_bridge *msm_dsi_manager_bridge_init(u8 id)
 	bridge = &dsi_bridge->base;
 	bridge->funcs = &dsi_mgr_bridge_funcs;
 
+	drm_bridge_add(bridge);
+
 	ret = drm_bridge_attach(encoder, bridge, NULL, 0);
 	if (ret)
 		goto fail;
@@ -738,6 +740,7 @@ struct drm_connector *msm_dsi_manager_ext_bridge_init(u8 id)
 
 void msm_dsi_manager_bridge_destroy(struct drm_bridge *bridge)
 {
+	drm_bridge_remove(bridge);
 }
 
 int msm_dsi_manager_cmd_xfer(int id, const struct mipi_dsi_msg *msg)
