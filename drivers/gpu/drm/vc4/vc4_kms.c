a7009590db1e84d951e1255133bffffe3c7a83df
Dom Cobley <popcornmix@gmail.com>
2022-04-27 14:45:35 +0100

    drm/vc4_kms: Protect hvs dereference on fkms
    fkms has a null hvs pointer so this crashes. Protect against it
    
    Signed-off-by: Dom Cobley <popcornmix@gmail.com>

--- drivers/gpu/drm/vc4/vc4_kms.c
+++ drivers/gpu/drm/vc4/vc4_kms.c
@@ -359,7 +359,7 @@ static void vc4_atomic_commit_tail(struct drm_atomic_state *state)
 	struct vc4_hvs_state *new_hvs_state;
 	struct drm_crtc *crtc;
 	struct vc4_hvs_state *old_hvs_state;
-	unsigned long max_clock_rate = rpi_firmware_clk_get_max_rate(hvs->core_clk);
+	unsigned long max_clock_rate = hvs ? rpi_firmware_clk_get_max_rate(hvs->core_clk): 0;
 	unsigned int channel;
 	int i;
 
