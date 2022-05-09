837c8c6f623b8991962f148ceb316c5b840e50a7
Wan Jiabing <wanjiabing@vivo.com>
2022-05-09 22:45:19 +0800

    net: phy: micrel: Fix incorrect variable type in micrel
    commit 12a4d677b1c34717443470c1492fe520638ef39a upstream.
    
    In lanphy_read_page_reg, calling __phy_read() might return a negative
    error code. Use 'int' to check the error code.
    
    Fixes: 7c2dcfa295b1 ("net: phy: micrel: Add support for LAN8804 PHY")
    Signed-off-by: Wan Jiabing <wanjiabing@vivo.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Link: https://lore.kernel.org/r/20220509144519.2343399-1-wanjiabing@vivo.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/phy/micrel.c
+++ drivers/net/phy/micrel.c
@@ -1594,7 +1594,7 @@ static int ksz886x_cable_test_get_status(struct phy_device *phydev,
 
 static int lanphy_read_page_reg(struct phy_device *phydev, int page, u32 addr)
 {
-	u32 data;
+	int data;
 
 	phy_lock_mdio_bus(phydev);
 	__phy_write(phydev, LAN_EXT_PAGE_ACCESS_CONTROL, page);
