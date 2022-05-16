875bdf9779811cf60e977aa2e7f0136169655eaa
Maxime Ripard <maxime@cerno.tech>
2022-05-16 15:25:12 +0200

    clk: Take into account uncached clocks in clk_set_rate_range()
    clk_set_rate_range() will use the last requested rate for the clock when
    it calls into the driver set_rate hook.
    
    However, if CLK_GET_RATE_NOCACHE is set on that clock, the last
    requested rate might not be matching the current rate of the clock. In
    such a case, let's read out the rate from the hardware and use that in
    our set_rate instead.
    
    Tested-by: Alexander Stein <alexander.stein@ew.tq-group.com> # imx8mp
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com> # exynos4210, meson g12b
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/clk/clk_test.c
+++ drivers/clk/clk_test.c
@@ -362,9 +362,37 @@ static void clk_test_uncached_set_range(struct kunit *test)
 	KUNIT_EXPECT_LE(test, rate, DUMMY_CLOCK_RATE_2);
 }
 
+/*
+ * Test that for an uncached clock, clk_set_rate_range() will work
+ * properly if the rate has changed in hardware.
+ *
+ * In this case, it means that if the rate wasn't initially in the range
+ * we're trying to set, but got changed at some point into the range
+ * without the kernel knowing about it, its rate shouldn't be affected.
+ */
+static void clk_test_uncached_updated_rate_set_range(struct kunit *test)
+{
+	struct clk_dummy_context *ctx = test->priv;
+	struct clk_hw *hw = &ctx->hw;
+	struct clk *clk = hw->clk;
+	unsigned long rate;
+
+	ctx->rate = DUMMY_CLOCK_RATE_1 + 1000;
+	KUNIT_ASSERT_EQ(test,
+			clk_set_rate_range(clk,
+					   DUMMY_CLOCK_RATE_1,
+					   DUMMY_CLOCK_RATE_2),
+			0);
+
+	rate = clk_get_rate(clk);
+	KUNIT_ASSERT_GT(test, rate, 0);
+	KUNIT_EXPECT_EQ(test, rate, DUMMY_CLOCK_RATE_1 + 1000);
+}
+
 static struct kunit_case clk_uncached_test_cases[] = {
 	KUNIT_CASE(clk_test_uncached_get_rate),
 	KUNIT_CASE(clk_test_uncached_set_range),
+	KUNIT_CASE(clk_test_uncached_updated_rate_set_range),
 	{}
 };
 
