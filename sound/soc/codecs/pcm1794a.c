748179b8830885e2f6d43d5da1cc33032d07dd86
Miquel <miquelblauw@hotmail.com>
2022-05-17 09:42:27 +0200

    Update pcm1794a.c

--- sound/soc/codecs/pcm1794a.c
+++ sound/soc/codecs/pcm1794a.c
@@ -28,8 +28,7 @@ static struct snd_soc_dai_driver pcm1794a_dai = {
 		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_8000_384000,
 		.formats = SNDRV_PCM_FMTBIT_S16_LE |
-			   SNDRV_PCM_FMTBIT_S24_LE |
-			   SNDRV_PCM_FMTBIT_S32_LE
+			   SNDRV_PCM_FMTBIT_S24_LE
 	},
 };
 
