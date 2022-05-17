1202d8f826a57a0958a05d132b38ea3597651675
Miquel <miquelblauw@hotmail.com>
2022-05-17 11:51:49 +0200

    Update dionaudio_kiwi.c

--- sound/soc/bcm/dionaudio_kiwi.c
+++ sound/soc/bcm/dionaudio_kiwi.c
@@ -1,10 +1,8 @@
 /*
- * ASoC Driver for Dion Audio Kiwi Streamer.
+ * ASoC Driver for Dion Audio Kiwi Streamer
  *
  * Author:      Miquel Blauw <info@dionaudio.nl>
  *              Copyright 2022
- *
- * Based on the software of the RPi-DAC writen by Florian Meier
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -25,18 +23,16 @@
 #include <sound/soc.h>
 #include <sound/jack.h>
 
-static int snd_rpi_dionaudio_kiwi_init(struct snd_soc_pcm_runtime *rtd)
-{
-	return 0;
-}
-
-static int snd_rpi_dionaudio_kiwi_hw_params(struct snd_pcm_substream *substream,
-				       struct snd_pcm_hw_params *params)
+static int snd_rpi_dionaudio_kiwi_hw_params(
+	struct snd_pcm_substream *substream, struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+	struct snd_soc_dai *cpu_dai = asoc_rtd_to_cpu(rtd, 0);
 
-	return snd_soc_dai_set_bclk_ratio(cpu_dai, 32*2);
+	unsigned int sample_bits =
+		snd_pcm_format_physical_width(params_format(params));
+
+	return snd_soc_dai_set_bclk_ratio(cpu_dai, sample_bits * 2);
 }
 
 /* machine stream operations */
@@ -44,77 +40,76 @@ static struct snd_soc_ops snd_rpi_dionaudio_kiwi_ops = {
 	.hw_params = snd_rpi_dionaudio_kiwi_hw_params,
 };
 
+SND_SOC_DAILINK_DEFS(dionaudio_kiwi,
+	DAILINK_COMP_ARRAY(COMP_CPU("bcm2708-i2s.0")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("pcm1794a-codec", "pcm1794a-hifi")),
+	DAILINK_COMP_ARRAY(COMP_PLATFORM("bcm2708-i2s.0")));
+
 static struct snd_soc_dai_link snd_rpi_dionaudio_kiwi_dai[] = {
 {
-	.name		= "DIONAUDIO-KIWI",
-	.stream_name	= "DIONAUDIO-KIWI HiFi",
-	.cpu_dai_name	= "bcm2708-i2s.0",
-	.codec_dai_name	= "pcm1794a-hifi",
-	.platform_name	= "bcm2708-i2s.0",
-	.codec_name	= "pcm1794a-codec",
-	.dai_fmt	= SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
-				SND_SOC_DAIFMT_CBS_CFS,
+	.name		= "DionAudio KIWI",
+	.stream_name	= "DionAudio KIWI STREAMER",
+	.dai_fmt	= SND_SOC_DAIFMT_I2S |
+			  SND_SOC_DAIFMT_NB_NF |
+			  SND_SOC_DAIFMT_CBS_CFS,
 	.ops		= &snd_rpi_dionaudio_kiwi_ops,
-	.init		= snd_rpi_dionaudio_kiwi_init,
+	SND_SOC_DAILINK_REG(dionaudio_kiwi),
 },
 };
 
 /* audio machine driver */
 static struct snd_soc_card snd_rpi_dionaudio_kiwi = {
-	.name         = "snd_rpi_dionaudio_kiwi",
-	.dai_link     = snd_rpi_dionaudio_kiwi_dai,
-	.num_links    = ARRAY_SIZE(snd_rpi_dionaudio_kiwi_dai),
+	.name		= "snd_rpi_dionaudio_kiwi",
+	.dai_link	= snd_rpi_dionaudio_kiwi_dai,
+	.num_links	= ARRAY_SIZE(snd_rpi_dionaudio_kiwi_dai),
 };
 
 static int snd_rpi_dionaudio_kiwi_probe(struct platform_device *pdev)
 {
+	struct device_node *np;
 	int ret = 0;
 
 	snd_rpi_dionaudio_kiwi.dev = &pdev->dev;
-	
-	if (pdev->dev.of_node) {
-		struct device_node *i2s_node;
-		struct snd_soc_dai_link *dai = &snd_rpi_dionaudio_kiwi_dai[0];
-		i2s_node = of_parse_phandle(pdev->dev.of_node, "i2s-controller", 0);
 
-		if (i2s_node) {
-			dai->cpu_dai_name = NULL;
-			dai->cpu_of_node = i2s_node;
-			dai->platform_name = NULL;
-			dai->platform_of_node = i2s_node;
+	np = pdev->dev.of_node;
+	if (np) {
+		struct snd_soc_dai_link *dai = &snd_rpi_dionaudio_kiwi_dai[0];
+		struct device_node *i2s_np;
+
+		i2s_np = of_parse_phandle(np, "i2s-controller", 0);
+		if (i2s_np) {
+			dai->cpus->dai_name = NULL;
+			dai->cpus->of_node = i2s_np;
+			dai->platforms->name = NULL;
+			dai->platforms->of_node = i2s_np;
 		}
 	}
-	
-	ret = snd_soc_register_card(&snd_rpi_dionaudio_kiwi);
-	if (ret)
-		dev_err(&pdev->dev, "snd_soc_register_card() failed: %d\n", ret);
+
+	ret = devm_snd_soc_register_card(&pdev->dev, &snd_rpi_dionaudio_kiwi);
+	if (ret && ret != -EPROBE_DEFER)
+		dev_err(&pdev->dev, "snd_soc_register_card() failed: %d\n",
+			ret);
 
 	return ret;
 }
 
-static int snd_rpi_dionaudio_kiwi_remove(struct platform_device *pdev)
-{
-	return snd_soc_unregister_card(&snd_rpi_dionaudio_kiwi);
-}
-
 static const struct of_device_id snd_rpi_dionaudio_kiwi_of_match[] = {
-	{ .compatible = "dionaudio,dionaudio-kiwi", },
-	{},
+	{ .compatible = "dionaudio,kiwi-pcm1794", },
+	{ /* sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, snd_rpi_dionaudio_kiwi_of_match);
 
 static struct platform_driver snd_rpi_dionaudio_kiwi_driver = {
-        .driver = {
-                .name   = "snd-dionaudio-kiwi",
-                .owner  = THIS_MODULE,
-                .of_match_table = snd_rpi_dionaudio_kiwi_of_match,
-        },
-        .probe          = snd_rpi_dionaudio_kiwi_probe,
-        .remove         = snd_rpi_dionaudio_kiwi_remove,
+	.driver = {
+		.name		= "snd-dionaudio-kiwi",
+		.owner		= THIS_MODULE,
+		.of_match_table	= snd_rpi_dionaudio_kiwi_of_match,
+	},
+	.probe  = snd_rpi_dionaudio_kiwi_probe,
 };
 
 module_platform_driver(snd_rpi_dionaudio_kiwi_driver);
 
-MODULE_AUTHOR("Florian Meier <florian.meier@koalo.de>");
-MODULE_DESCRIPTION("ASoC Driver for DionAudio-KIWI");
+MODULE_AUTHOR("Miquel Blauw <info@dionaudio.nl>");
+MODULE_DESCRIPTION("ASoC Driver for DionAudio KIWI");
 MODULE_LICENSE("GPL v2");
