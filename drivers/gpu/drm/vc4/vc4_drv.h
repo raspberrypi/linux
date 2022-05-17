683e5d52d79b764bfc74e1d1c0d4d7e4bf403768
Dom Cobley <popcornmix@gmail.com>
2022-05-17 12:46:42 +0100

    kms/vc4_hdmi: Refuse 4096x2160@60 hdmi modes
    These are no reliable without overclocking.
    See: https://github.com/raspberrypi/linux/issues/5034
    
    Signed-off-by: Dom Cobley <popcornmix@gmail.com>

--- drivers/gpu/drm/vc4/vc4_drv.h
+++ drivers/gpu/drm/vc4/vc4_drv.h
@@ -344,6 +344,12 @@ struct vc4_hvs {
 	struct drm_mm_node mitchell_netravali_filter;
 
 	struct debugfs_regset32 regset;
+
+	/*
+	 * 4096x2160@60 requires a core overclock to work, so register
+	 * whether that is sufficient.
+	 */
+	bool vc5_hdmi_enable_4096by2160;
 };
 
 struct vc4_plane {
