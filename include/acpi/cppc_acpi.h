2783414e6ef725bac946dc5d4d9288e34b6f5a13
Mario Limonciello <mario.limonciello@amd.com>
2022-07-05 13:29:15 -0500

    ACPI: CPPC: Don't require _OSC if X86_FEATURE_CPPC is supported
    [ Upstream commit 8b356e536e69f3a4d6778ae9f0858a1beadabb1f ]
    
    commit 72f2ecb7ece7 ("ACPI: bus: Set CPPC _OSC bits for all and
    when CPPC_LIB is supported") added support for claiming to
    support CPPC in _OSC on non-Intel platforms.
    
    This unfortunately caused a regression on a vartiety of AMD
    platforms in the field because a number of AMD platforms don't set
    the `_OSC` bit 5 or 6 to indicate CPPC or CPPC v2 support.
    
    As these AMD platforms already claim CPPC support via a dedicated
    MSR from `X86_FEATURE_CPPC`, use this enable this feature rather
    than requiring the `_OSC` on platforms with a dedicated MSR.
    
    If there is additional breakage on the shared memory designs also
    missing this _OSC, additional follow up changes may be needed.
    
    Fixes: 72f2ecb7ece7 ("Set CPPC _OSC bits for all and when CPPC_LIB is supported")
    Reported-by: Perry Yuan <perry.yuan@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/acpi/cppc_acpi.h
+++ include/acpi/cppc_acpi.h
@@ -144,6 +144,7 @@ extern bool acpi_cpc_valid(void);
 extern int acpi_get_psd_map(unsigned int cpu, struct cppc_cpudata *cpu_data);
 extern unsigned int cppc_get_transition_latency(int cpu);
 extern bool cpc_ffh_supported(void);
+extern bool cpc_supported_by_cpu(void);
 extern int cpc_read_ffh(int cpunum, struct cpc_reg *reg, u64 *val);
 extern int cpc_write_ffh(int cpunum, struct cpc_reg *reg, u64 val);
 #else /* !CONFIG_ACPI_CPPC_LIB */
