48fda9af1df971076416bace9bfbb5d6d89ba6c1
Kuniyuki Iwashima <kuniyu@amazon.com>
2022-07-11 17:15:24 -0700

    icmp: Fix a data-race around sysctl_icmp_echo_ignore_broadcasts.
    [ Upstream commit 66484bb98ed2dfa1dda37a32411483d8311ac269 ]
    
    While reading sysctl_icmp_echo_ignore_broadcasts, it can be changed
    concurrently.  Thus, we need to add READ_ONCE() to its reader.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/icmp.c
+++ net/ipv4/icmp.c
@@ -1239,7 +1239,7 @@ int icmp_rcv(struct sk_buff *skb)
 		 */
 		if ((icmph->type == ICMP_ECHO ||
 		     icmph->type == ICMP_TIMESTAMP) &&
-		    net->ipv4.sysctl_icmp_echo_ignore_broadcasts) {
+		    READ_ONCE(net->ipv4.sysctl_icmp_echo_ignore_broadcasts)) {
 			goto error;
 		}
 		if (icmph->type != ICMP_ECHO &&
