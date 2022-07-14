9d3e9e1e0856f4c905bbb870f16f42ae72477071
Jianglei Nie <niejianglei2021@163.com>
2022-07-14 14:37:46 +0800

    mm/damon/reclaim: fix potential memory leak in damon_reclaim_init()
    [ Upstream commit 188043c7f4f2bd662f2a55957d684fffa543e600 ]
    
    damon_reclaim_init() allocates a memory chunk for ctx with
    damon_new_ctx().  When damon_select_ops() fails, ctx is not released,
    which will lead to a memory leak.
    
    We should release the ctx with damon_destroy_ctx() when damon_select_ops()
    fails to fix the memory leak.
    
    Link: https://lkml.kernel.org/r/20220714063746.2343549-1-niejianglei2021@163.com
    Fixes: 4d69c3457821 ("mm/damon/reclaim: use damon_select_ops() instead of damon_{v,p}a_set_operations()")
    Signed-off-by: Jianglei Nie <niejianglei2021@163.com>
    Reviewed-by: SeongJae Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- mm/damon/reclaim.c
+++ mm/damon/reclaim.c
@@ -384,8 +384,10 @@ static int __init damon_reclaim_init(void)
 	if (!ctx)
 		return -ENOMEM;
 
-	if (damon_select_ops(ctx, DAMON_OPS_PADDR))
+	if (damon_select_ops(ctx, DAMON_OPS_PADDR)) {
+		damon_destroy_ctx(ctx);
 		return -EINVAL;
+	}
 
 	ctx->callback.after_aggregation = damon_reclaim_after_aggregation;
 
