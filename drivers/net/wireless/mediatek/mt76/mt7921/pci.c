7aa91aa9daa7dceb84ca401269733991e5dab252
Sean Wang <sean.wang@mediatek.com>
2022-07-21 06:25:37 +0800

    wifi: mt76: mt7921e: fix race issue between reset and suspend/resume
    [ Upstream commit ff6c4a6449793e9718ef2e9ad46864b63022648e ]
    
    It is unexpected that the reset work is running simultaneously with
    the suspend or resume context and it is possible that reset work is still
    running even after mt7921 is suspended if we don't fix the race issue.
    
    Thus, the suspend procedure should be waiting until the reset is completed
    at the beginning and ignore the subsequent the reset requests.
    
    In case there is an error that happens during either suspend or resume
    handler, we will schedule a reset task to recover the error before
    returning the error code to ensure we can immediately fix the error there.
    
    Fixes: 0c1ce9884607 ("mt76: mt7921: add wifi reset support")
    Co-developed-by: YN Chen <YN.Chen@mediatek.com>
    Signed-off-by: YN Chen <YN.Chen@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/mediatek/mt76/mt7921/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -368,6 +368,7 @@ static int mt7921_pci_suspend(struct pci_dev *pdev, pm_message_t state)
 	int i, err;
 
 	pm->suspended = true;
+	flush_work(&dev->reset_work);
 	cancel_delayed_work_sync(&pm->ps_work);
 	cancel_work_sync(&pm->wake_work);
 
@@ -433,6 +434,9 @@ restore_napi:
 restore_suspend:
 	pm->suspended = false;
 
+	if (err < 0)
+		mt7921_reset(&dev->mt76);
+
 	return err;
 }
 
@@ -451,7 +455,7 @@ static int mt7921_pci_resume(struct pci_dev *pdev)
 
 	err = mt7921_mcu_drv_pmctrl(dev);
 	if (err < 0)
-		return err;
+		goto failed;
 
 	mt7921_wpdma_reinit_cond(dev);
 
@@ -481,10 +485,11 @@ static int mt7921_pci_resume(struct pci_dev *pdev)
 		mt76_connac_mcu_set_deep_sleep(&dev->mt76, false);
 
 	err = mt76_connac_mcu_set_hif_suspend(mdev, false);
-	if (err)
-		return err;
-
+failed:
 	pm->suspended = false;
+
+	if (err < 0)
+		mt7921_reset(&dev->mt76);
 
 	return err;
 }
