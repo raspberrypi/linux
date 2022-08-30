dd88139cb53c87b6a3ad4ccbc04cdf66c196f8ec
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2022-08-30 15:39:05 +0200

    selftests/xsk: Add missing close() on netns fd
    [ Upstream commit 8a7d61bdc2fac2c460a2f32a062f5c6dbd21a764 ]
    
    Commit 1034b03e54ac ("selftests: xsk: Simplify cleanup of ifobjects")
    removed close on netns fd, which is not correct, so let us restore it.
    
    Fixes: 1034b03e54ac ("selftests: xsk: Simplify cleanup of ifobjects")
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Magnus Karlsson <magnus.karlsson@intel.com>
    Link: https://lore.kernel.org/bpf/20220830133905.9945-1-maciej.fijalkowski@intel.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/bpf/xdpxceiver.c
+++ tools/testing/selftests/bpf/xdpxceiver.c
@@ -1589,6 +1589,8 @@ static struct ifobject *ifobject_create(void)
 	if (!ifobj->umem)
 		goto out_umem;
 
+	ifobj->ns_fd = -1;
+
 	return ifobj;
 
 out_umem:
@@ -1600,6 +1602,8 @@ out_xsk_arr:
 
 static void ifobject_delete(struct ifobject *ifobj)
 {
+	if (ifobj->ns_fd != -1)
+		close(ifobj->ns_fd);
 	free(ifobj->umem);
 	free(ifobj->xsk_arr);
 	free(ifobj);
