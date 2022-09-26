9197a1364d41b342b3b59a9c8ef7a4920deb6409
Pavel Begunkov <asml.silence@gmail.com>
2022-09-27 00:44:39 +0100

    io_uring/rw: fix unexpected link breakage
    commit bf68b5b34311ee57ed40749a1257a30b46127556 upstream.
    
    req->cqe.res is set in io_read() to the amount of bytes left to be done,
    which is used to figure out whether to fail a read or not. However,
    io_read() may do another without returning, and we stash the previous
    value into ->bytes_done but forget to update cqe.res. Then we ask a read
    to do strictly less than cqe.res but expect the return to be exactly
    cqe.res.
    
    Fix the bug by updating cqe.res for retries.
    
    Cc: stable@vger.kernel.org
    Reported-and-Tested-by: Beld Zhang <beldzhang@gmail.com>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/3a1088440c7be98e5800267af922a67da0ef9f13.1664235732.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -4215,6 +4215,7 @@ static int io_read(struct io_kiocb *req, unsigned int issue_flags)
 			return -EAGAIN;
 		}
 
+		req->cqe.res = iov_iter_count(&s->iter);
 		/*
 		 * Now retry read with the IOCB_WAITQ parts set in the iocb. If
 		 * we get -EIOCBQUEUED, then we'll get a notification when the
