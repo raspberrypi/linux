f9467c175d6b45bc2ae6fdf7f2a4896b70a4fd46
Pavel Begunkov <asml.silence@gmail.com>
2022-10-21 11:16:39 +0100

    net: flag sockets supporting msghdr originated zerocopy
    commit e993ffe3da4bcddea0536b03be1031bf35cd8d85 upstream.
    
    We need an efficient way in io_uring to check whether a socket supports
    zerocopy with msghdr provided ubuf_info. Add a new flag into the struct
    socket flags fields.
    
    Cc: <stable@vger.kernel.org> # 6.0
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Link: https://lore.kernel.org/r/3dafafab822b1c66308bb58a0ac738b1e3f53f74.1666346426.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -1620,6 +1620,7 @@ int udp_init_sock(struct sock *sk)
 {
 	skb_queue_head_init(&udp_sk(sk)->reader_queue);
 	sk->sk_destruct = udp_destruct_sock;
+	set_bit(SOCK_SUPPORT_ZC, &sk->sk_socket->flags);
 	return 0;
 }
 EXPORT_SYMBOL_GPL(udp_init_sock);
