cecca649e97fc0573ce3a47df4621f02d7021f32
Haibo Chen <haibo.chen@nxp.com>
2022-10-24 13:26:39 +0800

    clk: imx93: correct the flexspi1 clock setting
    [ Upstream commit 62dfdbcc16e767b91ed35d4fc0428c86d4688505 ]
    
    Correct IMX93_CLK_FLEXSPI1_GATE CCGR setting. Otherwise the flexspi
    always can't be assigned to a parent clock when dump the clock tree.
    
    Fixes: 24defbe194b6 ("clk: imx: add i.MX93 clk")
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>
    Reviewed-by: Abel Vesa <abel.vesa@linaro.org>
    Signed-off-by: Abel Vesa <abel.vesa@linaro.org>
    Link: https://lore.kernel.org/r/1666589199-1199-1-git-send-email-haibo.chen@nxp.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/clk/imx/clk-imx93.c
+++ drivers/clk/imx/clk-imx93.c
@@ -162,7 +162,7 @@ static const struct imx93_clk_ccgr {
 	{ IMX93_CLK_MU_B_GATE,		"mu_b",		"bus_aon_root",		0x8500, },
 	{ IMX93_CLK_EDMA1_GATE,		"edma1",	"m33_root",		0x8540, },
 	{ IMX93_CLK_EDMA2_GATE,		"edma2",	"wakeup_axi_root",	0x8580, },
-	{ IMX93_CLK_FLEXSPI1_GATE,	"flexspi",	"flexspi_root",		0x8640, },
+	{ IMX93_CLK_FLEXSPI1_GATE,	"flexspi1",	"flexspi1_root",	0x8640, },
 	{ IMX93_CLK_GPIO1_GATE,		"gpio1",	"m33_root",		0x8880, },
 	{ IMX93_CLK_GPIO2_GATE,		"gpio2",	"bus_wakeup_root",	0x88c0, },
 	{ IMX93_CLK_GPIO3_GATE,		"gpio3",	"bus_wakeup_root",	0x8900, },
