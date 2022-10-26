c559a8b5cfa3db196ced0257b288f17027621348
Johan Hovold <johan+linaro@kernel.org>
2022-10-26 18:21:16 +0200

    phy: qcom-qmp-combo: fix NULL-deref on runtime resume
    commit 04948e757148f870a31f4887ea2239403f516c3c upstream.
    
    Commit fc64623637da ("phy: qcom-qmp-combo,usb: add support for separate
    PCS_USB region") started treating the PCS_USB registers as potentially
    separate from the PCS registers but used the wrong base when no PCS_USB
    offset has been provided.
    
    Fix the PCS_USB base used at runtime resume to prevent dereferencing a
    NULL pointer on platforms that do not provide a PCS_USB offset (e.g.
    SC7180).
    
    Fixes: fc64623637da ("phy: qcom-qmp-combo,usb: add support for separate PCS_USB region")
    Cc: stable@vger.kernel.org	# 5.20
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Reviewed-by: Andrew Halaney <ahalaney@redhat.com>
    Link: https://lore.kernel.org/r/20221026162116.26462-1-johan+linaro@kernel.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/phy/qualcomm/phy-qcom-qmp-combo.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-combo.c
@@ -1914,7 +1914,7 @@ static void qcom_qmp_phy_combo_enable_autonomous_mode(struct qmp_phy *qphy)
 static void qcom_qmp_phy_combo_disable_autonomous_mode(struct qmp_phy *qphy)
 {
 	const struct qmp_phy_cfg *cfg = qphy->cfg;
-	void __iomem *pcs_usb = qphy->pcs_usb ?: qphy->pcs_usb;
+	void __iomem *pcs_usb = qphy->pcs_usb ?: qphy->pcs;
 	void __iomem *pcs_misc = qphy->pcs_misc;
 
 	/* Disable i/o clamp_n on resume for normal mode */
