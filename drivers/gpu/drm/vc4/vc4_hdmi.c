e61c07f294b7552389f6a7d13321782bd288b522
Phil Elwell <phil@raspberrypi.com>
2022-11-05 14:08:01 +0000

    Revert "drm/vc4: hdmi: Rework hdmi_enable_4kp60 detection"
    This reverts commit 1f742ad86539135d7caceaeb9b01034053cf861e.

--- drivers/gpu/drm/vc4/vc4_hdmi.c
+++ drivers/gpu/drm/vc4/vc4_hdmi.c
@@ -47,7 +47,6 @@
 #include <linux/pm_runtime.h>
 #include <linux/rational.h>
 #include <linux/reset.h>
-#include <soc/bcm2835/raspberrypi-clocks.h>
 #include <sound/dmaengine_pcm.h>
 #include <sound/hdmi-codec.h>
 #include <sound/pcm_drm_eld.h>
@@ -3672,7 +3671,7 @@ static int vc4_hdmi_bind(struct device *dev, struct device *master, void *data)
 
 	if (variant->max_pixel_clock == 600000000) {
 		struct vc4_dev *vc4 = to_vc4_dev(drm);
-		unsigned long max_rate = rpi_firmware_clk_get_max_rate(vc4->hvs->core_clk);
+		long max_rate = clk_round_rate(vc4->hvs->core_clk, 550000000);
 
 		if (max_rate < 550000000)
 			vc4_hdmi->disable_4kp60 = true;
