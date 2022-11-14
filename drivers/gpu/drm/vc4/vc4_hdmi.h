f9d5042342d811650231bb007b592944a6f7928a
Dave Stevenson <dave.stevenson@raspberrypi.com>
2022-11-14 19:32:10 +0000

    drm/vc4: hdmi: Add property to allow manual config of RGB or YCbCr
    Add a custom property "Output format" that allows the overriding
    of the default colourspace choice in the way that the old
    firmware hdmi_pixel_encoding property did. If the chosen format is not
    supported, then it will still drop back to the older behaviour.
    
    This won't be acceptable to upstream, but it adds back the missing
    functionality of hdmi_pixel_encoding.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/gpu/drm/vc4/vc4_hdmi.h
+++ drivers/gpu/drm/vc4/vc4_hdmi.h
@@ -111,6 +111,7 @@ struct vc4_hdmi_audio {
 };
 
 enum vc4_hdmi_output_format {
+	VC4_HDMI_OUTPUT_AUTO,
 	VC4_HDMI_OUTPUT_RGB,
 	VC4_HDMI_OUTPUT_YUV422,
 	VC4_HDMI_OUTPUT_YUV444,
@@ -130,6 +131,7 @@ struct vc4_hdmi {
 	struct delayed_work scrambling_work;
 
 	struct drm_property *broadcast_rgb_property;
+	struct drm_property *output_format_property;
 
 	struct i2c_adapter *ddc;
 	void __iomem *hdmicore_regs;
@@ -223,6 +225,11 @@ struct vc4_hdmi {
 	 * for use outside of KMS hooks. Protected by @mutex.
 	 */
 	enum vc4_hdmi_output_format output_format;
+	/**
+	 * @requested_output_format: Copy of @vc4_connector_state.requested_output_format
+	 * for use outside of KMS hooks. Protected by @mutex.
+	 */
+	enum vc4_hdmi_output_format requested_output_format;
 
 	/**
 	 * @broadcast_rgb: Copy of @vc4_connector_state.broadcast_rgb
@@ -249,6 +256,7 @@ struct vc4_hdmi_connector_state {
 	unsigned long long		tmds_char_rate;
 	unsigned int 			output_bpc;
 	enum vc4_hdmi_output_format	output_format;
+	enum vc4_hdmi_output_format	requested_output_format;
 	int				broadcast_rgb;
 };
 
