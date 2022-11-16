5e3657dede365ef705c2cd5b84fdfe32f4c81df1
Shang XiaoJing <shangxiaojing@huawei.com>
2022-11-16 09:27:25 +0800

    i40e: Fix error handling in i40e_init_module()
    [ Upstream commit 479dd06149425b9e00477f52200872587af76a48 ]
    
    i40e_init_module() won't free the debugfs directory created by
    i40e_dbg_init() when pci_register_driver() failed. Add fail path to
    call i40e_dbg_exit() to remove the debugfs entries to prevent the bug.
    
    i40e: Intel(R) Ethernet Connection XL710 Network Driver
    i40e: Copyright (c) 2013 - 2019 Intel Corporation.
    debugfs: Directory 'i40e' with parent '/' already present!
    
    Fixes: 41c445ff0f48 ("i40e: main driver core")
    Signed-off-by: Shang XiaoJing <shangxiaojing@huawei.com>
    Reviewed-by: Leon Romanovsky <leonro@nvidia.com>
    Tested-by: Gurucharan G <gurucharanx.g@intel.com> (A Contingent worker at Intel)
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -16494,6 +16494,8 @@ static struct pci_driver i40e_driver = {
  **/
 static int __init i40e_init_module(void)
 {
+	int err;
+
 	pr_info("%s: %s\n", i40e_driver_name, i40e_driver_string);
 	pr_info("%s: %s\n", i40e_driver_name, i40e_copyright);
 
@@ -16511,7 +16513,14 @@ static int __init i40e_init_module(void)
 	}
 
 	i40e_dbg_init();
-	return pci_register_driver(&i40e_driver);
+	err = pci_register_driver(&i40e_driver);
+	if (err) {
+		destroy_workqueue(i40e_wq);
+		i40e_dbg_exit();
+		return err;
+	}
+
+	return 0;
 }
 module_init(i40e_init_module);
 
