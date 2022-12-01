0c83069940e918287762b24e85d4448fbdc47453
Jonathan Bell <jonathan@raspberrypi.com>
2022-12-01 16:59:44 +0000

    usb: xhci: add XHCI_VLI_HUB_TT_QUIRK
    The integrated USB2.0 hub in the VL805 chipset has a bug where it
    incorrectly determines the remaining available frame time before the
    host next sends a SOF packet with an incremented frame_number.
    
    See the USB2.0 specification sections 11.3 and 11.14.2.3.
    
    The hub's non-periodic TT handler can transmit the IN/OUT handshake
    token too late, so a following 64-byte DATA0/1 packet causes the ACK
    handshake to collide with the propagated SOF. This causes port babble.
    
    Avoid ringing doorbells for vulnerable endpoints during uFrame 7 if the
    TR is Idle to stop one source of babble. An IN transfer for a Running TR
    may happen at any time, so there's not much we can do about that.
    
    Ideally a hub firmware update to properly implement frame timeouts is
    needed, and to avoid spinning for up to 125us when submitting TDs to
    Idle rings.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    xhci: constrain XHCI_VLI_HUB_TT_QUIRK to old firmware versions
    
    VLI have a firmware update for the VL805 which resolves the incorrect
    frame time calculation in the hub's TT. Limit applying the quirk to
    known-bad firmwares.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1911,6 +1911,7 @@ struct xhci_hcd {
 #define XHCI_AVOID_DQ_ON_LINK	BIT_ULL(47)
 #define XHCI_VLI_TRB_CACHE_BUG	BIT_ULL(48)
 #define XHCI_VLI_SS_BULK_OUT_BUG	BIT_ULL(49)
+#define XHCI_VLI_HUB_TT_QUIRK	BIT_ULL(50)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
