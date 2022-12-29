d09b7a9d2f347a0d8387d1a0ad857b4adb92ebca
David Arinzon <darinzon@amazon.com>
2022-12-29 07:30:09 +0000

    net: ena: Fix rx_copybreak value update
    [ Upstream commit c7062aaee099f2f43d6f07a71744b44b94b94b34 ]
    
    Make the upper bound on rx_copybreak tighter, by
    making sure it is smaller than the minimum of mtu and
    ENA_PAGE_SIZE. With the current upper bound of mtu,
    rx_copybreak can be larger than a page. Such large
    rx_copybreak will not bring any performance benefit to
    the user and therefore makes no sense.
    
    In addition, the value update was only reflected in
    the adapter structure, but not applied for each ring,
    causing it to not take effect.
    
    Fixes: 1738cd3ed342 ("net: ena: Add a driver for Amazon Elastic Network Adapters (ENA)")
    Signed-off-by: Osama Abboud <osamaabb@amazon.com>
    Signed-off-by: Arthur Kiyanovski <akiyano@amazon.com>
    Signed-off-by: David Arinzon <darinzon@amazon.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/amazon/ena/ena_netdev.h
+++ drivers/net/ethernet/amazon/ena/ena_netdev.h
@@ -404,6 +404,8 @@ int ena_update_queue_sizes(struct ena_adapter *adapter,
 
 int ena_update_queue_count(struct ena_adapter *adapter, u32 new_channel_count);
 
+int ena_set_rx_copybreak(struct ena_adapter *adapter, u32 rx_copybreak);
+
 int ena_get_sset_count(struct net_device *netdev, int sset);
 
 enum ena_xdp_errors_t {
