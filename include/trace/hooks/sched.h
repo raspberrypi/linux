c0a323f5fa01862538d42721bd223028e7657942
Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>
2023-01-09 14:42:39 +0800

    ANDROID: sched: add vendor hook in find_new_ilb
    Add new vendor hook to find new ilb cpu for power tuning.
    
    We also set ilb initial is -1 by this patch, and we set ilb
    as nr_cpu_ids by our vendor when ilb cpu not be found.
    
    If ilb initial is 0, then when hook is not registered,
    find ilb will return CPU 0.
    But when hook is not registered, it should do for_each_cpu_and()
    to find best ilb cpu, instead of return CPU 0 direct.
    
    So we set ilb initial is -1, it means anyone doesn't register the hook
    or the registered hook doesn't do anything, instead of ilb cpu not
    be found by vendor.
    
    This hook was originally introduced in aosp/1728852 for android12-5.10.
    
    Bug: 190228983
    Bug: 264821176
    Change-Id: Idb5a8bd5862f7dd1fa30fa99aec3b42214375915
    Signed-off-by: Jing-Ting Wu <Jing-Ting.Wu@mediatek.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -334,6 +334,10 @@ DECLARE_RESTRICTED_HOOK(android_rvh_set_task_comm,
 	TP_PROTO(struct task_struct *tsk, bool exec),
 	TP_ARGS(tsk, exec), 1);
 
+DECLARE_RESTRICTED_HOOK(android_rvh_find_new_ilb,
+	TP_PROTO(struct cpumask *nohz_idle_cpus_mask, int *ilb),
+	TP_ARGS(nohz_idle_cpus_mask, ilb), 1);
+
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
