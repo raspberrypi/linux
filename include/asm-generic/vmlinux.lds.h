c0f60e0bb51d1444b7d6b94c0170af00ed66ffc2
Phil Elwell <phil@raspberrypi.com>
2023-01-12 10:57:12 +0000

    arch: fixup after fix to broken BuildID
    Since commit [1], the Kunit tests have failed with a linker error:
    
    ERROR:root:`.exit.text' referenced in section `.uml.exitcall.exit' of \
    arch/um/drivers/virtio_uml.o: defined in discarded section `.exit.text' \
    of arch/um/drivers/virtio_uml.o
    
    There's a suggestion that this is due to the use of an older binutils
    package.
    
    This patch, likely to soon be replaced by an official upstream change,
    makes the Kunit builds work again with binutils 2.34.
    
    See [2] for discussion of the problem, and the origin of this solution/
    workaround.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>
    
    [1] 99cb0d917ffa ("arch: fix broken BuildID for arm64 and riscv")
    [2] https://lore.kernel.org/lkml/20230110191404.132594-1-sj@kernel.org/T/

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -892,11 +892,10 @@
 #endif
 
 /*
- * Discard .note.GNU-stack, which is emitted as PROGBITS by the compiler.
+ * Separate note.GNU-stack, which is emitted as PROGBITS by the compiler.
  * Otherwise, the type of .notes section would become PROGBITS instead of NOTES.
  */
 #define NOTES								\
-	/DISCARD/ : { *(.note.GNU-stack) }				\
 	.notes : AT(ADDR(.notes) - LOAD_OFFSET) {			\
 		BOUNDED_SECTION_BY(.note.*, _notes)			\
 	} NOTES_HEADERS							\
@@ -1015,6 +1014,7 @@
 
 #define DISCARDS							\
 	/DISCARD/ : {							\
+	*(.note.GNU-stack)						\
 	EXIT_DISCARDS							\
 	EXIT_CALL							\
 	COMMON_DISCARDS							\
