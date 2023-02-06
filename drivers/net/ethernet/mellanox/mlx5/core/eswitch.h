863327e523dbbdfdbab1213aa415ea119846eb3d
Shay Drory <shayd@nvidia.com>
2023-02-06 11:52:02 +0200

    net/mlx5: E-switch, Devcom, sync devcom events and devcom comp register
    [ Upstream commit 8c253dfc89efde6b5faddf9e7400e5d17884e042 ]
    
    devcom events are sent to all registered component. Following the
    cited patch, it is possible for two components, e.g.: two eswitches,
    to send devcom events, while both components are registered. This
    means eswitch layer will do double un/pairing, which is double
    allocation and free of resources, even though only one un/pairing is
    needed. flow example:
    
    	cpu0					cpu1
    	----					----
    
     mlx5_devlink_eswitch_mode_set(dev0)
      esw_offloads_devcom_init()
       mlx5_devcom_register_component(esw0)
                                             mlx5_devlink_eswitch_mode_set(dev1)
                                              esw_offloads_devcom_init()
                                               mlx5_devcom_register_component(esw1)
                                               mlx5_devcom_send_event()
       mlx5_devcom_send_event()
    
    Hence, check whether the eswitches are already un/paired before
    free/allocation of resources.
    
    Fixes: 09b278462f16 ("net: devlink: enable parallel ops on netlink interface")
    Signed-off-by: Shay Drory <shayd@nvidia.com>
    Reviewed-by: Mark Bloch <mbloch@nvidia.com>
    Signed-off-by: Saeed Mahameed <saeedm@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -341,6 +341,7 @@ struct mlx5_eswitch {
 		u32             large_group_num;
 	}  params;
 	struct blocking_notifier_head n_head;
+	bool paired[MLX5_MAX_PORTS];
 };
 
 void esw_offloads_disable(struct mlx5_eswitch *esw);
