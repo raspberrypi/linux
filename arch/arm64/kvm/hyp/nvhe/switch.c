ca7c75c77f23b84ec2392b95f546f1c16142eff7
Fuad Tabba <tabba@google.com>
2023-02-06 13:06:45 +0000

    ANDROID: KVM: arm64: Use enum instead of helper for fp state
    Before the conversion of the various booleans into an enum
    representing the state,  this helper clarified things. Since the
    introduction of the enum, the helper obfuscates rather than
    helps.
    
    No functional change intended.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 278749606
    Change-Id: I83c870146ed2d910bf10d625d1048b95c8b23736

--- arch/arm64/kvm/hyp/nvhe/switch.c
+++ arch/arm64/kvm/hyp/nvhe/switch.c
@@ -53,7 +53,7 @@ static void __activate_traps(struct kvm_vcpu *vcpu)
 			val |= CPTR_EL2_TSM;
 	}
 
-	if (!guest_owns_fp_regs(vcpu)) {
+	if (vcpu->arch.fp_state != FP_STATE_GUEST_OWNED) {
 		if (has_hvhe())
 			val &= ~(CPACR_EL1_FPEN_EL0EN | CPACR_EL1_FPEN_EL1EN |
 				 CPACR_EL1_ZEN_EL0EN | CPACR_EL1_ZEN_EL1EN);
