1415683defb2a1f23ea70f1038bf6f4ddc715018
Mathias Nyman <mathias.nyman@linux.intel.com>
2023-02-06 15:38:52 +0200

    FROMLIST: xhci: sideband: add initial api to register a sideband entity
    Introduce XHCI sideband, which manages the USB endpoints being requested by
    a client driver.  This is used for when client drivers are attempting to
    offload USB endpoints to another entity for handling USB transfers.  XHCI
    sideband will allow for drivers to fetch the required information about the
    transfer ring, so the user can submit transfers independently.  Expose the
    required APIs for drivers to register and request for a USB endpoint and to
    manage XHCI secondary interrupters.
    
    Multiple ring segment page linking, proper endpoint clean up, and allowing
    module compliation added by Wesley Cheng to complete original concept code
    by Mathias Nyman.
    
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Co-developed-by: Wesley Cheng <quic_wcheng@quicinc.com>
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>
    
    Bug: 264371591
    Link: https://lore.kernel.org/linux-usb/20240228013619.29758-6-quic_wcheng@quicinc.com/
    Change-Id: Ie4fb80a525e413e35387786c1578258b6613faf5
    Signed-off-by: Wesley Cheng <quic_wcheng@quicinc.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -978,6 +978,8 @@ struct xhci_virt_ep {
 	int			next_frame_id;
 	/* Use new Isoch TRB layout needed for extended TBC support */
 	bool			use_extended_tbc;
+	/* set if this endpoint is controlled via sideband access*/
+	struct xhci_sideband			*sideband;
 };
 
 enum xhci_overhead_type {
@@ -1041,6 +1043,8 @@ struct xhci_virt_device {
 	u16				current_mel;
 	/* Used for the debugfs interfaces. */
 	void				*debugfs_private;
+	/* set if this device is registered for sideband access */
+	struct xhci_sideband			*sideband;
 };
 
 /*
