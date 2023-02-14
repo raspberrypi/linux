468c440b32e32f50e5cece76da36f2d9baece8d5
Liujie Xie <xieliujie@oppo.com>
2023-02-14 14:42:26 +0800

    ANDROID: vendor_hooks: Add hooks for mutex and rwsem optimistic spin
    These hooks help us do the following things:
    a) Record the number of mutex and rwsem optimistic spin.
    b) Monitor the time of mutex and rwsem optimistic spin.
    c) Make it possible if oems don't want mutex and rwsem to optimistic spin
    for a long time.
    
    Bug: 267565260
    Change-Id: I2bee30fb17946be85e026213b481aeaeaee2459f
    Signed-off-by: Liujie Xie <xieliujie@oppo.com>
    (cherry picked from commit d01f7e1269d2651768a0bd91a88f76339a1cb427)
    (cherry picked from commit 05b5ff11ad98c5896b352b4c376a84b63684e06c)

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -727,6 +727,7 @@ static inline bool rwsem_can_spin_on_owner(struct rw_semaphore *sem)
 	if ((flags & RWSEM_NONSPINNABLE) ||
 	    (owner && !(flags & RWSEM_READER_OWNED) && !owner_on_cpu(owner)))
 		ret = false;
+	trace_android_vh_rwsem_can_spin_on_owner(sem, &ret);
 
 	lockevent_cond_inc(rwsem_opt_fail, !ret);
 	return ret;
@@ -752,6 +753,8 @@ rwsem_spin_on_owner(struct rw_semaphore *sem)
 	struct task_struct *new, *owner;
 	unsigned long flags, new_flags;
 	enum owner_state state;
+	int cnt = 0;
+	bool time_out = false;
 
 	lockdep_assert_preemption_disabled();
 
@@ -761,6 +764,9 @@ rwsem_spin_on_owner(struct rw_semaphore *sem)
 		return state;
 
 	for (;;) {
+		trace_android_vh_rwsem_opt_spin_start(sem, &time_out, &cnt, true);
+		if (time_out)
+			break;
 		/*
 		 * When a waiting writer set the handoff flag, it may spin
 		 * on the owner as well. Once that writer acquires the lock,
@@ -825,6 +831,8 @@ static bool rwsem_optimistic_spin(struct rw_semaphore *sem)
 	int prev_owner_state = OWNER_NULL;
 	int loop = 0;
 	u64 rspin_threshold = 0;
+	int cnt = 0;
+	bool time_out = false;
 
 	/* sem->wait_lock should not be held when doing optimistic spinning */
 	if (!osq_lock(&sem->osq))
@@ -839,6 +847,9 @@ static bool rwsem_optimistic_spin(struct rw_semaphore *sem)
 	for (;;) {
 		enum owner_state owner_state;
 
+		trace_android_vh_rwsem_opt_spin_start(sem, &time_out, &cnt, false);
+		if (time_out)
+			break;
 		owner_state = rwsem_spin_on_owner(sem);
 		if (!(owner_state & OWNER_SPINNABLE))
 			break;
@@ -932,6 +943,7 @@ static bool rwsem_optimistic_spin(struct rw_semaphore *sem)
 		cpu_relax();
 	}
 	osq_unlock(&sem->osq);
+	trace_android_vh_rwsem_opt_spin_finish(sem, taken);
 done:
 	lockevent_cond_inc(rwsem_opt_fail, !taken);
 	return taken;
