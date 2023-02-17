29e4db3489257f70e4e0c826c08acf0f85eff3e7
Maxime Ripard <maxime@cerno.tech>
2023-02-17 15:34:30 +0100

    drm/vc4: hdmi: Add support for BCM2712 HDMI controllers
    The HDMI controllers found in the BCM2712 are largely the ones found in
    the BCM2711 with a different PHY.
    
    There's some difference with how timings are split between registers,
    and HDMI1 is now able to run at 4k/60Hz.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/vc4_hdmi.h
+++ drivers/gpu/drm/vc4/vc4_hdmi.h
@@ -263,4 +263,8 @@ void vc5_hdmi_phy_disable(struct vc4_hdmi *vc4_hdmi);
 void vc5_hdmi_phy_rng_enable(struct vc4_hdmi *vc4_hdmi);
 void vc5_hdmi_phy_rng_disable(struct vc4_hdmi *vc4_hdmi);
 
+void vc6_hdmi_phy_init(struct vc4_hdmi *vc4_hdmi,
+		       struct vc4_hdmi_connector_state *vc4_conn_state);
+void vc6_hdmi_phy_disable(struct vc4_hdmi *vc4_hdmi);
+
 #endif /* _VC4_HDMI_H_ */
