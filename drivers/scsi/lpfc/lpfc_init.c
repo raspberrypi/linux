22ca4fe8986585654891f76a203aec0c20dcb8e5
Linus Torvalds <torvalds@linux-foundation.org>
2023-03-06 12:15:13 -0800

    cpumask: fix incorrect cpumask scanning result checks
    [ Upstream commit 8ca09d5fa3549d142c2080a72a4c70ce389163cd ]
    
    It turns out that commit 596ff4a09b89 ("cpumask: re-introduce
    constant-sized cpumask optimizations") exposed a number of cases of
    drivers not checking the result of "cpumask_next()" and friends
    correctly.
    
    The documented correct check for "no more cpus in the cpumask" is to
    check for the result being equal or larger than the number of possible
    CPU ids, exactly _because_ we've always done those constant-sized
    cpumask scans using a widened type before.  So the return value of a
    cpumask scan should be checked with
    
    	if (cpu >= nr_cpu_ids)
    		...
    
    because the cpumask scan did not necessarily stop exactly *at* that
    maximum CPU id.
    
    But a few cases ended up instead using checks like
    
    	if (cpu == nr_cpumask_bits)
    		...
    
    which used that internal "widened" number of bits.  And that used to
    work pretty much by accident (ok, in this case "by accident" is simply
    because it matched the historical internal implementation of the cpumask
    scanning, so it was more of a "intentionally using implementation
    details rather than an accident").
    
    But the extended constant-sized optimizations then did that internal
    implementation differently, and now that code that did things wrong but
    matched the old implementation no longer worked at all.
    
    Which then causes subsequent odd problems due to using what ends up
    being an invalid CPU ID.
    
    Most of these cases require either unusual hardware or special uses to
    hit, but the random.c one triggers quite easily.
    
    All you really need is to have a sufficiently small CONFIG_NR_CPUS value
    for the bit scanning optimization to be triggered, but not enough CPUs
    to then actually fill that widened cpumask.  At that point, the cpumask
    scanning will return the NR_CPUS constant, which is _not_ the same as
    nr_cpumask_bits.
    
    This just does the mindless fix with
    
       sed -i 's/== nr_cpumask_bits/>= nr_cpu_ids/'
    
    to fix the incorrect uses.
    
    The ones in the SCSI lpfc driver in particular could probably be fixed
    more cleanly by just removing that repeated pattern entirely, but I am
    not emptionally invested enough in that driver to care.
    
    Reported-and-tested-by: Guenter Roeck <linux@roeck-us.net>
    Link: https://lore.kernel.org/lkml/481b19b5-83a0-4793-b4fd-194ad7b978c3@roeck-us.net/
    Reported-and-tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://lore.kernel.org/lkml/CAMuHMdUKo_Sf7TjKzcNDa8Ve+6QrK+P8nSQrSQ=6LTRmcBKNww@mail.gmail.com/
    Reported-by: Vernon Yang <vernon2gm@gmail.com>
    Link: https://lore.kernel.org/lkml/20230306160651.2016767-1-vernon2gm@gmail.com/
    Cc: Yury Norov <yury.norov@gmail.com>
    Cc: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/lpfc/lpfc_init.c
+++ drivers/scsi/lpfc/lpfc_init.c
@@ -12507,7 +12507,7 @@ lpfc_cpu_affinity_check(struct lpfc_hba *phba, int vectors)
 					goto found_same;
 				new_cpu = cpumask_next(
 					new_cpu, cpu_present_mask);
-				if (new_cpu == nr_cpumask_bits)
+				if (new_cpu >= nr_cpu_ids)
 					new_cpu = first_cpu;
 			}
 			/* At this point, we leave the CPU as unassigned */
@@ -12521,7 +12521,7 @@ found_same:
 			 * selecting the same IRQ.
 			 */
 			start_cpu = cpumask_next(new_cpu, cpu_present_mask);
-			if (start_cpu == nr_cpumask_bits)
+			if (start_cpu >= nr_cpu_ids)
 				start_cpu = first_cpu;
 
 			lpfc_printf_log(phba, KERN_INFO, LOG_INIT,
@@ -12557,7 +12557,7 @@ found_same:
 					goto found_any;
 				new_cpu = cpumask_next(
 					new_cpu, cpu_present_mask);
-				if (new_cpu == nr_cpumask_bits)
+				if (new_cpu >= nr_cpu_ids)
 					new_cpu = first_cpu;
 			}
 			/* We should never leave an entry unassigned */
@@ -12575,7 +12575,7 @@ found_any:
 			 * selecting the same IRQ.
 			 */
 			start_cpu = cpumask_next(new_cpu, cpu_present_mask);
-			if (start_cpu == nr_cpumask_bits)
+			if (start_cpu >= nr_cpu_ids)
 				start_cpu = first_cpu;
 
 			lpfc_printf_log(phba, KERN_INFO, LOG_INIT,
@@ -12648,7 +12648,7 @@ found_any:
 				goto found_hdwq;
 			}
 			new_cpu = cpumask_next(new_cpu, cpu_present_mask);
-			if (new_cpu == nr_cpumask_bits)
+			if (new_cpu >= nr_cpu_ids)
 				new_cpu = first_cpu;
 		}
 
@@ -12663,7 +12663,7 @@ found_any:
 				goto found_hdwq;
 
 			new_cpu = cpumask_next(new_cpu, cpu_present_mask);
-			if (new_cpu == nr_cpumask_bits)
+			if (new_cpu >= nr_cpu_ids)
 				new_cpu = first_cpu;
 		}
 
@@ -12674,7 +12674,7 @@ found_any:
  found_hdwq:
 		/* We found an available entry, copy the IRQ info */
 		start_cpu = cpumask_next(new_cpu, cpu_present_mask);
-		if (start_cpu == nr_cpumask_bits)
+		if (start_cpu >= nr_cpu_ids)
 			start_cpu = first_cpu;
 		cpup->hdwq = new_cpup->hdwq;
  logit:
