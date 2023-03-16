30940c2c311239df510df4c9bb3de50748c789d8
Fuad Tabba <tabba@google.com>
2023-03-16 09:04:58 +0000

    ANDROID: KVM: arm64: Specialize handling of host fpsimd state on EL2 trap n/vhe
    In subsequent patches, vhe/pKVM(nvhe) will diverge significantly
    on saving the host fpsimd/sve state when taking a guest fpsimd
    trap. Add a specialized helper to handle it.
    
    No functional change intended.
    
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Bug: 278749606
    Change-Id: Ib6b13cafad8bf568694804e3b55e0a5a4fcd70a4

--- arch/arm64/kvm/hyp/vhe/switch.c
+++ arch/arm64/kvm/hyp/vhe/switch.c
@@ -160,6 +160,11 @@ void deactivate_traps_vhe_put(struct kvm_vcpu *vcpu)
 	local_irq_restore(flags);
 }
 
+static void kvm_hyp_handle_fpsimd_host(struct kvm_vcpu *vcpu)
+{
+	__fpsimd_save_state(vcpu->arch.host_fpsimd_state);
+}
+
 static const exit_handler_fn hyp_exit_handlers[] = {
 	[0 ... ESR_ELx_EC_MAX]		= NULL,
 	[ESR_ELx_EC_CP15_32]		= kvm_hyp_handle_cp15_32,
