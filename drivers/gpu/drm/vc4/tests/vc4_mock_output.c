70e906d3c688491e181446afa27bea32ce241d6a
Maxime Ripard <maxime@cerno.tech>
2023-03-24 09:58:15 +0100

    drm/vc4: tests: Drop drm parameter for vc4_find_crtc_for_encoder
    The DRM device pointer and the DRM encoder pointer are redundant, since
    the latter is attached to the former and we can just follow the
    drm_encoder->dev pointer.
    
    Let's remove the drm_device pointer argument.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/tests/vc4_mock_output.c
+++ drivers/gpu/drm/vc4/tests/vc4_mock_output.c
@@ -77,7 +77,7 @@ int vc4_mock_atomic_add_output(struct kunit *test,
 	encoder = vc4_find_encoder_by_type(drm, type);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, encoder);
 
-	crtc = vc4_find_crtc_for_encoder(test, drm, encoder);
+	crtc = vc4_find_crtc_for_encoder(test, encoder);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, crtc);
 
 	output = container_of(encoder, struct vc4_dummy_output, encoder.base);
@@ -115,7 +115,7 @@ int vc4_mock_atomic_del_output(struct kunit *test,
 	encoder = vc4_find_encoder_by_type(drm, type);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, encoder);
 
-	crtc = vc4_find_crtc_for_encoder(test, drm, encoder);
+	crtc = vc4_find_crtc_for_encoder(test, encoder);
 	KUNIT_ASSERT_NOT_ERR_OR_NULL(test, crtc);
 
 	crtc_state = drm_atomic_get_crtc_state(state, crtc);
