8d5f29a9a5340853851f45eb298f597c53d989a9
Maxime Ripard <maxime@cerno.tech>
2023-04-14 11:24:37 +0200

    drm/vc4: tests: Use custom plane state for mock
    The current mock planes were just using the regular drm_plane_state,
    while the driver expect struct vc4_plane_state that subclasses
    drm_plane_state.
    
    Hook the proper implementations of reset, duplicate_state, destroy and
    atomic_check to create vc4_plane_state.
    
    Signed-off-by: Maxime Ripard <maxime@cerno.tech>

--- drivers/gpu/drm/vc4/tests/vc4_mock_plane.c
+++ drivers/gpu/drm/vc4/tests/vc4_mock_plane.c
@@ -7,6 +7,20 @@
 
 #include "vc4_mock.h"
 
+static const struct drm_plane_helper_funcs vc4_dummy_plane_helper_funcs = {
+	.atomic_check = vc4_plane_atomic_check,
+};
+
+static const struct drm_plane_funcs vc4_dummy_plane_funcs = {
+	.atomic_destroy_state	= vc4_plane_destroy_state,
+	.atomic_duplicate_state	= vc4_plane_duplicate_state,
+	.reset			= vc4_plane_reset,
+};
+
+static const uint32_t vc4_dummy_plane_formats[] = {
+	DRM_FORMAT_XRGB8888,
+};
+
 struct drm_plane *vc4_dummy_plane(struct kunit *test, struct drm_device *drm,
 				  enum drm_plane_type type)
 {
