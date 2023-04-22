2cae6a28d8c12c597e8656962271520434c61c48
Bosi Zhang <u201911157@hust.edu.cn>
2023-04-22 08:43:31 +0000

    clk: mediatek: fix of_iomap memory leak
    [ Upstream commit 3db7285e044144fd88a356f5b641b9cd4b231a77 ]
    
    Smatch reports:
    drivers/clk/mediatek/clk-mtk.c:583 mtk_clk_simple_probe() warn:
        'base' from of_iomap() not released on lines: 496.
    
    This problem was also found in linux-next. In mtk_clk_simple_probe(),
    base is not released when handling errors
    if clk_data is not existed, which may cause a leak.
    So free_base should be added here to release base.
    
    Fixes: c58cd0e40ffa ("clk: mediatek: Add mtk_clk_simple_probe() to simplify clock providers")
    Signed-off-by: Bosi Zhang <u201911157@hust.edu.cn>
    Reviewed-by: Dongliang Mu <dzm91@hust.edu.cn>
    Link: https://lore.kernel.org/r/20230422084331.47198-1-u201911157@hust.edu.cn
    Signed-off-by: Stephen Boyd <sboyd@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/clk/mediatek/clk-mtk.c
+++ drivers/clk/mediatek/clk-mtk.c
@@ -492,8 +492,10 @@ int mtk_clk_simple_probe(struct platform_device *pdev)
 	num_clks += mcd->num_mux_clks;
 
 	clk_data = mtk_alloc_clk_data(num_clks);
-	if (!clk_data)
-		return -ENOMEM;
+	if (!clk_data) {
+		r = -ENOMEM;
+		goto free_base;
+	}
 
 	if (mcd->fixed_clks) {
 		r = mtk_clk_register_fixed_clks(mcd->fixed_clks,
@@ -578,6 +580,7 @@ unregister_fixed_clks:
 					      mcd->num_fixed_clks, clk_data);
 free_data:
 	mtk_free_clk_data(clk_data);
+free_base:
 	if (mcd->shared_io && base)
 		iounmap(base);
 	return r;
