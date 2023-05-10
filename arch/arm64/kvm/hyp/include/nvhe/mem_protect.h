40cb94447d01a7ba2854cf328a8255eed404e5f7
Will Deacon <willdeacon@google.com>
2023-05-11 00:27:04 +0100

    ANDROID: KVM: arm64: Always unmap protected regions from the host
    As part of the host deprivilege process, memory regions advertised in
    the "pkvm,protected-region" devicetree nodes are unmapped from the
    host stage-2 page-table and annotated specially so that the memory can
    be used exclusively by hypervisor modules.
    
    Since the state of this memory in the host stage-2 may not be in the
    "owned" state (for example, if it corresponds to a "no-map" region),
    remove the initial check of the page state and allow unmapping of any
    pages from the trusted host.
    
    Bug: 278749606
    Bug: 280380533
    Cc: Quentin Perret <qperret@google.com>
    Fixes: 417e986b5484 ("ANDROID: KVM: arm64: Move addr_is_allowed_memory() check into host callback")
    Signed-off-by: Will Deacon <willdeacon@google.com>
    Change-Id: Ic28a2557d6d515b356645228fd41d3c921826489

--- arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
+++ arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -88,7 +88,6 @@ int __pkvm_guest_relinquish_to_host(struct pkvm_hyp_vcpu *vcpu,
 bool addr_is_memory(phys_addr_t phys);
 int host_stage2_idmap_locked(phys_addr_t addr, u64 size, enum kvm_pgtable_prot prot);
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, u8 owner_id);
-int host_stage2_protect_pages_locked(phys_addr_t addr, u64 size);
 int host_stage2_unmap_reg_locked(phys_addr_t start, u64 size);
 int kvm_host_prepare_stage2(void *pgt_pool_base);
 int kvm_guest_prepare_stage2(struct pkvm_hyp_vm *vm, void *pgd);
