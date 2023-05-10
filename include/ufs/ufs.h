f50d19de6ebd0370abf729b1dd3aa9a58320b491
Weichao Guo <guoweichao@oppo.corp-partner.google.com>
2023-05-10 19:59:52 +0800

    ANDROID: GKI: Add ANDROID_OEM_DATA in struct ufs_dev_info
    Add ANDROID_OEM_DATA(1) in struct ufs_dev_info to support more
    feature status for extend copy feature.
    
    Bug: 283021230
    
    Change-Id: I961dbd4d4a0939658ce55f608062107f4a0575a6
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Weichao Guo <guoweichao@oppo.corp-partner.google.com>
    (cherry picked from commit 72515e57f036651497985c2dc0c1d2caac62d487)

--- include/ufs/ufs.h
+++ include/ufs/ufs.h
@@ -14,6 +14,7 @@
 #include <linux/bitops.h>
 #include <linux/types.h>
 #include <linux/android_kabi.h>
+#include <linux/android_vendor.h>
 #include <uapi/scsi/scsi_bsg_ufs.h>
 
 /*
@@ -579,6 +580,7 @@ struct ufs_dev_info {
 	/* UFS EXT_IID Enable */
 	bool	b_ext_iid_en;
 
+	ANDROID_OEM_DATA(1);
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 };
