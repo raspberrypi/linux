d3c9538cbbc5c361a0e649cdb4dfcde8c1746913
Greg Kroah-Hartman <gregkh@google.com>
2023-05-12 13:14:41 +0000

    ANDROID: fix up android selinux build break.
    In commit e67b79850fcc ("selinux: stop passing selinux_state pointers
    and their offspring"), the selinux state pointers were switched to a
    global pointer.  This breaks the out-of-tree android networking state
    status logic, so switch it over to use the correct global selinux state
    structure instead.
    
    Fixes: e67b79850fcc ("selinux: stop passing selinux_state pointers and their offspring")
    Change-Id: Iaf1a7a7824bffd1c074e3d3a43b4608d8138a9f4
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2126,8 +2126,8 @@ static void security_load_policycaps(struct selinux_policy *policy)
 				i);
 	}
 
-	state->android_netlink_route = p->android_netlink_route;
-	state->android_netlink_getneigh = p->android_netlink_getneigh;
+	selinux_state.android_netlink_route = p->android_netlink_route;
+	selinux_state.android_netlink_getneigh = p->android_netlink_getneigh;
 	selinux_nlmsg_init();
 }
 
