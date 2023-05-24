b8cc360b6664005b5f4c82059100aed4b64e0864
Sooyong Suk <s.suk@samsung.com>
2023-05-24 09:34:19 +0900

    ANDROID: task_mmu: add vendor hook for swap entry
    Add vendor hook in smaps_pte_entry for swap entry
    - android_vh_smaps_pte_entry
    - android_vh_show_smap
    
    This vendor hook is to show more information for
    swap entries of a process based on the
    characteristics, such as written-back, same-filled
    or huge (uncompressed).
    
    Bug: 284059805
    
    Change-Id: Ie4a48ae42212c056992d34a10b026b60439d0012
    Signed-off-by: Sooyong Suk <s.suk@samsung.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -335,3 +335,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_tune_scan_type);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_cpuset_fork);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_madvise_swapin_walk_pmd_entry);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_process_madvise);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_smaps_pte_entry);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_show_smap);
