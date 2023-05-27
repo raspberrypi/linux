b44fa6a2045f0b3df454fc83e752051d293095e3
Douglas Anderson <dianders@chromium.org>
2023-05-26 18:41:31 -0700

    watchdog/hardlockup: keep kernel.nmi_watchdog sysctl as 0444 if probe fails
    [ Upstream commit 9ec272c586b07d1abf73438524bd12b1df9c5f9b ]
    
    Patch series "watchdog: Cleanup / fixes after buddy series v5 reviews".
    
    This patch series attempts to finish resolving the feedback received
    from Petr Mladek on the v5 series I posted.
    
    Probably the only thing that wasn't fully as clean as Petr requested was
    the Kconfig stuff.  I couldn't find a better way to express it without a
    more major overhaul.  In the very least, I renamed "NON_ARCH" to
    "PERF_OR_BUDDY" in the hopes that will make it marginally better.
    
    Nothing in this series is terribly critical and even the bugfixes are
    small.  However, it does cleanup a few things that were pointed out in
    review.
    
    This patch (of 10):
    
    The permissions for the kernel.nmi_watchdog sysctl have always been set at
    compile time despite the fact that a watchdog can fail to probe.  Let's
    fix this and set the permissions based on whether the hardlockup detector
    actually probed.
    
    Link: https://lkml.kernel.org/r/20230527014153.2793931-1-dianders@chromium.org
    Link: https://lkml.kernel.org/r/20230526184139.1.I0d75971cc52a7283f495aac0bd5c3041aadc734e@changeid
    Fixes: a994a3147e4c ("watchdog/hardlockup/perf: Implement init time detection of perf")
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Reported-by: Petr Mladek <pmladek@suse.com>
    Closes: https://lore.kernel.org/r/ZHCn4hNxFpY5-9Ki@alley
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -831,15 +831,6 @@ static struct ctl_table watchdog_sysctls[] = {
 		.extra2		= (void *)&sixty,
 	},
 	{
-		.procname       = "nmi_watchdog",
-		.data		= &watchdog_hardlockup_user_enabled,
-		.maxlen		= sizeof(int),
-		.mode		= NMI_WATCHDOG_SYSCTL_PERM,
-		.proc_handler   = proc_nmi_watchdog,
-		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_ONE,
-	},
-	{
 		.procname	= "watchdog_cpumask",
 		.data		= &watchdog_cpumask_bits,
 		.maxlen		= NR_CPUS,
@@ -902,10 +893,28 @@ static struct ctl_table watchdog_sysctls[] = {
 	{}
 };
 
+static struct ctl_table watchdog_hardlockup_sysctl[] = {
+	{
+		.procname       = "nmi_watchdog",
+		.data		= &watchdog_hardlockup_user_enabled,
+		.maxlen		= sizeof(int),
+		.mode		= 0444,
+		.proc_handler   = proc_nmi_watchdog,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{}
+};
+
 static void __init watchdog_sysctl_init(void)
 {
 	register_sysctl_init("kernel", watchdog_sysctls);
+
+	if (watchdog_hardlockup_available)
+		watchdog_hardlockup_sysctl[0].mode = 0644;
+	register_sysctl_init("kernel", watchdog_hardlockup_sysctl);
 }
+
 #else
 #define watchdog_sysctl_init() do { } while (0)
 #endif /* CONFIG_SYSCTL */
@@ -961,6 +970,8 @@ static int __init lockup_detector_check(void)
 	/* Make sure no work is pending. */
 	flush_work(&detector_work);
 
+	watchdog_sysctl_init();
+
 	return 0;
 
 }
@@ -980,5 +991,4 @@ void __init lockup_detector_init(void)
 		allow_lockup_detector_init_retry = true;
 
 	lockup_detector_setup();
-	watchdog_sysctl_init();
 }
