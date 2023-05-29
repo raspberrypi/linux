f6bfcb34b30c2384b6f261f65c090e2483ade5a4
Greg Kroah-Hartman <gregkh@google.com>
2023-05-29 09:52:24 +0000

    Revert "ANDROID: module: Add vendor hooks"
    This reverts commit 60e6687899e0f2244a9854444f4fdb940dba350d.
    
    The module.c changes in 6.4-rc1 conflict in big ways with these hooks,
    so they will have to be resubmitted after that merge if they are still
    needed for the tree.
    
    Bug: 248994334
    Cc: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    Change-Id: Iae15720beaa9ddd2c5fa66263b1c62f792c11370
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -28,7 +28,6 @@
 #include <trace/hooks/cpuidle_psci.h>
 #include <trace/hooks/vmscan.h>
 #include <trace/hooks/avc.h>
-#include <trace/hooks/module.h>
 #include <trace/hooks/selinux.h>
 #include <trace/hooks/syscall_check.h>
 #include <trace/hooks/gic.h>
@@ -80,10 +79,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_insert);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_node_delete);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_node_replace);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_avc_lookup);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_module_core_rw_nx);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_module_init_rw_nx);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_module_permit_before_init);
-EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_module_permit_after_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_selinux_is_initialized);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_mmap_file);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_file_open);
