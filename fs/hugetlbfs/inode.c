b1c2d0ded4dc33f3940d8fe8e5d1ed1f95d204af
Mike Kravetz <mike.kravetz@oracle.com>
2023-06-21 14:24:03 -0700

    hugetlb: revert use of page_cache_next_miss()
    commit fd4aed8d985a3236d0877ff6d0c80ad39d4ce81a upstream.
    
    Ackerley Tng reported an issue with hugetlbfs fallocate as noted in the
    Closes tag.  The issue showed up after the conversion of hugetlb page
    cache lookup code to use page_cache_next_miss.  User visible effects are:
    
    - hugetlbfs fallocate incorrectly returns -EEXIST if pages are presnet
      in the file.
    - hugetlb pages will not be included in core dumps if they need to be
      brought in via GUP.
    - userfaultfd UFFDIO_COPY will not notice pages already present in the
      cache.  It may try to allocate a new page and potentially return
      ENOMEM as opposed to EEXIST.
    
    Revert the use page_cache_next_miss() in hugetlb code.
    
    IMPORTANT NOTE FOR STABLE BACKPORTS:
    This patch will apply cleanly to v6.3.  However, due to the change of
    filemap_get_folio() return values, it will not function correctly.  This
    patch must be modified for stable backports.
    
    [dan.carpenter@linaro.org: fix hugetlbfs_pagecache_present()]
      Link: https://lkml.kernel.org/r/efa86091-6a2c-4064-8f55-9b44e1313015@moroto.mountain
    Link: https://lkml.kernel.org/r/20230621212403.174710-2-mike.kravetz@oracle.com
    Fixes: d0ce0e47b323 ("mm/hugetlb: convert hugetlb fault paths to use alloc_hugetlb_folio()")
    Signed-off-by: Mike Kravetz <mike.kravetz@oracle.com>
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Reported-by: Ackerley Tng <ackerleytng@google.com>
    Closes: https://lore.kernel.org/linux-mm/cover.1683069252.git.ackerleytng@google.com
    Reviewed-by: Sidhartha Kumar <sidhartha.kumar@oracle.com>
    Cc: Erdem Aktas <erdemaktas@google.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Vishal Annapurve <vannapurve@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sidhartha Kumar <sidhartha.kumar@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/hugetlbfs/inode.c
+++ fs/hugetlbfs/inode.c
@@ -821,7 +821,6 @@ static long hugetlbfs_fallocate(struct file *file, int mode, loff_t offset,
 		 */
 		struct folio *folio;
 		unsigned long addr;
-		bool present;
 
 		cond_resched();
 
@@ -845,10 +844,9 @@ static long hugetlbfs_fallocate(struct file *file, int mode, loff_t offset,
 		mutex_lock(&hugetlb_fault_mutex_table[hash]);
 
 		/* See if already present in mapping to avoid alloc/free */
-		rcu_read_lock();
-		present = page_cache_next_miss(mapping, index, 1) != index;
-		rcu_read_unlock();
-		if (present) {
+		folio = filemap_get_folio(mapping, index);
+		if (!IS_ERR(folio)) {
+			folio_put(folio);
 			mutex_unlock(&hugetlb_fault_mutex_table[hash]);
 			hugetlb_drop_vma_policy(&pseudo_vma);
 			continue;
