bf46b6249f2664bb5a3e9a8319bae30ab8dc904c
Borislav Petkov (AMD) <bp@alien8.de>
2023-06-29 17:43:40 +0200

    x86/srso: Add SRSO_NO support
    Upstream commit: 1b5277c0ea0b247393a9c426769fde18cff5e2f6
    
    Add support for the CPUID flag which denotes that the CPU is not
    affected by SRSO.
    
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -734,6 +734,9 @@ void kvm_set_cpu_caps(void)
 		F(NULL_SEL_CLR_BASE) | F(AUTOIBRS) | 0 /* PrefetchCtlMsr */
 	);
 
+	if (cpu_feature_enabled(X86_FEATURE_SRSO_NO))
+		kvm_cpu_cap_set(X86_FEATURE_SRSO_NO);
+
 	/*
 	 * Synthesize "LFENCE is serializing" into the AMD-defined entry in
 	 * KVM's supported CPUID if the feature is reported as supported by the
