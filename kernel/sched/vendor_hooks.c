9f6cb7cdd89d2ea6b5aa0574c2cd0b3e04d55944
Qais Yousef <qyousef@google.com>
2023-07-11 22:37:40 +0000

    ANDROID: fs/proc: Perform priority inheritance around access_remote_vm()
    It holds mmap_sem lock which is a hot path. Some debug daemons can end
    up holding this lock to get the cmdline of a process, which can result
    in slowing down this process.
    
    Add hooks around the calls to allow vendors to implement a simple prio
    inheritance scheme to deal with this.
    
    Bug: 344826816
    Bug: 289412815
    Signed-off-by: Qais Yousef <qyousef@google.com>
    Change-Id: I160637b30e5bd58d5978b25be8a21ce025175ec3
    (cherry picked from commit b821a3c8fc1efe12782061993b4feef026f8ce58)
    [Trivial conflict in include/trace/hooks/sched.h due to new code added.
    And in vendor_hooks.c due to code ordering]
    Signed-off-by: Qais Yousef <qyousef@google.com>

--- kernel/sched/vendor_hooks.c
+++ kernel/sched/vendor_hooks.c
@@ -117,3 +117,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_update_rt_rq_load_avg);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_uclamp_validate);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_sugov_sched_attr);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_iowait);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_prio_inheritance);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_prio_restore);
