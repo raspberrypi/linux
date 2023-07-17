4fb867eea02944c7b0b2f56ca7df868e26b739ed
Greg Kroah-Hartman <gregkh@google.com>
2023-07-17 20:30:28 +0000

    Revert "Revert "sched/core: Prevent race condition between cpuset and __sched_setscheduler()""
    This reverts commit 18695a120055687d79ee4f2644dafdff4cc100d4.
    
    It is fixed properly upstream now in 6.5-rc1, so revert this revert to
    take the correct upstream fix instead as keeping it would cause massive
    merge conflicts and just make things too confusing.
    
    Bug: 120446518 # OoT
    Bug: 151861772
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Lee Jones <joneslee@google.com>
    Change-Id: I4ec6152e015f650d7dee77533c612db22d5c8d62
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7773,6 +7773,9 @@ recheck:
 			return retval;
 	}
 
+	if (pi)
+		cpuset_read_lock();
+
 	/*
 	 * Make sure no PI-waiters arrive (or leave) while we are
 	 * changing the priority of the task:
@@ -7847,6 +7850,8 @@ change:
 	if (unlikely(oldpolicy != -1 && oldpolicy != p->policy)) {
 		policy = oldpolicy = -1;
 		task_rq_unlock(rq, p, &rf);
+		if (pi)
+			cpuset_read_unlock();
 		goto recheck;
 	}
 
@@ -7912,8 +7917,10 @@ change:
 	head = splice_balance_callbacks(rq);
 	task_rq_unlock(rq, p, &rf);
 
-	if (pi)
+	if (pi) {
+		cpuset_read_unlock();
 		rt_mutex_adjust_pi(p);
+	}
 
 	/* Run balance callbacks after we've adjusted the PI chain: */
 	balance_callbacks(rq, head);
@@ -7923,6 +7930,8 @@ change:
 
 unlock:
 	task_rq_unlock(rq, p, &rf);
+	if (pi)
+		cpuset_read_unlock();
 	return retval;
 }
 
@@ -8055,9 +8064,14 @@ do_sched_setscheduler(pid_t pid, int policy, struct sched_param __user *param)
 	rcu_read_lock();
 	retval = -ESRCH;
 	p = find_process_by_pid(pid);
-	if (p != NULL)
-		retval = sched_setscheduler(p, policy, &lparam);
+	if (likely(p))
+		get_task_struct(p);
 	rcu_read_unlock();
+
+	if (likely(p)) {
+		retval = sched_setscheduler(p, policy, &lparam);
+		put_task_struct(p);
+	}
 
 	return retval;
 }
