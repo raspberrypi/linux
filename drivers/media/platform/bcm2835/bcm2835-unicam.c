9a3b9fcbda8bde338cdd309a5ac214c7a0855cc9
Russell <91855044+octopus-russell@users.noreply.github.com>
2023-07-24 17:18:54 +0100

    bcm2835-unicam.c header typo

--- drivers/media/platform/bcm2835/bcm2835-unicam.c
+++ drivers/media/platform/bcm2835/bcm2835-unicam.c
@@ -62,7 +62,7 @@
 #include <linux/uaccess.h>
 #include <linux/videodev2.h>
 
-#include <media/mipi-cs2.h>
+#include <media/mipi-csi2.h>
 #include <media/v4l2-common.h>
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-dev.h>
