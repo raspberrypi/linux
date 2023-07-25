43cb8e4ed667bb14d85d9de6498a0f376428df40
Jacky Liu <qsliu@google.com>
2023-07-25 17:45:12 +0800

    ANDROID: cgroup: Cleanup android_rvh_cgroup_force_kthread_migration
    android_rvh_cgroup_force_kthread_migration was removed by commit
    b0ea1feeefe0 ("Revert "ANDROID: cgroup: Add
    android_rvh_cgroup_force_kthread_migration"") but was then accidentally
    added back by commit 5f657b04f4f2 ("ANDROID: subsystem-specific
    vendor_hooks.c for sched").
    It's not working, remove it again.
    
    Fixes: 5f657b04f4f2 ("ANDROID: subsystem-specific vendor_hooks.c for sched")
    Change-Id: Ia2d39824df2340f6b83050b2805a052ffa57f171
    Signed-off-by: Jacky Liu <qsliu@google.com>

--- include/trace/hooks/cgroup.h
+++ include/trace/hooks/cgroup.h
@@ -17,12 +17,7 @@ DECLARE_HOOK(android_vh_cgroup_set_task,
 
 DECLARE_HOOK(android_vh_cgroup_attach,
 	TP_PROTO(struct cgroup_subsys *ss, struct cgroup_taskset *tset),
-	TP_ARGS(ss, tset))
-
-DECLARE_RESTRICTED_HOOK(android_rvh_cgroup_force_kthread_migration,
-	TP_PROTO(struct task_struct *tsk, struct cgroup *dst_cgrp, bool *force_migration),
-	TP_ARGS(tsk, dst_cgrp, force_migration), 1);
-
+	TP_ARGS(ss, tset));
 
 DECLARE_RESTRICTED_HOOK(android_rvh_cpu_cgroup_attach,
 	TP_PROTO(struct cgroup_taskset *tset),
