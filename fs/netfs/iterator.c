0ba71114c3af9fa44a474d16a12cecd12e3779b9
David Howells <dhowells@redhat.com>
2023-07-26 11:57:56 +0100

    crypto, cifs: fix error handling in extract_iter_to_sg()
    [ Upstream commit f443fd5af5dbd531f880d3645d5dd36976cf087f ]
    
    Fix error handling in extract_iter_to_sg().  Pages need to be unpinned, not
    put in extract_user_to_sg() when handling IOVEC/UBUF sources.
    
    The bug may result in a warning like the following:
    
      WARNING: CPU: 1 PID: 20384 at mm/gup.c:229 __lse_atomic_add arch/arm64/include/asm/atomic_lse.h:27 [inline]
      WARNING: CPU: 1 PID: 20384 at mm/gup.c:229 arch_atomic_add arch/arm64/include/asm/atomic.h:28 [inline]
      WARNING: CPU: 1 PID: 20384 at mm/gup.c:229 raw_atomic_add include/linux/atomic/atomic-arch-fallback.h:537 [inline]
      WARNING: CPU: 1 PID: 20384 at mm/gup.c:229 atomic_add include/linux/atomic/atomic-instrumented.h:105 [inline]
      WARNING: CPU: 1 PID: 20384 at mm/gup.c:229 try_grab_page+0x108/0x160 mm/gup.c:252
      ...
      pc : try_grab_page+0x108/0x160 mm/gup.c:229
      lr : follow_page_pte+0x174/0x3e4 mm/gup.c:651
      ...
      Call trace:
       __lse_atomic_add arch/arm64/include/asm/atomic_lse.h:27 [inline]
       arch_atomic_add arch/arm64/include/asm/atomic.h:28 [inline]
       raw_atomic_add include/linux/atomic/atomic-arch-fallback.h:537 [inline]
       atomic_add include/linux/atomic/atomic-instrumented.h:105 [inline]
       try_grab_page+0x108/0x160 mm/gup.c:252
       follow_pmd_mask mm/gup.c:734 [inline]
       follow_pud_mask mm/gup.c:765 [inline]
       follow_p4d_mask mm/gup.c:782 [inline]
       follow_page_mask+0x12c/0x2e4 mm/gup.c:839
       __get_user_pages+0x174/0x30c mm/gup.c:1217
       __get_user_pages_locked mm/gup.c:1448 [inline]
       __gup_longterm_locked+0x94/0x8f4 mm/gup.c:2142
       internal_get_user_pages_fast+0x970/0xb60 mm/gup.c:3140
       pin_user_pages_fast+0x4c/0x60 mm/gup.c:3246
       iov_iter_extract_user_pages lib/iov_iter.c:1768 [inline]
       iov_iter_extract_pages+0xc8/0x54c lib/iov_iter.c:1831
       extract_user_to_sg lib/scatterlist.c:1123 [inline]
       extract_iter_to_sg lib/scatterlist.c:1349 [inline]
       extract_iter_to_sg+0x26c/0x6fc lib/scatterlist.c:1339
       hash_sendmsg+0xc0/0x43c crypto/algif_hash.c:117
       sock_sendmsg_nosec net/socket.c:725 [inline]
       sock_sendmsg+0x54/0x60 net/socket.c:748
       ____sys_sendmsg+0x270/0x2ac net/socket.c:2494
       ___sys_sendmsg+0x80/0xdc net/socket.c:2548
       __sys_sendmsg+0x68/0xc4 net/socket.c:2577
       __do_sys_sendmsg net/socket.c:2586 [inline]
       __se_sys_sendmsg net/socket.c:2584 [inline]
       __arm64_sys_sendmsg+0x24/0x30 net/socket.c:2584
       __invoke_syscall arch/arm64/kernel/syscall.c:38 [inline]
       invoke_syscall+0x48/0x114 arch/arm64/kernel/syscall.c:52
       el0_svc_common.constprop.0+0x44/0xe4 arch/arm64/kernel/syscall.c:142
       do_el0_svc+0x38/0xa4 arch/arm64/kernel/syscall.c:191
       el0_svc+0x2c/0xb0 arch/arm64/kernel/entry-common.c:647
       el0t_64_sync_handler+0xc0/0xc4 arch/arm64/kernel/entry-common.c:665
       el0t_64_sync+0x19c/0x1a0 arch/arm64/kernel/entry.S:591
    
    Link: https://lkml.kernel.org/r/20571.1690369076@warthog.procyon.org.uk
    Fixes: 018584697533 ("netfs: Add a function to extract an iterator into a scatterlist")
    Reported-by: syzbot+9b82859567f2e50c123e@syzkaller.appspotmail.com
    Link: https://lore.kernel.org/linux-mm/000000000000273d0105ff97bf56@google.com/
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Steve French <stfrench@microsoft.com>
    Cc: Sven Schnelle <svens@linux.ibm.com>
    Cc: Herbert Xu <herbert@gondor.apana.org.au>
    Cc: Jeff Layton <jlayton@kernel.org>
    Cc: Shyam Prasad N <nspmangalore@gmail.com>
    Cc: Rohith Surabattula <rohiths.msft@gmail.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Paolo Abeni <pabeni@redhat.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/netfs/iterator.c
+++ fs/netfs/iterator.c
@@ -151,7 +151,7 @@ static ssize_t netfs_extract_user_to_sg(struct iov_iter *iter,
 
 failed:
 	while (sgtable->nents > sgtable->orig_nents)
-		put_page(sg_page(&sgtable->sgl[--sgtable->nents]));
+		unpin_user_page(sg_page(&sgtable->sgl[--sgtable->nents]));
 	return res;
 }
 
