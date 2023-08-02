faf911577da297019088c2a95641e7fb3fd4bf3b
Phil Elwell <phil@raspberrypi.com>
2023-08-02 15:01:29 +0100

    dt-bindings: input: Add bindings for raspberrypi-button
    Add bindings for the firmware-based button driver.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

---
+++ include/dt-bindings/input/raspberrypi-button.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause OR GPL-2.0 */
+/*
+ * This header provides constants the raspberrypi-button driver.
+ */
+
+#ifndef _DT_BINDINGS_RASPBERRYPI_BUTTON_H
+#define _DT_BINDINGS_RASPBERRYPI_BUTTON_H
+
+#define RASPBERRYPI_BUTTON_POWER 0
+
+#endif
