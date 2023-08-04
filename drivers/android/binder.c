1ffbca5669e5aec32d016a9aada6317539c11e0c
Carlos Llamas <cmllamas@google.com>
2023-08-04 20:32:25 +0000

    Revert "ANDROID: vendor_hooks: Add hooks for improving binder trans"
    This reverts commit 5ec033b0cc6f095f5b08655cca2ddd383b1eebf1.
    
    Vendor hooks for binder shouldn't be used in android-mainline. Please
    send any required out-of-tree functionality upstream instead.
    
    Change-Id: I46ef2b56da7a755e5fdccbfc7aef98464ba7f362
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -577,7 +577,7 @@ static void binder_wakeup_poll_threads_ilocked(struct binder_proc *proc,
 		thread = rb_entry(n, struct binder_thread, rb_node);
 		if (thread->looper & BINDER_LOOPER_STATE_POLL &&
 		    binder_available_for_proc_work_ilocked(thread)) {
-			trace_android_vh_binder_wakeup_ilocked(thread->task, sync, proc);
+			trace_android_vh_binder_wakeup_ilocked(thread->task);
 			if (sync)
 				wake_up_interruptible_sync(&thread->wait);
 			else
@@ -637,7 +637,7 @@ static void binder_wakeup_thread_ilocked(struct binder_proc *proc,
 	assert_spin_locked(&proc->inner_lock);
 
 	if (thread) {
-		trace_android_vh_binder_wakeup_ilocked(thread->task, sync, proc);
+		trace_android_vh_binder_wakeup_ilocked(thread->task);
 		if (sync)
 			wake_up_interruptible_sync(&thread->wait);
 		else
@@ -4498,7 +4498,6 @@ static int binder_wait_for_work(struct binder_thread *thread,
 		if (do_proc_work)
 			list_add(&thread->waiting_thread_node,
 				 &proc->waiting_threads);
-		trace_android_vh_binder_wait_for_work(do_proc_work, thread, proc);
 		binder_inner_proc_unlock(proc);
 		schedule();
 		binder_inner_proc_lock(proc);
@@ -4856,7 +4855,6 @@ retry:
 			trd->sender_pid =
 				task_tgid_nr_ns(sender,
 						task_active_pid_ns(current));
-			trace_android_vh_sync_txn_recvd(thread->task, t_from->task);
 		} else {
 			trd->sender_pid = 0;
 		}
