a4bfc3f2920be55f161ca8dd533481b157cbbee6
Carlos Llamas <cmllamas@google.com>
2023-08-04 20:39:30 +0000

    Revert "ANDROID: binder: Add vendor hooks"
    This reverts commit 37d7ed1b228526cac0b61c7e459129cc96fcac71.
    
    Vendor hooks for binder shouldn't be used in android-mainline. Please
    send any required out-of-tree functionality upstream instead.
    
    Change-Id: Iaae37d2859c3c826ff4980f61bf8c15716bb024d
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- include/trace/hooks/binder.h
+++ include/trace/hooks/binder.h
@@ -21,12 +21,6 @@ DECLARE_HOOK(android_vh_binder_set_priority,
 DECLARE_HOOK(android_vh_binder_restore_priority,
 	TP_PROTO(struct binder_transaction *t, struct task_struct *task),
 	TP_ARGS(t, task));
-DECLARE_HOOK(android_vh_binder_wakeup_poll_threads_ilocked,
-	TP_PROTO(struct task_struct *task),
-	TP_ARGS(task));
-DECLARE_HOOK(android_vh_binder_wakeup_thread_ilocked,
-	TP_PROTO(struct task_struct *task),
-	TP_ARGS(task));
 #endif /* _TRACE_HOOK_BINDER_H */
 /* This part must be outside protection */
 #include <trace/define_trace.h>
