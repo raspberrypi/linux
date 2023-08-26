9e6075e112ee38bd2ac2309b925b4b74759054d2
Damien Le Moal <dlemoal@kernel.org>
2023-08-26 12:48:33 +0900

    scsi: Remove scsi device no_start_on_resume flag
    [ Upstream commit c4367ac83805a2322268c9736cd8ef9124063424 ]
    
    The scsi device flag no_start_on_resume is not set by any scsi low
    level driver. Remove it. This reverts the changes introduced by commit
    0a8589055936 ("ata,scsi: do not issue START STOP UNIT on resume").
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>
    Reviewed-by: Hannes Reinecke <hare@suse.de>
    Tested-by: Chia-Lin Kao (AceLan) <acelan.kao@canonical.com>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reviewed-by: Martin K. Petersen <martin.petersen@oracle.com>
    Stable-dep-of: 7a6bbc2829d4 ("scsi: sd: Do not repeat the starting disk message")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/scsi/sd.c
+++ drivers/scsi/sd.c
@@ -3968,7 +3968,7 @@ static int sd_resume(struct device *dev)
 static int sd_resume_common(struct device *dev, bool runtime)
 {
 	struct scsi_disk *sdkp = dev_get_drvdata(dev);
-	int ret = 0;
+	int ret;
 
 	if (!sdkp)	/* E.g.: runtime resume at the start of sd_probe() */
 		return 0;
@@ -3978,11 +3978,8 @@ static int sd_resume_common(struct device *dev, bool runtime)
 		return 0;
 	}
 
-	if (!sdkp->device->no_start_on_resume) {
-		sd_printk(KERN_NOTICE, sdkp, "Starting disk\n");
-		ret = sd_start_stop_device(sdkp, 1);
-	}
-
+	sd_printk(KERN_NOTICE, sdkp, "Starting disk\n");
+	ret = sd_start_stop_device(sdkp, 1);
 	if (!ret) {
 		sd_resume(dev);
 		sdkp->suspended = false;
