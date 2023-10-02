a8187a9ca2530372da49e5001bcfe2fc83b5dd95
Amir Goldstein <amir73il@gmail.com>
2023-10-02 14:21:49 +0300

    ovl: make use of ->layers safe in rcu pathwalk
    [ Upstream commit a535116d80339dbfe50b9b81b2f808c69eefbbc3 ]
    
    ovl_permission() accesses ->layers[...].mnt; we can't have ->layers
    freed without an RCU delay on fs shutdown.
    
    Fortunately, kern_unmount_array() that is used to drop those mounts
    does include an RCU delay, so freeing is delayed; unfortunately, the
    array passed to kern_unmount_array() is formed by mangling ->layers
    contents and that happens without any delays.
    
    The ->layers[...].name string entries are used to store the strings to
    display in "lowerdir=..." by ovl_show_options().  Those entries are not
    accessed in RCU walk.
    
    Move the name strings into a separate array ofs->config.lowerdirs and
    reuse the ofs->config.lowerdirs array as the temporary mount array to
    pass to kern_unmount_array().
    
    Reported-by: Al Viro <viro@zeniv.linux.org.uk>
    Link: https://lore.kernel.org/r/20231002023711.GP3389589@ZenIV/
    Acked-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Stable-dep-of: 32db51070850 ("ovl: fix regression in showing lowerdir mount option")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/overlayfs/ovl_entry.h
+++ fs/overlayfs/ovl_entry.h
@@ -8,6 +8,7 @@
 struct ovl_config {
 	char *upperdir;
 	char *workdir;
+	char **lowerdirs;
 	bool default_permissions;
 	int redirect_mode;
 	bool index;
@@ -38,16 +39,7 @@ struct ovl_layer {
 	int idx;
 	/* One fsid per unique underlying sb (upper fsid == 0) */
 	int fsid;
-	char *name;
 };
-
-/*
- * ovl_free_fs() relies on @mnt being the first member when unmounting
- * the private mounts created for each layer. Let's check both the
- * offset and type.
- */
-static_assert(offsetof(struct ovl_layer, mnt) == 0);
-static_assert(__same_type(typeof_member(struct ovl_layer, mnt), struct vfsmount *));
 
 struct ovl_path {
 	const struct ovl_layer *layer;
