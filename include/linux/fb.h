9e5ec48998ecec05987c9ae222b3a0871ebe5add
Dave Stevenson <dave.stevenson@raspberrypi.com>
2023-10-09 16:32:45 +0100

    fbdev: Allow client to request a particular /dev/fbN node
    Add a flag custom_fb_num to denote that the client has
    requested a specific fbdev node number via node.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -501,6 +501,7 @@ struct fb_info {
 	void *par;
 
 	bool skip_vt_switch; /* no VT switch on suspend/resume required */
+	bool custom_fb_num; /* Use value in node as the preferred node number */
 };
 
 /* This will go away
@@ -589,6 +590,7 @@ extern ssize_t fb_sys_write(struct fb_info *info, const char __user *buf,
 	.fb_imageblit	= sys_imageblit
 
 /* fbmem.c */
+extern void fb_set_lowest_dynamic_fb(int min_fb_dev);
 extern int register_framebuffer(struct fb_info *fb_info);
 extern void unregister_framebuffer(struct fb_info *fb_info);
 extern int fb_prepare_logo(struct fb_info *fb_info, int rotate);
