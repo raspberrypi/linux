786e8c64ce72d4ae0e1a92b0499124cbf70f00b0
Naushir Patuck <naush@raspberrypi.com>
2023-10-10 12:41:15 +0100

    drivers: media: rp1_cfe: Remove PISP specific MBUS formats
    Remove the MEDIA_BUS_FMT_PISP* format codcs entirely. For the image
    pad formats, use the 16-bit Bayer format mbus codes instead. For the
    config and stats pad formats, use MEDIA_BUS_FMT_FIXED.
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- drivers/media/platform/raspberrypi/rp1_cfe/cfe_fmts.h
+++ drivers/media/platform/raspberrypi/rp1_cfe/cfe_fmts.h
@@ -215,25 +215,25 @@ static const struct cfe_fmt formats[] = {
 	/* PiSP Compressed Mode 1 */
 	{
 		.fourcc = V4L2_PIX_FMT_PISP_COMP1_RGGB,
-		.code = MEDIA_BUS_FMT_PISP_COMP1_RGGB,
+		.code = MEDIA_BUS_FMT_SRGGB16_1X16,
 		.depth = 8,
 		.flags = CFE_FORMAT_FLAG_FE_OUT,
 	},
 	{
 		.fourcc = V4L2_PIX_FMT_PISP_COMP1_BGGR,
-		.code = MEDIA_BUS_FMT_PISP_COMP1_BGGR,
+		.code = MEDIA_BUS_FMT_SBGGR16_1X16,
 		.depth = 8,
 		.flags = CFE_FORMAT_FLAG_FE_OUT,
 	},
 	{
 		.fourcc = V4L2_PIX_FMT_PISP_COMP1_GBRG,
-		.code = MEDIA_BUS_FMT_PISP_COMP1_GBRG,
+		.code = MEDIA_BUS_FMT_SGBRG16_1X16,
 		.depth = 8,
 		.flags = CFE_FORMAT_FLAG_FE_OUT,
 	},
 	{
 		.fourcc = V4L2_PIX_FMT_PISP_COMP1_GRBG,
-		.code = MEDIA_BUS_FMT_PISP_COMP1_GRBG,
+		.code = MEDIA_BUS_FMT_SGRBG16_1X16,
 		.depth = 8,
 		.flags = CFE_FORMAT_FLAG_FE_OUT,
 	},
@@ -283,10 +283,12 @@ static const struct cfe_fmt formats[] = {
 	/* Frontend formats */
 	{
 		.fourcc = V4L2_META_FMT_RPI_FE_CFG,
+		.code = MEDIA_BUS_FMT_FIXED,
 		.flags = CFE_FORMAT_FLAG_META_OUT,
 	},
 	{
 		.fourcc = V4L2_META_FMT_RPI_FE_STATS,
+		.code = MEDIA_BUS_FMT_FIXED,
 		.flags = CFE_FORMAT_FLAG_META_CAP,
 	},
 };
