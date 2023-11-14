9544cd30b6cb028343450bcdd68472a2c37aeb2d
Evan Green <evan@rivosinc.com>
2023-11-14 09:12:39 -0500

    BACKPORT: riscv: add ISA extension parsing for scalar crypto
    The Scalar Crypto specification defines Zk as a shorthand for the
    Zkn, Zkr and Zkt extensions. The same follows for both Zkn, Zks and Zbk,
    which are all shorthands for various other extensions. The detailed
    breakdown can be found in their dt-binding entries.
    
    Since Zkn also implies the Zbkb, Zbkc and Zbkx extensions, simply passing
    "zk" through a DT should enable all of Zbkb, Zbkc, Zbkx, Zkn, Zkr and Zkt.
    For example, setting the "riscv,isa" DT property to "rv64imafdc_zk"
    should generate the following cpuinfo output:
    "rv64imafdc_zicntr_zicsr_zifencei_zihpm_zbkb_zbkc_zbkx_zknd_zkne_zknh_zkr_zkt"
    
    riscv_isa_ext_data grows a pair of new members, to permit setting the
    relevant bits for "bundled" extensions, both while parsing the ISA string
    and the new dedicated extension properties.
    
    Co-developed-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Evan Green <evan@rivosinc.com>
    Signed-off-by: Clément Léger <cleger@rivosinc.com>
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Link: https://lore.kernel.org/r/20231114141256.126749-4-cleger@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    (cherry picked from commit 0d8295ed975b6df487f0b4018770a60b070fc76d)
    
    Conflicts:
    	arch/riscv/include/asm/cpufeature.h
            arch/riscv/include/asm/hwcap.h
    
    (The conflicts were due to missing "riscv: Rearrange hwcap.h and
    cpufeature.h".  That would bring in another chain of dependencies, so
    for now I just resolved the conflicts in this commit instead.)
    
    Bug: 332317531
    Test: For whole topic: booted kernel in QEMU and ran crypto self-tests
    Change-Id: Iedec030391d86009b70492126c25dabe2de74de2
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- arch/riscv/include/asm/hwcap.h
+++ arch/riscv/include/asm/hwcap.h
@@ -61,8 +61,19 @@
 #define RISCV_ISA_EXT_SMSTATEEN		43
 #define RISCV_ISA_EXT_ZICOND		44
 #define RISCV_ISA_EXT_ZBC		45
+#define RISCV_ISA_EXT_ZBKB		46
+#define RISCV_ISA_EXT_ZBKC		47
+#define RISCV_ISA_EXT_ZBKX		48
+#define RISCV_ISA_EXT_ZKND		49
+#define RISCV_ISA_EXT_ZKNE		50
+#define RISCV_ISA_EXT_ZKNH		51
+#define RISCV_ISA_EXT_ZKR		52
+#define RISCV_ISA_EXT_ZKSED		53
+#define RISCV_ISA_EXT_ZKSH		54
+#define RISCV_ISA_EXT_ZKT		55
 
 #define RISCV_ISA_EXT_MAX		64
+#define RISCV_ISA_EXT_INVALID		U32_MAX
 
 #ifdef CONFIG_RISCV_M_MODE
 #define RISCV_ISA_EXT_SxAIA		RISCV_ISA_EXT_SMAIA
@@ -80,6 +91,8 @@ struct riscv_isa_ext_data {
 	const unsigned int id;
 	const char *name;
 	const char *property;
+	const unsigned int *subset_ext_ids;
+	const unsigned int subset_ext_size;
 };
 
 extern const struct riscv_isa_ext_data riscv_isa_ext[];
@@ -90,7 +103,7 @@ unsigned long riscv_isa_extension_base(const unsigned long *isa_bitmap);
 
 #define riscv_isa_extension_mask(ext) BIT_MASK(RISCV_ISA_EXT_##ext)
 
-bool __riscv_isa_extension_available(const unsigned long *isa_bitmap, int bit);
+bool __riscv_isa_extension_available(const unsigned long *isa_bitmap, unsigned int bit);
 #define riscv_isa_extension_available(isa_bitmap, ext)	\
 	__riscv_isa_extension_available(isa_bitmap, RISCV_ISA_EXT_##ext)
 
