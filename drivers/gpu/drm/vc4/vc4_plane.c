736b39b394a30ae5c443865eb597e5c9bb59853b
Dave Stevenson <dave.stevenson@raspberrypi.com>
2023-11-17 14:43:44 +0000

    drm/vc4: Drop planes that are completely off-screen
    It is permitted for a plane to be configured such that none
    of it is on-screen via either negative dest rectangle X,Y
    offset, or just an offset that is greater than the crtc
    dimensions.
    
    These planes were resized via drm_atomic_helper_check_plane_state
    such that the source rectangle had a zero width or height, but
    they still created a dlist entry even though they contributed
    no pixels. In the case of vc6_plane_mode_set, that it could result
    in negative values being written into registers, which caused
    incorrect behaviour.
    
    Drop planes that result in a source width or height of 0 pixels
    to avoid the incorrect rendering.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/gpu/drm/vc4/vc4_plane.c
+++ drivers/gpu/drm/vc4/vc4_plane.c
@@ -1108,6 +1108,12 @@ static int vc4_plane_mode_set(struct drm_plane *plane,
 	width = vc4_state->src_w[0] >> 16;
 	height = vc4_state->src_h[0] >> 16;
 
+	if (!width || !height) {
+		/* 0 source size probably means the plane is offscreen */
+		vc4_state->dlist_initialized = 1;
+		return 0;
+	}
+
 	/* SCL1 is used for Cb/Cr scaling of planar formats.  For RGB
 	 * and 4:4:4, scl1 should be set to scl0 so both channels of
 	 * the scaler do the same thing.  For YUV, the Y plane needs
@@ -1623,6 +1629,12 @@ static int vc6_plane_mode_set(struct drm_plane *plane,
 	width = vc4_state->src_w[0] >> 16;
 	height = vc4_state->src_h[0] >> 16;
 
+	if (!width || !height) {
+		/* 0 source size probably means the plane is offscreen */
+		vc4_state->dlist_initialized = 1;
+		return 0;
+	}
+
 	/* SCL1 is used for Cb/Cr scaling of planar formats.  For RGB
 	 * and 4:4:4, scl1 should be set to scl0 so both channels of
 	 * the scaler do the same thing.  For YUV, the Y plane needs
@@ -1993,6 +2005,9 @@ int vc4_plane_atomic_check(struct drm_plane *plane,
 		ret = vc4_plane_mode_set(plane, new_plane_state);
 	if (ret)
 		return ret;
+
+	if (!vc4_state->src_w[0] || !vc4_state->src_h[0])
+		return 0;
 
 	ret = vc4_plane_allocate_lbm(new_plane_state);
 	if (ret)
