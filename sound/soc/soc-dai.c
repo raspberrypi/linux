94da565664b04c85e7ae3fce88741b36b9c5fd7f
Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
2023-11-23 10:47:48 +0100

    ASoC: soc-dai: add flag to mute and unmute stream during trigger
    commit f0220575e65abe09c09cd17826a3cdea76e8d58f upstream.
    
    In some setups like Speaker amps which are very sensitive, ex: keeping them
    unmute without actual data stream for very short duration results in a
    static charge and results in pop and clicks. To minimize this, provide a way
    to mute and unmute such codecs during trigger callbacks.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>
    Tested-by: Johan Hovold <johan+linaro@kernel.org>
    Link: https://lore.kernel.org/r/20231027105747.32450-2-srinivas.kandagatla@linaro.org
    Signed-off-by: Mark Brown <broonie@kernel.org>
    [ johan: backport to v6.6.2 ]
    Signed-off-by: Johan Hovold <johan+linaro@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- sound/soc/soc-dai.c
+++ sound/soc/soc-dai.c
@@ -641,6 +641,10 @@ int snd_soc_pcm_dai_trigger(struct snd_pcm_substream *substream,
 			ret = soc_dai_trigger(dai, substream, cmd);
 			if (ret < 0)
 				break;
+
+			if (dai->driver->ops && dai->driver->ops->mute_unmute_on_trigger)
+				snd_soc_dai_digital_mute(dai, 0, substream->stream);
+
 			soc_dai_mark_push(dai, substream, trigger);
 		}
 		break;
@@ -650,6 +654,9 @@ int snd_soc_pcm_dai_trigger(struct snd_pcm_substream *substream,
 		for_each_rtd_dais(rtd, i, dai) {
 			if (rollback && !soc_dai_mark_match(dai, substream, trigger))
 				continue;
+
+			if (dai->driver->ops && dai->driver->ops->mute_unmute_on_trigger)
+				snd_soc_dai_digital_mute(dai, 1, substream->stream);
 
 			r = soc_dai_trigger(dai, substream, cmd);
 			if (r < 0)
