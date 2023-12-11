46ae25fadf44dd7bdda33197ebe43626637a51a2
Matthew Wilcox (Oracle) <willy@infradead.org>
2023-12-11 16:22:09 +0000

    UPSTREAM: mm: remove some calls to page_add_new_anon_rmap()
    We already have the folio in these functions, we just need to use it.
    folio_add_new_anon_rmap() didn't exist at the time they were converted to
    folios.
    
    Link: https://lkml.kernel.org/r/20231211162214.2146080-5-willy@infradead.org
    Change-Id: I47ce847643da6cee6944d5af31039397663274ce
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 2853b66b601a265306be709b4d86aaff7d92a0fc)
    Bug: 313807618
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- mm/userfaultfd.c
+++ mm/userfaultfd.c
@@ -217,7 +217,7 @@ int mfill_atomic_install_pte(pmd_t *dst_pmd,
 			folio_add_lru(folio);
 		folio_add_file_rmap_pte(folio, page, dst_vma);
 	} else {
-		page_add_new_anon_rmap(page, dst_vma, dst_addr);
+		folio_add_new_anon_rmap(folio, dst_vma, dst_addr);
 		folio_add_lru_vma(folio, dst_vma);
 	}
 
