ed3f5aef2148a3a23fafb74ede73abd1fe387b93
Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
2024-01-31 14:30:15 +0800

    ANDROID: kernel: Add restricted vendor hook in creds
    Add restricted vendor hook for creds, so we get the creds
    information to monitor cred lifetime. During the lifetime,
    we store the creds information in a standalone protected
    memory and keep track of integrity.
    
    These hooks are required for proper execution with the MTK hypervisor.
    
    Bug: 248994334
    Change-Id: I57fbb759452302fa1ba1e720c76bfe671eab96b5
    Signed-off-by: Kuan-Ying Lee <Kuan-Ying.Lee@mediatek.com>
    (cherry picked from commit b04dea9ffcfcce7f3b7fd022dd81062ca7efb936)

--- kernel/cred.c
+++ kernel/cred.c
@@ -20,6 +20,8 @@
 #include <linux/cn_proc.h>
 #include <linux/uidgid.h>
 
+#include <trace/hooks/creds.h>
+
 #if 0
 #define kdebug(FMT, ...)						\
 	printk("[%-5.5s%5u] " FMT "\n",					\
@@ -133,6 +135,7 @@ void exit_creds(struct task_struct *tsk)
 	key_put(tsk->cached_requested_key);
 	tsk->cached_requested_key = NULL;
 #endif
+	trace_android_rvh_exit_creds(tsk, cred);
 }
 
 /**
@@ -433,6 +436,7 @@ int commit_creds(struct cred *new)
 		inc_rlimit_ucounts(new->ucounts, UCOUNT_RLIMIT_NPROC, 1);
 	rcu_assign_pointer(task->real_cred, new);
 	rcu_assign_pointer(task->cred, new);
+	trace_android_rvh_commit_creds(task, new);
 	if (new->user != old->user || new->user_ns != old->user_ns)
 		dec_rlimit_ucounts(old->ucounts, UCOUNT_RLIMIT_NPROC, 1);
 
@@ -497,6 +501,7 @@ const struct cred *override_creds(const struct cred *new)
 	 */
 	get_new_cred((struct cred *)new);
 	rcu_assign_pointer(current->cred, new);
+	trace_android_rvh_override_creds(current, new);
 
 	kdebug("override_creds() = %p{%ld}", old,
 	       atomic_long_read(&old->usage));
@@ -519,6 +524,7 @@ void revert_creds(const struct cred *old)
 	       atomic_long_read(&old->usage));
 
 	rcu_assign_pointer(current->cred, old);
+	trace_android_rvh_revert_creds(current, old);
 	put_cred(override);
 }
 EXPORT_SYMBOL(revert_creds);
