1e124c763fb78a8c5576c29a97ce7f7a6f776dba
Dom Cobley <popcornmix@gmail.com>
2024-02-08 15:05:36 +0000

    Revert "fbdev: Don't cancel deferred work if pagelist empty"
    This reverts commit 1dc23c4753f0d9705f845cc64139b6aa3b476809.

--- drivers/video/fbdev/core/fb_defio.c
+++ drivers/video/fbdev/core/fb_defio.c
@@ -321,8 +321,7 @@ static void fb_deferred_io_lastclose(struct fb_info *info)
 	struct page *page;
 	int i;
 
-	if (!list_empty(&info->fbdefio->pagereflist))
-		cancel_delayed_work_sync(&info->deferred_work);
+	cancel_delayed_work_sync(&info->deferred_work);
 
 	/* clear out the mapping that we setup */
 	for (i = 0 ; i < info->fix.smem_len; i += PAGE_SIZE) {
