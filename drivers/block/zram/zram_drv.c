104c34d6f793113a6fff7e57c283e14b01496fb9
Vinayak Menon <vinmenon@codeaurora.org>
2024-02-12 16:02:43 -0800

    ANDROID: zram: allow zram to allocate CMA pages
    Though zram pages are movable, they aren't allowed to enter
    MIGRATE_CMA pageblocks. zram is not seen to pin pages for
    long which can cause an issue. Moreover allowing zram to
    pick CMA pages can be helpful in cases seen where zram order
    0 alloc fails when there are lots of free cma pages, resulting
    in kswapd or direct reclaim not making enough progress.
    
    Bug: 330201547
    Change-Id: I31f4a21781cdb31982a768daa59e9546d7667b08
    Link: https://lore.kernel.org/linux-mm/4c77bb100706b714213ff840d827a48e40ac9177.1604282969.git.cgoldswo@codeaurora.org/
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Chris Goldsworthy <cgoldswo@codeaurora.org>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    [isaacm@codeaurora.org: Resolve trivial merge conflicts]
    Signed-off-by: Isaac J. Manjarres <isaacm@codeaurora.org>
    Signed-off-by: Sukadev Bhattiprolu <quic_sukadev@quicinc.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1463,13 +1463,14 @@ compress_again:
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
 				__GFP_HIGHMEM |
-				__GFP_MOVABLE);
+				__GFP_MOVABLE |
+				__GFP_CMA);
 	if (IS_ERR_VALUE(handle)) {
 		zcomp_stream_put(zram->comps[ZRAM_PRIMARY_COMP]);
 		atomic64_inc(&zram->stats.writestall);
 		handle = zs_malloc(zram->mem_pool, comp_len,
 				GFP_NOIO | __GFP_HIGHMEM |
-				__GFP_MOVABLE);
+				__GFP_MOVABLE | __GFP_CMA);
 		if (IS_ERR_VALUE(handle))
 			return PTR_ERR((void *)handle);
 
