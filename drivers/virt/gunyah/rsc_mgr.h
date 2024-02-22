01e1f18752ebb91f6670592de463d5a2d66bbe7b
Elliot Berman <quic_eberman@quicinc.com>
2024-02-22 15:16:30 -0800

    FROMLIST: gunyah: rsc_mgr: Add resource manager RPC core
    The resource manager is a special virtual machine which is always
    running on a Gunyah system. It provides APIs for creating and destroying
    VMs, secure memory management, sharing/lending of memory between VMs,
    and setup of inter-VM communication. Calls to the resource manager are
    made via message queues.
    
    This patch implements the basic probing and RPC mechanism to make those
    API calls. Request/response calls can be made with gh_rm_call.
    Drivers can also register to notifications pushed by RM via
    gh_rm_register_notifier
    
    Specific API calls that resource manager supports will be implemented in
    subsequent patches.
    
    Bug: 338347082
    Link: https://lore.kernel.org/all/20240222-gunyah-v17-7-1e9da6763d38@quicinc.com/
    Change-Id: I014020b676a4db46bd7d64cc62df4eda5259a1f3
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

---
+++ drivers/virt/gunyah/rsc_mgr.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+#ifndef __GUNYAH_RSC_MGR_PRIV_H
+#define __GUNYAH_RSC_MGR_PRIV_H
+
+#include <linux/gunyah.h>
+#include <linux/notifier.h>
+#include <linux/types.h>
+
+#define GUNYAH_VMID_INVAL U16_MAX
+
+struct gunyah_rm;
+
+int gunyah_rm_notifier_register(struct gunyah_rm *rm,
+				struct notifier_block *nb);
+int gunyah_rm_notifier_unregister(struct gunyah_rm *rm,
+				  struct notifier_block *nb);
+
+
+int gunyah_rm_call(struct gunyah_rm *rsc_mgr, u32 message_id,
+		   const void *req_buf, size_t req_buf_size, void **resp_buf,
+		   size_t *resp_buf_size);
+
+#endif
