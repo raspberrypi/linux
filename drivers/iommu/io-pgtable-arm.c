BayLibre/android_kernel_common

ANDROID: KVM: arm64: iommu: Alloc domain memory from the pool
As now we can alloc contiguous memory, no need to pass pgd in alloc
domain HVC.

Bug: 277989609
Bug: 278749606
Change-Id: If074047d272e6c330b3bc10e4407ef5d8ed931e9
Signed-off-by: Mostafa Saleh <smostafa@google.com>

drivers/iommu/io-pgtable-arm.c
@@ -84,7 +84,7 @@ static void arm_lpae_free_pgtable(struct io_pgtable *iop)
 {
 	struct arm_lpae_io_pgtable *data = io_pgtable_ops_to_data(iop->ops);
 
-	__arm_lpae_free_pgtable(data, data->start_level, iop->pgd, false);
+	__arm_lpae_free_pgtable(data, data->start_level, iop->pgd);
 	kfree(data);
 }
 

raspberrypi/linux

drivers/iommu/io-pgtable-arm.c
@@ -84,7 +84,7 @@ static void arm_lpae_free_pgtable(struct io_pgtable *iop)
 {
 	struct arm_lpae_io_pgtable *data = io_pgtable_to_data(iop);
 
-	__arm_lpae_free_pgtable(data, data->start_level, data->pgd, false);
+	__arm_lpae_free_pgtable(data, data->start_level, data->pgd);
 	kfree(data);
 }
 