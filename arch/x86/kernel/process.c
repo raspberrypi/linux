BayLibre/android_kernel_common

ANDROID: 16K: x86_64: Allow stack randomization of twice page-size
In page-compat mode, let stack randomization be 2x__PAGE_SIZE instead of
the hard coded 8KB.

Bug: 315325080
Bug: 302403436
Change-Id: I63d44081bdabb1c4fafd69125351682c3c1ef632
Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

arch/x86/kernel/process.c
@@ -5,6 +5,7 @@
 #include <linux/kernel.h>
 #include <linux/mm.h>
 #include <linux/smp.h>
+#include <linux/page_size_compat.h>
 #include <linux/prctl.h>
 #include <linux/slab.h>
 #include <linux/sched.h>
@@ -985,7 +986,7 @@ early_param("idle", idle_setup);
 unsigned long arch_align_stack(unsigned long sp)
 {
 	if (!(current->personality & ADDR_NO_RANDOMIZE) && randomize_va_space)
-		sp -= prandom_u32_max(8192);
+		sp -= prandom_u32_max(__PAGE_SIZE << 1);
 	return sp & ~0xf;
 }
 

raspberrypi/linux

Change-Id: I9ab3c03a9a0fcd7210a1f0dc7834975ae2807473

arch/x86/kernel/process.c
@@ -4,6 +4,7 @@
 #include <linux/errno.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
+#include <linux/page_size_compat.h>
 #include <linux/smp.h>
 #include <linux/cpu.h>
 #include <linux/prctl.h>
@@ -1024,7 +1025,7 @@ early_param("idle", idle_setup);
 unsigned long arch_align_stack(unsigned long sp)
 {
 	if (!(current->personality & ADDR_NO_RANDOMIZE) && randomize_va_space)
-		sp -= get_random_u32_below(8192);
+		sp -= get_random_u32_below(__PAGE_SIZE << 1);
 	return sp & ~0xf;
 }
 