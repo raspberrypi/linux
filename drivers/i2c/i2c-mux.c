841bb8e98b3383fdb98b43251923b94e6c86f1cf
Phil Elwell <phil@raspberrypi.com>
2024-03-14 10:05:58 +0000

    overlays: i2c-mux: Add 'base' parameter
    Add a 'base' parameter to set an explicit bus number for the channels,
    where the requested bus number is base + channel ID.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/i2c/i2c-mux.c
+++ drivers/i2c/i2c-mux.c
@@ -360,7 +360,7 @@ int i2c_mux_add_adapter(struct i2c_mux_core *muxc,
 
 		of_property_read_u32(dev_node, "base-nr", &base_nr);
 		if (!force_nr)
-			force_nr = base_nr;
+			force_nr = base_nr + chan_id;
 
 		if (muxc->arbitrator)
 			mux_node = of_get_child_by_name(dev_node, "i2c-arb");
