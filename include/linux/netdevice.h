1cb84b885c7d000aa9a008da32a1a6d984c2ad26
Eric Dumazet <edumazet@google.com>
2024-03-14 20:08:45 +0000

    net: move dev->state into net_device_read_txrx group
    [ Upstream commit f6e0a4984c2e7244689ea87b62b433bed9d07e94 ]
    
    dev->state can be read in rx and tx fast paths.
    
    netif_running() which needs dev->state is called from
    - enqueue_to_backlog() [RX path]
    - __dev_direct_xmit()  [TX path]
    
    Fixes: 43a71cd66b9c ("net-device: reorganize net_device fast path variables")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Coco Li <lixiaoyan@google.com>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Link: https://lore.kernel.org/r/20240314200845.3050179-1-edumazet@google.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2144,6 +2144,7 @@ struct net_device {
 		struct pcpu_sw_netstats __percpu	*tstats;
 		struct pcpu_dstats __percpu		*dstats;
 	};
+	unsigned long		state;
 	unsigned int		flags;
 	unsigned short		hard_header_len;
 	netdev_features_t	features;
@@ -2189,7 +2190,6 @@ struct net_device {
 	 *	part of the usual set specified in Space.c.
 	 */
 
-	unsigned long		state;
 
 	struct list_head	dev_list;
 	struct list_head	napi_list;
