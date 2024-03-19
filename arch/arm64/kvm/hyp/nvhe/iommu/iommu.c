1d4500a65460d63d3832c044ce8654e910bc8132
Vincent Donnefort <vdonnefort@google.com>
2024-03-19 16:08:51 +0000

    ANDROID: KVM: arm64: Fix un-initialized iommu into mgt_allocator_ops
    iommu_host_pool is only initialized after the iommu driver has (i.e.
    kvm_iommu_ops is set). Meanwhile, mgt_allocator_ops can be called at any
    time. There's therefore a need to check if the driver (and by extension
    iommu_host_pool) has been initialized into each mgt_allocator_ops.
    
    Bug: 277989609
    Bug: 278749606
    Bug: 330300498
    Fixes: 73a7ad55e3aa ("ANDROID: KVM: arm64: Add {refill,recliam}_hyp_pool")
    Change-Id: I7afe1628ec4898e203c89b98f1708cea1593175d
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
+++ arch/arm64/kvm/hyp/nvhe/iommu/iommu.c
@@ -84,16 +84,29 @@ int kvm_iommu_request(struct kvm_hyp_req *req)
 
 int kvm_iommu_refill(struct kvm_hyp_memcache *host_mc)
 {
+	if (!kvm_iommu_ops)
+		return -EINVAL;
+
+	/* Paired with smp_wmb() in kvm_iommu_init() */
+	smp_rmb();
 	return refill_hyp_pool(&iommu_host_pool, host_mc);
 }
 
 void kvm_iommu_reclaim(struct kvm_hyp_memcache *host_mc, int target)
 {
+	if (!kvm_iommu_ops)
+		return;
+
+	smp_rmb();
 	reclaim_hyp_pool(&iommu_host_pool, host_mc, target);
 }
 
 int kvm_iommu_reclaimable(void)
 {
+	if (!kvm_iommu_ops)
+		return 0;
+
+	smp_rmb();
 	return hyp_pool_free_pages(&iommu_host_pool);
 }
 
@@ -549,7 +562,11 @@ int kvm_iommu_init(struct kvm_iommu_ops *ops, unsigned long init_arg)
 		return ret;
 
 	ret = hyp_pool_init_empty(&iommu_host_pool, 64 /* order = 6*/);
+	if (!ret) {
+		/* Ensure iommu_host_pool is ready _before_ iommu_ops is set */
+		smp_wmb();
+		kvm_iommu_ops = ops;
+	}
 
-	kvm_iommu_ops = ops;
 	return ret;
 }
