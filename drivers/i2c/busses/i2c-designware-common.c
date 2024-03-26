82e016e0101caa69492ebac22dec85dc4857d8b1
Phil Elwell <phil@raspberrypi.com>
2024-03-26 15:57:46 +0000

    i2c: designware: Add support for bus clear feature
    Newer versions of the DesignWare I2C block support the detection of
    stuck signals, and a mechanism to recover from them. Add the required
    software support to the driver.
    
    This change was prompted by the observation that reading a single byte
    from register 0 of a VEML7700 seems to cause it to issue an ACK too
    early, and the controller to complain about losing arbitration. There
    is a suspicion that this may be a more widespread problem, but at least
    this patch prevents the bus from locking up.
    
    See: https://github.com/raspberrypi/linux/issues/6057
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/i2c/busses/i2c-designware-common.c
+++ drivers/i2c/busses/i2c-designware-common.c
@@ -63,6 +63,8 @@ static const char *const abort_sources[] = {
 		"slave lost the bus while transmitting data to a remote master",
 	[ABRT_SLAVE_RD_INTX] =
 		"incorrect slave-transmitter mode configuration",
+	[ABRT_SLAVE_SDA_STUCK_AT_LOW] =
+		"SDA stuck at low",
 };
 
 static int dw_reg_read(void *context, unsigned int reg, unsigned int *val)
@@ -677,8 +679,16 @@ int i2c_dw_wait_bus_not_busy(struct dw_i2c_dev *dev)
 int i2c_dw_handle_tx_abort(struct dw_i2c_dev *dev)
 {
 	unsigned long abort_source = dev->abort_source;
+	unsigned int reg;
 	int i;
 
+	if (abort_source & DW_IC_TX_ABRT_SLAVE_SDA_STUCK_AT_LOW) {
+		regmap_write(dev->map, DW_IC_ENABLE,
+			     DW_IC_ENABLE_ENABLE | DW_IC_ENABLE_BUS_RECOVERY);
+		regmap_read_poll_timeout(dev->map, DW_IC_ENABLE, reg,
+					 !(reg & DW_IC_ENABLE_BUS_RECOVERY),
+					 1100, 200000);
+	}
 	if (abort_source & DW_IC_TX_ABRT_NOACK) {
 		for_each_set_bit(i, &abort_source, ARRAY_SIZE(abort_sources))
 			dev_dbg(dev->dev,
@@ -693,6 +703,8 @@ int i2c_dw_handle_tx_abort(struct dw_i2c_dev *dev)
 		return -EAGAIN;
 	if (abort_source & DW_IC_TX_ABRT_GCALL_READ)
 		return -EINVAL; /* wrong msgs[] data */
+	if (abort_source & DW_IC_TX_ABRT_SLAVE_SDA_STUCK_AT_LOW)
+		return -EREMOTEIO;
 
 	return -EIO;
 }
