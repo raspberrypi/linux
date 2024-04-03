6cee2429a624530592e2f1b0dda4487ecdbcdfef
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2024-04-03 16:29:27 +0200

    Revert "workqueue.c: Increase workqueue name length"
    This reverts commit 43f0cec175f92c7a01e43d5d6f276262670a97ed which is commit
    31c89007285d365aa36f71d8fb0701581c770a27 upstream.
    
    The workqueue patches backported to 6.8.y caused some reported
    regressions, so revert them for now.
    
    Reported-by: Thorsten Leemhuis <regressions@leemhuis.info>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Sasha Levin <sashal@kernel.org>
    Cc: Audra Mitchell <audra@redhat.com>
    Link: https://lore.kernel.org/all/ce4c2f67-c298-48a0-87a3-f933d646c73b@leemhuis.info/
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -108,7 +108,7 @@ enum {
 	RESCUER_NICE_LEVEL	= MIN_NICE,
 	HIGHPRI_NICE_LEVEL	= MIN_NICE,
 
-	WQ_NAME_LEN		= 32,
+	WQ_NAME_LEN		= 24,
 };
 
 /*
@@ -4666,7 +4666,6 @@ struct workqueue_struct *alloc_workqueue(const char *fmt,
 	va_list args;
 	struct workqueue_struct *wq;
 	struct pool_workqueue *pwq;
-	int len;
 
 	/*
 	 * Unbound && max_active == 1 used to imply ordered, which is no longer
@@ -4693,11 +4692,8 @@ struct workqueue_struct *alloc_workqueue(const char *fmt,
 	}
 
 	va_start(args, max_active);
-	len = vsnprintf(wq->name, sizeof(wq->name), fmt, args);
+	vsnprintf(wq->name, sizeof(wq->name), fmt, args);
 	va_end(args);
-
-	if (len >= WQ_NAME_LEN)
-		pr_warn_once("workqueue: name exceeds WQ_NAME_LEN. Truncating to: %s\n", wq->name);
 
 	max_active = max_active ?: WQ_DFL_ACTIVE;
 	max_active = wq_clamp_max_active(max_active, flags, wq->name);
