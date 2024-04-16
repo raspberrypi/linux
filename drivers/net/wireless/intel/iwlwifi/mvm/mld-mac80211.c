1ea06a34f79cff9642b5d329ac99f8a406166878
Johannes Berg <johannes.berg@intel.com>
2024-04-16 13:54:05 +0300

    wifi: iwlwifi: mvm: init vif works only once
    [ Upstream commit 0bcc2155983e03c41b21a356af87ae839a6b3ead ]
    
    It's dangerous to re-initialize works repeatedly, especially
    delayed ones that have an associated timer, and even more so
    if they're not necessarily canceled inbetween. This can be
    the case for these workers here during FW restart scenarios,
    so make sure to initialize it only once.
    
    While at it, also ensure it is cancelled correctly.
    
    Fixes: f67806140220 ("iwlwifi: mvm: disconnect in case of bad channel switch parameters")
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Link: https://msgid.link/20240416134215.ddf8eece5eac.I4164f5c9c444b64a9abbaab14c23858713778e35@changeid
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/intel/iwlwifi/mvm/mld-mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mld-mac80211.c
@@ -13,6 +13,8 @@ static int iwl_mvm_mld_mac_add_interface(struct ieee80211_hw *hw,
 
 	mutex_lock(&mvm->mutex);
 
+	iwl_mvm_mac_init_mvmvif(mvm, mvmvif);
+
 	mvmvif->mvm = mvm;
 
 	/* Not much to do here. The stack will not allow interface
