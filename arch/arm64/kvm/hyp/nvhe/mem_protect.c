e6f69387b3a437213831bf2bbaa8cb780e7a7c4c
Keir Fraser <keirf@google.com>
2024-04-18 11:35:45 +0000

    ANDROID: KVM: arm64: Eagerly map pages when setting host as owner
    Rather than annotating and mapping on demand, simply map as soon
    as ownership changes.
    
    Bug: 278749606
    Bug: 308373293
    Bug: 311571169
    Change-Id: I966058fcb71ce0c9a151dd61d8f6729cc64e04b0
    Signed-off-by: Keir Fraser <keirf@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -662,10 +662,15 @@ int host_stage2_set_owner_locked(phys_addr_t addr, u64 size, u8 owner_id)
 	if (owner_id > KVM_MAX_OWNER_ID)
 		return -EINVAL;
 
-	annotation = kvm_init_invalid_leaf_owner(owner_id);
-
-	ret = host_stage2_try(kvm_pgtable_stage2_annotate, &host_mmu.pgt,
-			      addr, size, &host_s2_pool, annotation);
+	if (owner_id == PKVM_ID_HOST) {
+		prot = default_host_prot(addr_is_memory(addr));
+		ret = host_stage2_idmap_locked(addr, size, prot, false);
+	} else {
+		annotation = kvm_init_invalid_leaf_owner(owner_id);
+		ret = host_stage2_try(kvm_pgtable_stage2_annotate,
+				      &host_mmu.pgt,
+				      addr, size, &host_s2_pool, annotation);
+	}
 	if (ret || !addr_is_memory(addr))
 		return ret;
 
