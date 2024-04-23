8e24beb3c2b08a4763f920399a9cc577ed440a1a
Bob Zhou <bob.zhou@amd.com>
2024-04-23 13:32:39 +0800

    drm/amdgpu: fix double free err_addr pointer warnings
    [ Upstream commit 506c245f3f1cd989cb89811a7f06e04ff8813a0d ]
    
    In amdgpu_umc_bad_page_polling_timeout, the amdgpu_umc_handle_bad_pages
    will be run many times so that double free err_addr in some special case.
    So set the err_addr to NULL to avoid the warnings.
    
    Signed-off-by: Bob Zhou <bob.zhou@amd.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_umc.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_umc.c
@@ -170,6 +170,7 @@ static void amdgpu_umc_handle_bad_pages(struct amdgpu_device *adev,
 	}
 
 	kfree(err_data->err_addr);
+	err_data->err_addr = NULL;
 
 	mutex_unlock(&con->page_retirement_lock);
 }
