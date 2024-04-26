bafe40d9af4f2d581fa946576fb41a6339830c01
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2024-04-26 18:33:19 +0300

    media: uapi: v4l: Don't expose generic metadata formats to userspace
    The generic metadata pixel formats (V4L2_META_FMT_GENERIC_*) are meant
    to be used in conjunction with device-specific media bus codes. Those
    codes are work in progress and not available in the upstream kernel yet.
    To make sure the generic metadata pixel formats won't be used by
    userspace until we have the full infrastructure in place, keep their
    definition private to the kernel for now.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    (cherry picked from commit d69c8429ea80af02e89e5b3eecb78e417ad049c8)
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -880,6 +880,7 @@ struct v4l2_pix_format {
 /* The metadata format identifier for FE configuration buffers. */
 #define V4L2_META_FMT_RPI_FE_STATS v4l2_fourcc('R', 'P', 'F', 'S')
 
+#ifdef __KERNEL__
 /*
  * Line-based metadata formats. Remember to update v4l_fill_fmtdesc() when
  * adding new ones!
@@ -891,6 +892,7 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_GENERIC_CSI2_16	v4l2_fourcc('M', 'C', '1', 'G') /* 16-bit CSI-2 packed 8-bit metadata */
 #define V4L2_META_FMT_GENERIC_CSI2_20	v4l2_fourcc('M', 'C', '1', 'K') /* 20-bit CSI-2 packed 8-bit metadata */
 #define V4L2_META_FMT_GENERIC_CSI2_24	v4l2_fourcc('M', 'C', '1', 'O') /* 24-bit CSI-2 packed 8-bit metadata */
+#endif
 
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
