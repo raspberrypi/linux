raspberrypi/linux

commit 5fb8f1c931c2f690bce45a962711b02a8a101e75

ANDROID: fuse-bpf: add bpf solution for splice read
filemap_splice_read is implemented through readahead and read_folio,
which results in sending fuse-bpf files to FuseDaemon.

When a backing file exists, redirect the splice read to the lowlevel
filesystem.

Bug: 337714402
Signed-off-by: Ed Tsai <ed.tsai@mediatek.com>
Change-Id: Ic22ad18a770fdf6911b2b13483b099da51edab10

fs/fuse/file.c
@@ -2765,6 +2765,20 @@ static int fuse_file_flock(struct file *file, int cmd, struct file_lock *fl)
 	return err;
 }
 
+static ssize_t fuse_splice_read(struct file *in, loff_t *ppos,
+		struct pipe_inode_info *pipe, size_t len, unsigned int flags)
+{
+	struct fuse_file *ff = in->private_data;
+
+#ifdef CONFIG_FUSE_BPF
+	/* TODO - this is simply passthrough, not a proper BPF filter */
+	if (ff->backing_file)
+		return fuse_splice_read_backing(in, ppos, pipe, len, flags);
+#endif
+
+	return filemap_splice_read(in, ppos, pipe, len, flags);
+}
+
 static sector_t fuse_bmap(struct address_space *mapping, sector_t block)
 {
 	struct inode *inode = mapping->host;
@@ -3377,7 +3391,7 @@ static const struct file_operations fuse_file_operations = {
 	.lock		= fuse_file_lock,
 	.get_unmapped_area = thp_get_unmapped_area,
 	.flock		= fuse_file_flock,
-	.splice_read	= filemap_splice_read,
+	.splice_read	= fuse_splice_read,
 	.splice_write	= iter_file_splice_write,
 	.unlocked_ioctl	= fuse_file_ioctl,
 	.compat_ioctl	= fuse_file_compat_ioctl,

fs/fuse/backing.c
@@ -10,6 +10,7 @@
 #include <linux/filelock.h>
 #include <linux/filter.h>
 #include <linux/fs_stack.h>
+#include <linux/splice.h>
 #include <linux/namei.h>
 
 #include "../internal.h"
@@ -972,6 +973,18 @@ void *fuse_file_write_iter_finalize(struct fuse_bpf_args *fa,
 	struct fuse_write_iter_out *fwio = fa->out_args[0].value;
 
 	return ERR_PTR(fwio->ret);
+}
+
+ssize_t fuse_splice_read_backing(struct file *in, loff_t *ppos,
+		struct pipe_inode_info *pipe, size_t len, unsigned long flags)
+{
+	struct fuse_file *ff = in->private_data;
+	ssize_t ret;
+
+	ret = vfs_splice_read(ff->backing_file, ppos, pipe, len, flags);
+	fuse_file_accessed(in, ff->backing_file);
+
+	return ret;
 }
 
 long fuse_backing_ioctl(struct file *file, unsigned int command, unsigned long arg, int flags)

fs/fuse/fuse_i.h
@@ -1709,6 +1709,9 @@ int fuse_file_write_iter_backing(struct fuse_bpf_args *fa,
 void *fuse_file_write_iter_finalize(struct fuse_bpf_args *fa,
 		struct kiocb *iocb, struct iov_iter *from);
 
+ssize_t fuse_splice_read_backing(struct file *in, loff_t *ppos,
+		struct pipe_inode_info *pipe, size_t len, unsigned long flags);
+
 long fuse_backing_ioctl(struct file *file, unsigned int command, unsigned long arg, int flags);
 
 int fuse_file_flock_backing(struct file *file, int cmd, struct file_lock *fl);

commit bbe55af332b7dee412c2eee61936b2978db2f7d5

ANDROID: fuse-bpf: Fix build breakage without CONFIG_FUSE_BPF
Fix build error if CONFIG_FUSE_BPF not defined:

common/fs/fuse/file.c:2771:20: error: unused variable 'ff' [-Werror,-Wunused-variable]
2771 | struct fuse_file *ff = in->private_data;
| ^~
1 error generated.

Fixes: 5fb8f1c931c2 ("ANDROID: fuse-bpf: add bpf solution for splice read")
Change-Id: I43d194a1d326fd61c5faafbb524f6ca484fb29f6
Signed-off-by: Paul Lawrence <paullawrence@google.com>

fs/fuse/file.c
@@ -2768,9 +2768,9 @@ static int fuse_file_flock(struct file *file, int cmd, struct file_lock *fl)
 static ssize_t fuse_splice_read(struct file *in, loff_t *ppos,
 		struct pipe_inode_info *pipe, size_t len, unsigned int flags)
 {
+#ifdef CONFIG_FUSE_BPF
 	struct fuse_file *ff = in->private_data;
 
-#ifdef CONFIG_FUSE_BPF
 	/* TODO - this is simply passthrough, not a proper BPF filter */
 	if (ff->backing_file)
 		return fuse_splice_read_backing(in, ppos, pipe, len, flags);