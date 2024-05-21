8691bc2283633d86ae24c16a95d24cd6b7d4e112
Naushir Patuck <naush@raspberrypi.com>
2024-05-21 12:56:17 +0100

    drivers: media: pci: Add Hailo accelerator device drivers
    Add version 4.17.1 of the Hailo PCIe device drivers.
    Sourced from https://github.com/hailo-ai/hailort-drivers/
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    
    drivers: media: pcie: hailo: Fix include paths
    
    An attempt to fix the include paths - they look reasonable, but the
    GitHub auto-builds fail.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>
    
    drivers: media: pci: Update Hailo accelerator device driver to v4.18.0
    
    Sourced from https://github.com/hailo-ai/hailort-drivers/
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    
    drivers: media: pci: Add wrapper after removal of follow_pfn
    
    drivers: media: pci: Fix Hailo compile warnings
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>
    
    drivers: media: pci: Update Hailo accelerator device driver to v4.19
    
    Sourced from https://github.com/hailo-ai/hailort-drivers/
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    
    drivers: media: pci: Update Hailo accelerator device driver to v4.20
    
    Sourced from https://github.com/hailo-ai/hailort-drivers
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    
    drivers: pci: hailo: Fix kernel warning when calling find_vdma()
    
    Calling this function without holding the mmap_read_lock causes the
    kernel to throw an error message, spamming the dmesg logs when running
    the Hailo hardware.
    
    Fix it by adding the approprite lock/unlock functions around find_vdma().
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>
    
    drivers: pci: hailo: Better lock handling when calling find_vdma()
    
    Due to possible instabilities, reduce the mmap read lock time to only
    cover the call to find_vdma().
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

---
+++ drivers/media/pci/hailo/src/sysfs.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+/**
+ * Copyright (c) 2019-2024 Hailo Technologies Ltd. All rights reserved.
+ **/
+
+#ifndef _HAILO_PCI_SYSFS_H_
+#define _HAILO_PCI_SYSFS_H_
+
+#include <linux/sysfs.h>
+
+extern const struct attribute_group **g_hailo_dev_groups;
+
+#endif /* _HAILO_PCI_SYSFS_H_ */
