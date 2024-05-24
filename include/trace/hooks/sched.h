cf517265715b61ed142f7bbfbaf6eea27632378d
ryan.xiao <ryan.xiao@mediatek.com>
2024-05-24 18:09:26 +0800

    ANDROID: sched: add vendor hook to modify affinity
    We add vendor hook at __set_cpus_allowed_ptr for modifying the affinity
    mask
    
    Bug: 330475923
    
    Change-Id: I5ed4aee3ec12e0d097fd92118ae4bb1325a77533
    Signed-off-by: ryan.xiao <ryan.xiao@mediatek.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -370,6 +370,11 @@ DECLARE_HOOK(android_vh_account_process_tick_gran,
 	TP_PROTO(struct task_struct *p, struct rq *rq, int user_tick, int *ticks),
 	TP_ARGS(p, rq, user_tick, ticks));
 
+struct affinity_context;
+DECLARE_RESTRICTED_HOOK(android_rvh_set_cpus_allowed_ptr,
+	TP_PROTO(struct task_struct *p, struct affinity_context *ctx, bool *skip_user_ptr),
+	TP_ARGS(p, ctx, skip_user_ptr), 1);
+
 DECLARE_RESTRICTED_HOOK(android_rvh_set_cpus_allowed_comm,
 	TP_PROTO(struct task_struct *p, const struct cpumask *new_mask),
 	TP_ARGS(p, new_mask), 1);
