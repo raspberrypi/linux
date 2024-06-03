836e7c0b32ce38c952f50f17ef1448161c59f226
Dom Cobley <popcornmix@gmail.com>
2024-06-03 19:09:07 +0100

    Revert "net: bcmgenet: Reset RBUF on first open"
    This reverts commit 2334a9d32430ace16406312466445c6c95bf4e1f.

--- drivers/net/ethernet/broadcom/genet/bcmgenet.c
+++ drivers/net/ethernet/broadcom/genet/bcmgenet.c
@@ -3309,7 +3309,7 @@ static void bcmgenet_get_hw_addr(struct bcmgenet_priv *priv,
 }
 
 /* Returns a reusable dma control register value */
-static u32 bcmgenet_dma_disable(struct bcmgenet_priv *priv, bool flush_rx)
+static u32 bcmgenet_dma_disable(struct bcmgenet_priv *priv)
 {
 	unsigned int i;
 	u32 reg;
@@ -3333,14 +3333,6 @@ static u32 bcmgenet_dma_disable(struct bcmgenet_priv *priv, bool flush_rx)
 	bcmgenet_umac_writel(priv, 1, UMAC_TX_FLUSH);
 	udelay(10);
 	bcmgenet_umac_writel(priv, 0, UMAC_TX_FLUSH);
-
-	if (flush_rx) {
-	    reg = bcmgenet_rbuf_ctrl_get(priv);
-	    bcmgenet_rbuf_ctrl_set(priv, reg | BIT(0));
-	    udelay(10);
-	    bcmgenet_rbuf_ctrl_set(priv, reg);
-	    udelay(10);
-	}
 
 	return dma_ctrl;
 }
@@ -3405,8 +3397,8 @@ static int bcmgenet_open(struct net_device *dev)
 
 	bcmgenet_set_hw_addr(priv, dev->dev_addr);
 
-	/* Disable RX/TX DMA and flush TX and RX queues */
-	dma_ctrl = bcmgenet_dma_disable(priv, true);
+	/* Disable RX/TX DMA and flush TX queues */
+	dma_ctrl = bcmgenet_dma_disable(priv);
 
 	/* Reinitialize TDMA and RDMA and SW housekeeping */
 	ret = bcmgenet_init_dma(priv);
@@ -4291,7 +4283,7 @@ static int bcmgenet_resume(struct device *d)
 			bcmgenet_hfb_create_rxnfc_filter(priv, rule);
 
 	/* Disable RX/TX DMA and flush TX queues */
-	dma_ctrl = bcmgenet_dma_disable(priv, false);
+	dma_ctrl = bcmgenet_dma_disable(priv);
 
 	/* Reinitialize TDMA and RDMA and SW housekeeping */
 	ret = bcmgenet_init_dma(priv);
