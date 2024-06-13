30febe1eb2020c206b9e1017d0670814c4f628e6
William McVicker <willmcvicker@google.com>
2024-06-13 22:31:20 +0000

    Revert "Revert "ANDROID: syscall_check: add vendor hook for open..."
    Revert submission 3100087
    
    Reason for revert: This vendor hook is used by mediatek and was added
    to their symbol list in https://r.android.com/3129673.
    
    Reverted changes: /q/submissionid:3100087
    
    Bug: 346668305
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: I651a1caeb8ebdbe7dfde0908b20f27c028244921

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -208,6 +208,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_exit_creds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_override_creds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_revert_creds);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_mmap_file);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_check_file_open);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_disable_thermal_cooling_stats);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_v3_suspend);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_gic_v3_set_affinity);
--- fs/open.c
+++ fs/open.c
@@ -36,6 +36,7 @@
 #include <linux/filelock.h>
 
 #include "internal.h"
+#include <trace/hooks/syscall_check.h>
 
 int do_truncate(struct mnt_idmap *idmap, struct dentry *dentry,
 		loff_t length, unsigned int time_attrs, struct file *filp)
@@ -912,6 +913,7 @@ static int do_dentry_open(struct file *f,
 		error = -ENODEV;
 		goto cleanup_all;
 	}
+	trace_android_vh_check_file_open(f);
 
 	error = security_file_open(f);
 	if (error)
