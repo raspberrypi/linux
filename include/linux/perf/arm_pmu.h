70333072b0b4ed1ec00b5f845c81c341d9d93096
Anshuman Khandual <anshuman.khandual@arm.com>
2024-06-13 11:47:25 +0530

    drivers: perf: arm_pmu: Add infrastructure for branch stack sampling
    In order to support the Branch Record Buffer Extension (BRBE), we need to
    extend the arm_pmu framework with some basic infrastructure for branch
    stack sampling which arm_pmu drivers can opt-in to using. Subsequent
    patches will use this to add support for BRBE in the PMUv3 driver.
    
    With BRBE, the hardware records branches into a hardware FIFO, which will
    be sampled by software when perf events overflow. A task may be context-
    switched an arbitrary number of times between overflows, and to avoid
    losing samples we need to save the current records when a task is context-
    switched out. To do these we'll need to use the pmu::sched_task() callback,
    and we'll also need to allocate some per-task storage space via event flag
    PERF_ATTACH_TASK_DATA.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    ----
    Changes in V18:
    
    - Scan valid branch stack events in armpmu_start() to create merged filter
    - Updated the commit message
    
     drivers/perf/arm_pmu.c       | 42 +++++++++++++++++++++++++++++++++---
     include/linux/perf/arm_pmu.h | 32 ++++++++++++++++++++++++++-
     2 files changed, 70 insertions(+), 4 deletions(-)

--- include/linux/perf/arm_pmu.h
+++ include/linux/perf/arm_pmu.h
@@ -46,6 +46,18 @@ static_assert((PERF_EVENT_FLAG_ARCH & ARMPMU_EVT_63BIT) == ARMPMU_EVT_63BIT);
 	},								\
 }
 
+/*
+ * Maximum branch record entries which could be processed
+ * for core perf branch stack sampling support, regardless
+ * of the hardware support available on a given ARM PMU.
+ */
+#define MAX_BRANCH_RECORDS 64
+
+struct branch_records {
+	struct perf_branch_stack	branch_stack;
+	struct perf_branch_entry	branch_entries[MAX_BRANCH_RECORDS];
+};
+
 /* The events for a given PMU register set. */
 struct pmu_hw_events {
 	/*
@@ -66,6 +78,17 @@ struct pmu_hw_events {
 	struct arm_pmu		*percpu_pmu;
 
 	int irq;
+
+	struct branch_records	*branches;
+
+	/* Active context for task events */
+	void			*branch_context;
+
+	/* Active events requesting branch records */
+	unsigned int		branch_users;
+
+	/* Active branch sample type filters */
+	unsigned long		branch_sample_type;
 };
 
 enum armpmu_attr_groups {
@@ -96,8 +119,15 @@ struct arm_pmu {
 	void		(*stop)(struct arm_pmu *);
 	void		(*reset)(void *);
 	int		(*map_event)(struct perf_event *event);
+	void		(*sched_task)(struct perf_event_pmu_context *pmu_ctx, bool sched_in);
+	bool		(*branch_stack_init)(struct perf_event *event);
+	void		(*branch_stack_add)(struct perf_event *event, struct pmu_hw_events *cpuc);
+	void		(*branch_stack_del)(struct perf_event *event, struct pmu_hw_events *cpuc);
+	void		(*branch_stack_reset)(void);
 	int		num_events;
-	bool		secure_access; /* 32-bit ARM only */
+	unsigned int	secure_access	: 1, /* 32-bit ARM only */
+			has_branch_stack: 1, /* 64-bit ARM only */
+			reserved	: 30;
 #define ARMV8_PMUV3_MAX_COMMON_EVENTS		0x40
 	DECLARE_BITMAP(pmceid_bitmap, ARMV8_PMUV3_MAX_COMMON_EVENTS);
 #define ARMV8_PMUV3_EXT_COMMON_EVENT_BASE	0x4000
