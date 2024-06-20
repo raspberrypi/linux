3c0e2823d280506fa6d8afd04ca5ecb0b8e8c82b
Jonathan Bell <jonathan@raspberrypi.com>
2024-06-20 14:31:20 +0100

    mmc: restrict posted write counts for SD cards in CQ mode
    Command Queueing requires Write Cache and Power off Notification support
    from the card - but using the write cache forms a contract with the host
    whereby the card expects to be told about impending power-down.
    
    The implication is that (for performance) the card can do unsafe things
    with pending write data - including reordering what gets committed to
    nonvolatile storage at what time.
    
    Exposed SD slots and platforms powered by hotpluggable means (i.e.
    Raspberry Pis) can't guarantee that surprise removal won't happen.
    
    To limit the scope for cards to invent new ways to trash filesystems,
    limit pending writes to 1 (equivalent to the non-CQ behaviour).
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>
    
    fixup: mmc: restrict posted write counts for SD cards in CQ mode
    
    Leaving card->max_posted_writes unintialised was a bad thing to do.
    
    Also, cqe_enable is 1 if hsq is enabled as hsq substitutes the cqhci
    implementation with its own.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -1657,6 +1657,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		card->ocr = ocr;
 		card->type = MMC_TYPE_MMC;
 		card->rca = 1;
+		card->max_posted_writes = 1;
 		memcpy(card->raw_cid, cid, sizeof(card->raw_cid));
 	}
 
@@ -1922,6 +1923,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 				pr_info("%s: Host Software Queue enabled\n",
 					mmc_hostname(host));
 			}
+			card->max_posted_writes = card->ext_csd.cmdq_depth;
 		}
 	}
 
