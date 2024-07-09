5f323d579223be129903693dc7d806a0bc39b28d
David Lechner <dlechner@baylibre.com>
2024-07-08 20:05:29 -0500

    spi: add defer_optimize_message controller flag
    [ Upstream commit ca52aa4c60f76566601b42e935b8a78f0fb4f8eb ]
    
    Adding spi_optimize_message() broke the spi-mux driver because it
    calls spi_async() from it's transfer_one_message() callback. This
    resulted in passing an incorrectly optimized message to the controller.
    For example, if the underlying controller has an optimize_message()
    callback, this would have not been called and can cause a crash when
    the underlying controller driver tries to transfer the message.
    
    Also, since the spi-mux driver swaps out the controller pointer by
    replacing msg->spi, __spi_unoptimize_message() was being called with a
    different controller than the one used in __spi_optimize_message(). This
    could cause a crash when attempting to free the message resources when
    __spi_unoptimize_message() is called in spi_finalize_current_message()
    since it is being called with a controller that did not allocate the
    resources.
    
    This is fixed by adding a defer_optimize_message flag for controllers.
    This flag causes all of the spi_[maybe_][un]optimize_message() calls to
    be a no-op (other than attaching a pointer to the spi device to the
    message).
    
    This allows the spi-mux driver to pass an unmodified message to
    spi_async() in spi_mux_transfer_one_message() after the spi device has
    been swapped out. This causes __spi_optimize_message() and
    __spi_unoptimize_message() to be called only once per message and with
    the correct/same controller in each case.
    
    Reported-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Closes: https://lore.kernel.org/linux-spi/Zn6HMrYG2b7epUxT@pengutronix.de/
    Reported-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Closes: https://lore.kernel.org/linux-spi/20240628-awesome-discerning-bear-1621f9-mkl@pengutronix.de/
    Fixes: 7b1d87af14d9 ("spi: add spi_optimize_message() APIs")
    Signed-off-by: David Lechner <dlechner@baylibre.com>
    Link: https://patch.msgid.link/20240708-spi-mux-fix-v1-2-6c8845193128@baylibre.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -2137,7 +2137,8 @@ static void __spi_unoptimize_message(struct spi_message *msg)
  */
 static void spi_maybe_unoptimize_message(struct spi_message *msg)
 {
-	if (!msg->pre_optimized && msg->optimized)
+	if (!msg->pre_optimized && msg->optimized &&
+	    !msg->spi->controller->defer_optimize_message)
 		__spi_unoptimize_message(msg);
 }
 
@@ -4285,6 +4286,11 @@ static int __spi_optimize_message(struct spi_device *spi,
 static int spi_maybe_optimize_message(struct spi_device *spi,
 				      struct spi_message *msg)
 {
+	if (spi->controller->defer_optimize_message) {
+		msg->spi = spi;
+		return 0;
+	}
+
 	if (msg->pre_optimized)
 		return 0;
 
@@ -4315,6 +4321,13 @@ int spi_optimize_message(struct spi_device *spi, struct spi_message *msg)
 {
 	int ret;
 
+	/*
+	 * Pre-optimization is not supported and optimization is deferred e.g.
+	 * when using spi-mux.
+	 */
+	if (spi->controller->defer_optimize_message)
+		return 0;
+
 	ret = __spi_optimize_message(spi, msg);
 	if (ret)
 		return ret;
@@ -4341,6 +4354,9 @@ EXPORT_SYMBOL_GPL(spi_optimize_message);
  */
 void spi_unoptimize_message(struct spi_message *msg)
 {
+	if (msg->spi->controller->defer_optimize_message)
+		return;
+
 	__spi_unoptimize_message(msg);
 	msg->pre_optimized = false;
 }
