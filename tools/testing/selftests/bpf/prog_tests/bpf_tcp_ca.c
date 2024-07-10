691ec7043122c9c8c46d84f6e6cd85d13d50cd93
Geliang Tang <tanggeliang@kylinos.cn>
2024-07-10 21:10:16 +0800

    selftests/bpf: Null checks for links in bpf_tcp_ca
    [ Upstream commit eef0532e900c20a6760da829e82dac3ee18688c5 ]
    
    Run bpf_tcp_ca selftests (./test_progs -t bpf_tcp_ca) on a Loongarch
    platform, some "Segmentation fault" errors occur:
    
    '''
     test_dctcp:PASS:bpf_dctcp__open_and_load 0 nsec
     test_dctcp:FAIL:bpf_map__attach_struct_ops unexpected error: -524
     #29/1    bpf_tcp_ca/dctcp:FAIL
     test_cubic:PASS:bpf_cubic__open_and_load 0 nsec
     test_cubic:FAIL:bpf_map__attach_struct_ops unexpected error: -524
     #29/2    bpf_tcp_ca/cubic:FAIL
     test_dctcp_fallback:PASS:dctcp_skel 0 nsec
     test_dctcp_fallback:PASS:bpf_dctcp__load 0 nsec
     test_dctcp_fallback:FAIL:dctcp link unexpected error: -524
     #29/4    bpf_tcp_ca/dctcp_fallback:FAIL
     test_write_sk_pacing:PASS:open_and_load 0 nsec
     test_write_sk_pacing:FAIL:attach_struct_ops unexpected error: -524
     #29/6    bpf_tcp_ca/write_sk_pacing:FAIL
     test_update_ca:PASS:open 0 nsec
     test_update_ca:FAIL:attach_struct_ops unexpected error: -524
     settcpca:FAIL:setsockopt unexpected setsockopt: \
    					actual -1 == expected -1
     (network_helpers.c:99: errno: No such file or directory) \
    					Failed to call post_socket_cb
     start_test:FAIL:start_server_str unexpected start_server_str: \
    					actual -1 == expected -1
     test_update_ca:FAIL:ca1_ca1_cnt unexpected ca1_ca1_cnt: \
    					actual 0 <= expected 0
     #29/9    bpf_tcp_ca/update_ca:FAIL
     #29      bpf_tcp_ca:FAIL
     Caught signal #11!
     Stack trace:
     ./test_progs(crash_handler+0x28)[0x5555567ed91c]
     linux-vdso.so.1(__vdso_rt_sigreturn+0x0)[0x7ffffee408b0]
     ./test_progs(bpf_link__update_map+0x80)[0x555556824a78]
     ./test_progs(+0x94d68)[0x5555564c4d68]
     ./test_progs(test_bpf_tcp_ca+0xe8)[0x5555564c6a88]
     ./test_progs(+0x3bde54)[0x5555567ede54]
     ./test_progs(main+0x61c)[0x5555567efd54]
     /usr/lib64/libc.so.6(+0x22208)[0x7ffff2aaa208]
     /usr/lib64/libc.so.6(__libc_start_main+0xac)[0x7ffff2aaa30c]
     ./test_progs(_start+0x48)[0x55555646bca8]
     Segmentation fault
    '''
    
    This is because BPF trampoline is not implemented on Loongarch yet,
    "link" returned by bpf_map__attach_struct_ops() is NULL. test_progs
    crashs when this NULL link passes to bpf_link__update_map(). This
    patch adds NULL checks for all links in bpf_tcp_ca to fix these errors.
    If "link" is NULL, goto the newly added label "out" to destroy the skel.
    
    v2:
     - use "goto out" instead of "return" as Eduard suggested.
    
    Fixes: 06da9f3bd641 ("selftests/bpf: Test switching TCP Congestion Control algorithms.")
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Reviewed-by: Alan Maguire <alan.maguire@oracle.com>
    Link: https://lore.kernel.org/r/b4c841492bd4ed97964e4e61e92827ce51bf1dc9.1720615848.git.tanggeliang@kylinos.cn
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/bpf_tcp_ca.c
+++ tools/testing/selftests/bpf/prog_tests/bpf_tcp_ca.c
@@ -396,7 +396,8 @@ static void test_update_ca(void)
 		return;
 
 	link = bpf_map__attach_struct_ops(skel->maps.ca_update_1);
-	ASSERT_OK_PTR(link, "attach_struct_ops");
+	if (!ASSERT_OK_PTR(link, "attach_struct_ops"))
+		goto out;
 
 	do_test("tcp_ca_update", NULL);
 	saved_ca1_cnt = skel->bss->ca1_cnt;
@@ -410,6 +411,7 @@ static void test_update_ca(void)
 	ASSERT_GT(skel->bss->ca2_cnt, 0, "ca2_ca2_cnt");
 
 	bpf_link__destroy(link);
+out:
 	tcp_ca_update__destroy(skel);
 }
 
@@ -425,7 +427,8 @@ static void test_update_wrong(void)
 		return;
 
 	link = bpf_map__attach_struct_ops(skel->maps.ca_update_1);
-	ASSERT_OK_PTR(link, "attach_struct_ops");
+	if (!ASSERT_OK_PTR(link, "attach_struct_ops"))
+		goto out;
 
 	do_test("tcp_ca_update", NULL);
 	saved_ca1_cnt = skel->bss->ca1_cnt;
@@ -438,6 +441,7 @@ static void test_update_wrong(void)
 	ASSERT_GT(skel->bss->ca1_cnt, saved_ca1_cnt, "ca2_ca1_cnt");
 
 	bpf_link__destroy(link);
+out:
 	tcp_ca_update__destroy(skel);
 }
 
@@ -452,7 +456,8 @@ static void test_mixed_links(void)
 		return;
 
 	link_nl = bpf_map__attach_struct_ops(skel->maps.ca_no_link);
-	ASSERT_OK_PTR(link_nl, "attach_struct_ops_nl");
+	if (!ASSERT_OK_PTR(link_nl, "attach_struct_ops_nl"))
+		goto out;
 
 	link = bpf_map__attach_struct_ops(skel->maps.ca_update_1);
 	ASSERT_OK_PTR(link, "attach_struct_ops");
@@ -465,6 +470,7 @@ static void test_mixed_links(void)
 
 	bpf_link__destroy(link);
 	bpf_link__destroy(link_nl);
+out:
 	tcp_ca_update__destroy(skel);
 }
 
@@ -507,7 +513,8 @@ static void test_link_replace(void)
 	bpf_link__destroy(link);
 
 	link = bpf_map__attach_struct_ops(skel->maps.ca_update_2);
-	ASSERT_OK_PTR(link, "attach_struct_ops_2nd");
+	if (!ASSERT_OK_PTR(link, "attach_struct_ops_2nd"))
+		goto out;
 
 	/* BPF_F_REPLACE with a wrong old map Fd. It should fail!
 	 *
@@ -530,6 +537,7 @@ static void test_link_replace(void)
 
 	bpf_link__destroy(link);
 
+out:
 	tcp_ca_update__destroy(skel);
 }
 
