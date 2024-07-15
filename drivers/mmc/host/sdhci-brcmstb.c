0861b9483b64cb341829c2a17d54da7170293fa6
Jonathan Bell <jonathan@raspberrypi.com>
2024-07-15 13:57:01 +0100

    mmc: brcmstb: don't squash card-busy detection on bcm2712
    Commit 485d9421719b  ("mmc: sdhci-brcmstb: check R1_STATUS for
    erase/trim/discard") introduced a new flag and defaulted to disabling
    card busy detection across all platforms with this controller.
    
    This is required for IO voltage switching, as the card drives CMD low
    while the switch is in progress.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/mmc/host/sdhci-brcmstb.c
+++ drivers/mmc/host/sdhci-brcmstb.c
@@ -488,6 +488,7 @@ static struct brcmstb_match_priv match_priv_74165b0 = {
 };
 
 static const struct brcmstb_match_priv match_priv_2712 = {
+	.flags = BRCMSTB_MATCH_FLAGS_USE_CARD_BUSY,
 	.hs400es = sdhci_brcmstb_hs400es,
 	.cfginit = sdhci_brcmstb_cfginit_2712,
 	.ops = &sdhci_brcmstb_ops_2712,
