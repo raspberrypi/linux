d81ab7242c480d4b1030cd4827f6a044281ea1b7
Dave Stevenson <dave.stevenson@raspberrypi.com>
2024-07-23 11:27:16 +0100

    media: platform: Move bcm2835-unicam compatible to downstream driver
    The upstream Unicam driver needs a set of userland changes to get
    libcamera to run, and those aren't written or merged yet.
    
    Move the "brcm,bcm2835-unicam" compatible from the upstream driver
    to the old downstream version so that users can run libcamera
    against 6.10.
    Once the libcamera changes have been merged this can be reverted
    to use the upstream driver.
    
    If using the non-legacy compatible then assume we want to use
    media-controller API for configuration.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/media/platform/broadcom/bcm2835-unicam/bcm2835-unicam.c
+++ drivers/media/platform/broadcom/bcm2835-unicam/bcm2835-unicam.c
@@ -2755,7 +2755,6 @@ static void unicam_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id unicam_of_match[] = {
-	{ .compatible = "brcm,bcm2835-unicam", },
 	{ .compatible = "brcm,bcm2835-unicam-upstream", },
 	{ /* sentinel */ },
 };
