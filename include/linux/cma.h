f147d6c80f9495d5d08d4345f33c787d5fcd2e5e
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2024-07-29 16:53:18 +0100

    numa/emulation: Check emulated zones around the CMA window
    ... Make sure CMA zones do not straddle the emulated NUMA nodes ...
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>

--- include/linux/cma.h
+++ include/linux/cma.h
@@ -61,4 +61,6 @@ extern int cma_for_each_area(int (*it)(struct cma *cma, void *data), void *data)
 extern bool cma_intersects(struct cma *cma, unsigned long start, unsigned long end);
 
 extern void cma_reserve_pages_on_error(struct cma *cma);
+
+int cma_check_range(u64 *start, u64 *end);
 #endif
