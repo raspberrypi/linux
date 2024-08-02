547fbd1b7b0dfb4fa0213b32fefe37a7d10ba937
Naushir Patuck <naush@raspberrypi.com>
2024-08-02 11:01:24 +0100

    drivers: media: pci: Update Hailo accelerator device driver to v4.18.0
    Sourced from https://github.com/hailo-ai/hailort-drivers/
    
    Signed-off-by: Naushir Patuck <naush@raspberrypi.com>

--- drivers/media/pci/hailo/common/utils.h
+++ drivers/media/pci/hailo/common/utils.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: MIT
 /**
  * Copyright (c) 2019-2022 Hailo Technologies Ltd. All rights reserved.
  **/
@@ -10,6 +10,12 @@
 
 #define hailo_clear_bit(bit, pval)  { *(pval) &= ~(1 << bit); }
 #define hailo_test_bit(pos,var_addr)  ((*var_addr) & (1<<(pos)))
+
+#define READ_BITS_AT_OFFSET(amount_bits, offset, initial_value) \
+    (((initial_value) >> (offset)) & ((1 << (amount_bits)) - 1))
+#define WRITE_BITS_AT_OFFSET(amount_bits, offset, initial_value, value) \
+    (((initial_value) & ~(((1 << (amount_bits)) - 1) << (offset))) | \
+    (((value) & ((1 << (amount_bits)) - 1)) << (offset)))
 
 #ifdef __cplusplus
 extern "C"
@@ -26,6 +32,22 @@ static inline void hailo_set_bit(int nr, u32* addr) {
 	u32 *p = addr + BIT_WORD(nr);
 
 	*p  |= mask;
+}
+
+static inline uint8_t ceil_log2(uint32_t n)
+{
+    uint8_t result = 0;
+
+    if (n <= 1) {
+        return 0;
+    }
+
+    while (n > 1) {
+        result++;
+        n = (n + 1) >> 1;
+    }
+
+    return result;
 }
 
 #ifndef DIV_ROUND_UP
