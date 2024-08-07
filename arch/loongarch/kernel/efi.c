e32f7c84438f04a5c9b8c4fd00d08fc888700f6a
Miao Wang <shankerwangmiao@gmail.com>
2024-08-07 17:37:11 +0800

    LoongArch: Enable general EFI poweroff method
    commit e688c220732e518c2eb1639e9ef77d4a9311713c upstream.
    
    efi_shutdown_init() can register a general sys_off handler named
    efi_power_off(). Enable this by providing efi_poweroff_required(),
    like arm and x86. Since EFI poweroff is also supported on LoongArch,
    and the enablement makes the poweroff function usable for hardwares
    which lack ACPI S5.
    
    We prefer ACPI poweroff rather than EFI poweroff (like x86), so we only
    require EFI poweroff if acpi_gbl_reduced_hardware or acpi_no_s5 is true.
    
    Cc: stable@vger.kernel.org
    Acked-by: Ard Biesheuvel <ardb@kernel.org>
    Signed-off-by: Miao Wang <shankerwangmiao@gmail.com>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/loongarch/kernel/efi.c
+++ arch/loongarch/kernel/efi.c
@@ -66,6 +66,12 @@ void __init efi_runtime_init(void)
 	set_bit(EFI_RUNTIME_SERVICES, &efi.flags);
 }
 
+bool efi_poweroff_required(void)
+{
+	return efi_enabled(EFI_RUNTIME_SERVICES) &&
+		(acpi_gbl_reduced_hardware || acpi_no_s5);
+}
+
 unsigned long __initdata screen_info_table = EFI_INVALID_TABLE_ADDR;
 
 static void __init init_screen_info(void)
