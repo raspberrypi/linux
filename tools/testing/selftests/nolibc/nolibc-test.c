c07e212bb2ce1c362dd3a3516475fa0813f1967a
Thomas Weißschuh <linux@weissschuh.net>
2024-08-07 23:51:44 +0200

    selftests/nolibc: avoid passing NULL to printf("%s")
    [ Upstream commit f1a58f61d88642ae1e6e97e9d72d73bc70a93cb8 ]
    
    Clang on higher optimization levels detects that NULL is passed to
    printf("%s") and warns about it.
    While printf() from nolibc gracefully handles that NULL,
    it is undefined behavior as per POSIX, so the warning is reasonable.
    Avoid the warning by transforming NULL into a non-NULL placeholder.
    
    Reviewed-by: Shuah Khan <skhan@linuxfoundation.org>
    Acked-by: Willy Tarreau <w@1wt.eu>
    Link: https://lore.kernel.org/r/20240807-nolibc-llvm-v2-8-c20f2f5fc7c2@weissschuh.net
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/nolibc/nolibc-test.c
+++ tools/testing/selftests/nolibc/nolibc-test.c
@@ -534,7 +534,7 @@ int expect_strzr(const char *expr, int llen)
 {
 	int ret = 0;
 
-	llen += printf(" = <%s> ", expr);
+	llen += printf(" = <%s> ", expr ? expr : "(null)");
 	if (expr) {
 		ret = 1;
 		result(llen, FAIL);
@@ -553,7 +553,7 @@ int expect_strnz(const char *expr, int llen)
 {
 	int ret = 0;
 
-	llen += printf(" = <%s> ", expr);
+	llen += printf(" = <%s> ", expr ? expr : "(null)");
 	if (!expr) {
 		ret = 1;
 		result(llen, FAIL);
