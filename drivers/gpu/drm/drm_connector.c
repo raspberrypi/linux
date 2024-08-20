ec227ec46c33150a1346cb194d23b9549f01273f
Dave Stevenson <dave.stevenson@raspberrypi.com>
2024-08-20 16:25:10 +0100

    drm: Set non-desktop property to true for writeback and virtual connectors
    The non-desktop property "Indicates the output should be ignored for
    purposes of displaying a standard desktop environment or console."
    
    That sounds like it should be true for all writeback and virtual
    connectors as you shouldn't render a desktop to them, so set it
    by default.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/gpu/drm/drm_connector.c
+++ drivers/gpu/drm/drm_connector.c
@@ -363,7 +363,8 @@ static int __drm_connector_init(struct drm_device *dev,
 
 	drm_object_attach_property(&connector->base,
 				   config->non_desktop_property,
-				   0);
+				   (connector_type != DRM_MODE_CONNECTOR_VIRTUAL &&
+				   connector_type != DRM_MODE_CONNECTOR_WRITEBACK) ? 0 : 1);
 	drm_object_attach_property(&connector->base,
 				   config->tile_property,
 				   0);
