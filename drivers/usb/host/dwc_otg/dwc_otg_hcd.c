cd33a575ba307fafc1d4d981e86594d6bcb51219
Jonathan Bell <jonathan@raspberrypi.com>
2024-09-12 13:22:48 +0100

    drivers: dwc_otg: add ticket-based spinlock for ARM64
    The ARM64 architecture uses qspinlock which has a fast and slow path.
    This isn't ideal for all claimers of a lock operating in interrupt
    context. Add a ticket-based lock similar to the armv6/7 implementation.
    
    Based on an upstream patch that was abandoned in favour of qspinlock.
    
    Link: https://patchwork.kernel.org/project/linux-arm-kernel/patch/1381330468-32625-2-git-send-email-will.deacon@arm.com/
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/dwc_otg/dwc_otg_hcd.c
+++ drivers/usb/host/dwc_otg/dwc_otg_hcd.c
@@ -1044,10 +1044,6 @@ int dwc_otg_hcd_init(dwc_otg_hcd_t * hcd, dwc_otg_core_if_t * core_if)
 		}
 		DWC_MEMSET(hcd->fiq_state, 0, (sizeof(struct fiq_state) + (sizeof(struct fiq_channel_state) * num_channels)));
 
-#ifdef CONFIG_ARM64
-		spin_lock_init(&hcd->fiq_state->lock);
-#endif
-
 		hcd->fiq_state->dummy_send = DWC_DMA_ALLOC_ATOMIC(dev, 16,
 							 &hcd->fiq_state->dummy_send_dma);
 
