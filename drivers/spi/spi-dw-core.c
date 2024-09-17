b3aa800b0afa263ffcce95ec95555fd4ff7348a0
Phil Elwell <phil@raspberrypi.com>
2024-09-17 20:06:43 +0100

    Revert "spi: dw: don't immediately kill DMA transfers if an error occurs"
    This reverts commit 3af7822df36e36b5a74d877df7654695c0e0d34a.

--- drivers/spi/spi-dw-core.c
+++ drivers/spi/spi-dw-core.c
@@ -200,18 +200,7 @@ int dw_spi_check_status(struct dw_spi *dws, bool raw)
 
 	/* Generically handle the erroneous situation */
 	if (ret) {
-		/*
-		 * Forcibly halting the controller can cause DMA to hang.
-		 * Defer to dw_spi_handle_err outside of interrupt context
-		 * and mask further interrupts for the current transfer.
-		 */
-		if (dws->dma_mapped) {
-			dw_spi_mask_intr(dws, 0xff);
-			dw_readl(dws, DW_SPI_ICR);
-		} else {
-			dw_spi_reset_chip(dws);
-		}
-
+		dw_spi_reset_chip(dws);
 		if (dws->host->cur_msg)
 			dws->host->cur_msg->status = ret;
 	}
