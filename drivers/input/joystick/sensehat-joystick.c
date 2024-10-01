bdb00151ff537c119cea7125e665a9bee1f76c58
Dom Cobley <popcornmix@gmail.com>
2024-10-01 13:17:19 +0100

    Revert "Input: sensehat-joystick : Revert to downstream keymap"
    This reverts commit 7343a026127ad201573a6244221973f1aaa23e10.

--- drivers/input/joystick/sensehat-joystick.c
+++ drivers/input/joystick/sensehat-joystick.c
@@ -28,7 +28,7 @@ struct sensehat_joystick {
 };
 
 static const unsigned int keymap[] = {
-	KEY_DOWN, KEY_RIGHT, KEY_UP, KEY_ENTER, KEY_LEFT
+	BTN_DPAD_DOWN, BTN_DPAD_RIGHT, BTN_DPAD_UP, BTN_SELECT, BTN_DPAD_LEFT,
 };
 
 static irqreturn_t sensehat_joystick_report(int irq, void *cookie)
