48a7f1ca2880d22217db59a1fd4914c17dd4285c
John Cox <john.cox@raspberrypi.com>
2024-11-04 17:57:30 +0000

    media: platform: Add Raspberry Pi HEVC decoder driver
    The BCM2711 and BCM2712 SoCs used on Rapsberry Pi 4 and Raspberry
    Pi 5 boards include an HEVC decoder block. Add a driver for it.
    
    Signed-off-by: John Cox <john.cox@raspberrypi.com>
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    
    media: hevc_dec: Add in downstream single planar SAND variant
    
    Upstream will take the multi-planar SAND format, but add back
    in the downstream single planar variant for backwards compatibility
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    
    media: hevc_dec: Add module parameter for video_nr
    
    To avoid user complaints that /dev/video0 isn't their USB
    webcam, add downstream patch that allows setting the preferred
    video device number.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>
    
    media/hevc_d: Fix old-style bit_size handling support
    
    Code to select the old way of setting the bit length of a slice got
    lost when we moved to 6.18. This fixes it.
    
    Signed-off-by: John Cox <jc@kynesim.co.uk>
    
    media/hevc_d: Fix height rounding for old sand formats
    
    Round old sand format heights to 16 rather than 8 as Pi4 ISP
    input requires this (gives EFAULT otherwise). As ISP cannot
    take new sand formats do not change them.
    
    Signed-off-by: John Cox <jc@kynesim.co.uk>

---
+++ drivers/media/platform/raspberrypi/hevc_dec/hevc_d_h265.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Raspberry Pi HEVC driver
+ *
+ * Copyright (C) 2025 Raspberry Pi Ltd
+ *
+ */
+
+#ifndef _HEVC_D_H265_H_
+#define _HEVC_D_H265_H_
+#include "hevc_d.h"
+
+extern const struct v4l2_ctrl_ops hevc_d_hevc_sps_ctrl_ops;
+extern const struct v4l2_ctrl_ops hevc_d_hevc_pps_ctrl_ops;
+
+int hevc_d_h265_start(struct hevc_d_ctx *ctx);
+void hevc_d_h265_stop(struct hevc_d_ctx *ctx);
+void hevc_d_h265_trigger(struct hevc_d_ctx *ctx);
+
+void hevc_d_device_run(void *priv);
+
+#endif
