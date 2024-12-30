acfb75eeb3304d08cbd5b5a8c77379459ec6a04e
Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
2024-12-30 10:33:34 +0300

    fs/ntfs3: Mark inode as bad as soon as error detected in mi_enum_attr()
    [ Upstream commit 2afd4d267e6dbaec8d3ccd4f5396cb84bc67aa2e ]
    
    Extended the `mi_enum_attr()` function interface with an additional
    parameter, `struct ntfs_inode *ni`, to allow marking the inode
    as bad as soon as an error is detected.
    
    Reported-by: syzbot+73d8fc29ec7cba8286fa@syzkaller.appspotmail.com
    Signed-off-by: Konstantin Komarov <almaz.alexandrovich@paragon-software.com>
    Stable-dep-of: 7c4841e2a627 ("fs/ntfs3: fix slab-out-of-bounds write in ni_create_attr_list()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ntfs3/attrib.c
+++ fs/ntfs3/attrib.c
@@ -789,7 +789,8 @@ pack_runs:
 		if (err)
 			goto out;
 
-		attr = mi_find_attr(mi, NULL, type, name, name_len, &le->id);
+		attr = mi_find_attr(ni, mi, NULL, type, name, name_len,
+				    &le->id);
 		if (!attr) {
 			err = -EINVAL;
 			goto bad_inode;
@@ -1197,7 +1198,7 @@ repack:
 			goto out;
 		}
 
-		attr = mi_find_attr(mi, NULL, ATTR_DATA, NULL, 0, &le->id);
+		attr = mi_find_attr(ni, mi, NULL, ATTR_DATA, NULL, 0, &le->id);
 		if (!attr) {
 			err = -EINVAL;
 			goto out;
@@ -1821,7 +1822,7 @@ repack:
 				goto out;
 			}
 
-			attr = mi_find_attr(mi, NULL, ATTR_DATA, NULL, 0,
+			attr = mi_find_attr(ni, mi, NULL, ATTR_DATA, NULL, 0,
 					    &le->id);
 			if (!attr) {
 				err = -EINVAL;
@@ -2066,8 +2067,8 @@ int attr_collapse_range(struct ntfs_inode *ni, u64 vbo, u64 bytes)
 				}
 
 				/* Look for required attribute. */
-				attr = mi_find_attr(mi, NULL, ATTR_DATA, NULL,
-						    0, &le->id);
+				attr = mi_find_attr(ni, mi, NULL, ATTR_DATA,
+						    NULL, 0, &le->id);
 				if (!attr) {
 					err = -EINVAL;
 					goto out;
