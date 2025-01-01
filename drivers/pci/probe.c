31be3bcba35fca6ced70f5b1361bcc4487a9b716
Phil Elwell <phil@raspberrypi.com>
2025-01-01 12:47:30 +0000

    Revert "PCI: Warn if no host bridge NUMA node info"
    This reverts commit ad5086108b9f0361929aa9a79cf959ab5681d249.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -1037,9 +1037,6 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 	else
 		pr_info("PCI host bridge to bus %s\n", name);
 
-	if (nr_node_ids > 1 && pcibus_to_node(bus) == NUMA_NO_NODE)
-		dev_warn(&bus->dev, "Unknown NUMA node; performance will be reduced\n");
-
 	/* Check if the boot configuration by FW needs to be preserved */
 	bridge->preserve_config = pci_preserve_config(bridge);
 
