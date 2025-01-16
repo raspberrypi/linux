178d5244dc1c26df37e54a6e1ea5fdadfe6bc940
Dave Stevenson <dave.stevenson@raspberrypi.com>
2025-01-16 18:24:17 +0000

    media: hevc_dec: Add module parameter for video_nr
    To avoid user complaints that /dev/video0 isn't their USB
    webcam, add downstream patch that allows setting the preferred
    video device number.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/media/platform/raspberrypi/hevc_dec/hevc_d.c
+++ drivers/media/platform/raspberrypi/hevc_dec/hevc_d.c
@@ -25,6 +25,15 @@
 #include "hevc_d_video.h"
 #include "hevc_d_hw.h"
 
+/*
+ * Default /dev/videoN node number.
+ * Deliberately avoid the very low numbers as these are often taken by webcams
+ * etc, and simple apps tend to only go for /dev/video0.
+ */
+static int video_nr = 19;
+module_param(video_nr, int, 0644);
+MODULE_PARM_DESC(video_nr, "decoder video device number");
+
 static const struct hevc_d_control hevc_d_ctrls[] = {
 	{
 		.cfg = {
@@ -373,7 +382,7 @@ static int hevc_d_probe(struct platform_device *pdev)
 	dev->mdev.ops = &hevc_d_m2m_media_ops;
 	dev->v4l2_dev.mdev = &dev->mdev;
 
-	ret = video_register_device(vfd, VFL_TYPE_VIDEO, -1);
+	ret = video_register_device(vfd, VFL_TYPE_VIDEO, video_nr);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
 		goto err_m2m;
