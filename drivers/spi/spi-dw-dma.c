bf8f5ce43b4cb31beb332daaf425728f53129330
Phil Elwell <phil@raspberrypi.com>
2025-02-04 15:25:30 +0000

    fixup! spi: dw: Wait for idle after TX
    Relax a bit harder - transmission of the last bits may take a while.
    
    Signed-off-by: Phil Elwell <phil@raspberrypi.com>

--- drivers/spi/spi-dw-dma.c
+++ drivers/spi/spi-dw-dma.c
@@ -305,8 +305,9 @@ static int dw_spi_dma_wait_tx_done(struct dw_spi *dws,
 	}
 
 	if (!xfer->rx_buf) {
+		delay.value = dws->n_bytes * BITS_PER_BYTE;
 		while (dw_readl(dws, DW_SPI_SR) & DW_SPI_SR_BUSY)
-			cpu_relax();
+			spi_delay_exec(&delay, xfer);
 	}
 
 	return 0;
