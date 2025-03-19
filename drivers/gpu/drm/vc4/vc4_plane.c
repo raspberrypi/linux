29b4cfdd5dad79fb721b19598a9b0d3328607e45
Dave Stevenson <dave.stevenson@raspberrypi.com>
2025-03-19 15:34:04 +0000

    drm/vc4: Correct arithmetic for shifting between columns of SAND images
    Commit 69dbba71ac11 ("drm/vc4: Add algorithmic handling for SAND")
    lost a multiplication by the tile width when doing the pointer arithmetic
    for cropping off columns for vc6.
    
    Correct that computation.
    
    Signed-off-by: Dave Stevenson <dave.stevenson@raspberrypi.com>

--- drivers/gpu/drm/vc4/vc4_plane.c
+++ drivers/gpu/drm/vc4/vc4_plane.c
@@ -1519,7 +1519,7 @@ static int vc4_plane_mode_set(struct drm_plane *plane,
 
 			tile = src_x / pix_per_tile;
 
-			offsets[i] += pitch[i] * tile;
+			offsets[i] += pitch[i] * tile * tile_width;
 			offsets[i] += src_y / (i ? v_subsample : 1) * tile_width;
 			offsets[i] += x_off & ~(i ? 1 : 0);
 		}
@@ -1995,7 +1995,7 @@ static int vc6_plane_mode_set(struct drm_plane *plane,
 
 			tile = src_x / pix_per_tile;
 
-			offsets[i] += pitch[i] * tile;
+			offsets[i] += pitch[i] * tile * tile_width;
 			offsets[i] += src_y / (i ? v_subsample : 1) * tile_width;
 			offsets[i] += x_off & ~(i ? 1 : 0);
 
