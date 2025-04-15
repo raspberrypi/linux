a134bad272297103672991302e00bcb2d43920fb
Jonathan Bell <jonathan@raspberrypi.com>
2025-04-15 14:44:07 +0100

    usb: xhci: default to Intel scheme for calculating U1/U2 timeouts
    By default, the System Exit Latency and Maximum Exit Latency are used to
    calculate hub port U1 and U2 timeout values. This has the effect of
    aggressively power-managing a SuperSpeed link but devices are known to
    report unfeasibly short device exit latencies in their descriptors,
    which under certain usage conditions can significantly degrade
    throughput as the link spends longer retraining than being in a useable
    state.
    
    The Intel heuristic approach calculates a reasonably large
    endpoint-dependent U1 timeout, and uses a minimum U2 timeout that is
    several multiples of typical U2 exit latencies.
    
    Add a module parameter that defaults to using this scheme.
    
    This should have the effect of squelching interop edge-cases where LPM
    noticeably degrades performance, and avoid the usual workaround where
    userspace manually disables it.
    
    Signed-off-by: Jonathan Bell <jonathan@raspberrypi.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -41,6 +41,10 @@ static unsigned long long quirks;
 module_param(quirks, ullong, S_IRUGO);
 MODULE_PARM_DESC(quirks, "Bit flags for quirks to be enabled as default");
 
+static int sandbag_lpm = 1;
+module_param(sandbag_lpm, int, S_IRUGO | S_IWUSR);
+MODULE_PARM_DESC(sandbag_lpm, "Use relaxed U1/U2 port LPM timeouts");
+
 static bool td_on_ring(struct xhci_td *td, struct xhci_ring *ring)
 {
 	struct xhci_segment *seg;
@@ -4983,7 +4987,7 @@ static u16 xhci_calculate_u1_timeout(struct xhci_hcd *xhci,
 		}
 	}
 
-	if (xhci->quirks & (XHCI_INTEL_HOST | XHCI_ZHAOXIN_HOST))
+	if (sandbag_lpm || xhci->quirks & (XHCI_INTEL_HOST | XHCI_ZHAOXIN_HOST))
 		timeout_ns = xhci_calculate_intel_u1_timeout(udev, desc);
 	else
 		timeout_ns = udev->u1_params.sel;
@@ -5047,7 +5051,7 @@ static u16 xhci_calculate_u2_timeout(struct xhci_hcd *xhci,
 		}
 	}
 
-	if (xhci->quirks & (XHCI_INTEL_HOST | XHCI_ZHAOXIN_HOST))
+	if (sandbag_lpm || xhci->quirks & (XHCI_INTEL_HOST | XHCI_ZHAOXIN_HOST))
 		timeout_ns = xhci_calculate_intel_u2_timeout(udev, desc);
 	else
 		timeout_ns = udev->u2_params.sel;
