05dc6785c94cee0a398b9ba4d818156aa3f7343a
Mark W <mwp@mwp.id.au>
2025-05-06 17:08:02 +0930

    Update panel-ilitek-ili9881c.c
    Correcting display dimension typo

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -2306,7 +2306,7 @@ static const struct drm_display_mode cfaf7201280a0_050tx_default_mode = {
 	.vsync_end	= 1280 + 16 + 8,
 	.vtotal		= 1280 + 16 + 8 + 16,
 	.width_mm	= 62,
-	.height_mm	= 1108
+	.height_mm	= 110,
 };
 
 static const struct drm_display_mode rpi_5inch_default_mode = {
