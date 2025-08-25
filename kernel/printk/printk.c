c5366c72af8b762ba955f92cc55d63857c0aaeaa
Jinchao Wang <wangjinchao600@gmail.com>
2025-08-25 10:29:36 +0800

    panic/printk: replace other_cpu_in_panic() with panic_on_other_cpu()
    [ Upstream commit d4a36db5639db032a434aef968f9188a600139ec ]
    
    The helper other_cpu_in_panic() duplicated logic already provided by
    panic_on_other_cpu().
    
    Remove other_cpu_in_panic() and update all users to call
    panic_on_other_cpu() instead.
    
    This removes redundant code and makes panic handling consistent.
    
    Link: https://lkml.kernel.org/r/20250825022947.1596226-9-wangjinchao600@gmail.com
    Signed-off-by: Jinchao Wang <wangjinchao600@gmail.com>
    Cc: Anna Schumaker <anna.schumaker@oracle.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: "Darrick J. Wong" <djwong@kernel.org>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: Doug Anderson <dianders@chromium.org>
    Cc: "Guilherme G. Piccoli" <gpiccoli@igalia.com>
    Cc: Helge Deller <deller@gmx.de>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Joanthan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Joel Granados <joel.granados@kernel.org>
    Cc: John Ogness <john.ogness@linutronix.de>
    Cc: Kees Cook <kees@kernel.org>
    Cc: Li Huafei <lihuafei1@huawei.com>
    Cc: "Luck, Tony" <tony.luck@intel.com>
    Cc: Luo Gengkun <luogengkun@huaweicloud.com>
    Cc: Max Kellermann <max.kellermann@ionos.com>
    Cc: Nam Cao <namcao@linutronix.de>
    Cc: oushixiong <oushixiong@kylinos.cn>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Qianqiang Liu <qianqiang.liu@163.com>
    Cc: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Sohil Mehta <sohil.mehta@intel.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Thomas Gleinxer <tglx@linutronix.de>
    Cc: Thomas Zimemrmann <tzimmermann@suse.de>
    Cc: Thorsten Blum <thorsten.blum@linux.dev>
    Cc: Ville Syrjala <ville.syrjala@linux.intel.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Yicong Yang <yangyicong@hisilicon.com>
    Cc: Yunhui Cui <cuiyunhui@bytedance.com>
    Cc: Yury Norov (NVIDIA) <yury.norov@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Stable-dep-of: 36630cafbeed ("printk: Fix possible console use-after-free")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -347,17 +347,6 @@ static void __up_console_sem(unsigned long ip)
 #define up_console_sem() __up_console_sem(_RET_IP_)
 
 /*
- * Return true if a panic is in progress on a remote CPU.
- *
- * On true, the local CPU should immediately release any printing resources
- * that may be needed by the panic CPU.
- */
-bool other_cpu_in_panic(void)
-{
-	return (panic_in_progress() && !panic_on_this_cpu());
-}
-
-/*
  * This is used for debugging the mess that is the VT code by
  * keeping track if we have the console semaphore held. It's
  * definitely not the perfect debug tool (we don't know if _WE_
@@ -2366,7 +2355,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 	 * non-panic CPUs are generating any messages, they will be
 	 * silently dropped.
 	 */
-	if (other_cpu_in_panic() &&
+	if (panic_on_other_cpu() &&
 	    !debug_non_panic_cpus &&
 	    !panic_triggering_all_cpu_backtrace)
 		return 0;
@@ -2802,7 +2791,7 @@ void console_lock(void)
 	might_sleep();
 
 	/* On panic, the console_lock must be left to the panic cpu. */
-	while (other_cpu_in_panic())
+	while (panic_on_other_cpu())
 		msleep(1000);
 
 	down_console_sem();
@@ -2822,7 +2811,7 @@ EXPORT_SYMBOL(console_lock);
 int console_trylock(void)
 {
 	/* On panic, the console_lock must be left to the panic cpu. */
-	if (other_cpu_in_panic())
+	if (panic_on_other_cpu())
 		return 0;
 	if (down_trylock_console_sem())
 		return 0;
@@ -3197,7 +3186,7 @@ static bool console_flush_all(bool do_cond_resched, u64 *next_seq, bool *handove
 			any_progress = true;
 
 			/* Allow panic_cpu to take over the consoles safely. */
-			if (other_cpu_in_panic())
+			if (panic_on_other_cpu())
 				goto abandon;
 
 			if (do_cond_resched)
