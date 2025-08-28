684efb2c86c887685f9aa65e1a21b3df6c1f822d
Eric Dumazet <edumazet@google.com>
2025-08-28 19:58:23 +0000

    ipv4: start using dst_dev_rcu()
    [ Upstream commit 6ad8de3cefdb6ffa6708b21c567df0dbf82c43a8 ]
    
    Change icmpv4_xrlim_allow(), ip_defrag() to prevent possible UAF.
    
    Change ipmr_prepare_xmit(), ipmr_queue_fwd_xmit(), ip_mr_output(),
    ipv4_neigh_lookup() to use lockdep enabled dst_dev_rcu().
    
    Fixes: 4a6ce2b6f2ec ("net: introduce a new function dst_dev_put()")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Link: https://patch.msgid.link/20250828195823.3958522-9-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv4/ipmr.c
+++ net/ipv4/ipmr.c
@@ -1894,7 +1894,7 @@ static void ipmr_queue_xmit(struct net *net, struct mr_table *mrt,
 			goto out_free;
 	}
 
-	dev = rt->dst.dev;
+	dev = dst_dev_rcu(&rt->dst);
 
 	if (skb->len+encap > dst_mtu(&rt->dst) && (ntohs(iph->frag_off) & IP_DF)) {
 		/* Do not fragment multicasts. Alas, IPv4 does not
