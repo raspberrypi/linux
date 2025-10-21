7883c7fe6ff8d5de659f786c3f8832da59dbb499
Jai Luthra <jai.luthra@ideasonboard.com>
2025-10-21 13:53:24 +0530

    DONOTCOMMIT: arm64: armv8_deprecated: Drop deprecated CP15 warning
    Signed-off-by: Jai Luthra <jai.luthra@ideasonboard.com>

--- arch/arm64/kernel/armv8_deprecated.c
+++ arch/arm64/kernel/armv8_deprecated.c
@@ -295,8 +295,8 @@ static int cp15barrier_handler(struct pt_regs *regs, u32 instr)
 	}
 
 ret:
-	pr_warn_ratelimited("\"%s\" (%ld) uses deprecated CP15 Barrier instruction at 0x%llx\n",
-			current->comm, (unsigned long)current->pid, regs->pc);
+	/*pr_warn_ratelimited("\"%s\" (%ld) uses deprecated CP15 Barrier instruction at 0x%llx\n",*/
+			/*current->comm, (unsigned long)current->pid, regs->pc);*/
 
 	arm64_skip_faulting_instruction(regs, 4);
 	return 0;
