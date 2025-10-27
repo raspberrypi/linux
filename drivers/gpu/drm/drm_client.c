a6325e2807dc2b985a2fea363794e0ed52e02698
Thomas Zimmermann <tzimmermann@suse.de>
2025-10-27 13:09:16 +0100

    drm/client: Remove drm_client_framebuffer_delete()
    [ Upstream commit 3e3153325fd3693d0f9fe235c4afbcd68ef102e1 ]
    
    Release client buffers with drm_client_buffer_delete() instead of
    drm_client_framebuffer_delete(). The latter is just a tiny wrapper
    around the former.
    
    Move the test for !buffer into drm_client_buffer_delete(), although
    all callers appear to always have a valid pointer.
    
    v2:
    - test for !buffer before deref-ing pointer (Jocelyn, Dan)
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Jocelyn Falempe <jfalempe@redhat.com>
    Tested-by: Francesco Valla <francesco@valla.it>
    Link: https://patch.msgid.link/20251027121042.143588-6-tzimmermann@suse.de
    Stable-dep-of: f4f2bba28df9 ("drm/log: Fix infinite loop when scale is too large for display")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/drm_client.c
+++ drivers/gpu/drm/drm_client.c
@@ -173,11 +173,19 @@ void drm_client_release(struct drm_client_dev *client)
 }
 EXPORT_SYMBOL(drm_client_release);
 
-static void drm_client_buffer_delete(struct drm_client_buffer *buffer)
+/**
+ * drm_client_buffer_delete - Delete a client buffer
+ * @buffer: DRM client buffer
+ */
+void drm_client_buffer_delete(struct drm_client_buffer *buffer)
 {
-	struct drm_gem_object *gem = buffer->fb->obj[0];
+	struct drm_gem_object *gem;
 	int ret;
 
+	if (!buffer)
+		return;
+
+	gem = buffer->fb->obj[0];
 	drm_gem_vunmap(gem, &buffer->map);
 
 	ret = drm_mode_rmfb(buffer->client->dev, buffer->fb->base.id, buffer->client->file);
@@ -189,6 +197,7 @@ static void drm_client_buffer_delete(struct drm_client_buffer *buffer)
 
 	kfree(buffer);
 }
+EXPORT_SYMBOL(drm_client_buffer_delete);
 
 static struct drm_client_buffer *
 drm_client_buffer_create(struct drm_client_dev *client, u32 width, u32 height,
@@ -372,7 +381,7 @@ EXPORT_SYMBOL(drm_client_buffer_vunmap);
  *
  * This function creates a &drm_client_buffer which consists of a
  * &drm_framebuffer backed by a dumb buffer.
- * Call drm_client_framebuffer_delete() to free the buffer.
+ * Call drm_client_buffer_delete() to free the buffer.
  *
  * Returns:
  * Pointer to a client buffer or an error pointer on failure.
@@ -415,19 +424,6 @@ err_drm_mode_destroy_dumb:
 	return ERR_PTR(ret);
 }
 EXPORT_SYMBOL(drm_client_framebuffer_create);
-
-/**
- * drm_client_framebuffer_delete - Delete a client framebuffer
- * @buffer: DRM client buffer (can be NULL)
- */
-void drm_client_framebuffer_delete(struct drm_client_buffer *buffer)
-{
-	if (!buffer)
-		return;
-
-	drm_client_buffer_delete(buffer);
-}
-EXPORT_SYMBOL(drm_client_framebuffer_delete);
 
 /**
  * drm_client_framebuffer_flush - Manually flush client framebuffer
