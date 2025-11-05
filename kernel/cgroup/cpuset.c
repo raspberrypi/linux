4fd62a9418ab918475edb183ca7f57efcf1ef61c
Gabriele Monaco <gmonaco@redhat.com>
2025-11-04 23:38:44 -0500

    cgroup/cpuset: Rename update_unbound_workqueue_cpumask() to update_isolation_cpumasks()
    [ Upstream commit 55939cf28a48dad27d6906e57d3b45905bb0d001 ]
    
    update_unbound_workqueue_cpumask() updates unbound workqueues settings
    when there's a change in isolated CPUs, but it can be used for other
    subsystems requiring updated when isolated CPUs change.
    
    Generalise the name to update_isolation_cpumasks() to prepare for other
    functions unrelated to workqueues to be called in that spot.
    
    [longman: Change the function name to update_isolation_cpumasks()]
    
    Acked-by: Frederic Weisbecker <frederic@kernel.org>
    Acked-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Gabriele Monaco <gmonaco@redhat.com>
    Signed-off-by: Waiman Long <longman@redhat.com>
    Reviewed-by: Chen Ridong <chenridong@huaweicloud.com>
    Reviewed-by: Chen Ridong <chenridong@huawei.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Stable-dep-of: 26d3a59e0241 ("cgroup/cpuset: Use WRITE_ONCE() for shared prs_err updates")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1410,7 +1410,7 @@ static bool partition_xcpus_del(int old_prs, struct cpuset *parent,
 	return isolcpus_updated;
 }
 
-static void update_unbound_workqueue_cpumask(bool isolcpus_updated)
+static void update_isolation_cpumasks(bool isolcpus_updated)
 {
 	int ret;
 
@@ -1574,7 +1574,7 @@ static int remote_partition_enable(struct cpuset *cs, int new_prs,
 	list_add(&cs->remote_sibling, &remote_children);
 	cpumask_copy(cs->effective_xcpus, tmp->new_cpus);
 	spin_unlock_irq(&callback_lock);
-	update_unbound_workqueue_cpumask(isolcpus_updated);
+	update_isolation_cpumasks(isolcpus_updated);
 	cpuset_force_rebuild();
 	cs->prs_err = 0;
 
@@ -1622,7 +1622,7 @@ static void remote_partition_disable(struct cpuset *cs, struct tmpmasks *tmp)
 	compute_excpus(cs, cs->effective_xcpus);
 	reset_partition_data(cs);
 	spin_unlock_irq(&callback_lock);
-	update_unbound_workqueue_cpumask(isolcpus_updated);
+	update_isolation_cpumasks(isolcpus_updated);
 	cpuset_force_rebuild();
 
 	/*
@@ -1691,7 +1691,7 @@ static void remote_cpus_update(struct cpuset *cs, struct cpumask *xcpus,
 	if (xcpus)
 		cpumask_copy(cs->exclusive_cpus, xcpus);
 	spin_unlock_irq(&callback_lock);
-	update_unbound_workqueue_cpumask(isolcpus_updated);
+	update_isolation_cpumasks(isolcpus_updated);
 	if (adding || deleting)
 		cpuset_force_rebuild();
 
@@ -2049,7 +2049,7 @@ write_error:
 		WARN_ON_ONCE(parent->nr_subparts < 0);
 	}
 	spin_unlock_irq(&callback_lock);
-	update_unbound_workqueue_cpumask(isolcpus_updated);
+	update_isolation_cpumasks(isolcpus_updated);
 
 	if ((old_prs != new_prs) && (cmd == partcmd_update))
 		update_partition_exclusive_flag(cs, new_prs);
@@ -3074,7 +3074,7 @@ out:
 	else if (isolcpus_updated)
 		isolated_cpus_update(old_prs, new_prs, cs->effective_xcpus);
 	spin_unlock_irq(&callback_lock);
-	update_unbound_workqueue_cpumask(isolcpus_updated);
+	update_isolation_cpumasks(isolcpus_updated);
 
 	/* Force update if switching back to member & update effective_xcpus */
 	update_cpumasks_hier(cs, &tmpmask, !new_prs);
