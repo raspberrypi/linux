5c350e3b6065f9bea61f6e3835bc7c28f4505a42
Shayne Chen <shayne.chen@mediatek.com>
2025-11-06 14:41:55 +0800

    wifi: mt76: mt7996: support fixed rate for link station
    [ Upstream commit feb06d4556203cd27cf3fa31147d43f28f329653 ]
    
    Introduce mt7996_link_sta_add_debugfs() to extend fixed rate support for
    MLO link station.
    
    Co-developed-by: Howard Hsu <howard-yh.hsu@mediatek.com>
    Signed-off-by: Howard Hsu <howard-yh.hsu@mediatek.com>
    Signed-off-by: Shayne Chen <shayne.chen@mediatek.com>
    Link: https://patch.msgid.link/20251106064203.1000505-4-shayne.chen@mediatek.com
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Stable-dep-of: ce35ecffc96e ("wifi: mt76: mt7996: fix MLD ID in MAC TXD and HIF TXP")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -2371,6 +2371,7 @@ const struct ieee80211_ops mt7996_ops = {
 	.twt_teardown_request = mt7996_twt_teardown_request,
 #ifdef CONFIG_MAC80211_DEBUGFS
 	.sta_add_debugfs = mt7996_sta_add_debugfs,
+	.link_sta_add_debugfs = mt7996_link_sta_add_debugfs,
 #endif
 	.set_radar_background = mt7996_set_radar_background,
 #ifdef CONFIG_NET_MEDIATEK_SOC_WED
