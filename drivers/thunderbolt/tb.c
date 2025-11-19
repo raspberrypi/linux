f06e9fbae78a51832211b4495a19412c7d49ecb4
Mika Westerberg <mika.westerberg@linux.intel.com>
2025-11-19 12:49:30 +0200

    thunderbolt: Set tb->root_switch to NULL when domain is stopped
    [ Upstream commit e56249d8a68e712f3b60e1f3fdbb5b4fea146468 ]
    
    Similarly what we do with the firmware connection manager. This makes
    tb_xdp_handle_request() return error to the remote host. However, we
    need to make sure we keep the uuid alive so that we can reply until the
    whole domain is released.
    
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/thunderbolt/tb.c
+++ drivers/thunderbolt/tb.c
@@ -2850,6 +2850,7 @@ static void tb_stop(struct tb *tb)
 		tb_tunnel_free(tunnel);
 	}
 	tb_switch_remove(tb->root_switch);
+	tb->root_switch = NULL;
 	tcm->hotplug_active = false; /* signal tb_handle_hotplug to quit */
 }
 
--- drivers/thunderbolt/xdomain.c
+++ drivers/thunderbolt/xdomain.c
@@ -754,7 +754,7 @@ static void tb_xdp_handle_request(struct work_struct *work)
 
 	mutex_lock(&tb->lock);
 	if (tb->root_switch)
-		uuid = tb->root_switch->uuid;
+		uuid = kmemdup(tb->root_switch->uuid, sizeof(*uuid), GFP_KERNEL);
 	else
 		uuid = NULL;
 	mutex_unlock(&tb->lock);
@@ -868,6 +868,7 @@ static void tb_xdp_handle_request(struct work_struct *work)
 	}
 
 out:
+	kfree(uuid);
 	kfree(xw->pkg);
 	kfree(xw);
 
@@ -2329,6 +2330,9 @@ static struct tb_xdomain *switch_find_xdomain(struct tb_switch *sw,
 	const struct tb_xdomain_lookup *lookup)
 {
 	struct tb_port *port;
+
+	if (!sw)
+		return NULL;
 
 	tb_switch_for_each_port(sw, port) {
 		struct tb_xdomain *xd;
