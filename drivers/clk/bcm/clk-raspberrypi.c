a6e4124d552218e122788cb760094b08bace1404
Phil Elwell <phil@raspberrypi.com>
2025-11-25 15:08:28 +0000

    Revert "clk: bcm: rpi: Maximize V3D clock"
    See: https://github.com/raspberrypi/linux/issues/7155
    
    This reverts commit 6526402b9bac873d7a64c6e81eb53307d8471f08.

--- drivers/clk/bcm/clk-raspberrypi.c
+++ drivers/clk/bcm/clk-raspberrypi.c
@@ -68,7 +68,6 @@ struct raspberrypi_clk_variant {
 	char		*clkdev;
 	unsigned long	min_rate;
 	bool		minimize;
-	bool		maximize;
 	u32		flags;
 };
 
@@ -136,7 +135,7 @@ raspberrypi_clk_variants[RPI_FIRMWARE_NUM_CLK_ID] = {
 	},
 	[RPI_FIRMWARE_V3D_CLK_ID] = {
 		.export = true,
-		.maximize = true,
+		.minimize = true,
 	},
 	[RPI_FIRMWARE_PIXEL_CLK_ID] = {
 		.export = true,
@@ -386,9 +385,6 @@ static struct clk_hw *raspberrypi_clk_register(struct raspberrypi_clk *rpi,
 			return ERR_PTR(ret);
 		}
 	}
-
-	if (variant->maximize)
-		variant->min_rate = max_rate;
 
 	if (variant->min_rate) {
 		unsigned long rate;
