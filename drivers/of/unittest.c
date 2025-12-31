58baf98d1bdab897fd796f39a16061bade229d71
Zilin Guan <zilin@seu.edu.cn>
2025-12-31 11:49:15 +0000

    of: unittest: Fix memory leak in unittest_data_add()
    [ Upstream commit 235a1eb8d2dcc49a6cf0a5ee1aa85544a5d0054b ]
    
    In unittest_data_add(), if of_resolve_phandles() fails, the allocated
    unittest_data is not freed, leading to a memory leak.
    
    Fix this by using scope-based cleanup helper __free(kfree) for automatic
    resource cleanup. This ensures unittest_data is automatically freed when
    it goes out of scope in error paths.
    
    For the success path, use retain_and_null_ptr() to transfer ownership
    of the memory to the device tree and prevent double freeing.
    
    Fixes: 2eb46da2a760 ("of/selftest: Use the resolver to fixup phandles")
    Suggested-by: Rob Herring <robh@kernel.org>
    Co-developed-by: Jianhao Xu <jianhao.xu@seu.edu.cn>
    Signed-off-by: Jianhao Xu <jianhao.xu@seu.edu.cn>
    Signed-off-by: Zilin Guan <zilin@seu.edu.cn>
    Link: https://patch.msgid.link/20251231114915.234638-1-zilin@seu.edu.cn
    Signed-off-by: Rob Herring (Arm) <robh@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/of/unittest.c
+++ drivers/of/unittest.c
@@ -1871,7 +1871,6 @@ static void attach_node_and_children(struct device_node *np)
  */
 static int __init unittest_data_add(void)
 {
-	void *unittest_data;
 	void *unittest_data_align;
 	struct device_node *unittest_data_node = NULL, *np;
 	/*
@@ -1890,7 +1889,7 @@ static int __init unittest_data_add(void)
 	}
 
 	/* creating copy */
-	unittest_data = kmalloc(size + FDT_ALIGN_SIZE, GFP_KERNEL);
+	void *unittest_data __free(kfree) = kmalloc(size + FDT_ALIGN_SIZE, GFP_KERNEL);
 	if (!unittest_data)
 		return -ENOMEM;
 
@@ -1900,12 +1899,10 @@ static int __init unittest_data_add(void)
 	ret = of_fdt_unflatten_tree(unittest_data_align, NULL, &unittest_data_node);
 	if (!ret) {
 		pr_warn("%s: unflatten testcases tree failed\n", __func__);
-		kfree(unittest_data);
 		return -ENODATA;
 	}
 	if (!unittest_data_node) {
 		pr_warn("%s: testcases tree is empty\n", __func__);
-		kfree(unittest_data);
 		return -ENODATA;
 	}
 
@@ -1924,7 +1921,6 @@ static int __init unittest_data_add(void)
 	/* attach the sub-tree to live tree */
 	if (!of_root) {
 		pr_warn("%s: no live tree to attach sub-tree\n", __func__);
-		kfree(unittest_data);
 		rc = -ENODEV;
 		goto unlock;
 	}
@@ -1944,6 +1940,8 @@ static int __init unittest_data_add(void)
 
 	EXPECT_END(KERN_INFO,
 		   "Duplicate name in testcase-data, renamed to \"duplicate-name#1\"");
+
+	retain_and_null_ptr(unittest_data);
 
 unlock:
 	of_overlay_mutex_unlock();
