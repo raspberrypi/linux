4d72bf8a0398cb8c0dac76c0709b9c68cd9b0e08
Yu Kuai <yukuai@fnnas.com>
2026-01-15 01:12:31 +0800

    md: merge mddev faillast_dev into mddev_flags
    [ Upstream commit 4f6d2e648cbe963b328cb8815290676da3866434 ]
    
    There is not need to use a separate field in struct mddev, there are no
    functional changes.
    
    Link: https://lore.kernel.org/linux-raid/20260114171241.3043364-4-yukuai@fnnas.com
    Signed-off-by: Yu Kuai <yukuai@fnnas.com>
    Reviewed-by: Li Nan <linan122@huawei.com>
    Stable-dep-of: 140234b2380f ("md/raid1: create serial pool adding rdev to array with serialize_policy=1")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/md/md.h
+++ drivers/md/md.h
@@ -313,6 +313,7 @@ struct md_cluster_info;
  * @MD_BROKEN: This is used to stop writes and mark array as failed.
  * @MD_DELETED: This device is being deleted
  * @MD_HAS_SUPERBLOCK: There is persistence sb in member disks.
+ * @MD_FAILLAST_DEV: Allow last rdev to be removed.
  *
  * change UNSUPPORTED_MDDEV_FLAGS for each array type if new flag is added
  */
@@ -329,6 +330,7 @@ enum mddev_flags {
 	MD_BROKEN,
 	MD_DELETED,
 	MD_HAS_SUPERBLOCK,
+	MD_FAILLAST_DEV,
 };
 
 enum mddev_sb_flags {
@@ -589,7 +591,6 @@ struct mddev {
 	/* The sequence number for sync thread */
 	atomic_t sync_seq;
 
-	bool	fail_last_dev:1;
 	bool	serialize_policy:1;
 };
 
