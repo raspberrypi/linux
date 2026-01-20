35f27c3e574d67e5260b7d58c4e10436050b7a1c
Maíra Canal <mairacanal@riseup.net>
2026-01-20 08:09:22 -0300

    drm/v3d: Add module parameter to enable MMU error logging
    MMU error messages are useful to help developers quickly identify
    issues in userspace graphics drivers, but always printing them can
    swamp the kernel log.
    
    Add a module parameter, ``debug_mmu``, to gate MMU error logging.
    Logging is disabled by default and can be enabled when needed with
    ``v3d.debug_mmu=1``.
    
    Signed-off-by: Maíra Canal <mairacanal@riseup.net>

--- drivers/gpu/drm/v3d/v3d_irq.c
+++ drivers/gpu/drm/v3d/v3d_irq.c
@@ -183,7 +183,7 @@ v3d_hub_irq(int irq, void *arg)
 			"GMP",
 		};
 		const char *client = "?";
-		static int logged_error;
+		static bool logged_error;
 
 		V3D_WRITE(V3D_MMU_CTL, V3D_READ(V3D_MMU_CTL));
 
@@ -193,16 +193,18 @@ v3d_hub_irq(int irq, void *arg)
 				client = v3d41_axi_ids[axi_id];
 		}
 
-		if (!logged_error)
-		dev_err(v3d->drm.dev, "MMU error from client %s (%d) at 0x%llx%s%s%s\n",
-			client, axi_id, (long long)vio_addr,
-			((intsts & V3D_HUB_INT_MMU_WRV) ?
-			 ", write violation" : ""),
-			((intsts & V3D_HUB_INT_MMU_PTI) ?
-			 ", pte invalid" : ""),
-			((intsts & V3D_HUB_INT_MMU_CAP) ?
-			 ", cap exceeded" : ""));
-		logged_error = 1;
+		if (!logged_error || debug_mmu) {
+			dev_err(v3d->drm.dev, "MMU error from client %s (%d) at 0x%llx%s%s%s\n",
+				client, axi_id, (long long)vio_addr,
+				((intsts & V3D_HUB_INT_MMU_WRV) ?
+				 ", write violation" : ""),
+				((intsts & V3D_HUB_INT_MMU_PTI) ?
+				 ", pte invalid" : ""),
+				((intsts & V3D_HUB_INT_MMU_CAP) ?
+				 ", cap exceeded" : ""));
+		}
+		logged_error = true;
+
 		status = IRQ_HANDLED;
 	}
 
