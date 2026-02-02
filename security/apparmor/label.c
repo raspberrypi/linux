60aee9cbb82e0bff4407c1f5a702164dfdd13c9d
John Johansen <john.johansen@canonical.com>
2026-02-02 03:37:18 -0800

    apparmor: fix kernel-doc comments for inview
    [ Upstream commit 3734b9463bd4fb5ac350842db55e2e0ccbf1b7a5 ]
    
    subns was renamed inview to better reflect the function of the flag.
    Unfortunately the kernel-doc was not properly updated in 2 places.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202602020737.vGCZFds1-lkp@intel.com/
    Closes: https://lore.kernel.org/oe-kbuild-all/202602021427.PvvDjgyL-lkp@intel.com/
    Fixes: 796c146fa6c82 ("apparmor: split xxx_in_ns into its two separate semantic use cases")
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- security/apparmor/label.c
+++ security/apparmor/label.c
@@ -1334,7 +1334,7 @@ fail:
  * @rules: ruleset to search
  * @label: label to check access permissions for
  * @start: state to start match in
- * @subns: whether to match labels in view or only in scope
+ * @inview: whether to match labels in view or only in scope
  * @request: permissions to request
  * @perms: an initialized perms struct to add accumulation to
  *
@@ -1397,7 +1397,7 @@ fail:
  * @rules: ruleset to search
  * @label: label to match (NOT NULL)
  * @state: state to start in
- * @subns: whether to match labels in view or only in scope
+ * @inview: whether to match labels in view or only in scope
  * @request: permission request
  * @perms: Returns computed perms (NOT NULL)
  *
