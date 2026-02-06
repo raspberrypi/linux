a3313111b5d9046af60b370c93eec105b27380c1
Daniel Hodges <git@danielhodges.dev>
2026-02-06 13:52:07 -0500

    wifi: ath6kl: fix use-after-free in aggr_reset_state()
    commit ba7debb4dd6427386862220e8335a53a4bfc235d upstream.
    
    The aggr_reset_state() function uses timer_delete() (non-synchronous)
    for the aggregation timer before proceeding to delete TID state and
    before the structure is freed by callers like aggr_module_destroy().
    
    If the timer callback (aggr_timeout) is executing when aggr_reset_state()
    is called, the callback will continue to access aggr_conn fields like
    rx_tid[] and stat[] which may be freed immediately after by
    kfree(aggr_info->aggr_conn) in aggr_module_destroy().
    
    Additionally, the timer callback can re-arm itself via mod_timer() while
    aggr_reset_state() is running, creating a more complex race condition.
    
    Use timer_delete_sync() instead to ensure any running timer callback
    has completed before returning.
    
    Fixes: bdcd81707973 ("Add ath6kl cleaned up driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Daniel Hodges <git@danielhodges.dev>
    Reviewed-by: Vasanthakumar Thiagarajan <vasanthakumar.thiagarajan@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260206185207.30098-1-git@danielhodges.dev
    Signed-off-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/wireless/ath/ath6kl/txrx.c
+++ drivers/net/wireless/ath/ath6kl/txrx.c
@@ -1830,7 +1830,7 @@ void aggr_reset_state(struct aggr_info_conn *aggr_conn)
 		return;
 
 	if (aggr_conn->timer_scheduled) {
-		timer_delete(&aggr_conn->timer);
+		timer_delete_sync(&aggr_conn->timer);
 		aggr_conn->timer_scheduled = false;
 	}
 
