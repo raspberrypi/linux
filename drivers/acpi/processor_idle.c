fcc577939931aa5a25266df80bc19cd033c33142
Huisong Li <lihuisong@huawei.com>
2026-03-11 14:50:36 +0800

    ACPI: processor: idle: Remove redundant cstate check in acpi_processor_power_init
    [ Upstream commit db19103ea847ed139da59a2fb71773081c12cd40 ]
    
    The function acpi_processor_cstate_first_run_checks() is responsible
    for updating max_cstate and performing initial hardware validation.
    
    Currently, this function is invoked within acpi_processor_power_init().
    However, the initialization flow already ensures this is called during
    acpi_processor_register_idle_driver().  Therefore, the call in
    acpi_processor_power_init() is redundant and effectively performs no work,
    so remove it.
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>
    Link: https://patch.msgid.link/20260311065038.4151558-2-lihuisong@huawei.com
    [ rjw: Changelog edits ]
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Stable-dep-of: 66c62e6773c5 ("ACPI: processor: Add cpuidle driver check in acpi_processor_register_idle_driver()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/acpi/processor_idle.c
+++ drivers/acpi/processor_idle.c
@@ -1423,8 +1423,6 @@ int acpi_processor_power_init(struct acpi_processor *pr)
 	if (disabled_by_idle_boot_param())
 		return 0;
 
-	acpi_processor_cstate_first_run_checks();
-
 	if (!acpi_processor_get_power_info(pr))
 		pr->flags.power_setup_done = 1;
 
