4cc4d6fb08e75725df5577cf44b66099a4851ab8
Eric Dumazet <edumazet@google.com>
2026-03-12 20:18:23 +0000

    net: dropreason: add SKB_DROP_REASON_RECURSION_LIMIT
    [ Upstream commit d15d3de94a4766fb43d7fe7a72ed0479fb268131 ]
    
    ip[6]tunnel_xmit() can drop packets if a too deep recursion level
    is detected.
    
    Add SKB_DROP_REASON_RECURSION_LIMIT drop reason.
    
    We will use this reason later in __dev_queue_xmit().
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reviewed-by: Joe Damato <joe@dama.to>
    Link: https://patch.msgid.link/20260312201824.203093-2-edumazet@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 6860b467f569 ("xfrm: propagate -EINPROGRESS from validate_xmit_xfrm()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/net/dropreason-core.h
+++ include/net/dropreason-core.h
@@ -129,6 +129,7 @@
 	FN(DUALPI2_STEP_DROP)		\
 	FN(PSP_INPUT)			\
 	FN(PSP_OUTPUT)			\
+	FN(RECURSION_LIMIT)		\
 	FNe(MAX)
 
 /**
@@ -616,6 +617,8 @@ enum skb_drop_reason {
 	SKB_DROP_REASON_PSP_INPUT,
 	/** @SKB_DROP_REASON_PSP_OUTPUT: PSP output checks failed */
 	SKB_DROP_REASON_PSP_OUTPUT,
+	/** @SKB_DROP_REASON_RECURSION_LIMIT: Dead loop on virtual device. */
+	SKB_DROP_REASON_RECURSION_LIMIT,
 	/**
 	 * @SKB_DROP_REASON_MAX: the maximum of core drop reasons, which
 	 * shouldn't be used as a real 'reason' - only for tracing code gen
