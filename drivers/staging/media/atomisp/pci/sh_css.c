3040b1d34a1f888a694a9d4ffa4b5779ef8d109b
Jose A. Perez de Azpillaga <azpijr@gmail.com>
2026-03-26 22:34:07 +0100

    staging: media: atomisp: fix loop shadowing in ia_css_stream_destroy()
    [ Upstream commit 9087395a383212ab1beaefcbe3b57ed131c7823d ]
    
    The nested loop inside the IS_ISP2401 block incorrectly uses the same
    variable 'i' as the outer loop. This shadows the outer loop variable
    and causes premature termination or skipped array elements.
    
    Change the inner loop to use a new variable 'j' to prevent this.
    
    Fixes: 113401c67386 ("media: atomisp: sh_css: Removed #ifdef ISP2401 to make code generic")
    Signed-off-by: Jose A. Perez de Azpillaga <azpijr@gmail.com>
    Reviewed-by: Dan Carpenter <dan.carpenter@linaro.org>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/staging/media/atomisp/pci/sh_css.c
+++ drivers/staging/media/atomisp/pci/sh_css.c
@@ -8192,7 +8192,7 @@ ERR:
 int
 ia_css_stream_destroy(struct ia_css_stream *stream)
 {
-	int i;
+	int i, j;
 	int err = 0;
 
 	IA_CSS_ENTER_PRIVATE("stream = %p", stream);
@@ -8223,10 +8223,10 @@ ia_css_stream_destroy(struct ia_css_stream *stream)
 					sp_pipeline_input_terminal =
 						&sh_css_sp_group.pipe_io[sp_thread_id].input;
 
-					for (i = 0; i < IA_CSS_STREAM_MAX_ISYS_STREAM_PER_CH; i++) {
+					for (j = 0; j < IA_CSS_STREAM_MAX_ISYS_STREAM_PER_CH; j++) {
 						ia_css_isys_stream_h isys_stream =
-							&sp_pipeline_input_terminal->context.virtual_input_system_stream[i];
-						if (stream->config.isys_config[i].valid && isys_stream->valid)
+							&sp_pipeline_input_terminal->context.virtual_input_system_stream[j];
+						if (stream->config.isys_config[j].valid && isys_stream->valid)
 							ia_css_isys_stream_destroy(isys_stream);
 					}
 				}
