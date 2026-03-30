453812e92c760b3995622d98acd358fe4702a088
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-03-30 09:07:55 +0200

    timens: Add a __free() wrapper for put_time_ns()
    [ Upstream commit c2de5a5be4d60af5f928a2dd2b0f73e17358e346 ]
    
    The wrapper will be used to simplify cleanups of 'struct time_namespace'.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Link: https://patch.msgid.link/20260330-timens-cleanup-v1-1-936e91c9dd30@linutronix.de
    Stable-dep-of: 06aba58e5849 ("time/namespace: Validate nanosecond field in proc_timens_set_offset()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/time_namespace.h
+++ include/linux/time_namespace.h
@@ -8,6 +8,7 @@
 #include <linux/ns_common.h>
 #include <linux/err.h>
 #include <linux/time64.h>
+#include <linux/cleanup.h>
 
 struct user_namespace;
 extern struct user_namespace init_user_ns;
@@ -166,5 +167,7 @@ static inline ktime_t timens_ktime_to_host(clockid_t clockid, ktime_t tim)
 	return tim;
 }
 #endif
+
+DEFINE_FREE(time_ns, struct time_namespace *, if (_T) put_time_ns(_T))
 
 #endif /* _LINUX_TIMENS_H */
