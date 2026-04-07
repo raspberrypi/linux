b1d311224479ea5b77ba67e3d999d70f03f8396a
Jiawen Wu <jiawenwu@trustnetic.com>
2026-04-07 10:56:11 +0800

    net: wangxun: replace busy-wait reset flag with kernel mutex
    [ Upstream commit d48df7e7c3fb456d2dc907707a124dab8462eb19 ]
    
    Replace the busy-wait loop using test_and_set_bit(WX_STATE_RESETTING)
    with a proper per-device mutex to serialize reset operations.
    
    The reset flag is reserved for other code paths (like watchdog), which
    need tocheck if a reset is in process.
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>
    Link: https://patch.msgid.link/20260407025616.33652-5-jiawenwu@trustnetic.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: 7bf29145d7a9 ("net: txgbe: fix MISC interrupt unmasking in non-MSI-X mode and device shutdown")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/wangxun/libwx/wx_type.h
+++ drivers/net/ethernet/wangxun/libwx/wx_type.h
@@ -1394,6 +1394,7 @@ struct wx {
 
 	struct timer_list service_timer;
 	struct work_struct service_task;
+	struct mutex reset_lock; /* mutex for reset */
 };
 
 #define WX_INTR_ALL (~0ULL)
@@ -1470,21 +1471,6 @@ wr32ptp(struct wx *wx, u32 reg, u32 value)
 static inline struct wx *phylink_to_wx(struct phylink_config *config)
 {
 	return container_of(config, struct wx, phylink_config);
-}
-
-static inline int wx_set_state_reset(struct wx *wx)
-{
-	u8 timeout = 50;
-
-	while (test_and_set_bit(WX_STATE_RESETTING, wx->state)) {
-		timeout--;
-		if (!timeout)
-			return -EBUSY;
-
-		usleep_range(1000, 2000);
-	}
-
-	return 0;
 }
 
 static inline unsigned int wx_rx_pg_order(struct wx_ring *ring)
