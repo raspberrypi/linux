a0ef2fc89d28ca62923376c4b8ffaa57136a36be
Tianxiang Chen <nanmu@xiaomi.com>
2026-04-08 22:19:14 +0800

    cpufreq: Fix hotplug-suspend race during reboot
    commit a9029dd55696c651ee46912afa2a166fa456bb3e upstream.
    
    During system reboot, cpufreq_suspend() is called via the
    kernel_restart() -> device_shutdown() path. Unlike the normal system
    suspend path, the reboot path does not call freeze_processes(), so
    userspace processes and kernel threads remain active.
    
    This allows CPU hotplug operations to run concurrently with
    cpufreq_suspend(). The original code has no synchronization with CPU
    hotplug, leading to a race condition where governor_data can be freed
    by the hotplug path while cpufreq_suspend() is still accessing it,
    resulting in a null pointer dereference:
    
      Unable to handle kernel NULL pointer dereference
      Call Trace:
       do_kernel_fault+0x28/0x3c
       cpufreq_suspend+0xdc/0x160
       device_shutdown+0x18/0x200
       kernel_restart+0x40/0x80
       arm64_sys_reboot+0x1b0/0x200
    
    Fix this by adding cpus_read_lock()/cpus_read_unlock() to
    cpufreq_suspend() to block CPU hotplug operations while suspend is in
    progress.
    
    Fixes: 65650b35133f ("cpufreq: Avoid cpufreq_suspend() deadlock on system shutdown")
    Signed-off-by: Tianxiang Chen <nanmu@xiaomi.com>
    Reviewed-by: Zhongqiu Han <zhongqiu.han@oss.qualcomm.com>
    Cc: All applicable <stable@vger.kernel.org>
    [ rjw: Changelog edits ]
    Link: https://patch.msgid.link/20260408141914.35281-1-nanmu@xiaomi.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1958,6 +1958,7 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
+	cpus_read_lock();
 	if (!has_target() && !cpufreq_driver->suspend)
 		goto suspend;
 
@@ -1977,6 +1978,7 @@ void cpufreq_suspend(void)
 
 suspend:
 	cpufreq_suspended = true;
+	cpus_read_unlock();
 }
 
 /**
