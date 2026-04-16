4b9c05d21fdc0b31ff0255f24a6301b4daa7fba7
Alice Ryhl <aliceryhl@google.com>
2026-04-16 13:10:54 +0000

    drm/gpuvm: take refcount on DRM device
    [ Upstream commit c2d72717e0a9dd01c6a1bac61a1462a8e04bd179 ]
    
    Currently GPUVM relies on the owner implicitly holding a refcount to the
    drm device, and it does not implicitly take a refcount on the drm
    device. This design is error-prone, so take a refcount on the device.
    
    Suggested-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Fixes: 546ca4d35dcc ("drm/gpuvm: convert WARN() to drm_WARN() variants")
    Link: https://patch.msgid.link/20260416-gpuvm-drm-dev-get-v1-1-f3bc06571e73@google.com
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/drm_gpuvm.c
+++ drivers/gpu/drm/drm_gpuvm.c
@@ -25,6 +25,7 @@
  *
  */
 
+#include <drm/drm_drv.h>
 #include <drm/drm_gpuvm.h>
 #include <drm/drm_print.h>
 
@@ -1117,6 +1118,7 @@ drm_gpuvm_init(struct drm_gpuvm *gpuvm, const char *name,
 	gpuvm->drm = drm;
 	gpuvm->r_obj = r_obj;
 
+	drm_dev_get(drm);
 	drm_gem_object_get(r_obj);
 
 	drm_gpuvm_warn_check_overflow(gpuvm, start_offset, range);
@@ -1160,13 +1162,15 @@ static void
 drm_gpuvm_free(struct kref *kref)
 {
 	struct drm_gpuvm *gpuvm = container_of(kref, struct drm_gpuvm, kref);
+	struct drm_device *drm = gpuvm->drm;
 
 	drm_gpuvm_fini(gpuvm);
 
-	if (drm_WARN_ON(gpuvm->drm, !gpuvm->ops->vm_free))
+	if (drm_WARN_ON(drm, !gpuvm->ops->vm_free))
 		return;
 
 	gpuvm->ops->vm_free(gpuvm);
+	drm_dev_put(drm);
 }
 
 /**
