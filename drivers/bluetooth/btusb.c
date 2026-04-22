e90ad21e290d923ad8a228959846362b7d363ef3
Hrvoje Nuic <hrvoje.nuic@gmail.com>
2026-04-22 23:26:47 +0200

    Bluetooth: btusb: Add Mercusys MA530 for Realtek RTL8761BUV
    [ Upstream commit ce21a5cf3d1fd92b84ea9ad2b7c7240aff2162d2 ]
    
    Add the USB ID for the Mercusys MA530 Bluetooth adapter. The device uses
    a Realtek RTL8761BUV controller and works with the existing Realtek setup
    path.
    
    The device reports vendor ID 0x2c4e and product ID 0x0115, and loads the
    rtl_bt/rtl8761bu_fw.bin firmware successfully with this quirk.
    
    Signed-off-by: Hrvoje Nuic <hrvoje.nuic@gmail.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -771,6 +771,8 @@ static const struct usb_device_id quirks_table[] = {
 	{ USB_DEVICE(0x2ff8, 0xb011), .driver_info = BTUSB_REALTEK },
 
 	/* Additional Realtek 8761BUV Bluetooth devices */
+	{ USB_DEVICE(0x2c4e, 0x0115), .driver_info = BTUSB_REALTEK |
+						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x2357, 0x0604), .driver_info = BTUSB_REALTEK |
 						     BTUSB_WIDEBAND_SPEECH },
 	{ USB_DEVICE(0x0b05, 0x190e), .driver_info = BTUSB_REALTEK |
