8200ffd8259f0944cacd37e64b42f327932edaf2
Hsiu Che Yu <yu.whisper.personal@gmail.com>
2026-04-25 18:16:19 +0800

    rust: alloc: fix `Vec::extend_with` SAFETY comment
    [ Upstream commit f497aae6ded43f91b1dbf29a35d7062823635640 ]
    
    Fix an incorrect operator in the SAFETY comment, changing `<` to `<=`,
    since `Vec::reserve` guarantees capacity for exactly n additional elements,
    so the equal case should be included.
    
    Signed-off-by: Hsiu Che Yu <yu.whisper.personal@gmail.com>
    Reviewed-by: Alexandre Courbot <acourbot@nvidia.com>
    Fixes: 2aac4cd7dae3d ("rust: alloc: implement kernel `Vec` type")
    Link: https://patch.msgid.link/18fc8eee2f057a6bfbcadae156d1d0b7c40d0077.1777111268.git.yu.whisper.personal@gmail.com
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- rust/kernel/alloc/kvec.rs
+++ rust/kernel/alloc/kvec.rs
@@ -754,7 +754,7 @@ impl<T: Clone, A: Allocator> Vec<T, A> {
         spare[n - 1].write(value);
 
         // SAFETY:
-        // - `self.len() + n < self.capacity()` due to the call to reserve above,
+        // - `self.len() + n <= self.capacity()` due to the call to reserve above,
         // - the loop and the line above initialized the next `n` elements.
         unsafe { self.inc_len(n) };
 
