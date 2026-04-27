ebd9e64e92f74435818307dca124b4d3f67ef2df
Yury Norov <ynorov@nvidia.com>
2026-04-27 18:57:05 -0400

    bitfield: wire __bf_shf to __builtin_ctzll
    [ Upstream commit 09472f591aa0b72c2dd6c693f48b2d6fea66c7ba ]
    
    __bf_shf() is currently based on built-in ffsll. It's more
    straightforward to wire it to __builtin_ctzll, which makes it a pure
    rename.
    
    Worth to notice that __builtin_ffsll() is buggy on GCC before 14.1:
    
      int main() {
          sizeof(struct {
              int t : !(__builtin_ffsll(~0ULL) + 1 < 0);
          });
      }
    
      test.c: In function 'main':
      test.c:3:21: error: bit-field 't' width not an integer constant
          3 |                 int t : !(__builtin_ffsll(~0ULL) + 1 < 0);
            |                     ^
    
    Link: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=124699
    Reported-by: Matt Coster <matt.coster@imgtec.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202603222211.A2XiR1YU-lkp@intel.com/
    Signed-off-by: Yury Norov <ynorov@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/linux/bitfield.h
+++ include/linux/bitfield.h
@@ -42,7 +42,7 @@
  *  FIELD_MODIFY(REG_FIELD_C, &reg, c);
  */
 
-#define __bf_shf(x) (__builtin_ffsll(x) - 1)
+#define __bf_shf __builtin_ctzll
 
 #define __scalar_type_to_unsigned_cases(type)				\
 		unsigned type:	(unsigned type)0,			\
