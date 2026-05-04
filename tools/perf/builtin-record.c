de93ef83f99e8d6d477da107703e76ddc079a1b1
Athira Rajeev <atrajeev@linux.ibm.com>
2026-05-04 20:43:20 +0530

    powerpc tools perf: Initialize error code in auxtrace_record_init function
    [ Upstream commit 789d22d77879eabb042627f6627cdb62787bc142 ]
    
    perf trace record fails some cases in powerpc
    
     # perf test "perf trace record and replay"
     128: perf trace record and replay                                    : FAILED!
    
      # perf trace record sleep 1
      # echo $?
        32
    
    This is happening because of non-zero err value from
    auxtrace_record__init() function.
    
     static int record__auxtrace_init(struct record *rec)
     {
            int err;
    
            if ((rec->opts.auxtrace_snapshot_opts || rec->opts.auxtrace_sample_opts)
                && record__threads_enabled(rec)) {
                    pr_err("AUX area tracing options are not available in parallel streaming mode.\n");
                    return -EINVAL;
            }
    
            if (!rec->itr) {
                    rec->itr = auxtrace_record__init(rec->evlist, &err);
                    if (err)
                            return err;
            }
    
    Here "int err" is not initialised. The code expects "err" to be set from
    auxtrace_record__init() function.
    
    Update auxtrace_record__init() in arch/powerpc/util/auxtrace.c to clear
    err value in the beginning.
    
    - Clear err value in beginning of function. Any fail later will
    set appropriate return code to err.
    
    - Even if we haven't found any event for auxtrace, perf record
    should continue for other events. NULL return
    will indicate that there is no auxtrace record initialized.
    
    - Not having "err" set here will affect monitoring of other events
    also because perf record will fail seeing random value in err.
    
    Set err to -EINVAL before invoking auxtrace_record__init() in
    builtin-record.c
    
    With the fix,
    
      # perf trace record sleep 1
      [ perf record: Woken up 2 times to write data ]
      [ perf record: Captured and wrote 0.033 MB perf.data (228 samples) ]
    
    Fixes: 1dbfaf94cf66ec4b ("perf powerpc: Add basic CONFIG_AUXTRACE support for VPA pmu on powerpc")
    Reviewed-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Athira Rajeev <atrajeev@linux.ibm.com>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Cc: Athira Rajeev <atrajeev@linux.ibm.com>
    Cc: Hari Bathini <hbathini@linux.vnet.ibm.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: linuxppc-dev@lists.ozlabs.org
    Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Shivani Nittor <shivani@linux.ibm.com>
    Cc: Tanushree Shah <tanushree.shah@ibm.com>
    Cc: Tejas Manhas <tejas.manhas1@ibm.com>
    Cc: Thomas Richter <tmricht@linux.ibm.com>
    Signed-off-by: Arnaldo Carvalho de Melo <acme@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/perf/builtin-record.c
+++ tools/perf/builtin-record.c
@@ -867,6 +867,7 @@ static int record__auxtrace_init(struct record *rec)
 	}
 
 	if (!rec->itr) {
+		err = -EINVAL;
 		rec->itr = auxtrace_record__init(rec->evlist, &err);
 		if (err)
 			return err;
