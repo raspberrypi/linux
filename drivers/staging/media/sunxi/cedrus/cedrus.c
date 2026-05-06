378bd9cc837c36a1673e9fc45de186ed48f764c0
Myeonghun Pak <mhun512@gmail.com>
2026-05-06 21:41:16 +0900

    media: cedrus: clean up media device on probe failure
    commit 2c869b6969f3061cbbdab587f4c0a88bd7fc3cc9 upstream.
    
    cedrus_probe() initializes the media device before registering the video
    device, the media controller, and the media device. If any of those later
    steps fails, probe returns without calling media_device_cleanup(), so the
    media device internals initialized by media_device_init() are left behind.
    
    Add a media-device cleanup label to the probe unwind path and route video
    registration failures through it as well.
    
    Fixes: 50e761516f2b8c ("media: platform: Add Cedrus VPU decoder driver")
    Cc: stable@vger.kernel.org
    Reviewed-by: Paul Kocialkowski <paulk@sys-base.io>
    Co-developed-by: Ijae Kim <ae878000@gmail.com>
    Signed-off-by: Ijae Kim <ae878000@gmail.com>
    Signed-off-by: Myeonghun Pak <mhun512@gmail.com>
    Signed-off-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>
    Signed-off-by: Hans Verkuil <hverkuil+cisco@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/media/sunxi/cedrus/cedrus.c
+++ drivers/staging/media/sunxi/cedrus/cedrus.c
@@ -509,7 +509,7 @@ static int cedrus_probe(struct platform_device *pdev)
 	ret = video_register_device(vfd, VFL_TYPE_VIDEO, 0);
 	if (ret) {
 		v4l2_err(&dev->v4l2_dev, "Failed to register video device\n");
-		goto err_m2m;
+		goto err_media;
 	}
 
 	v4l2_info(&dev->v4l2_dev,
@@ -535,7 +535,8 @@ err_m2m_mc:
 	v4l2_m2m_unregister_media_controller(dev->m2m_dev);
 err_video:
 	video_unregister_device(&dev->vfd);
-err_m2m:
+err_media:
+	media_device_cleanup(&dev->mdev);
 	v4l2_m2m_release(dev->m2m_dev);
 err_v4l2:
 	v4l2_device_unregister(&dev->v4l2_dev);
