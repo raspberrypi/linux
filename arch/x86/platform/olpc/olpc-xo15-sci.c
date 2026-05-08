df6d71c9a818eed13aa0f1f5d20d3ed201451269
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-05-08 20:00:27 +0200

    x86/platform/olpc: xo15: Drop wakeup source on driver removal
    [ Upstream commit cc966553e6ff0849978b5754531b768b0ff54985 ]
    
    Prevent leaking a wakeup source object after removing the driver by
    adding appropriate cleanup code to its remove callback function.
    
    Fixes: a0f30f592d2d ("x86, olpc: Add XO-1.5 SCI driver")
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/2069931.usQuhbGJ8B@rafael.j.wysocki
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/platform/olpc/olpc-xo15-sci.c
+++ arch/x86/platform/olpc/olpc-xo15-sci.c
@@ -186,6 +186,7 @@ err_sysfs:
 
 static void xo15_sci_remove(struct acpi_device *device)
 {
+	device_init_wakeup(&device->dev, false);
 	acpi_disable_gpe(NULL, xo15_sci_gpe);
 	acpi_remove_gpe_handler(NULL, xo15_sci_gpe, xo15_sci_gpe_handler);
 	cancel_work_sync(&sci_work);
