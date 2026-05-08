cbf0628c0b7db86c5ce36451f088101546acbf1d
Krzysztof Wilczyński <kwilczynski@kernel.org>
2026-05-08 04:35:24 +0000

    PCI/sysfs: Add CAP_SYS_ADMIN check to __resource_resize_store()
    [ Upstream commit 25ec7f57deceec633f27bc93b615f951e2ade814 ]
    
    Currently, the __resource_resize_store() allows writing to the
    resourceN_resize sysfs attribute to change a BAR's size without checking
    for capabilities, currently relying only on the file access check.
    
    Resizing a BAR modifies PCI device configuration and can disrupt active
    drivers.  After the upcoming conversion to static attributes, it will also
    trigger resource file updates via sysfs_update_groups().
    
    Add a CAP_SYS_ADMIN check to prevent unprivileged users from performing BAR
    resize operations.
    
    Signed-off-by: Krzysztof Wilczyński <kwilczynski@kernel.org>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Tested-by: Shivaprasad G Bhat <sbhat@linux.ibm.com>
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Link: https://patch.msgid.link/20260508043543.217179-6-kwilczynski@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/pci/pci-sysfs.c
+++ drivers/pci/pci-sysfs.c
@@ -1605,6 +1605,9 @@ static ssize_t __resource_resize_store(struct device *dev, int n,
 	int ret;
 	u16 cmd;
 
+	if (!capable(CAP_SYS_ADMIN))
+		return -EPERM;
+
 	if (kstrtoul(buf, 0, &size) < 0)
 		return -EINVAL;
 
