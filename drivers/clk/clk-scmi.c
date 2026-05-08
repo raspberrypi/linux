b9c2cbac6810da3e1ece67932847d74710850a18
Cristian Marussi <cristian.marussi@arm.com>
2026-05-08 16:32:46 +0100

    clk: scmi: Fix clock rate rounding
    [ Upstream commit d0c81a38d06d446d341532700b3a4a43d3b00eb1 ]
    
    While the do_div() helper used for rounding expects its divisor argument
    to be a 32bits quantity, the currently provided divisor parameter is a
    64bit value that, as a consequence, is silently truncated and a possible
    source of bugs.
    
    Fix by using the proper div64_ul helper.
    
    Cc: Michael Turquette <mturquette@baylibre.com>
    Cc: Stephen Boyd <sboyd@kernel.org>
    Cc: linux-clk@vger.kernel.org
    Fixes: 7a8655e19bdb ("clk: scmi: Fix the rounding of clock rate")
    Signed-off-by: Cristian Marussi <cristian.marussi@arm.com>
    Tested-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Link: https://patch.msgid.link/20260508153300.2224715-2-cristian.marussi@arm.com
    Reviewed-by: Brian Masney <bmasney@redhat.com>
    Signed-off-by: Sudeep Holla <sudeep.holla@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/clk/clk-scmi.c
+++ drivers/clk/clk-scmi.c
@@ -10,9 +10,9 @@
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/of.h>
+#include <linux/math64.h>
 #include <linux/module.h>
 #include <linux/scmi_protocol.h>
-#include <asm/div64.h>
 
 #define NOT_ATOMIC	false
 #define ATOMIC		true
@@ -78,7 +78,7 @@ static long scmi_clk_round_rate(struct clk_hw *hw, unsigned long rate,
 
 	ftmp = rate - fmin;
 	ftmp += clk->info->range.step_size - 1; /* to round up */
-	do_div(ftmp, clk->info->range.step_size);
+	ftmp = div64_ul(ftmp, clk->info->range.step_size);
 
 	return ftmp * clk->info->range.step_size + fmin;
 }
