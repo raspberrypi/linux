71b7da959031f1cb216e6871cfd84ee82cd50c39
Johan Hovold <johan@kernel.org>
2026-05-11 16:37:10 +0200

    i2c: core: fix adapter debugfs creation
    commit 07d5fb537928aad4369aaff0cbae73ba38a719af upstream.
    
    Clients can be registered from bus notifier callbacks so the debugfs
    directory needs to be created before registering the adapter as clients
    use that directory as their debugfs parent.
    
    Move debugfs creation before adapter registration to avoid having
    clients create their debugfs directories in the debugfs root (which is
    also more likely to fail due to name collisions).
    
    Note that failure to allocate the adapter name must now be handled
    explicitly as debugfs_create_dir() cannot handle a NULL name (unlike
    device_add() which returns an error).
    
    Fixes: 73febd775bdb ("i2c: create debugfs entry per adapter")
    Cc: stable@vger.kernel.org	# 6.8
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -1557,7 +1557,10 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 		goto out_list;
 	}
 
-	dev_set_name(&adap->dev, "i2c-%d", adap->nr);
+	res = dev_set_name(&adap->dev, "i2c-%d", adap->nr);
+	if (res)
+		goto err_remove_irq_domain;
+
 	adap->dev.bus = &i2c_bus_type;
 	adap->dev.type = &i2c_adapter_type;
 	device_initialize(&adap->dev);
@@ -1579,13 +1582,13 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 	idr_replace(&i2c_adapter_idr, adap, adap->nr);
 	mutex_unlock(&core_lock);
 
+	adap->debugfs = debugfs_create_dir(dev_name(&adap->dev), i2c_debugfs_root);
+
 	res = device_add(&adap->dev);
 	if (res) {
 		pr_err("adapter '%s': can't register device (%d)\n", adap->name, res);
-		goto err_put_adap;
+		goto err_remove_debugfs;
 	}
-
-	adap->debugfs = debugfs_create_dir(dev_name(&adap->dev), i2c_debugfs_root);
 
 	res = i2c_setup_smbus_alert(adap);
 	if (res)
@@ -1610,13 +1613,14 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 
 out_reg:
 	i2c_deregister_clients(adap);
-	debugfs_remove_recursive(adap->debugfs);
 	device_del(&adap->dev);
+err_remove_debugfs:
+	debugfs_remove_recursive(adap->debugfs);
 err_put_adap:
 	init_completion(&adap->dev_released);
 	put_device(&adap->dev);
 	wait_for_completion(&adap->dev_released);
-
+err_remove_irq_domain:
 	i2c_host_notify_irq_teardown(adap);
 out_list:
 	mutex_lock(&core_lock);
