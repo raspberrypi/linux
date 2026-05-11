9ec02cc9a04e502847a8313ea690001ae2051a27
Johan Hovold <johan@kernel.org>
2026-05-11 16:37:06 +0200

    i2c: core: fix irq domain leak on adapter registration failure
    commit 8ce19524e4cc2462685f596a6402fbd8fb984ab2 upstream.
    
    Make sure to tear down the host notify irq domain on adapter
    registration failure to avoid leaking it.
    
    This issue was flagged by Sashiko when reviewing another adapter
    registration fix.
    
    Fixes: 4d5538f5882a ("i2c: use an IRQ to report Host Notify events, not alert")
    Cc: stable@vger.kernel.org	# 4.10
    Cc: Benjamin Tissoires <bentiss@kernel.org>
    Signed-off-by: Johan Hovold <johan@kernel.org>
    Signed-off-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -1579,7 +1579,7 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 	if (res) {
 		pr_err("adapter '%s': can't register device (%d)\n", adap->name, res);
 		put_device(&adap->dev);
-		goto out_list;
+		goto err_remove_irq_domain;
 	}
 
 	adap->debugfs = debugfs_create_dir(dev_name(&adap->dev), i2c_debugfs_root);
@@ -1615,6 +1615,8 @@ out_reg:
 	init_completion(&adap->dev_released);
 	device_unregister(&adap->dev);
 	wait_for_completion(&adap->dev_released);
+err_remove_irq_domain:
+	i2c_host_notify_irq_teardown(adap);
 out_list:
 	mutex_lock(&core_lock);
 	idr_remove(&i2c_adapter_idr, adap->nr);
