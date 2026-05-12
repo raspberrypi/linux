1b788fa4cf5493fa432d8673c65e49d36c72d6e0
Al Viro <viro@zeniv.linux.org.uk>
2026-05-12 01:23:29 -0400

    configfs_depend_prep(): pass configfs_dirent instead of dentry
    [ Upstream commit 764682e0118432260191d194edbdaff208260483 ]
    
    Again, the only thing it uses dentry for is dentry->d_fsdata; for the
    recursive call the situation is the same as with configfs_detach_prep()
    and the same observation about ->s_dentry->d_fsdata applies.
    
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Breno Leitao <leitao@debian.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/configfs/dir.c
+++ fs/configfs/dir.c
@@ -1088,14 +1088,11 @@ static int configfs_dump(struct configfs_dirent *sd, int level)
  * much on the stack, though, so folks that need this function - be careful
  * about your stack!  Patches will be accepted to make it iterative.
  */
-static int configfs_depend_prep(struct dentry *origin,
+static int configfs_depend_prep(struct configfs_dirent *sd,
 				struct config_item *target)
 {
-	struct configfs_dirent *child_sd, *sd;
+	struct configfs_dirent *child_sd;
 	int ret = 0;
-
-	BUG_ON(!origin || !origin->d_fsdata);
-	sd = origin->d_fsdata;
 
 	if (sd->s_element == target)  /* Boo-yah */
 		goto out;
@@ -1104,8 +1101,7 @@ static int configfs_depend_prep(struct dentry *origin,
 		if ((child_sd->s_type & CONFIGFS_DIR) &&
 		    !(child_sd->s_type & CONFIGFS_USET_DROPPING) &&
 		    !(child_sd->s_type & CONFIGFS_USET_CREATING)) {
-			ret = configfs_depend_prep(child_sd->s_dentry,
-						   target);
+			ret = configfs_depend_prep(child_sd, target);
 			if (!ret)
 				goto out;  /* Child path boo-yah */
 		}
@@ -1126,7 +1122,7 @@ static int configfs_do_depend_item(struct dentry *subsys_dentry,
 
 	spin_lock(&configfs_dirent_lock);
 	/* Scan the tree, return 0 if found */
-	ret = configfs_depend_prep(subsys_dentry, target);
+	ret = configfs_depend_prep(subsys_dentry->d_fsdata, target);
 	if (ret)
 		goto out_unlock_dirent_lock;
 
