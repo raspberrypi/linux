29fef371331e87311822000535f491ffc3d7a64f
Shiwu Zhang <shiwu.zhang@amd.com>
2026-05-13 13:54:58 +0800

    drm/amdgpu: fix buffer overflow during vBIOS update
    [ Upstream commit 6229898d46c0cbc238a6fe9f11c1ea12cf8bb9c5 ]
    
    Clamp the buffer postion to write by setting the bin attribute
    to the maximum buffer size so that VFS layer will block the
    out-of-bounds accessing.
    
    Signed-off-by: Shiwu Zhang <shiwu.zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_psp.c
@@ -4296,7 +4296,7 @@ rel_buf:
  */
 static const struct bin_attribute psp_vbflash_bin_attr = {
 	.attr = {.name = "psp_vbflash", .mode = 0660},
-	.size = 0,
+	.size = AMD_VBIOS_FILE_MAX_SIZE_B,
 	.write = amdgpu_psp_vbflash_write,
 	.read = amdgpu_psp_vbflash_read,
 };
