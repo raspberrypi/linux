e38226d308820773aeea62d969975cd41a716661
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2026-05-19 14:53:41 +0100

    mmc: renesas_sdhi: Add OF entry for RZ/G2N SoC
    [ Upstream commit 5ce500d31a1625d8fe7ede950201b8df076bdd48 ]
    
    The RZ/G2N (R8A774B1) SoC was previously handled via the generic
    "renesas,rcar-gen3-sdhi" fallback compatible string. However, because
    the SDHI IP on RZ/G2N is identical with the R-Car M3-N (R8A77965), it
    requires the specific quirks and configuration defined in
    `of_r8a77965_compatible` rather than the generic Gen3 data.
    
    Add the explicit "renesas,sdhi-r8a774b1" match entry to map it correctly.
    Note that the DT binding file renesas,sdhi.yaml does not need an update
    as the entry for this SoC is already present.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Reviewed-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Ulf Hansson <ulfh@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/mmc/host/renesas_sdhi_internal_dmac.c
+++ drivers/mmc/host/renesas_sdhi_internal_dmac.c
@@ -279,6 +279,7 @@ static const struct renesas_sdhi_of_data_with_quirks of_rza2_compatible = {
 static const struct of_device_id renesas_sdhi_internal_dmac_of_match[] = {
 	{ .compatible = "renesas,sdhi-r7s9210", .data = &of_rza2_compatible, },
 	{ .compatible = "renesas,sdhi-mmc-r8a77470", .data = &of_rcar_gen3_compatible, },
+	{ .compatible = "renesas,sdhi-r8a774b1", .data = &of_r8a77965_compatible, },
 	{ .compatible = "renesas,sdhi-r8a774e1", .data = &of_r8a7795_compatible, },
 	{ .compatible = "renesas,sdhi-r8a7795", .data = &of_r8a7795_compatible, },
 	{ .compatible = "renesas,sdhi-r8a77961", .data = &of_r8a77961_compatible, },
