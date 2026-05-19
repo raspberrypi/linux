79527ce8d343e7d6291c88a4f2393dbca5c59aae
Jeremy Klarenbeek <jeremy.klarenbeek99@gmail.com>
2026-05-19 10:41:57 +0200

    drm/amd/pm/si: Fix updating clock limits from power states
    [ Upstream commit e6c5d36756e7d4d260e2365fc4d01226f1973152 ]
    
    VBIOS can contain conflicting values between:
    - the maximum allowed clocks and voltages on AC or DC
    - the clocks and voltages in power states on AC or DC
    
    Update maximum clock (and voltage) limits for both AC/DC
    and take the highest value from the VBIOS limits and
    the performance/battery power states. Previously this
    was only done for AC, but is also needed for DC.
    
    This commit fixes the behaviour on some laptop GPUs,
    where the VBIOS limit was set to the lowest possible
    clock frequency, so the GPU was stuck on the lowest
    possible power level on battery.
    
    Some affected GPUs are:
    FirePro W4170M (Dell Precision M2800)
    Radeon HD 8790M (Dell Latitude E6540)
    and possibly other laptop GPUs.
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Co-developed-by: Timur Kristóf <timur.kristof@gmail.com>
    Signed-off-by: Timur Kristóf <timur.kristof@gmail.com>
    Signed-off-by: Jeremy Klarenbeek <jeremy.klarenbeek99@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/pm/legacy-dpm/si_dpm.c
+++ drivers/gpu/drm/amd/pm/legacy-dpm/si_dpm.c
@@ -7238,6 +7238,7 @@ static void si_parse_pplib_clock_info(struct amdgpu_device *adev,
 	struct evergreen_power_info *eg_pi = evergreen_get_pi(adev);
 	struct si_power_info *si_pi = si_get_pi(adev);
 	struct  si_ps *ps = si_get_ps(rps);
+	struct amdgpu_clock_and_voltage_limits *limits;
 	u16 leakage_voltage;
 	struct rv7xx_pl *pl = &ps->performance_levels[index];
 	int ret;
@@ -7297,12 +7298,30 @@ static void si_parse_pplib_clock_info(struct amdgpu_device *adev,
 		si_pi->mvdd_bootup_value = mvdd;
 	}
 
+	/*
+	 * Update maximum allowed clock limits.
+	 * VBIOS can contain conflicting values between:
+	 * - the maximum allowed clocks and voltages on AC or DC
+	 * - the clocks and voltages in power states on AC or DC
+	 */
 	if ((rps->class & ATOM_PPLIB_CLASSIFICATION_UI_MASK) ==
-	    ATOM_PPLIB_CLASSIFICATION_UI_PERFORMANCE) {
-		adev->pm.dpm.dyn_state.max_clock_voltage_on_ac.sclk = pl->sclk;
-		adev->pm.dpm.dyn_state.max_clock_voltage_on_ac.mclk = pl->mclk;
-		adev->pm.dpm.dyn_state.max_clock_voltage_on_ac.vddc = pl->vddc;
-		adev->pm.dpm.dyn_state.max_clock_voltage_on_ac.vddci = pl->vddci;
+	    ATOM_PPLIB_CLASSIFICATION_UI_PERFORMANCE)
+		limits = &adev->pm.dpm.dyn_state.max_clock_voltage_on_ac;
+	else if ((rps->class & ATOM_PPLIB_CLASSIFICATION_UI_MASK) ==
+		 ATOM_PPLIB_CLASSIFICATION_UI_BATTERY)
+		limits = &adev->pm.dpm.dyn_state.max_clock_voltage_on_dc;
+	else
+		limits = NULL;
+
+	if (limits) {
+		if (pl->sclk > limits->sclk)
+			limits->sclk = pl->sclk;
+		if (pl->mclk > limits->mclk)
+			limits->mclk = pl->mclk;
+		if (pl->vddc > limits->vddc)
+			limits->vddc = pl->vddc;
+		if (pl->vddci > limits->vddci)
+			limits->vddci = pl->vddci;
 	}
 }
 
