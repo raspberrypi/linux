971ba53c117476772a4c7299637d520c8f52162c
Bard Liao <yung-chuan.liao@linux.intel.com>
2026-05-20 10:57:20 +0800

    soundwire: only handle alert events when the peripheral is attached
    [ Upstream commit 38cd651ebce7065a81c7e950d9e2ea1572304605 ]
    
    It doesn't make sense to handle an alert event when the peripheral is
    not attached. The slave->status could be SDW_SLAVE_ATTACHED or
    SDW_SLAVE_ALERT when it is attached on the bus.
    
    Signed-off-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.dev>
    Link: https://patch.msgid.link/20260520025720.1999367-1-yung-chuan.liao@linux.intel.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -1989,6 +1989,10 @@ int sdw_handle_slave_status(struct sdw_bus *bus,
 			break;
 
 		case SDW_SLAVE_ALERT:
+			if (slave->status != SDW_SLAVE_ATTACHED &&
+			    slave->status != SDW_SLAVE_ALERT)
+				continue;
+
 			ret = sdw_handle_slave_alerts(slave);
 			if (ret < 0)
 				dev_err(&slave->dev,
