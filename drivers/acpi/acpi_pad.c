53b70c86e249a1c3a61aa78a84831300546d54a3
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-05-21 16:06:48 +0200

    ACPI: PAD: Fix teardown ordering in acpi_pad_remove()
    [ Upstream commit 5776575d9a7e50e77c45e0ab0271c000496f341d ]
    
    The ACPI notify handler installed by acpi_pad_probe() needs to be
    removed before calling acpi_pad_idle_cpus() in acpi_pad_remove()
    so it doesn't schedule idle time injection on some CPUs again.
    
    Fixes: 8e0af5141ab9 ("ACPI: create Processor Aggregator Device driver")
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Link: https://patch.msgid.link/2064153.usQuhbGJ8B@rafael.j.wysocki
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/acpi/acpi_pad.c
+++ drivers/acpi/acpi_pad.c
@@ -435,12 +435,12 @@ static int acpi_pad_probe(struct platform_device *pdev)
 
 static void acpi_pad_remove(struct platform_device *pdev)
 {
+	acpi_dev_remove_notify_handler(ACPI_COMPANION(&pdev->dev),
+				       ACPI_DEVICE_NOTIFY, acpi_pad_notify);
+
 	mutex_lock(&isolated_cpus_lock);
 	acpi_pad_idle_cpus(0);
 	mutex_unlock(&isolated_cpus_lock);
-
-	acpi_dev_remove_notify_handler(ACPI_COMPANION(&pdev->dev),
-				       ACPI_DEVICE_NOTIFY, acpi_pad_notify);
 }
 
 static const struct acpi_device_id pad_device_ids[] = {
