33b29764f6c4e1dbe720978b7299b95dd12560d0
Dan Carpenter <error27@gmail.com>
2026-05-25 10:16:11 +0300

    iio: backend: fix uninitialized data in debugfs
    commit a6e8b14a4897d0b6df9744f33d0a30e6b92368eb upstream.
    
    If the *ppos value is non-zero then simple_write_to_buffer() will not
    initialize the start of the buf[] buffer.  Non-zero ppos values aren't
    going to work at all.  Check for that at the start of the function and
    return -ENOSPC.
    
    Fixes: cdf01e0809a4 ("iio: backend: add debugFs interface")
    Signed-off-by: Dan Carpenter <error27@gmail.com>
    Cc: <Stable@vger.kernel.org>
    Signed-off-by: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iio/industrialio-backend.c
+++ drivers/iio/industrialio-backend.c
@@ -156,7 +156,7 @@ static ssize_t iio_backend_debugfs_write_reg(struct file *file,
 	ssize_t rc;
 	int ret;
 
-	if (count >= sizeof(buf))
+	if (*ppos != 0 || count >= sizeof(buf))
 		return -ENOSPC;
 
 	rc = simple_write_to_buffer(buf, sizeof(buf) - 1, ppos, userbuf, count);
