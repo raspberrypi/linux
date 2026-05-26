22aa70f9a0544643ec37d442b6fcb1833d804462
Geliang Tang <tanggeliang@kylinos.cn>
2026-05-26 17:28:05 +0800

    nvmet-tcp: check return value of nvmet_tcp_set_queue_sock
    [ Upstream commit 7ef789703e2b91775dcb36b2efa46325be31a2a0 ]
    
    The return value of nvmet_tcp_set_queue_sock() is currently ignored in
    nvmet_tcp_tls_handshake_done(). If it fails (e.g., due to the socket
    not being in TCP_ESTABLISHED state), the socket callbacks will not be
    properly set, leading to queue and socket leakage.
    
    Fix this by capturing the return value and calling
    nvmet_tcp_schedule_release_queue() on failure to ensure proper cleanup.
    
    Fixes: 675b453e0241 ("nvmet-tcp: enable TLS handshake upcall")
    Reviewed-by: Hannes Reinecke <hare@kernel.org>
    Reviewed-by: Chaitanya Kulkarni <kch@nvidia.com>
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/target/tcp.c
+++ drivers/nvme/target/tcp.c
@@ -1844,10 +1844,11 @@ static void nvmet_tcp_tls_handshake_done(void *data, int status,
 	if (!status)
 		status = nvmet_tcp_tls_key_lookup(queue, peerid);
 
+	if (!status)
+		status = nvmet_tcp_set_queue_sock(queue);
+
 	if (status)
 		nvmet_tcp_schedule_release_queue(queue);
-	else
-		nvmet_tcp_set_queue_sock(queue);
 	kref_put(&queue->kref, nvmet_tcp_release_queue);
 }
 
