3b51d6f07a19996703c5a2d770ac72fb7538ebe3
Steven Rostedt <rostedt@goodmis.org>
2026-06-01 13:07:46 -0400

    tracing/eprobes: Allow use of BTF names to dereference pointers
    [ Upstream commit 69efd863a78584b9416ed6be0e1e7349124b4a00 ]
    
    Add syntax to the parsing of eprobes to be able to typecast a trace event
    field that is a pointer to a structure.
    
    Currently, a dereference must be a number, where the user has to figure
    out manually the offset of a member of a structure that they want to
    dereference.
    
    But for event probes that records a field that happens to be a pointer to
    a structure, it cannot dereference these values with BTF naming, but
    must use numerical offsets.
    
    For example, to find out what device a sk_buff is pointing to in the
    net_dev_xmit trace event, one must first use gdb to find the offsets of the
    members of the structures:
    
     (gdb) p &((struct sk_buff *)0)->dev
     $1 = (struct net_device **) 0x10
     (gdb) p &((struct net_device *)0)->name
     $2 = (char (*)[16]) 0x118
    
    And then use the raw numbers to dereference:
    
      # echo 'e:xmit net.net_dev_xmit +0x118(+0x10($skbaddr)):string' >> dynamic_events
    
    If BTF is in the kernel, then instead, the skbaddr can be typecast to
    sk_buff and use the normal dereference logic.
    
      # echo 'e:xmit net.net_dev_xmit (sk_buff)skbaddr->dev->name:string' >> dynamic_events
      # echo 1 > events/eprobes/xmit/enable
      # cat trace
    [..]
        sshd-session-1022    [000] b..2.   860.249343: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.250061: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.250142: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.263553: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.283820: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.302716: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.322905: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.342828: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.362268: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.382335: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.400856: xmit: (net.net_dev_xmit) arg1="enp7s0"
        sshd-session-1022    [000] b..2.   860.419893: xmit: (net.net_dev_xmit) arg1="enp7s0"
    
    The syntax is simply: (STRUCT)(FIELD)->MEMBER[->MEMBER..]
    
    Also add comments around the #else and #endif of #ifdef CONFIG_PROBE_EVENTS_BTF_ARGS
    to know what they are for.
    
    Link: https://lore.kernel.org/all/20260601130746.2139d926@gandalf.local.home/
    
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Stable-dep-of: 251a8fe1b9ae ("tracing/probes: Remove WARN_ON_ONCE from parse_btf_arg")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/trace_probe.h
+++ kernel/trace/trace_probe.h
@@ -422,7 +422,9 @@ struct traceprobe_parse_context {
 	const struct btf_param *params;	/* Parameter of the function */
 	s32 nr_params;			/* The number of the parameters */
 	struct btf *btf;		/* The BTF to be used */
+	struct btf *struct_btf;		/* The BTF to be used for structs */
 	const struct btf_type *last_type;	/* Saved type */
+	const struct btf_type *last_struct;	/* Saved structure */
 	u32 last_bitoffs;		/* Saved bitoffs */
 	u32 last_bitsize;		/* Saved bitsize */
 	struct trace_probe *tp;
@@ -563,7 +565,8 @@ extern int traceprobe_define_arg_fields(struct trace_event_call *event_call,
 	C(NEED_STRING_TYPE,	"$comm and immediate-string only accepts string type"),\
 	C(TOO_MANY_ARGS,	"Too many arguments are specified"),	\
 	C(TOO_MANY_EARGS,	"Too many entry arguments specified"),	\
-	C(EVENT_TOO_BIG,	"Event too big (too many fields?)"),
+	C(EVENT_TOO_BIG,	"Event too big (too many fields?)"),  \
+	C(TYPECAST_NOT_EVENT,	"Typecasts are only for eprobe fields"),
 
 #undef C
 #define C(a, b)		TP_ERR_##a
