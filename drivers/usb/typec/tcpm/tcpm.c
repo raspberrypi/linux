0bc177820bd38a3ae77e53b55be770cce2dd0887
Andy Yan <andyshrk@163.com>
2026-06-04 18:50:24 +0800

    usb: typec: tcpm: Fix VDM type for Enter Mode commands
    commit 9cff680e47632b7723cb19f9c5e63669063c3417 upstream.
    
    VDO() second parameter is VDM type (bit 15): 1 for SVDM, 0 for UVDM.
    Using 'vdo ? 2 : 1' corrupts SVID low bit when vdo is non-NULL
    (2 << 15 = BIT(16)). Enter Mode is always SVDM, hardcode to 1.
    
    Fixes: 8face9aa57c8 ("usb: typec: Add parameter for the VDO to typec_altmode_enter()")
    Cc: stable <stable@kernel.org>
    Signed-off-by: Andy Yan <andyshrk@163.com>
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Link: https://patch.msgid.link/20260604105059.18750-1-andyshrk@163.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2928,7 +2928,7 @@ static int tcpm_altmode_enter(struct typec_altmode *altmode, u32 *vdo)
 	if (svdm_version < 0)
 		return svdm_version;
 
-	header = VDO(altmode->svid, vdo ? 2 : 1, svdm_version, CMD_ENTER_MODE);
+	header = VDO(altmode->svid, 1, svdm_version, CMD_ENTER_MODE);
 	header |= VDO_OPOS(altmode->mode);
 
 	return tcpm_queue_vdm_unlocked(port, header, vdo, vdo ? 1 : 0, TCPC_TX_SOP);
@@ -2976,7 +2976,7 @@ static int tcpm_cable_altmode_enter(struct typec_altmode *altmode, enum typec_pl
 	if (svdm_version < 0)
 		return svdm_version;
 
-	header = VDO(altmode->svid, vdo ? 2 : 1, svdm_version, CMD_ENTER_MODE);
+	header = VDO(altmode->svid, 1, svdm_version, CMD_ENTER_MODE);
 	header |= VDO_OPOS(altmode->mode);
 
 	return tcpm_queue_vdm_unlocked(port, header, vdo, vdo ? 1 : 0, TCPC_TX_SOP_PRIME);
