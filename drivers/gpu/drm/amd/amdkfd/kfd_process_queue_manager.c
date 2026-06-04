e2dbdd14c3352483a89e4a9afda05d0f3fcc2c83
David Francis <David.Francis@amd.com>
2026-06-04 15:04:03 -0400

    drm/amdkfd: Properly acquire queue buffers in CRIU restore
    [ Upstream commit 20a5e7ffdfecddc34c60a6b4483f42acf3d8731d ]
    
    When kfd_queue_acquire_buffers() was split off from
    set_queue_properties_from_user(), set_queue_properties_from_criu()
    was missed. Thus, set_queue_properties_from_criu() is not
    filling out the buffer fields of queue_properties, which
    can come up when subsequent code expects them to be non-null.
    
    Add the proper call to kfd_queue_acquire_buffers(), and also
    use the right cast types in set_queue_properties_from_criu()
    (which were missed at the same time)
    
    Signed-off-by: David Francis <David.Francis@amd.com>
    Reviewed-by: Kent Russell <kent.russell@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 88ed96abbbe27b70193544fbc1ee06448c274714)
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdkfd/kfd_process_queue_manager.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process_queue_manager.c
@@ -978,8 +978,8 @@ static void set_queue_properties_from_criu(struct queue_properties *qp,
 	qp->priority = q_data->priority;
 	qp->queue_address = q_data->q_address;
 	qp->queue_size = q_data->q_size;
-	qp->read_ptr = (uint32_t *) q_data->read_ptr_addr;
-	qp->write_ptr = (uint32_t *) q_data->write_ptr_addr;
+	qp->read_ptr = (void __user *)q_data->read_ptr_addr;
+	qp->write_ptr = (void __user *)q_data->write_ptr_addr;
 	qp->eop_ring_buffer_address = q_data->eop_ring_buffer_address;
 	qp->eop_ring_buffer_size = q_data->eop_ring_buffer_size;
 	qp->ctx_save_restore_area_address = q_data->ctx_save_restore_area_address;
@@ -1051,11 +1051,19 @@ int kfd_criu_restore_queue(struct kfd_process *p,
 	memset(&qp, 0, sizeof(qp));
 	set_queue_properties_from_criu(&qp, q_data);
 
+	ret = kfd_queue_acquire_buffers(pdd, &qp);
+	if (ret) {
+		pr_debug("failed to acquire user queue buffers for CRIU\n");
+		goto exit;
+	}
+
 	print_queue_properties(&qp);
 
 	ret = pqm_create_queue(&p->pqm, pdd->dev, NULL, &qp, &queue_id, q_data, mqd, ctl_stack,
 				NULL);
 	if (ret) {
+		kfd_queue_unref_bo_vas(pdd, &qp);
+		kfd_queue_release_buffers(pdd, &qp);
 		pr_err("Failed to create new queue err:%d\n", ret);
 		goto exit;
 	}
