e36095d8d922bb26ce860231aacf0cd14edea07c
Hyunwoo Kim <imv4bel@gmail.com>
2026-06-06 23:44:52 +0900

    KVM: x86: hyper-v: Bound the bank index when querying sparse banks
    commit 4721f8160f17554b003e8928bb61e6c9b2fe92a3 upstream.
    
    When checking if a VP ID is included in a sparse bank set, explicitly check
    that the ID can actually be contained in a sparse bank (the TLFS allows for
    a maximum of 64 banks of 64 vCPUs each).  When handling a paravirtual TLB
    flush for L2, the VP ID is copied verbatim from the enlightened VMCS,
    without any bounds check, i.e. isn't guaranteed to be under the limit of
    4096.
    
    Failure to check the bounds of the VP ID leads to an out-of-bounds read
    when testing the sparse bank, and super strictly speaking could lead to KVM
    performing an unnecessary TLB flush for an L2 vCPU.
    
      ==================================================================
      BUG: KASAN: use-after-free in hv_is_vp_in_sparse_set+0x85/0x100 [kvm]
      Read of size 8 at addr ffff88811ba5f598 by task hyperv_evmcs/2802
    
      CPU: 12 UID: 1000 PID: 2802 Comm: hyperv_evmcs Not tainted 7.1.0-rc2 #7 PREEMPT
      Hardware name: QEMU Standard PC (Q35 + ICH9, 2009), BIOS 0.0.0 02/06/2015
      Call Trace:
       <TASK>
       dump_stack_lvl+0x51/0x60
       print_report+0xcb/0x5d0
       kasan_report+0xb4/0xe0
       kasan_check_range+0x35/0x1b0
       hv_is_vp_in_sparse_set+0x85/0x100 [kvm]
       kvm_hv_flush_tlb+0xe9e/0x16c0 [kvm]
       kvm_hv_hypercall+0xe6b/0x1e60 [kvm]
       vmx_handle_exit+0x485/0x1b60 [kvm_intel]
       kvm_arch_vcpu_ioctl_run+0x22e3/0x5070 [kvm]
       kvm_vcpu_ioctl+0x5d0/0x10c0 [kvm]
       __x64_sys_ioctl+0x129/0x1a0
       do_syscall_64+0xb9/0xcf0
       entry_SYSCALL_64_after_hwframe+0x4b/0x53
      RIP: 0033:0x7f0e62d1a9bf
       </TASK>
    
      The buggy address belongs to the physical page:
      page: refcount:0 mapcount:0 mapping:0000000000000000 index:0xffffffffffffffff pfn:0x11ba5f
      flags: 0x4000000000000000(zone=1)
      raw: 4000000000000000 0000000000000000 00000000ffffffff 0000000000000000
      raw: ffffffffffffffff 0000000000000000 00000000ffffffff 0000000000000000
      page dumped because: kasan: bad access detected
    
      Memory state around the buggy address:
       ffff88811ba5f480: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
       ffff88811ba5f500: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
      >ffff88811ba5f580: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
                                  ^
       ffff88811ba5f600: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
       ffff88811ba5f680: ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff ff
      ==================================================================
      Disabling lock debugging due to kernel taint
    
    Opportunistically add a compile time assertion to ensure the maximum number
    of sparse banks exactly matches the number of possible bits in the passed
    in mask.
    
    Cc: stable@vger.kernel.org
    Fixes: c58a318f6090 ("KVM: x86: hyper-v: L2 TLB flush")
    Signed-off-by: Hyunwoo Kim <imv4bel@gmail.com>
    Reviewed-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Link: https://patch.msgid.link/aiQyZIJtO-2Aj_xN@v4bel
    [sean: add KASAN splat, drop comment, add assert, massage changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/hyperv.c
+++ arch/x86/kvm/hyperv.c
@@ -1839,6 +1839,11 @@ static bool hv_is_vp_in_sparse_set(u32 vp_id, u64 valid_bank_mask, u64 sparse_ba
 	int valid_bit_nr = vp_id / HV_VCPUS_PER_SPARSE_BANK;
 	unsigned long sbank;
 
+	BUILD_BUG_ON(BITS_PER_TYPE(valid_bank_mask) != HV_MAX_SPARSE_VCPU_BANKS);
+
+	if (valid_bit_nr >= HV_MAX_SPARSE_VCPU_BANKS)
+		return false;
+
 	if (!test_bit(valid_bit_nr, (unsigned long *)&valid_bank_mask))
 		return false;
 
