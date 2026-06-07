550cfb8a81181331d4d0f76ab75ee58a0bf41e3e
Ralph Boehme <slow@samba.org>
2026-06-07 15:23:18 +0200

    smb/client: fix chown/chgrp with SMB3 POSIX Extensions
    commit 760ef2c579c2609cf17fb1cd5392f64d42d43d33 upstream.
    
    Ownership (chown) and group (chgrp) modifications were being ignored when
    mounting with SMB3 POSIX Extensions unless CIFS_MOUNT_CIFS_ACL or
    CIFS_MOUNT_MODE_FROM_SID were also explicitly set.
    
    Fix this by checking for posix_extensions in cifs_setattr_nounix() when
    updating UID and GID, ensuring that id_mode_to_cifs_acl() is called to map
    and set the ownership/group information on the server.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/inode.c
+++ fs/smb/client/inode.c
@@ -3348,7 +3348,8 @@ cifs_setattr_nounix(struct dentry *direntry, struct iattr *attrs)
 	if (attrs->ia_valid & ATTR_GID)
 		gid = attrs->ia_gid;
 
-	if (sbflags & (CIFS_MOUNT_CIFS_ACL | CIFS_MOUNT_MODE_FROM_SID)) {
+	if ((sbflags & (CIFS_MOUNT_CIFS_ACL | CIFS_MOUNT_MODE_FROM_SID)) ||
+	    cifs_sb_master_tcon(cifs_sb)->posix_extensions) {
 		if (uid_valid(uid) || gid_valid(gid)) {
 			mode = NO_CHANGE_64;
 			rc = id_mode_to_cifs_acl(inode, full_path, &mode,
