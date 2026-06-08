14812174d720d83a79ed44ad0295a14cf40182a6
Krishna Chomal <krishna.chomal108@gmail.com>
2026-06-08 19:12:55 +0530

    platform/x86: hp-wmi: Add support for Omen 16-ap0xxx (8E35)
    commit 56b7981c6f21670c0a1a62e6d2f9afb380e2596d upstream.
    
    The HP Omen 16-ap0xxx (board ID: 8E35) has the same WMI interface as
    other Victus S boards, but requires quirks for correctly switching
    thermal profile.
    
    Add the DMI board name to victus_s_thermal_profile_boards[] table and
    map it to omen_v1_legacy_thermal_params.
    
    Testing on board 8E35 confirmed that platform profile is registered
    successfully and fan RPMs are readable and controllable.
    
    Tested-by: Ahmet Öztürk <sivasli-ahmet@gmx.de>
    Reported-by: Ahmet Öztürk <sivasli-ahmet@gmx.de>
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=221523
    Cc: stable@vger.kernel.org # v6.18+
    Signed-off-by: Krishna Chomal <krishna.chomal108@gmail.com>
    Link: https://patch.msgid.link/20260608134255.36280-1-krishna.chomal108@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/platform/x86/hp/hp-wmi.c
+++ drivers/platform/x86/hp/hp-wmi.c
@@ -261,6 +261,10 @@ static const struct dmi_system_id victus_s_thermal_profile_boards[] __initconst 
 		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8D87") },
 		.driver_data = (void *)&omen_v1_no_ec_thermal_params,
 	},
+	{
+		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8E35") },
+		.driver_data = (void *)&omen_v1_legacy_thermal_params,
+	},
 	{},
 };
 
