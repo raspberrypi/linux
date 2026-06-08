88b8918b2f8dbc506b75a62c8931ffa19eceed55
Yuho Choi <dbgh9129@gmail.com>
2026-06-08 12:22:30 -0400

    sctp: Unwind address notifier registration on failure
    [ Upstream commit c8459ee2fef502d6ef6c063751c33d9ac7943eab ]
    
    sctp_v4_add_protocol() and sctp_v6_add_protocol() register their
    address notifiers before registering the SCTP protocol handlers. If
    protocol registration fails, the functions return without unregistering
    the notifiers.
    
    Unregister the notifiers on the protocol registration failure paths.
    Also propagate notifier registration failures instead of ignoring them.
    
    Signed-off-by: Yuho Choi <dbgh9129@gmail.com>
    Link: https://patch.msgid.link/20260608162230.46644-1-dbgh9129@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/sctp/protocol.c
+++ net/sctp/protocol.c
@@ -1279,12 +1279,18 @@ static void sctp_v4_protosw_exit(void)
 
 static int sctp_v4_add_protocol(void)
 {
+	int ret;
+
 	/* Register notifier for inet address additions/deletions. */
-	register_inetaddr_notifier(&sctp_inetaddr_notifier);
+	ret = register_inetaddr_notifier(&sctp_inetaddr_notifier);
+	if (ret)
+		return ret;
 
 	/* Register SCTP with inet layer.  */
-	if (inet_add_protocol(&sctp_protocol, IPPROTO_SCTP) < 0)
+	if (inet_add_protocol(&sctp_protocol, IPPROTO_SCTP) < 0) {
+		unregister_inetaddr_notifier(&sctp_inetaddr_notifier);
 		return -EAGAIN;
+	}
 
 	return 0;
 }
