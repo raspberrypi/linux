85873b1bd3664af5cba608a014cd2df930d77f3e
John Johansen <john.johansen@canonical.com>
2026-06-14 16:16:59 -0700

    apparmor: fix label can not be immediately before a declaration
    [ Upstream commit 1ed40bd525c00d22af666016af9aef7167f8085f ]
    
    Fix error reported by kernel test robot
    
    security/apparmor/policy.c:1381:2: error: a label can only be part of
    a statement and a declaration is not a statement
    
    All errors (new ones prefixed by >>):
    
       security/apparmor/policy.c: In function 'aa_replace_profiles':
    >> security/apparmor/policy.c:1381:2: error: a label can only be part
       of a statement and a declaration is not a statement
         ssize_t udata_sz = udata->size;
         ^~~~~
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202606150525.npax8WiH-lkp@intel.com/
    Fixes: 7b42f95813dc9 ("apparmor: fix potential UAF in aa_replace_profiles")
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- security/apparmor/policy.c
+++ security/apparmor/policy.c
@@ -1373,9 +1373,10 @@ ssize_t aa_replace_profiles(struct aa_ns *policy_ns, struct aa_label *label,
 	mutex_unlock(&ns->lock);
 
 out:
+	aa_put_ns(ns);
+
 	ssize_t udata_sz = udata->size;
 
-	aa_put_ns(ns);
 	aa_put_profile_loaddata(udata);
 	kfree(ns_name);
 
