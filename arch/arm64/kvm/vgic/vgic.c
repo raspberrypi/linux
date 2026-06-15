b1a89d12d35a8256d2b170ced0b1c86851f3def2
Marc Zyngier <maz@kernel.org>
2026-06-15 19:16:25 +0100

    KVM: arm64: vgic: Handle race between interrupt affinity change and LPI disabling
    commit 7258770e5814f15e8308ebda82ac9acf6964ba8e upstream.
    
    Hyunwoo Kim reports some really bad races should the following
    situation occur:
    
    - LPI-I is pending in vcpu-B's AP list
    - vcpu-A writes to vcpu-B's RD to disable its LPIs
    - vcpu-C moves I from B to C
    
    If the last two race nicely enough, vgic_prune_ap_list() can drop
    the irq and AP list locks, reacquire them, and in the interval
    the irq has been freed. UAF follows.
    
    The fix is two-fold:
    
    - Before dropping the irq and ap_list locks, take a reference on
      the irq
    
    - Do not try to handle migration of the pending bit: there is no
      expectation that this state is retained, as per the architecture
    
    With that, we're sure that the interrupt is still around, and we
    safely remove it from the AP list as it has no target at this
    stage (unless another interrupt fires, but that's another story).
    
    Reported-by: Hyunwoo Kim <imv4bel@gmail.com>
    Tested-by: Hyunwoo Kim <imv4bel@gmail.com>
    Link: https://lore.kernel.org/r/ailsCnyoS82r_QRz@v4bel
    Link: https://patch.msgid.link/20260615181625.3029352-1-maz@kernel.org
    Fixes: 5dd4b924e390a ("KVM: arm/arm64: vgic: Add refcounting for IRQs")
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/kvm/vgic/vgic.c
+++ arch/arm64/kvm/vgic/vgic.c
@@ -204,6 +204,7 @@ void vgic_flush_pending_lpis(struct kvm_vcpu *vcpu)
 	list_for_each_entry_safe(irq, tmp, &vgic_cpu->ap_list_head, ap_list) {
 		if (irq_is_lpi(vcpu->kvm, irq->intid)) {
 			raw_spin_lock(&irq->irq_lock);
+			irq->pending_latch = false;
 			list_del(&irq->ap_list);
 			irq->vcpu = NULL;
 			raw_spin_unlock(&irq->irq_lock);
@@ -795,7 +796,11 @@ retry:
 			continue;
 		}
 
-		/* This interrupt looks like it has to be migrated. */
+		/*
+		 * This interrupt looks like it has to be migrated,
+		 * make sure it is kept alive while locks are dropped.
+		 */
+		vgic_get_irq_ref(irq);
 
 		raw_spin_unlock(&irq->irq_lock);
 		raw_spin_unlock(&vgic_cpu->ap_list_lock);
@@ -839,6 +844,8 @@ retry:
 		raw_spin_unlock(&irq->irq_lock);
 		raw_spin_unlock(&vcpuB->arch.vgic_cpu.ap_list_lock);
 		raw_spin_unlock(&vcpuA->arch.vgic_cpu.ap_list_lock);
+
+		deleted_lpis |= vgic_put_irq_norelease(vcpu->kvm, irq);
 
 		if (target_vcpu_needs_kick) {
 			kvm_make_request(KVM_REQ_IRQ_PENDING, target_vcpu);
