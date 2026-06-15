8667010fc6c4fc05fc40d39c06f3984d9a0d0a9b
Pengpeng Hou <pengpeng@iscas.ac.cn>
2026-06-15 17:19:18 +0800

    gpio: mlxbf3: fail probe if gpiochip registration fails
    [ Upstream commit 0482862a90169f4daaba0ed31a85d8304bf51e04 ]
    
    mlxbf3_gpio_probe() logs a devm_gpiochip_add_data() failure but still
    returns success. That leaves the platform device bound even though the
    GPIO chip was not registered.
    
    Return the registration error so probe failure matches the missing
    gpiochip state.
    
    Fixes: cd33f216d241 ("gpio: mlxbf3: Add gpio driver support")
    Signed-off-by: Pengpeng Hou <pengpeng@iscas.ac.cn>
    Reviewed-by: Linus Walleij <linusw@kernel.org>
    Link: https://patch.msgid.link/20260615091918.43333-1-pengpeng@iscas.ac.cn
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@oss.qualcomm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpio/gpio-mlxbf3.c
+++ drivers/gpio/gpio-mlxbf3.c
@@ -252,7 +252,8 @@ static int mlxbf3_gpio_probe(struct platform_device *pdev)
 
 	ret = devm_gpiochip_add_data(dev, &gs->gc, gs);
 	if (ret)
-		dev_err_probe(dev, ret, "Failed adding memory mapped gpiochip\n");
+		return dev_err_probe(dev, ret,
+				     "Failed adding memory mapped gpiochip\n");
 
 	return 0;
 }
