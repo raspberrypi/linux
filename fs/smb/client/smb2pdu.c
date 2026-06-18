00b0fa425941438b664950a8ee65dfba2def4336
Henrique Carvalho <henrique.carvalho@suse.com>
2026-06-18 17:34:38 -0300

    smb: client: fix query directory replay double-free
    commit 9647492b5e41954be59d5157eddbcd4cdc1656f7 upstream.
    
    A response-bearing attempt can return a replayable error and free its
    response buffer. If SMB2_query_directory_init() fails before the next send,
    cleanup retains the previous buffer type and frees that response again.
    
    Reset response bookkeeping before each attempt to prevent the stale free.
    
    Fixes: 4f1fffa23769 ("cifs: commands that are retried should have replay flag set")
    Cc: stable@vger.kernel.org
    Signed-off-by: Henrique Carvalho <henrique.carvalho@suse.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/smb2pdu.c
+++ fs/smb/client/smb2pdu.c
@@ -5668,6 +5668,8 @@ send_set_info(const unsigned int xid, struct cifs_tcon *tcon,
 
 replay_again:
 	/* reinitialize for possible replay */
+	resp_buftype = CIFS_NO_BUFFER;
+	memset(&rsp_iov, 0, sizeof(rsp_iov));
 	flags = 0;
 	server = cifs_pick_channel(ses);
 
