562d5e6f9b994058e3b72536f5a1fd461a30bc16
André Draszik <andre.draszik@linaro.org>
2026-06-18 13:00:39 +0100

    dma-fence: use correct callback in dma_fence_timeline_name()
    commit 032a6f6ce21fc701468fd15403d2f53a30107f5b upstream.
    
    dma_fence_timeline_name() is a wrapper around
    dma_fence_ops::get_timeline_name(). Since the blamed commit below, it
    calls an incorrect callback.
    
    Update it to restore functionality by calling the intended callback.
    
    Fixes: 62918542b7bf ("dma-fence: Fix sparse warnings due __rcu annotations")
    Cc: <stable@vger.kernel.org> # v7.1+
    [tursulin: added cc stable]
    Signed-off-by: André Draszik <andre.draszik@linaro.org>
    Reviewed-by: Philipp Stanner <phasta@kernel.org>
    Signed-off-by: Tvrtko Ursulin <tursulin@ursulin.net>
    Link: https://lore.kernel.org/r/20260618-linux-drm_crtc_fix-v1-1-801f29c9853d@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/dma-buf/dma-fence.c
+++ drivers/dma-buf/dma-fence.c
@@ -1202,7 +1202,7 @@ const char __rcu *dma_fence_timeline_name(struct dma_fence *fence)
 	/* RCU protection is required for safe access to returned string */
 	ops = rcu_dereference(fence->ops);
 	if (!dma_fence_test_signaled_flag(fence))
-		return (const char __rcu *)ops->get_driver_name(fence);
+		return (const char __rcu *)ops->get_timeline_name(fence);
 	else
 		return (const char __rcu *)"signaled-timeline";
 }
