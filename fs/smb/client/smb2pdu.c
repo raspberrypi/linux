14498ff5ce0f272ce0ef988721413e06b7038972
Henrique Carvalho <henrique.carvalho@suse.com>
2026-06-18 17:34:33 -0300

    smb: client: fix double-free in SMB2_open() replay
    commit b55e182f2324bc6a604c21a47aa6c448f719a532 upstream.
    
    A response-bearing attempt can return a replayable error and free its
    response buffer. If SMB2_open_init() fails before the next send, cleanup
    retains the previous buffer type and frees that response again.
    
    Reset response bookkeeping before each attempt to prevent the stale free.
    
    Fixes: 4f1fffa23769 ("cifs: commands that are retried should have replay flag set")
    Cc: stable@vger.kernel.org
    Signed-off-by: Henrique Carvalho <henrique.carvalho@suse.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/smb2pdu.c
+++ fs/smb/client/smb2pdu.c
@@ -3205,6 +3205,8 @@ SMB2_open(const unsigned int xid, struct cifs_open_parms *oparms, __le16 *path,
 
 replay_again:
 	/* reinitialize for possible replay */
+	resp_buftype = CIFS_NO_BUFFER;
+	memset(&rsp_iov, 0, sizeof(rsp_iov));
 	flags = 0;
 	server = cifs_pick_channel(ses);
 	oparms->replay = !!(retries);
