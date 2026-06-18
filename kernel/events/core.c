cd8dab11046c03ab07899ce3fe2aa7d1a9702cf1
Peter Zijlstra <peterz@infradead.org>
2026-06-18 14:18:27 +0200

    perf: Fix addr_filter_ranges lifetime
    [ Upstream commit e62d4192e593630f355094adc467058a05bdc935 ]
    
    Lee Jia Jie reported that since event::addr_filter_ranges is used
    under RCU, it should be RCU freed.
    
    Reported-by: Lee Jia Jie <jiajie.lee@starlabs.sg>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5124,6 +5124,7 @@ static void free_event_rcu(struct rcu_head *head)
 	if (event->ns)
 		put_pid_ns(event->ns);
 	perf_event_free_filter(event);
+	kfree(event->addr_filter_ranges);
 	kmem_cache_free(perf_event_cache, event);
 }
 
@@ -5335,8 +5336,6 @@ static void __free_event(struct perf_event *event)
 {
 	if (event->attach_state & PERF_ATTACH_CALLCHAIN)
 		put_callchain_buffers();
-
-	kfree(event->addr_filter_ranges);
 
 	if (event->attach_state & PERF_ATTACH_EXCLUSIVE)
 		exclusive_event_destroy(event);
