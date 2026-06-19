ad58fa1cb11e4ceb702786a494352c7ff2eca1ed
Antoniu Miclaus <antoniu.miclaus@analog.com>
2026-06-19 17:34:43 +0300

    iio: light: bh1780: fix PM runtime leak on error path
    commit dd72e6c3cdea05cad24e99710939086f7a113fb5 upstream.
    
    Move pm_runtime_put_autosuspend() before the error check to ensure
    the PM runtime reference count is always decremented after
    pm_runtime_get_sync(), regardless of whether the read operation
    succeeds or fails.
    
    Fixes: 1f0477f18306 ("iio: light: new driver for the ROHM BH1780")
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>
    Reviewed-by: Linus Walleij <linusw@kernel.org>
    Cc: <Stable@vger.kernel.org>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    [ moved both pm_runtime_mark_last_busy() and pm_runtime_put_autosuspend() before the error check instead of just pm_runtime_put_autosuspend() ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Elizaveta Tereshkina <etereshkina@astralinux.ru>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/iio/light/bh1780.c
+++ drivers/iio/light/bh1780.c
@@ -109,10 +109,10 @@ static int bh1780_read_raw(struct iio_dev *indio_dev,
 		case IIO_LIGHT:
 			pm_runtime_get_sync(&bh1780->client->dev);
 			value = bh1780_read_word(bh1780, BH1780_REG_DLOW);
-			if (value < 0)
-				return value;
 			pm_runtime_mark_last_busy(&bh1780->client->dev);
 			pm_runtime_put_autosuspend(&bh1780->client->dev);
+			if (value < 0)
+				return value;
 			*val = value;
 
 			return IIO_VAL_INT;
