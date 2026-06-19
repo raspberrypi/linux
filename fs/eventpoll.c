89dbf792b525ad7fa00b2cec747a5479efa1d323
Christian Brauner <brauner@kernel.org>
2026-06-19 16:58:41 +0200

    eventpoll: use hlist_is_singular_node() in __ep_remove()
    [ Upstream commit 3d9fd0abc94d8cd430cc7cd7d37ce5e5aae2cd2b ]
    
    Replace the open-coded "epi is the only entry in file->f_ep" check
    with hlist_is_singular_node(). Same semantics, and the helper avoids
    the head-cacheline access in the common false case.
    
    Link: https://patch.msgid.link/20260423-work-epoll-uaf-v1-1-2470f9eec0f5@kernel.org
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>
    Stable-dep-of: a6dc643c6931 ("eventpoll: fix ep_remove struct eventpoll / struct file UAF")
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -827,7 +827,7 @@ static bool __ep_remove(struct eventpoll *ep, struct epitem *epi, bool force)
 
 	to_free = NULL;
 	head = file->f_ep;
-	if (head->first == &epi->fllink && !epi->fllink.next) {
+	if (hlist_is_singular_node(&epi->fllink, head)) {
 		/* See eventpoll_release() for details. */
 		WRITE_ONCE(file->f_ep, NULL);
 		if (!is_file_epoll(file)) {
