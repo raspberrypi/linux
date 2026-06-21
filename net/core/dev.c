0943e331500c3605f3ad0808002eec0f7ac2d8e5
Petr Wozniak <petr.wozniak@gmail.com>
2026-06-21 12:03:26 +0200

    xfrm: propagate -EINPROGRESS from validate_xmit_xfrm()
    [ Upstream commit 6860b467f569f732b11cbc588ae7e195e90e7e23 ]
    
    validate_xmit_xfrm() returns NULL both when a packet is dropped and
    when it is stolen by async crypto (-EINPROGRESS from ->xmit()).
    Callers cannot distinguish the two cases.
    
    f53c723902d1 ("net: Add asynchronous callbacks for xfrm on layer 2.")
    changed the semantics of a NULL return from "dropped" to "stolen or
    dropped", but __dev_queue_xmit() was not updated.  On virtual/bridge
    interfaces (noqueue qdisc) __dev_queue_xmit() initialises rc=-ENOMEM
    and jumps to out: when skb is NULL, returning -ENOMEM to the caller
    even though the packet will be delivered correctly via xfrm_dev_resume().
    
    Return ERR_PTR(-EINPROGRESS) from validate_xmit_xfrm() for the async
    case so callers can tell it apart from a real drop.  Update
    __dev_queue_xmit() to handle ERR_PTR(-EINPROGRESS) from
    validate_xmit_skb() correctly.  Update validate_xmit_skb_list() to
    use IS_ERR_OR_NULL() so that ERR_PTR(-EINPROGRESS) is not mistakenly
    added to the transmitted list.
    
    Fixes: f53c723902d1 ("net: Add asynchronous callbacks for xfrm on layer 2.")
    Suggested-by: Sabrina Dubroca <sd@queasysnail.net>
    Signed-off-by: Petr Wozniak <petr.wozniak@gmail.com>
    Signed-off-by: Steffen Klassert <steffen.klassert@secunet.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/dev.c
+++ net/core/dev.c
@@ -4016,6 +4016,9 @@ out_free:
 	return NULL;
 }
 
+/* Returns the skb on success, NULL if dropped, or ERR_PTR(-EINPROGRESS)
+ * if stolen by async xfrm crypto (delivered via xfrm_dev_resume()).
+ */
 static struct sk_buff *validate_xmit_skb(struct sk_buff *skb, struct net_device *dev, bool *again)
 {
 	netdev_features_t features;
@@ -4087,7 +4090,7 @@ struct sk_buff *validate_xmit_skb_list(struct sk_buff *skb, struct net_device *d
 		skb->prev = skb;
 
 		skb = validate_xmit_skb(skb, dev, again);
-		if (!skb)
+		if (IS_ERR_OR_NULL(skb))
 			continue;
 
 		if (!head)
@@ -4858,8 +4861,11 @@ int __dev_queue_xmit(struct sk_buff *skb, struct net_device *sb_dev)
 			goto recursion_alert;
 
 		skb = validate_xmit_skb(skb, dev, &again);
-		if (!skb)
+		if (IS_ERR_OR_NULL(skb)) {
+			if (PTR_ERR(skb) == -EINPROGRESS)
+				rc = NET_XMIT_SUCCESS;
 			goto out;
+		}
 
 		HARD_TX_LOCK(dev, txq, cpu);
 
