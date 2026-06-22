68eab5a64ddbc0bb34f6241f7983fc03eb4aeaf2
Ce Sun <cesun102@amd.com>
2026-06-22 23:05:09 +0800

    drm/amdgpu: fix resource leak on ACP reset timeout
    commit 020da7c5aac5b86bad8a1571f6eda6b8cff9331d upstream.
    
    When ACP soft reset poll times out, original code returns early without cleanup,
    leaking MFD child devices, genpd links and all ACP heap allocations.
    
    Replace direct early return with goto out to force run all cleanup logic
    regardless of reset success, preserve timeout error code for caller.
    
    Signed-off-by: Ce Sun <cesun102@amd.com>
    Reviewed-by: Tao Zhou <tao.zhou1@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 98073e4328d7a8d75d03696ab27f6de70ef1aeda)
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_acp.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_acp.c
@@ -505,6 +505,7 @@ static int acp_hw_fini(struct amdgpu_ip_block *ip_block)
 	u32 val = 0;
 	u32 count = 0;
 	struct amdgpu_device *adev = ip_block->adev;
+	int ret = 0;
 
 	/* return early if no ACP */
 	if (!adev->acp.acp_genpd) {
@@ -526,7 +527,8 @@ static int acp_hw_fini(struct amdgpu_ip_block *ip_block)
 			break;
 		if (--count == 0) {
 			dev_err(&adev->pdev->dev, "Failed to reset ACP\n");
-			return -ETIMEDOUT;
+			ret = -ETIMEDOUT;
+			goto out;
 		}
 		udelay(100);
 	}
@@ -543,11 +545,12 @@ static int acp_hw_fini(struct amdgpu_ip_block *ip_block)
 			break;
 		if (--count == 0) {
 			dev_err(&adev->pdev->dev, "Failed to reset ACP\n");
-			return -ETIMEDOUT;
+			ret = -ETIMEDOUT;
+			goto out;
 		}
 		udelay(100);
 	}
-
+out:
 	device_for_each_child(adev->acp.parent, NULL,
 			      acp_genpd_remove_device);
 
@@ -556,7 +559,7 @@ static int acp_hw_fini(struct amdgpu_ip_block *ip_block)
 	kfree(adev->acp.acp_genpd);
 	kfree(adev->acp.acp_cell);
 
-	return 0;
+	return ret;
 }
 
 static int acp_suspend(struct amdgpu_ip_block *ip_block)
