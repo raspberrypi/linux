a99a617701186dc68c7b330d35fc2253f48e2ab2
David Howells <dhowells@redhat.com>
2026-06-22 10:08:49 +0100

    afs: Fix missing NULL pointer check in afs_break_some_callbacks()
    [ Upstream commit 794a01110390c1b76f59ece773fb0fbfd89c6f5c ]
    
    Fix afs_break_some_callbacks() to check to see if afs_lookup_volume_rcu()
    returned NULL (e.g. the specified volume is unknown).
    
    Fixes: 8230fd8217b7 ("afs: Make callback processing more efficient.")
    Closes: https://sashiko.dev/#/patchset/20260609081738.770127-1-dhowells%40redhat.com
    Signed-off-by: David Howells <dhowells@redhat.com>
    Link: https://patch.msgid.link/20260622090856.2746629-16-dhowells@redhat.com
    cc: Marc Dionne <marc.dionne@auristor.com>
    cc: linux-afs@lists.infradead.org
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/afs/callback.c
+++ fs/afs/callback.c
@@ -220,7 +220,11 @@ static void afs_break_some_callbacks(struct afs_server *server,
 
 	rcu_read_lock();
 	volume = afs_lookup_volume_rcu(server->cell, vid);
-	if (cbb->fid.vnode == 0 && cbb->fid.unique == 0) {
+	if (!volume) {
+		/* Ignore breaks on unknown volumes. */
+		rcu_read_unlock();
+		*_count = 0;
+	} else if (cbb->fid.vnode == 0 && cbb->fid.unique == 0) {
 		afs_break_volume_callback(server, volume);
 		*_count -= 1;
 		if (*_count)
