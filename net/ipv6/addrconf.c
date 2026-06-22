20255f2cb0f7a82ca5967faff48bb241f06903c0
Fernando Fernandez Mancera <fmancera@suse.de>
2026-06-22 15:08:52 +0200

    ipv6: fix error handling in disable_ipv6 sysctl
    [ Upstream commit c779441e5070e2268bdfe77f6e2e0de926c431e3 ]
    
    When writing to the disable_ipv6 sysctl, if proc_dointvec() fails to
    parse the input, it returns a negative error code. The current
    implementation is overwriting that error for write operations.
    
    This results in a silent failure, it returns a successful write although
    the configuration was not modified at all. When modifying the "all"
    variant it can also modify the configuration of existing interfaces to
    the wrong value.
    
    Fix this by checking the return value of proc_dointvec() and returning
    early on failure.
    
    Fixes: 56d417b12e57 ("IPv6: Add 'autoconf' and 'disable_ipv6' module parameters")
    Reviewed-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: Fernando Fernandez Mancera <fmancera@suse.de>
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Link: https://patch.msgid.link/20260622130857.5115-2-fmancera@suse.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -6458,6 +6458,8 @@ static int addrconf_sysctl_disable(const struct ctl_table *ctl, int write,
 	lctl.data = &val;
 
 	ret = proc_dointvec(&lctl, write, buffer, lenp, ppos);
+	if (ret)
+		return ret;
 
 	if (write)
 		ret = addrconf_disable_ipv6(ctl, valp, val);
