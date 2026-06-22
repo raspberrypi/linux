c5330cd9003d93859169287a066b070c1263e039
Chen Cheng <chencheng@fnnas.com>
2026-06-22 20:46:49 +0800

    md/raid5: protect bitmap batch counters aka seq_flush/seq_write consistency
    [ Upstream commit f565925810cb8bc799421485770e15d922ef766a ]
    
    kcsan detect race :
    - raid5d() closes the current bitmap batch by updating
    	conf->seq_flush under conf->device_lock.
    - __add_stripe_bio() read conf->seq_flush without that
    	lock when assigning sh->bm_seq.
    
    so, protect seq_flush/seq_write consistency for multiple CPUs by
    READ_ONCE()/WRITE_ONCE() under the path without held device_lock.
    
    re-explain the stripe batch sequence number update flow:
    1. sh->bm_seq declare which batch number the stripe belongs to
       when perform bitmap-related write.
    	==> bm_seq = seq_flush+1
    
    2. stripe be handled,
    	* if sh->bm_seq - conf->seq_write > 0, means the
    	  batch stripes **newer than** the last written
    	  batch, it cannot proceed yet, queued on bitmap_list.
    	* otherwise , has already proceed.
    
    3. raid5d() `++seq_flush` to closes the current batch, means
    	* no more stripes join that old batch
    	* just-closed batch ready to write-out to disk
    
    4. raid5d() calls bitmap hooks unplug() or writeout, then,
       `++seq_write` to the same as bm_seq.
    
    - seq_flush - for producer, to close batches.
    - seq_write - for consumer, the checkpoint number.
    
    the report:
    ====================================
    BUG: KCSAN: data-race in __add_stripe_bio / raid5d
    
    write to 0xffff88ba5625d470 of 4 bytes by task 82401 on cpu 0:
     raid5d+0x1d9/0xba0
     [.....]
    
    read to 0xffff88ba5625d470 of 4 bytes by task 82421 on cpu 8:
     __add_stripe_bio+0x332/0x400
     raid5_make_request+0x6ac/0x2930
     md_handle_request+0x4a2/0xa40
     md_submit_bio+0x109/0x1a0
     __submit_bio+0x2ec/0x390
     [.....]
    
    Fixes: 7c13edc87510 ("md: incorporate new plugging into raid5.")
    
    v1 -> v2:
    - remove WRITE_ONCE(conf->seq_write) in held device_lock path.
    - remove READ_ONCE(conf->seq_flush) in held device_lock path.
    
    Signed-off-by: Chen Cheng <chencheng@fnnas.com>
    Reviewed-by: Yu Kuai <yukuai@fygo.io>
    Link: https://patch.msgid.link/20260622124649.1780233-1-chencheng@fnnas.com
    Signed-off-by: Yu Kuai <yukuai@fygo.io>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/md/raid5.c
+++ drivers/md/raid5.c
@@ -3545,7 +3545,7 @@ static void __add_stripe_bio(struct stripe_head *sh, struct bio *bi,
 		 sh->dev[dd_idx].sector);
 
 	if (conf->mddev->bitmap && firstwrite && !sh->batch_head) {
-		sh->bm_seq = conf->seq_flush+1;
+		sh->bm_seq = READ_ONCE(conf->seq_flush) + 1;
 		set_bit(STRIPE_BIT_DELAY, &sh->state);
 	}
 }
@@ -5751,7 +5751,7 @@ static void make_discard_request(struct mddev *mddev, struct bio *bi)
 		}
 		spin_unlock_irq(&sh->stripe_lock);
 		if (conf->mddev->bitmap) {
-			sh->bm_seq = conf->seq_flush + 1;
+			sh->bm_seq = READ_ONCE(conf->seq_flush) + 1;
 			set_bit(STRIPE_BIT_DELAY, &sh->state);
 		}
 
@@ -6788,11 +6788,13 @@ static void raid5d(struct md_thread *thread)
 		if (
 		    !list_empty(&conf->bitmap_list)) {
 			/* Now is a good time to flush some bitmap updates */
-			conf->seq_flush++;
+			int seq = conf->seq_flush + 1;
+
+			WRITE_ONCE(conf->seq_flush, seq);
 			spin_unlock_irq(&conf->device_lock);
 			mddev->bitmap_ops->unplug(mddev, true);
 			spin_lock_irq(&conf->device_lock);
-			conf->seq_write = conf->seq_flush;
+			conf->seq_write = seq;
 			activate_bit_delay(conf, conf->temp_inactive_list);
 		}
 		raid5_activate_delayed(conf);
