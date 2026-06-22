50456f445feeeaf05eceb04663217c1040a4dade
Zhan Xusheng <zhanxusheng@xiaomi.com>
2026-06-22 16:11:36 +0800

    erofs: handle 48-bit blocks_hi for compressed inodes
    [ Upstream commit 803d09a554055aba160a62abd1e4b1260b899dc1 ]
    
    Combine i_nb.blocks_hi with i_u.blocks_lo when computing
    inode->i_blocks for compressed inodes, mirroring the startblk_hi
    handling for unencoded inodes a few lines above.  Also evaluate
    the shift in u64 to avoid truncation.
    
    Fixes: efb2aef569b3 ("erofs: add encoded extent on-disk definition")
    Fixes: 1d191b4ca51d ("erofs: implement encoded extent metadata")
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Zhan Xusheng <zhanxusheng@xiaomi.com>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/erofs/inode.c
+++ fs/erofs/inode.c
@@ -191,8 +191,9 @@ static int erofs_read_inode(struct inode *inode)
 		err = -EFSCORRUPTED;
 		goto err_out;
 	} else {
-		inode->i_blocks = le32_to_cpu(copied.i_u.blocks_lo) <<
-				(sb->s_blocksize_bits - 9);
+		inode->i_blocks = (le32_to_cpu(copied.i_u.blocks_lo) |
+			((u64)le16_to_cpu(copied.i_nb.blocks_hi) << 32)) <<
+				  (sb->s_blocksize_bits - 9);
 	}
 
 	if (vi->datalayout == EROFS_INODE_CHUNK_BASED) {
