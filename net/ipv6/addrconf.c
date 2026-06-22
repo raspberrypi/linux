b060606bc7e4614746dc93a245e06e923a027146
Fernando Fernandez Mancera <fmancera@suse.de>
2026-06-22 15:08:53 +0200

    ipv6: fix error handling in ignore_routes_with_linkdown sysctl
    [ Upstream commit cf4f2b14401f29ccac56393ca9e4b42a2505f540 ]
    
    When writing to the ignore_routes_with_linkdown sysctl, if
    proc_dointvec() fails to parse the input, it returns a negative error
    code. The current implementation is overwriting that error for write
    operations.
    
    This results in a silent failure, it returns a successful write although
    the configuration was not modified at all. When modifying the "all"
    variant it can also modify the configuration of existing interfaces to
    the wrong value.
    
    Fix this by checking the return value of proc_dointvec() and returning
    early on failure.
    
    Fixes: 35103d11173b ("net: ipv6 sysctl option to ignore routes when nexthop link is down")
    Reviewed-by: Nicolas Dichtel <nicolas.dichtel@6wind.com>
    Signed-off-by: Fernando Fernandez Mancera <fmancera@suse.de>
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Link: https://patch.msgid.link/20260622130857.5115-3-fmancera@suse.de
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -6651,6 +6651,8 @@ int addrconf_sysctl_ignore_routes_with_linkdown(const struct ctl_table *ctl,
 	lctl.data = &val;
 
 	ret = proc_dointvec(&lctl, write, buffer, lenp, ppos);
+	if (ret)
+		return ret;
 
 	if (write)
 		ret = addrconf_fixup_linkdown(ctl, valp, val);
