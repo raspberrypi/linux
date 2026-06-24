5c61c7b562edf1d70163ab172a95413858f72fb5
Shuaisong Yang <yangshuaisong@h-partners.com>
2026-06-24 22:13:17 +0800

    net: hns3: refactor MAC autoneg and speed configuration
    [ Upstream commit c01f6e6bdc1ccd21b2d07d23f50b82437b8cbf88 ]
    
    Extract the MAC autoneg and speed/duplex/lane configuration logic out
    of hclge_mac_init() and encapsulate it into a new dedicated helper
    function hclge_set_autoneg_speed_dup().
    
    In the init path (hclge_init_ae_dev), this helper is now called after
    hclge_update_port_info() so that firmware-reported autoneg values are
    already populated before applying the link configuration.
    
    Introduce a separate req_lane_num field in struct hclge_mac to isolate
    the user-requested lane count from mac.lane_num, which firmware may
    overwrite via hclge_get_sfp_info() with stale values from a prior link
    lifecycle (e.g., lane_num=4 from 100G). During probe, req_lane_num is
    initialized to 0, which instructs firmware to auto-select the correct
    lane count for the current speed, rather than reusing the firmware-
    reported mac.lane_num that may be inconsistent with the target speed.
    This prevents probe failures from mismatched (speed, lane_num) pairs.
    
    In the reset path (hclge_reset_ae_dev), it runs immediately after
    hclge_mac_init(), using the previously cached req_* values to restore
    the link without re-querying firmware.
    
    Signed-off-by: Shuaisong Yang <yangshuaisong@h-partners.com>
    Signed-off-by: Jijie Shao <shaojijie@huawei.com>
    Link: https://patch.msgid.link/20260624141319.271439-3-shaojijie@huawei.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: c711f6d1cee9 ("net: hns3: fix permanent link down deadlock after reset")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_main.c
@@ -1577,6 +1577,11 @@ static int hclge_configure(struct hclge_dev *hdev)
 	hdev->hw.mac.req_autoneg = AUTONEG_ENABLE;
 	hdev->hw.mac.req_duplex = DUPLEX_FULL;
 
+	/* When lane_num is 0, the firmware will automatically
+	 * select the appropriate lane_num based on the speed.
+	 */
+	hdev->hw.mac.req_lane_num = 0;
+
 	hclge_parse_link_mode(hdev, cfg.speed_ability);
 
 	hdev->hw.mac.max_speed = hclge_get_max_speed(cfg.speed_ability);
@@ -2652,6 +2657,7 @@ static int hclge_cfg_mac_speed_dup_h(struct hnae3_handle *handle, int speed,
 	if (ret)
 		return ret;
 
+	hdev->hw.mac.req_lane_num = lane_num;
 	hdev->hw.mac.req_speed = (u32)speed;
 	hdev->hw.mac.req_duplex = duplex;
 
@@ -2956,20 +2962,6 @@ static int hclge_mac_init(struct hclge_dev *hdev)
 
 	if (!test_bit(HCLGE_STATE_RST_HANDLING, &hdev->state))
 		hdev->hw.mac.duplex = HCLGE_MAC_FULL;
-
-	if (hdev->hw.mac.support_autoneg) {
-		ret = hclge_set_autoneg_en(hdev, hdev->hw.mac.autoneg);
-		if (ret)
-			return ret;
-	}
-
-	if (!hdev->hw.mac.autoneg) {
-		ret = hclge_cfg_mac_speed_dup_hw(hdev, hdev->hw.mac.req_speed,
-						 hdev->hw.mac.req_duplex,
-						 hdev->hw.mac.lane_num);
-		if (ret)
-			return ret;
-	}
 
 	mac->link = 0;
 
@@ -11748,6 +11740,27 @@ static int hclge_set_wol(struct hnae3_handle *handle,
 	return ret;
 }
 
+static int hclge_set_autoneg_speed_dup(struct hclge_dev *hdev)
+{
+	int ret;
+
+	if (hdev->hw.mac.support_autoneg) {
+		ret = hclge_set_autoneg_en(hdev, hdev->hw.mac.autoneg);
+		if (ret)
+			return ret;
+	}
+
+	if (!hdev->hw.mac.autoneg) {
+		ret = hclge_cfg_mac_speed_dup_hw(hdev, hdev->hw.mac.req_speed,
+						 hdev->hw.mac.req_duplex,
+						 hdev->hw.mac.req_lane_num);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 static int hclge_init_ae_dev(struct hnae3_ae_dev *ae_dev)
 {
 	struct pci_dev *pdev = ae_dev->pdev;
@@ -11908,6 +11921,13 @@ static int hclge_init_ae_dev(struct hnae3_ae_dev *ae_dev)
 	ret = hclge_update_port_info(hdev);
 	if (ret)
 		goto err_ptp_uninit;
+
+	ret = hclge_set_autoneg_speed_dup(hdev);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"failed to set autoneg speed duplex, ret = %d\n", ret);
+		goto err_ptp_uninit;
+	}
 
 	INIT_KFIFO(hdev->mac_tnl_log);
 
@@ -12236,6 +12256,13 @@ static int hclge_reset_ae_dev(struct hnae3_ae_dev *ae_dev)
 	ret = hclge_mac_init(hdev);
 	if (ret) {
 		dev_err(&pdev->dev, "Mac init error, ret = %d\n", ret);
+		return ret;
+	}
+
+	ret = hclge_set_autoneg_speed_dup(hdev);
+	if (ret) {
+		dev_err(&pdev->dev,
+			"failed to set autoneg speed duplex, ret = %d\n", ret);
 		return ret;
 	}
 
