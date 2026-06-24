57e1f2cd6a0e890ee2eadbd93de3dcd5e22653a5
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2026-06-25 08:34:48 +0900

    tracing/probes: Make the $ prefix mandatory for comm access
    [ Upstream commit a369299c3f785cf556bbef2de2db0aa2d294c4c9 ]
    
    Since $comm or $COMM are not event field but special fetcharg
    variables to access current->comm, It should not be accessed
    without '$' prefix even with typecast.
    
    Link: https://lore.kernel.org/all/178231209724.732967.12049805699091810641.stgit@devnote2/
    
    Fixes: 69efd863a785 ("tracing/eprobes: Allow use of BTF names to dereference pointers")
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/trace_probe.c
+++ kernel/trace/trace_probe.c
@@ -342,10 +342,6 @@ static int parse_trace_event(char *arg, struct fetch_insn *code,
 	ret = parse_trace_event_arg(arg, code, ctx);
 	if (!ret)
 		return 0;
-	if (strcmp(arg, "comm") == 0 || strcmp(arg, "COMM") == 0) {
-		code->op = FETCH_OP_COMM;
-		return 0;
-	}
 	return -EINVAL;
 }
 
@@ -1073,8 +1069,14 @@ static int parse_probe_vars(char *orig_arg, const struct fetch_type *t,
 	int len;
 
 	if (ctx->flags & TPARG_FL_TEVENT) {
-		if (parse_trace_event(arg, code, ctx) < 0)
+		if (parse_trace_event(arg, code, ctx) < 0) {
+			/* 'comm' should be checked after field parsing. */
+			if (strcmp(arg, "comm") == 0 || strcmp(arg, "COMM") == 0) {
+				code->op = FETCH_OP_COMM;
+				return 0;
+			}
 			goto inval;
+		}
 		return 0;
 	}
 
