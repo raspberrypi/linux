7b5f4362c41ac9a40cb7aee078ef81afd7dae052
Alex Deucher <alexander.deucher@amd.com>
2026-06-26 16:33:56 -0400

    drm/gfx12: Program DB_RING_CONTROL
    [ Upstream commit 402ebe22b267783f3b05b714df224fad0223b278 ]
    
    This is needed to allocate occlusion counters across
    both gfx pipes. GFX 12 only has one gfx pipe, so no functional
    change since the default value is 1.
    
    Reviewed-by: Timur Kristóf <timur.kristof@gmail.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Stable-dep-of: 4261cbc7b03f ("drm/amdgpu: read TRUNCATE_COORD_MODE on gfx12")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/amdgpu/gfx_v12_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v12_0.c
@@ -1822,6 +1822,15 @@ static void gfx_v12_0_constants_init(struct amdgpu_device *adev)
 	gfx_v12_0_get_cu_info(adev, &adev->gfx.cu_info);
 	gfx_v12_0_get_tcc_info(adev);
 	adev->gfx.config.pa_sc_tile_steering_override = 0;
+	/* Program DB_RING_CONTROL for multiple GFX pipes
+	 * Default power up value is 1.
+	 * Possible values:
+	 * 0 - split occlusion counters between gfx pipes
+	 * 1 - all occlusion counters to pipe 0
+	 * 2 - all occlusion counters to pipe 1
+	 */
+	WREG32_FIELD15_PREREG(GC, 0, DB_RING_CONTROL, COUNTER_CONTROL,
+			      (adev->gfx.me.num_pipe_per_me > 1) ? 0 : 1);
 
 	/* Set whether texture coordinate truncation is conformant. */
 	tmp = RREG32_SOC15(GC, 0, regTA_CNTL2);
