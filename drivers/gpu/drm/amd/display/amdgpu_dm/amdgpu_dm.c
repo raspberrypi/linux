679f23f0a3606afcef1ffabd72222f00a54ad9e3
WenTao Liang <vulab@iscas.ac.cn>
2026-06-28 15:27:40 +0800

    drm/amd/display: set new_stream to NULL after release
    commit 9fa26b9eed6195bf840f39ac183b9a6237548755 upstream.
    
    In dm_update_crtc_state(), the skip_modeset path releases new_stream
    via dc_stream_release() but does not set the pointer to NULL.
    
    If a later error (e.g., color management failure) triggers the fail
    label, the error path calls dc_stream_release() again on the same
    dangling pointer, causing a double release and potential use-after-free.
    
    Fix this by setting new_stream to NULL after the initial release.
    
    Fixes: 9b690ef3c704 ("drm/amd/display: Avoid full modeset when not required")
    Signed-off-by: WenTao Liang <vulab@iscas.ac.cn>
    Reviewed-by: George Zhang <george.zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 99f3af19073b3ddbfd96e789124cce12c4277b28)
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -11383,6 +11383,7 @@ skip_modeset:
 	/* Release extra reference */
 	if (new_stream)
 		dc_stream_release(new_stream);
+	new_stream = NULL;
 
 	/*
 	 * We want to do dc stream updates that do not require a
