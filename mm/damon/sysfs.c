54439210c91e45c412d2db1b10da4b7d992db3f0
SJ Park <sj@kernel.org>
2026-06-28 15:01:10 -0700

    mm/damon/sysfs: kobject_del() target (normal), context and kdamond dirs
    commit 263af33a72d1995ae6cdc22b08d527e2bda17259 upstream.
    
    Patch series "mm/damon/sysfs: kobject_del() directories that users can
    create/remove".
    
    DAMON sysfs interface allows users to create and remove arbitrary number
    of directories on sysfs, using a few files having 'nr_' prefix.  For
    example, 'nr_kdamonds'.  When the user writes a number 'N' to the files,
    directories having name starting from '0' to 'N - 1' are created in the
    same directory.  The pre-existing number-named directories are removed
    before creating the new directories.
    
    For the removal of the existing directories, DAMON sysfs interface use
    only kobject_put().  Because DAMON sysfs interface is the only kernel
    component that manages the directories, there is no problem in normal
    situations.  However, if CONFIG_DEBUG_KOBJECT_RELEASE is enabled, the
    removal of dirs are delayed.  Let's suppose a user writes a non-zero
    number to the 'nr_*' files while there are pre-existing number-named
    directories, on the config enabled kernel.  DAMON sysfs interface
    decreases the reference counts of the existing directories and immediately
    creates new directories.  Because the removal of the sysfs directories is
    delayed, it shows some pre-existing directories of the same names when it
    tries to create the new directories, and fails.
    
    For example, the issue can be triggered like below:
    
        # grep DEBUG_KOBJECT_RELEASE /boot/config-$(uname -r)
        CONFIG_DEBUG_KOBJECT_RELEASE=y
        # ls
        nr_kdamonds
        # echo 1 > nr_kdamonds
        # echo 1 > nr_kdamonds
        bash: echo: write error: File exists
        # dmesg
        [...]
        [  300.880458] kobject: kobject_add_internal failed for 0 with -EEXIST, don't try to register things with the same name in the same directory.
        [...]
    
    Some of the error handling paths of the directories also lack the
    kobject_del() call.  If the user uses nr_* file right after the errors,
    similar issues can happen.
    
    This doesn't cause catastrophic issues like kernel panics or memory
    corruptions.  Users can work around by removing all directories first
    (write 0 to the nr_* files) and then create new directories after
    confirming the old directories are gone.  But, this is definitely a bug
    that causes a bad user experience.
    
    Fix the issues by calling kobject_del() before creating new directories.
    
    
    This patch (of 11)
    
    On CONFIG_DEBUG_KOBJECT_RELEASE enabled kernel, lack of kobject_del()
    could cause directories creation failures due to the name conflicts.  Fix
    those issues for normal creation paths of target, context and kdamond
    directories, and error paths of context and kdamond directories by adding
    kobject_del() calls.
    
    Note that this fix for target directories is not complete since it has a
    similar issue in the damon_sysfs_targets_add_dirs() error path.  Because
    the normal path issue and the error path issue are introduced by different
    commits, this commit is fixing only the normal path issue.  A commit for
    the error path will be added next.
    
    Link: https://lore.kernel.org/20260628220121.97360-1-sj@kernel.org
    Link: https://lore.kernel.org/20260628220121.97360-2-sj@kernel.org
    Fixes: c951cd3b8901 ("mm/damon: implement a minimal stub for sysfs-based DAMON interface")
    Signed-off-by: SJ Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org> # 5.18.x
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/damon/sysfs.c
+++ mm/damon/sysfs.c
@@ -333,6 +333,7 @@ static void damon_sysfs_targets_rm_dirs(struct damon_sysfs_targets *targets)
 
 	for (i = 0; i < targets->nr; i++) {
 		damon_sysfs_target_rm_dirs(targets_arr[i]);
+		kobject_del(&targets_arr[i]->kobj);
 		kobject_put(&targets_arr[i]->kobj);
 	}
 	targets->nr = 0;
@@ -1644,6 +1645,7 @@ static void damon_sysfs_contexts_rm_dirs(struct damon_sysfs_contexts *contexts)
 
 	for (i = 0; i < contexts->nr; i++) {
 		damon_sysfs_context_rm_dirs(contexts_arr[i]);
+		kobject_del(&contexts_arr[i]->kobj);
 		kobject_put(&contexts_arr[i]->kobj);
 	}
 	contexts->nr = 0;
@@ -1682,13 +1684,15 @@ static int damon_sysfs_contexts_add_dirs(struct damon_sysfs_contexts *contexts,
 
 		err = damon_sysfs_context_add_dirs(context);
 		if (err)
-			goto out;
+			goto del_out;
 
 		contexts_arr[i] = context;
 		contexts->nr++;
 	}
 	return 0;
 
+del_out:
+	kobject_del(&context->kobj);
 out:
 	damon_sysfs_contexts_rm_dirs(contexts);
 	kobject_put(&context->kobj);
@@ -2505,6 +2509,7 @@ static void damon_sysfs_kdamonds_rm_dirs(struct damon_sysfs_kdamonds *kdamonds)
 
 	for (i = 0; i < kdamonds->nr; i++) {
 		damon_sysfs_kdamond_rm_dirs(kdamonds_arr[i]);
+		kobject_del(&kdamonds_arr[i]->kobj);
 		kobject_put(&kdamonds_arr[i]->kobj);
 	}
 	kdamonds->nr = 0;
@@ -2559,13 +2564,15 @@ static int damon_sysfs_kdamonds_add_dirs(struct damon_sysfs_kdamonds *kdamonds,
 
 		err = damon_sysfs_kdamond_add_dirs(kdamond);
 		if (err)
-			goto out;
+			goto del_out;
 
 		kdamonds_arr[i] = kdamond;
 		kdamonds->nr++;
 	}
 	return 0;
 
+del_out:
+	kobject_del(&kdamond->kobj);
 out:
 	damon_sysfs_kdamonds_rm_dirs(kdamonds);
 	kobject_put(&kdamond->kobj);
