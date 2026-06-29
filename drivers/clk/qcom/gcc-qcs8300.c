f7a1cf9f679da5c1f007232fcd3396dc3989d1fc
Loic Poulain <loic.poulain@oss.qualcomm.com>
2026-06-29 21:22:56 +0200

    clk: qcom: gcc-qcs8300: Use retention for USB power domains
    [ Upstream commit d8638610e0c9ebab2800b7ad6c2c2a3737090da9 ]
    
    The USB subsystem does not expect to lose its state on suspend:
    
      xhci-hcd xhci-hcd.1.auto: xHC error in resume, USBSTS 0x401, Reinit
      usb usb1: root hub lost power or was reset
    
    To maintain state during suspend, the relevant GDSCs need to stay in
    retention mode, like they do on other similar SoCs. Change the mode to
    PWRSTS_RET_ON to fix.
    
    Fixes: 95eeb2ffce73 ("clk: qcom: Add support for Global Clock Controller on QCS8300")
    Signed-off-by: Loic Poulain <loic.poulain@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260629-monza-suspend-v1-2-b601d8a2f2f8@oss.qualcomm.com
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/clk/qcom/gcc-qcs8300.c
+++ drivers/clk/qcom/gcc-qcs8300.c
@@ -3305,7 +3305,7 @@ static struct gdsc gcc_usb20_prim_gdsc = {
 	.pd = {
 		.name = "gcc_usb20_prim_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 	.flags = RETAIN_FF_ENABLE | POLL_CFG_GDSCR,
 };
 
@@ -3317,7 +3317,7 @@ static struct gdsc gcc_usb30_prim_gdsc = {
 	.pd = {
 		.name = "gcc_usb30_prim_gdsc",
 	},
-	.pwrsts = PWRSTS_OFF_ON,
+	.pwrsts = PWRSTS_RET_ON,
 	.flags = RETAIN_FF_ENABLE | POLL_CFG_GDSCR,
 };
 
