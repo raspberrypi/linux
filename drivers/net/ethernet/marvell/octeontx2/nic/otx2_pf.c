23d917acd9c9a9fd999688ec3fdde7aa58ab8a14
Dawei Feng <dawei.feng@seu.edu.cn>
2026-06-30 15:16:25 +0800

    octeontx2-pf: fix SQB pointer leak on init failure
    commit 62e7df6d042aeebd5efb581074e28865c04477be upstream.
    
    otx2_init_hw_resources() initializes SQ aura and pool resources before
    several later setup steps. On failure, err_free_sq_ptrs only frees SQB
    pages, leaving the per-SQ sqb_ptrs arrays behind.
    
    Use otx2_free_sq_res() for the SQ unwind path and let it free sqb_ptrs
    even when sq->sqe has not been allocated yet.
    
    The bug was first flagged by an experimental analysis tool we are
    developing for kernel memory-management bugs while analyzing
    v6.13-rc1. The tool is still under development and is not yet publicly
    available. Manual inspection confirms that the bug is still
    present in v7.1.1.
    
    An x86_64 allyesconfig build showed no new warnings. As we do not have an
    OcteonTX2 PF device and the corresponding AF mailbox setup to test with,
    no runtime testing was able to be performed.
    
    Fixes: caa2da34fd25 ("octeontx2-pf: Initialize and config queues")
    Cc: stable@vger.kernel.org
    Reviewed-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Signed-off-by: Dawei Feng <dawei.feng@seu.edu.cn>
    Link: https://patch.msgid.link/20260630071625.349996-1-dawei.feng@seu.edu.cn
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_pf.c
@@ -1569,15 +1569,15 @@ static void otx2_free_sq_res(struct otx2_nic *pf)
 	otx2_sq_free_sqbs(pf);
 	for (qidx = 0; qidx < otx2_get_total_tx_queues(pf); qidx++) {
 		sq = &qset->sq[qidx];
-		/* Skip freeing Qos queues if they are not initialized */
-		if (!sq->sqe)
-			continue;
-		qmem_free(pf->dev, sq->sqe);
-		qmem_free(pf->dev, sq->sqe_ring);
-		qmem_free(pf->dev, sq->cpt_resp);
-		qmem_free(pf->dev, sq->tso_hdrs);
-		qmem_free(pf->dev, sq->timestamps);
-		kfree(sq->sg);
+		/* sq->sqe is not initialized for unused QoS queues */
+		if (sq->sqe) {
+			qmem_free(pf->dev, sq->sqe);
+			qmem_free(pf->dev, sq->sqe_ring);
+			qmem_free(pf->dev, sq->cpt_resp);
+			qmem_free(pf->dev, sq->tso_hdrs);
+			qmem_free(pf->dev, sq->timestamps);
+			kfree(sq->sg);
+		}
 		kfree(sq->sqb_ptrs);
 	}
 }
@@ -1712,13 +1712,12 @@ int otx2_init_hw_resources(struct otx2_nic *pf)
 	return err;
 
 err_free_nix_queues:
-	otx2_free_sq_res(pf);
 	otx2_free_cq_res(pf);
 	otx2_ctx_disable(mbox, NIX_AQ_CTYPE_RQ, false);
 err_free_txsch:
 	otx2_txschq_stop(pf);
 err_free_sq_ptrs:
-	otx2_sq_free_sqbs(pf);
+	otx2_free_sq_res(pf);
 err_free_rq_ptrs:
 	otx2_free_aura_ptr(pf, AURA_NIX_RQ);
 	otx2_ctx_disable(mbox, NPA_AQ_CTYPE_POOL, true);
