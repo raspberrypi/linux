835a2f9d9f1749995203be3c0c07d27e2aec58f4
Shuicheng Lin <shuicheng.lin@intel.com>
2026-06-30 19:22:21 +0000

    drm/xe/userptr: Stub notifier_lock helpers when DRM_GPUSVM=n
    commit f5ef65adf81da3dbce4e692e48c1754c0bb95da0 upstream.
    
    When CONFIG_DRM_GPUSVM=n (e.g. um-allyesconfig), the only caller of
    xe_pt_svm_userptr_notifier_lock() is compiled out, triggering:
    
      drivers/gpu/drm/xe/xe_pt.c:1418:13: warning:
        'xe_pt_svm_userptr_notifier_lock' defined but not used
        [-Wunused-function]
    
    The helpers cannot simply be removed in this case: the matching
    xe_pt_svm_userptr_notifier_unlock() is also referenced from
    xe_pt_update_ops_run(), which lives outside any DRM_GPUSVM ifdef and is
    gated only at runtime by pt_update_ops->needs_svm_lock. The symbol must
    exist in all builds.
    
    Provide empty static inline stubs for !DRM_GPUSVM, matching the pattern
    used by xe_svm_notifier_lock()/_unlock() in xe_svm.h.
    
    Fixes: dca6e08c923a ("drm/xe/userptr: Hold notifier_lock for write on inject test path")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202606302210.QqcLbOEN-lkp@intel.com/
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patch.msgid.link/20260630192221.2998168-1-shuicheng.lin@intel.com
    Signed-off-by: Shuicheng Lin <shuicheng.lin@intel.com>
    (cherry picked from commit 3359422bf0a1140e96d783a19a397686e580a3ca)
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/xe/xe_pt.c
+++ drivers/gpu/drm/xe/xe_pt.c
@@ -1407,6 +1407,7 @@ static int xe_pt_pre_commit(struct xe_migrate_pt_update *pt_update)
 				     pt_update_ops, rftree);
 }
 
+#if IS_ENABLED(CONFIG_DRM_GPUSVM)
 /*
  * Acquire/release the svm notifier_lock around xe_pt_svm_userptr_pre_commit()
  * and the matching late release in xe_pt_update_ops_run(). Read mode by
@@ -1433,6 +1434,10 @@ static void xe_pt_svm_userptr_notifier_unlock(struct xe_vm *vm)
 	xe_svm_notifier_unlock(vm);
 #endif
 }
+#else
+static inline void xe_pt_svm_userptr_notifier_lock(struct xe_vm *vm) { }
+static inline void xe_pt_svm_userptr_notifier_unlock(struct xe_vm *vm) { }
+#endif
 
 #if IS_ENABLED(CONFIG_DRM_GPUSVM)
 #ifdef CONFIG_DRM_XE_USERPTR_INVAL_INJECT
