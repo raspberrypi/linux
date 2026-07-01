2ca18df1c2611f70eb3eb487e02ae85eb703b284
Yuyang Huang <yuyanghuang@google.com>
2026-07-02 08:50:14 +0900

    ipv4: igmp: remove multicast group from hash table on device destruction
    commit 7993211bde166471dffac074dc965489f86531f8 upstream.
    
    When a device is destroyed under RTNL, ip_mc_destroy_dev() iterates through
    the multicast list and calls ip_ma_put() on each membership, scheduling
    them for RCU reclamation. However, they are not unlinked from the device's
    multicast hash table (mc_hash).
    
    Since the device remains published in dev->ip_ptr until after
    ip_mc_destroy_dev() completes, concurrent RCU readers traversing mc_hash
    can still locate and access the multicast group after its refcount is
    decremented. If the RCU callback runs and frees the group while a reader is
    accessing it, a use-after-free occurs.
    
    Fix this by unlinking the multicast group from mc_hash using
    ip_mc_hash_remove() before scheduling it for reclamation.
    
    BUG: KASAN: slab-use-after-free in ip_check_mc_rcu+0x149/0x3f0
    Read of size 4 at addr ffff888009bf1408 by task mausezahn/2276
    
    Call Trace:
     <IRQ>
     dump_stack_lvl+0x67/0x90
     print_report+0x175/0x7c0
     kasan_report+0x147/0x180
     ip_check_mc_rcu+0x149/0x3f0
     udp_v4_early_demux+0x36d/0x12d0
     ip_rcv_finish_core+0xb8b/0x1390
     ip_rcv_finish+0x54/0x120
     NF_HOOK+0x213/0x2b0
     __netif_receive_skb+0x126/0x340
     process_backlog+0x4f2/0xf00
     __napi_poll+0x92/0x2c0
     net_rx_action+0x583/0xc60
     handle_softirqs+0x236/0x7f0
     do_softirq+0x57/0x80
     </IRQ>
    
    Allocated by task 2239:
     kasan_save_track+0x3e/0x80
     __kasan_kmalloc+0x72/0x90
     ____ip_mc_inc_group+0x31a/0xa40
     __ip_mc_join_group+0x334/0x3f0
     do_ip_setsockopt+0x16fa/0x2010
     ip_setsockopt+0x3f/0x90
     do_sock_setsockopt+0x1ad/0x300
    
    Freed by task 0:
     kasan_save_track+0x3e/0x80
     kasan_save_free_info+0x40/0x50
     __kasan_slab_free+0x3a/0x60
     __rcu_free_sheaf_prepare+0xd4/0x220
     rcu_free_sheaf+0x36/0x190
     rcu_core+0x8d9/0x12f0
     handle_softirqs+0x236/0x7f0
    
    Fixes: e9897071350b ("igmp: hash a hash table to speedup ip_check_mc_rcu()")
    Cc: stable@vger.kernel.org
    Signed-off-by: Yuyang Huang <yuyanghuang@google.com>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Ido Schimmel <idosch@nvidia.com>
    Link: https://patch.msgid.link/20260701235014.73505-1-yuyanghuang@google.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- net/ipv4/igmp.c
+++ net/ipv4/igmp.c
@@ -1904,6 +1904,7 @@ void ip_mc_destroy_dev(struct in_device *in_dev)
 #endif
 
 	while ((i = rtnl_dereference(in_dev->mc_list)) != NULL) {
+		ip_mc_hash_remove(in_dev, i);
 		in_dev->mc_list = i->next_rcu;
 		in_dev->mc_count--;
 		ip_mc_clear_src(i);
