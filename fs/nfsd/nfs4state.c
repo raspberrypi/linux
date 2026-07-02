d2081d45172ec60d03e3f9ce4a27656abece8024
Scott Mayhew <smayhew@redhat.com>
2026-07-02 16:27:48 -0400

    nfsd: fix file change detection in CB_GETATTR
    commit 304d81a2fbf2b454def4debcb38ea173911b72cd upstream.
    
    RFC 8881, section 10.4.3 doesn't say anything about caching the file
    size in the delegation record, nor does it say anything about comparing
    a cached file size with the size reported by the client in the
    CB_GETATTR reply for the purpose of determining if the client holds
    modified data for the file.
    
    What section 10.4.3 of RFC 8881 does say is that the server should
    compare the *current* file size with the size reported by the client
    holding the delegation in the CB_GETATTR reply, and if they differ to
    treat it as a modification regardless of the change attribute retrieved
    via the CB_GETATTR.
    
    Doing otherwise would cause the server to believe the client holding the
    delegation has a modified version of the file, even if the client
    flushed the modifications to the server prior to the CB_GETATTR.  This
    would have the added side effect of subsequent CB_GETATTRs causing
    updates to the mtime, ctime, and change attribute even if the client
    holding the delegation makes no further updates to the file.
    
    Modify nfsd4_deleg_getattr_conflict() to obtain the current file size
    via i_size_read().  Retain the ncf_cur_fsize field, since it's a
    convenient way to return the file size back to nfsd4_encode_fattr4(),
    but don't use it for the purpose of detecting file changes.  Remove the
    unnecessary initialization of ncf_cur_fsize in nfs4_open_delegation().
    
    Also, if we recall the delegation (because the client didn't respond to
    the CB_GETATTR), then skip the logic that checks the nfs4_cb_fattr
    fields.
    
    Fixes: c5967721e106 ("NFSD: handle GETATTR conflict with write delegation")
    Cc: stable@vger.kernel.org
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>
    [ cel: no deleg_ts in 6.12.y; dropped the now-dead ncf_cur_fsize init ]
    Signed-off-by: Chuck Lever <cel@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -6086,7 +6086,6 @@ nfs4_open_delegation(struct nfsd4_open *open, struct nfs4_ol_stateid *stp,
 			goto out_no_deleg;
 		}
 		open->op_delegate_type = NFS4_OPEN_DELEGATE_WRITE;
-		dp->dl_cb_fattr.ncf_cur_fsize = stat.size;
 		dp->dl_cb_fattr.ncf_initial_cinfo = nfsd4_change_attribute(&stat);
 		trace_nfsd_deleg_write(&dp->dl_stid.sc_stateid);
 	} else {
@@ -9040,11 +9039,15 @@ nfsd4_deleg_getattr_conflict(struct svc_rqst *rqstp, struct dentry *dentry,
 		if (status != nfserr_jukebox ||
 		    !nfsd_wait_for_delegreturn(rqstp, inode))
 			goto out_status;
+		status = nfs_ok;
+		goto out_status;
 	}
-	if (!ncf->ncf_file_modified &&
-	    (ncf->ncf_initial_cinfo != ncf->ncf_cb_change ||
-	     ncf->ncf_cur_fsize != ncf->ncf_cb_fsize))
-		ncf->ncf_file_modified = true;
+	if (!ncf->ncf_file_modified) {
+		if (ncf->ncf_initial_cinfo != ncf->ncf_cb_change)
+			ncf->ncf_file_modified = true;
+		else if (i_size_read(inode) != ncf->ncf_cb_fsize)
+			ncf->ncf_file_modified = true;
+	}
 	if (ncf->ncf_file_modified) {
 		int err;
 
