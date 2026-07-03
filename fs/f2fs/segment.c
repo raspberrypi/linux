89479a27fa4e1e11f378b3724944eabceb84f114
Chao Yu <chao@kernel.org>
2026-07-03 14:03:17 -0400

    f2fs: fix potential deadlock in gc_merge path of f2fs_balance_fs()
    [ Upstream commit 8b4468ec023d0d1b4669dfb867588997cc03a06b ]
    
    When we mount device w/ gc_merge mount option, we may suffer below
    potential deadlock:
    
    Kworker					GC trehad			Truncator
    - f2fs_write_cache_pages
     - f2fs_write_single_data_page
      - f2fs_do_write_data_page
       - folio_start_writeback  --- set writeback flag on folio
       - f2fs_outplace_write_data
       : cached folio in internal bio cache
      - f2fs_balance_fs
       - wake_up(gc_thread)
       : wake up gc thread to run foreground GC
       - finish_wait(fggc_wq)
       : wait on the waitqueue --- wait on GC thread to finish the work
    									- truncate_inode_pages_range
    									 - __filemap_get_folio(, FGP_LOCK)  --- lock folio
    									 - truncate_inode_partial_folio
    									  - folio_wait_writeback            --- wait on writeback being cleared
    					- do_garbage_collect
    					 - move_data_page
    					  - f2fs_get_lock_data_folio
    					   - lock on folio  --- blocked on folio's lock
    
    In order to avoid such deadlock, let's call below functions to commit
    cached bios in GC_MERGE path of f2fs_balance_fs() as the same as we did
    in NOGC_MERGE path.
    - f2fs_submit_merged_write(sbi, DATA);
    - f2fs_submit_all_merged_ipu_writes(sbi);
    
    Cc: stable@kernel.org
    Fixes: 351df4b20115 ("f2fs: add segment operations")
    Cc: Ruipeng Qi <ruipengqi3@gmail.com>
    Reported: Sandeep Dhavale <dhavale@google.com>
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Chao Yu <chaseyu@google.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -443,6 +443,13 @@ void f2fs_balance_fs(struct f2fs_sb_info *sbi, bool need)
 	if (has_enough_free_secs(sbi, 0, 0))
 		return;
 
+	/*
+	 * Submit all cached OPU/IPU DATA bios before triggering
+	 * foreground GC to avoid potential deadlocks.
+	 */
+	f2fs_submit_merged_write(sbi, DATA);
+	f2fs_submit_all_merged_ipu_writes(sbi);
+
 	if (test_opt(sbi, GC_MERGE) && sbi->gc_thread &&
 				sbi->gc_thread->f2fs_gc_task) {
 		DEFINE_WAIT(wait);
@@ -461,13 +468,6 @@ void f2fs_balance_fs(struct f2fs_sb_info *sbi, bool need)
 			.should_migrate_blocks = false,
 			.err_gc_skipped = false,
 			.nr_free_secs = 1 };
-
-		/*
-		 * Submit all cached OPU/IPU DATA bios before triggering
-		 * foreground GC to avoid potential deadlocks.
-		 */
-		f2fs_submit_merged_write(sbi, DATA);
-		f2fs_submit_all_merged_ipu_writes(sbi);
 
 		f2fs_down_write(&sbi->gc_lock);
 		stat_inc_gc_call_count(sbi, FOREGROUND);
