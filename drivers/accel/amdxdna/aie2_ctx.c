267809e2c56fbea486f7250c8a4acddcc3c54dc5
Lizhi Hou <lizhi.hou@amd.com>
2026-07-06 15:12:28 -0700

    accel/amdxdna: Fix potential amdxdna_umap lifetime race
    [ Upstream commit 14f172eff9c19f8043a9858845f33cd034f3a41e ]
    
    amdxdna_umap_release() calls the blocking mmu_interval_notifier_remove()
    before removing the object from abo->mem.umap_list. If
    aie2_populate_range() runs concurrently, it may obtain a reference to an
    amdxdna_umap that is being released, leading to a potential use-after-free.
    
    Use kref_get_unless_zero() in aie2_populate_range() when acquiring a
    reference. If the reference count has already dropped to zero, release
    is in progress and the entry is skipped.
    
    Fixes: e486147c912f ("accel/amdxdna: Add BO import and export")
    Reviewed-by: Max Zhen <max.zhen@amd.com>
    Signed-off-by: Lizhi Hou <lizhi.hou@amd.com>
    Link: https://patch.msgid.link/20260706221228.434099-1-lizhi.hou@amd.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/accel/amdxdna/aie2_ctx.c
+++ drivers/accel/amdxdna/aie2_ctx.c
@@ -794,7 +794,7 @@ again:
 	found = false;
 	down_write(&xdna->notifier_lock);
 	list_for_each_entry(mapp, &abo->mem.umap_list, node) {
-		if (mapp->invalid) {
+		if (mapp->invalid && kref_get_unless_zero(&mapp->refcnt)) {
 			found = true;
 			break;
 		}
@@ -805,7 +805,7 @@ again:
 		up_write(&xdna->notifier_lock);
 		return 0;
 	}
-	kref_get(&mapp->refcnt);
+
 	up_write(&xdna->notifier_lock);
 
 	XDNA_DBG(xdna, "populate memory range %lx %lx",
