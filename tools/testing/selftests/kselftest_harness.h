c3aa2187da64a59aa425c3d489241dc417ab3833
David Matlack <dmatlack@google.com>
2026-07-06 18:31:54 +0000

    selftests: harness: Mark test fixture objects __maybe_unused
    [ Upstream commit 37ffa24c9d07edcd414d34283e02af3f3866cf12 ]
    
    Mark _##fixture_name##_##test_name##_object __maybe_unused since it may
    not ever be read. This pointer is only read in XFAIL_ADD(), which tests
    are not required to use.
    
    clang made a change to -Wunused-but-set-variable (split out into its own
    subwarning, -Wunused-but-set-global) that causes this warning to be
    emitted for various selftests and can be upgraded to an error in
    selftest that set -Werror.
    
    VFIO selftests have been broken since commit ff556bd98348 ("vfio:
    selftests: Add -Wall and -Werror to the Makefile"), and the net
    selftests builds have been noisy due to -Wall.
    
    Fixes: 24cf65a62266 ("selftests/harness: Share _metadata between forked processes")
    Reported-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reported-by: Aaron Lewis <aaronlewis@google.com>
    Reviewed-by: Alex Williamson <alex@shazbot.org>
    Reviewed-by: Kuniyuki Iwashima <kuniyu@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Reviewed-by: Nathan Chancellor <nathan@kernel.org>
    Link: https://lore.kernel.org/r/20260706183154.2660394-1-dmatlack@google.com
    Signed-off-by: Alex Williamson <alex@shazbot.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/testing/selftests/kselftest_harness.h
+++ tools/testing/selftests/kselftest_harness.h
@@ -467,7 +467,7 @@ static inline void __kselftest_memset_safe(void *s, int c, size_t n)
 		} \
 		__test_check_assert(_metadata); \
 	} \
-	static struct __test_metadata *_##fixture_name##_##test_name##_object; \
+	static struct __test_metadata *_##fixture_name##_##test_name##_object __maybe_unused; \
 	static void __attribute__((constructor(KSELFTEST_PRIO_TEST))) \
 			_register_##fixture_name##_##test_name(void) \
 	{ \
