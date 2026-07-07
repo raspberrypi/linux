54b6a5c615a007989028dda2bb6dd7959436b207
Pali Rohár <pali@kernel.org>
2026-07-07 11:03:18 +0200

    cifs: Fix support for creating SFU socket
    [ Upstream commit dde3929e8d901ae9a7c29d0577ce9ca5cb4db35d ]
    
    SFU sockets are natively supported by Interix 3.0 subsystem and also by
    later versions. It is part of Microsoft SFU (Windows Services for UNIX) and
    Microsoft SUA (Subsystem for UNIX-based Applications). They can be created
    and existing (stored on local disk or remote SMB share) can be recognized.
    
    SFU sockets are recognized also by NFS server included in Windows Server.
    Windows NFS server versions since Windows Server 2012 uses new reparse
    point format for storing new sockets, but still can recognize this old
    format (also in the latest Windows Server 2022 version).
    
    SFU-style socket is a regular file which has system attribute set and
    content of the file is one zero byte.
    
    These SFU-style sockets are already recognized by Linux SMB client.
    
    But Linux SMB client is currently creating new SFU socket in different
    format which is not compatible with all those SFU applications. Fix this by
    creating new sockets in correct SFU format which would be recognized by all
    SFU, SUA, NFS and existing Linux SMB clients.
    
    This change affects only creating new sockets when mount option -o sfu is used.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Acked-by: Paulo Alcantara (Red Hat) <pc@manguebit.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/smb/client/smb2ops.c
+++ fs/smb/client/smb2ops.c
@@ -5345,10 +5345,9 @@ int __cifs_sfu_make_node(unsigned int xid, struct inode *inode,
 		data = (u8 *)symname_utf16;
 		break;
 	case S_IFSOCK:
-		type_len = 8;
-		strscpy(type, "LnxSOCK");
-		data = (u8 *)&pdev;
-		data_len = sizeof(pdev);
+		/* SFU socket is system file with one zero byte */
+		type_len = 1;
+		type[0] = '\0';
 		break;
 	case S_IFIFO:
 		type_len = 8;
