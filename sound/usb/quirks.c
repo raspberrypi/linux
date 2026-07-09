f4e23e661a259975d1c5ddbc40df9521ad33a540
Takashi Iwai <tiwai@suse.de>
2026-07-09 11:56:06 +0200

    ALSA: usb-audio: Skip DSD quirk for Musical Fidelity M6s DAC
    [ Upstream commit 93b47e66cc6d6c6382d44b44f5e7f6fc3a7b38c3 ]
    
    Salvador reported that the recent fix for applying the DSD quirk to
    Musical Fidelity devices broke for his M6s DAC model (2772:0502).
    
    Although this is basically a firmware bug, the model in question is
    fairly old, and no further firmware update can be expected, so it'd be
    better to address in the driver side.
    
    As an ad hoc workaround, skip the DSD quirk for this device by adding
    an empty quirk entry of 2772:0502; this essentially skips the later
    DSD quirk entry by the match with the vendor 2772.
    
    Fixes: da3a7efff64e ("ALSA: usb-audio: Update for native DSD support quirks")
    Reported-by: Salvador Blaya <tiniebla6@gmail.com>
    Closes: https://lore.kernel.org/CAOdyq+qFaqCh=tK_wNnA64hv5pQuA1Y09ANxQ=xK8yR-t4mf9Q@mail.gmail.com
    Tested-by: Salvador Blaya <tiniebla6@gmail.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260709095614.1418838-1-tiwai@suse.de
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -2380,6 +2380,8 @@ static const struct usb_audio_quirk_flags_table quirk_flags_table[] = {
 		   QUIRK_FLAG_DSD_RAW),
 	DEVICE_FLG(0x2708, 0x0002, /* Audient iD14 */
 		   QUIRK_FLAG_IGNORE_CTL_ERROR),
+	DEVICE_FLG(0x2772, 0x0502, /* Musical Fidelity M6s DAC */
+		   0), /* for avoiding QUIRK_FLAG_DSD_RAW with vendor match */
 	DEVICE_FLG(0x2912, 0x30c8, /* Audioengine D1 */
 		   QUIRK_FLAG_GET_SAMPLE_RATE),
 	DEVICE_FLG(0x2a70, 0x1881, /* OnePlus Technology (Shenzhen) Co., Ltd. BE02T */
