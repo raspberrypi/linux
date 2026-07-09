cd2976b4a5398ee1d0192401ad09f92370e2f3ff
Sean Christopherson <seanjc@google.com>
2026-07-09 13:49:35 -0700

    KVM: x86: Serialize writes to disabled_quirks using kvm->lock
    commit ba76b23ed36ab230fc2577aba24f65851114902f upstream.
    
    Protect writes to disabled_quirks with kvm->lock to ensure KVM doesn't
    clobber state in the unlikely scenario that userspace disables disparate
    quirks from multiple tasks.  More importantly, this will allow wrapping
    accesses with {READ,WRITE}_ONCE without "needing" to also guard the writer
    with a useless and confusing READ_ONCE (since the RMW wouldn't be atomic
    anyways).
    
    Ideally, KVM would disallow disabling quirks once quirks are "live", but
    that would be a potentially breaking userspace ABI change, and while all
    existing quirks are fully live only after vCPUs have been created, several
    MMU-related quirks, IGNORE_GUEST_PAT and SLOT_ZAP_ALL, are partially live
    at all times.  Because populating MMUs requires a vCPU, the guest-visible
    behavior of IGNORE_GUEST_PAT and SLOT_ZAP_ALL requires a vCPU, but for KVM
    itself, processing the quirk (or not) has functional impact, i.e. for all
    intents and purposes, KVM can't prevent those quirks from being disabled
    after they've been consumed.
    
    Cc: stable@vger.kernel.org # 6.12.x
    Reviewed-by: Michael Roth <michael.roth@amd.com>
    Link: https://patch.msgid.link/20260709204948.1988414-6-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -6552,7 +6552,9 @@ int kvm_vm_ioctl_enable_cap(struct kvm *kvm,
 			break;
 		fallthrough;
 	case KVM_CAP_DISABLE_QUIRKS:
+		mutex_lock(&kvm->lock);
 		kvm->arch.disabled_quirks |= cap->args[0] & kvm_caps.supported_quirks;
+		mutex_unlock(&kvm->lock);
 		r = 0;
 		break;
 	case KVM_CAP_SPLIT_IRQCHIP: {
