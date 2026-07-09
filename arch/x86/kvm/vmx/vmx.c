3dcfb04dd43b16fa1240fc6487fff578ad57264c
Sean Christopherson <seanjc@google.com>
2026-07-09 09:22:46 +0200

    KVM: VMX: Grab vmcs12 on CR8 interception update iff vCPU is in guest mode
    [ Upstream commit 7ef78d71ca713d8c00f7c34ddcf276c808143f77 ]
    
    When updating CR8 intercepts, get vmcs12 if and only if the vCPU is in
    guest mode so that a future change can have update CR8 intercepts during
    vCPU creation, without running afoul of get_vmcs12()'s lockdep assertion.
    
      ------------[ cut here ]------------
      debug_locks && !(lock_is_held(&(&vcpu->mutex)->dep_map) || !refcount_read(&vcpu->kvm->users_count))
      WARNING: arch/x86/kvm/vmx/nested.h:61 at get_vmcs12 arch/x86/kvm/vmx/nested.h:60 [inline], CPU#0: syz.2.19/5879
      WARNING: arch/x86/kvm/vmx/nested.h:61 at vmx_update_cr8_intercept+0x3de/0x4e0 arch/x86/kvm/vmx/vmx.c:6879, CPU#0: syz.2.19/5879
      Modules linked in:
      CPU: 0 UID: 0 PID: 5879 Comm: syz.2.19 Not tainted syzkaller #0 PREEMPT(full)
      Hardware name: QEMU Standard PC (Q35 + ICH9, 2009), BIOS 1.16.2-debian-1.16.2-1 04/01/2014
      RIP: 0010:get_vmcs12 arch/x86/kvm/vmx/nested.h:60 [inline]
      RIP: 0010:vmx_update_cr8_intercept+0x3de/0x4e0 arch/x86/kvm/vmx/vmx.c:6879
      Call Trace:
       <TASK>
       apic_update_ppr arch/x86/kvm/lapic.c:984 [inline]
       kvm_lapic_reset+0x1c24/0x2980 arch/x86/kvm/lapic.c:3023
       kvm_vcpu_reset+0x44c/0x1bf0 arch/x86/kvm/x86.c:12986
       kvm_arch_vcpu_create+0x746/0x8b0 arch/x86/kvm/x86.c:12847
       kvm_vm_ioctl_create_vcpu+0x428/0x930 virt/kvm/kvm_main.c:4201
       kvm_vm_ioctl+0x893/0xd50 virt/kvm/kvm_main.c:5159
       vfs_ioctl fs/ioctl.c:51 [inline]
       __do_sys_ioctl fs/ioctl.c:597 [inline]
       __se_sys_ioctl+0xfc/0x170 fs/ioctl.c:583
       do_syscall_x64 arch/x86/entry/syscall_64.c:63 [inline]
       do_syscall_64+0x174/0x580 arch/x86/entry/syscall_64.c:94
       entry_SYSCALL_64_after_hwframe+0x77/0x7f
       </TASK>
    
    No functional change intended.
    
    Reported-by: syzbot ci <syzbot+ci493c6d734b63e050@syzkaller.appspotmail.com>
    Closes: https://lore.kernel.org/all/6a2adf3b.3b0a2d4e.8c8d1.0012.GAE@google.com
    Cc: stable@vger.kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-ID: <20260618174347.1981064-2-seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Carlos López <clopez@suse.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -6734,11 +6734,10 @@ static noinstr void vmx_l1d_flush(struct kvm_vcpu *vcpu)
 
 void vmx_update_cr8_intercept(struct kvm_vcpu *vcpu, int tpr, int irr)
 {
-	struct vmcs12 *vmcs12 = get_vmcs12(vcpu);
 	int tpr_threshold;
 
 	if (is_guest_mode(vcpu) &&
-		nested_cpu_has(vmcs12, CPU_BASED_TPR_SHADOW))
+	    nested_cpu_has(get_vmcs12(vcpu), CPU_BASED_TPR_SHADOW))
 		return;
 
 	tpr_threshold = (irr == -1 || tpr < irr) ? 0 : irr;
