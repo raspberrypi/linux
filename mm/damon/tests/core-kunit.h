cd6d7f45abc825002343801f6b621dc254e07ada
SJ Park <sj@kernel.org>
2026-07-12 09:54:30 -0700

    mm/damon/core: skip aging from repeated aggressive merging
    commit 0250dbe08c730d003ef9f484da56ae09a1ea0c4c upstream.
    
    The number of DAMON regions could temporarily exceed the user-defined
    maximum number of regions limit for corner cases.  For example, users
    could lower the limit via runtime parameters update.  For such a case,
    kdamond_merge_regions() repeats merging regions in the case doubling the
    merge threshold.  The repeated merge operation could update the age of
    regions multiple times.  This corrupts the monitoring results.  Fix the
    issue by asking the merge operation to skip aging for the corner case.
    
    The user impact is degradation of the monitoring quality.  The impact
    should be mild, since the degradation is only temporal, and it is not
    common to happen in realistic setups.
    
    The issue was discovered [1,2] by Sashiko.
    
    Link: https://lore.kernel.org/20260712165432.87609-1-sj@kernel.org
    Link: https://lore.kernel.org/20260621203548.10718-1-sj@kernel.org [1]
    Link: https://lore.kernel.org/20260709145425.96247-1-sj@kernel.org [2]
    Fixes: 310d6c15e910 ("mm/damon/core: merge regions aggressively when max_nr_regions is unmet")
    Signed-off-by: SJ Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org> # 6.10
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: SJ Park <sj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/damon/tests/core-kunit.h
+++ mm/damon/tests/core-kunit.h
@@ -266,7 +266,7 @@ static void damon_test_merge_regions_of(struct kunit *test)
 		damon_add_region(r, t);
 	}
 
-	damon_merge_regions_of(t, 9, 9999);
+	damon_merge_regions_of(t, 9, 9999, true);
 	/* 0-112, 114-130, 130-156, 156-170 */
 	KUNIT_EXPECT_EQ(test, damon_nr_regions(t), 5u);
 	if (damon_nr_regions(t) != 5)
