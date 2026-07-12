7babc25d8dd5b7642920fbc0737cfd1469e7025b
Leon Romanovsky <leonro@nvidia.com>
2026-07-12 04:37:36 -0400

    RDMA/mlx5: Drain RCU callbacks during module teardown
    [ Upstream commit e37cdd75f8d61c1123d324ae5667ac3da562290e ]
    
    devx_free_subscription() can remain queued after the last DevX event file
    drops its module reference or an auxiliary driver detaches its devices.
    mlx5_ib can then unload before the callback runs.
    
    Registration error unwind has the same risk because driver registration
    can attach existing devices before failing. Wait after all drivers have
    stopped.
    
    Fixes: 6898d1c661d7 ("RDMA/mlx5: Use RCU and direct refcounts to keep memory alive")
    Reported-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Closes: https://lore.kernel.org/linux-rdma/20260708092316.Qb39F_B0@linutronix.de/
    Link: https://patch.msgid.link/20260709-unload-rcu-v1-2-fccd27211e5a@nvidia.com
    Acked-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -5225,6 +5225,7 @@ mp_err:
 dd_err:
 	mlx5r_rep_cleanup();
 rep_err:
+	rcu_barrier();
 	mlx5_ib_qp_event_cleanup();
 qp_event_err:
 	destroy_workqueue(mlx5_ib_event_wq);
@@ -5238,6 +5239,7 @@ static void __exit mlx5_ib_cleanup(void)
 	auxiliary_driver_unregister(&mlx5r_driver);
 	auxiliary_driver_unregister(&mlx5r_mp_driver);
 	mlx5r_rep_cleanup();
+	rcu_barrier();
 
 	mlx5_ib_qp_event_cleanup();
 	destroy_workqueue(mlx5_ib_event_wq);
