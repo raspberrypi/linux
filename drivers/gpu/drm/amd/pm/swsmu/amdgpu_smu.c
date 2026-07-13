42e80ecdb18888d6615720c8638ac1d368c61017
Mario Limonciello <mario.limonciello@amd.com>
2026-07-13 14:53:13 -0500

    drm/amd: Create a device link between APU display and XHCI devices
    commit 75c8746b9d0a0317d8a58aa0efa2ff2f7359908b upstream.
    
    Some AMD APU multi-function devices expose an integrated USB xHCI
    controller. In some circumstances (such as larger VRAM), the PM core
    can resume can fail when the xHCI controller is resuming in parallel
    with the GPU/display function.
    
    On affected systems, the xHCI controller can complete pci_pm_resume
    and start resuming USB devices while the GPU is still in its much
    longer resume path. This race condition leads to USB device resume
    failures followed by:
    
      xhci_hcd ...: xHCI host not responding to stop endpoint command
      xhci_hcd ...: HC died; cleaning up
    
    Create a device link from any xHCI controller sharing the same PCIe
    root port as the APU display function. The link uses DL_FLAG_STATELESS
    and DL_FLAG_PM_RUNTIME to ensure the GPU completes its resume before
    the xHCI controller begins resuming USB devices.
    
    This device link is done specifically in amdgpu so that if the
    platform firmware has been modified such that this issue doesn't happen
    the version can be detected and the workaround skipped.
    
    Suggested-by: Aaron Ma <aaron.ma@canonical.com>
    Reported-by: mrh@frame.work
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=221073
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Tested-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Tested-by: Alexander F <superveridical@gmail.com>
    Tested-by: Francis DB <francisdb@gmail.com>
    Link: https://patch.msgid.link/20260713195313.1739762-1-mario.limonciello@amd.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 07c93d7eeb0d990bc1b8e3b1eafa464bc9feee97)
    Cc: stable@vger.kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -1365,6 +1365,14 @@ static void smu_feature_cap_init(struct smu_context *smu)
 	bitmap_zero(fea_cap->cap_map, SMU_FEATURE_CAP_ID__COUNT);
 }
 
+static int smu_set_power_dep(struct smu_context *smu, bool enable)
+{
+	if (!smu->ppt_funcs->set_power_dep)
+		return 0;
+
+	return smu->ppt_funcs->set_power_dep(smu, enable);
+}
+
 static int smu_sw_init(struct amdgpu_ip_block *ip_block)
 {
 	struct amdgpu_device *adev = ip_block->adev;
@@ -1426,6 +1434,8 @@ static int smu_sw_init(struct amdgpu_ip_block *ip_block)
 	if (!smu->ppt_funcs->get_fan_control_mode)
 		smu->adev->pm.no_fan = true;
 
+	smu_set_power_dep(smu, true);
+
 	return 0;
 }
 
@@ -1447,6 +1457,8 @@ static int smu_sw_fini(struct amdgpu_ip_block *ip_block)
 	}
 
 	smu_fini_microcode(smu);
+
+	smu_set_power_dep(smu, false);
 
 	return 0;
 }
