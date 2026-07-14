a8e3a0e8997deb886278898a0b6f19374f49d111
Liang Luo <luoliang@kylinos.cn>
2026-07-14 11:20:51 +0800

    tools/sched_ext: scx_qmap: Fix stale API name in comment
    [ Upstream commit 35f9cbbacb671e587c84e992e7b0098c39e895a4 ]
    
    The comment above dispatch_highpri() still references
    scx_bpf_dispatch[_vtime]_from_dsq(), which was renamed to
    scx_bpf_dsq_move[_vtime]() in v6.13 to unload the overloaded
    "dispatch" verb. The code below already uses the new names; only the
    comment was left behind during the rename.
    
    Fixes: 5cbb302880f5 ("sched_ext: Rename scx_bpf_dispatch[_vtime]_from_dsq*() -> scx_bpf_dsq_move[_vtime]*()")
    Signed-off-by: Liang Luo <luoliang@kylinos.cn>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- tools/sched_ext/scx_qmap.bpf.c
+++ tools/sched_ext/scx_qmap.bpf.c
@@ -495,7 +495,7 @@ static void update_core_sched_head_seq(struct task_struct *p)
  * moving them to HIGHPRI_DSQ and then consuming them first. This makes minor
  * difference only when dsp_batch is larger than 1.
  *
- * scx_bpf_dispatch[_vtime]_from_dsq() are allowed both from ops.dispatch() and
+ * scx_bpf_dsq_move[_vtime]() are allowed both from ops.dispatch() and
  * non-rq-lock holding BPF programs. As demonstration, this function is called
  * from qmap_dispatch() and monitor_timerfn().
  */
