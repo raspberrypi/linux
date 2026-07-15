eefcd3ca245c1056c43c8a5a3cc1ec475e1d08cd
Luke Wang <ziniu.wang_1@nxp.com>
2026-07-15 15:18:11 +0800

    mmc: sdhci-esdhc-imx: remove unnecessary mmc_card_wake_sdio_irq check for tuning save/restore
    commit 899160e2774d9952e9f2770b38f701ff1906c0b2 upstream.
    
    The tuning save/restore during system PM is conditioned on
    mmc_card_wake_sdio_irq(), but this check is unrelated to whether
    tuning values need to be preserved. The actual requirement is that
    the card keeps power during suspend and the controller is a uSDHC.
    
    SDIO devices using out-of-band GPIO wakeup maintain power during
    suspend but do not set the SDIO IRQ wake flag. In this case the
    tuning delay values are not saved/restored.
    
    Remove the unnecessary mmc_card_wake_sdio_irq() condition from both
    the suspend save and resume restore paths.
    
    Fixes: c63d25cdc59a ("mmc: sdhci-esdhc-imx: Save tuning value when card stays powered in suspend")
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Haibo Chen <haibo.chen@nxp.com>
    Signed-off-by: Luke Wang <ziniu.wang_1@nxp.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Ulf Hansson <ulfh@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -2041,8 +2041,7 @@ static int sdhci_esdhc_suspend(struct device *dev)
 	 * to save the tuning delay value just in case the usdhc
 	 * lost power during system PM.
 	 */
-	if (mmc_card_keep_power(host->mmc) && mmc_card_wake_sdio_irq(host->mmc) &&
-	    esdhc_is_usdhc(imx_data))
+	if (mmc_card_keep_power(host->mmc) && esdhc_is_usdhc(imx_data))
 		sdhc_esdhc_tuning_save(host);
 
 	if (device_may_wakeup(dev)) {
@@ -2107,8 +2106,7 @@ static int sdhci_esdhc_resume(struct device *dev)
 	 * restore the saved tuning delay value for the device which keep
 	 * power during system PM.
 	 */
-	if (mmc_card_keep_power(host->mmc) && mmc_card_wake_sdio_irq(host->mmc) &&
-	    esdhc_is_usdhc(imx_data))
+	if (mmc_card_keep_power(host->mmc) && esdhc_is_usdhc(imx_data))
 		sdhc_esdhc_tuning_restore(host);
 
 	pm_runtime_put_autosuspend(dev);
