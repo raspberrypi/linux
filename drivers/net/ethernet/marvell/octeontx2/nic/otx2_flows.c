6c4e18135cc035c0be5b7a13d0c4267a2ab2f250
Suman Ghosh <sumang@marvell.com>
2026-07-15 10:50:07 +0530

    octeontx2-vf: set TC flower flag on MCAM entry allocation
    [ Upstream commit 0d4d31e3cc5dd6204fa1495c4107f5075acce5ed ]
    
    When MCAM entries are allocated for a VF netdev via the devlink
    mcam_count parameter, only OTX2_FLAG_NTUPLE_SUPPORT was set. That
    enabled ethtool ntuple filters but not tc flower offload. Also set
    OTX2_FLAG_TC_FLOWER_SUPPORT when entries are successfully allocated.
    
    Fixes: 2da489432747 ("octeontx2-pf: devlink params support to set mcam entry count")
    Signed-off-by: Suman Ghosh <sumang@marvell.com>
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20260715052007.2099851-1-rkannoth@marvell.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_flows.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_flows.c
@@ -270,6 +270,7 @@ exit:
 	if (allocated) {
 		pfvf->flags |= OTX2_FLAG_MCAM_ENTRIES_ALLOC;
 		pfvf->flags |= OTX2_FLAG_NTUPLE_SUPPORT;
+		pfvf->flags |= OTX2_FLAG_TC_FLOWER_SUPPORT;
 	}
 
 	if (allocated != count)
