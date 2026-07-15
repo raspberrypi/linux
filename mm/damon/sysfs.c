7d4ecfb507f338de5b9c4b54a73f02a00e6a3557
SJ Park <sj@kernel.org>
2026-07-14 20:10:00 -0700

    mm/damon/sysfs: read addr_unit only once in damon_sysfs_apply_inputs()
    commit ab4d9358e32316fa39b5f1f5360292135978c3d9 upstream.
    
    damon_sysfs_apply_inputs() reads addr_unit twice.  It could race with
    addr_unit_store().  As a result, the min_region_sz could wrongly be set
    up.  Read it once.
    
    The user impact is trivial.  Sane users ain't update the parameter in
    parallel.  Even if it happens, the DAMON core layer handles the wrong
    min_region_sz (!is_power_of_2()).  Even if somehow the race ended up
    making a min_region_sz that is different from the user's intention but
    still valid, only monitoring itself runs differently than expected.  No
    critical consequences like kernel panic or memory corruption happen.
    
    The issue was discovered [1] by Sashiko.
    
    Link: https://lore.kernel.org/20260715031002.108504-6-sj@kernel.org
    Link: https://lore.kernel.org/20260714142950.100711-1-sj@kernel.org [1]
    Fixes: 540a2aebc657 ("mm/damon/sysfs: implement addr_unit file under context dir")
    Signed-off-by: SJ Park <sj@kernel.org>
    Cc: <stable@vger.kernel.org> # 6.18.x
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: SJ Park <sj@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/damon/sysfs.c
+++ mm/damon/sysfs.c
@@ -1445,11 +1445,11 @@ static int damon_sysfs_apply_inputs(struct damon_ctx *ctx,
 	err = damon_select_ops(ctx, sys_ctx->ops_id);
 	if (err)
 		return err;
-	ctx->addr_unit = sys_ctx->addr_unit;
+	ctx->addr_unit = READ_ONCE(sys_ctx->addr_unit);
 	/* addr_unit is respected by only DAMON_OPS_PADDR */
 	if (sys_ctx->ops_id == DAMON_OPS_PADDR)
 		ctx->min_sz_region = max(
-				DAMON_MIN_REGION / sys_ctx->addr_unit, 1);
+				DAMON_MIN_REGION / ctx->addr_unit, 1);
 	err = damon_sysfs_set_attrs(ctx, sys_ctx->attrs);
 	if (err)
 		return err;
