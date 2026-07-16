b3ce55b4a9da29645fe8e3c583cb02abe6ec2d3c
Praveen Talari <praveen.talari@oss.qualcomm.com>
2026-07-16 13:55:45 +0530

    spi: geni-qcom: Fix sticky ret causing wrong return value on invalid proto
    [ Upstream commit 2c1c13da3a3a639d2ac7221e1a5e57945cbc7235 ]
    
    spi_geni_init() reuses 'ret' after it has already been set by the
    runtime PM acquire check earlier in the function. When an invalid
    protocol is later detected, the function returns this stale 'ret'
    value instead of a proper error code, so it can end up returning 0
    (or some other non-error value) even though the protocol check
    failed.
    
    Fix this by returning -EINVAL directly on both invalid-proto paths.
    
    Fixes: d8e9ea989acb ("spi: qcom-geni: Fix missing error check on pm_runtime_get_sync()")
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <error27@gmail.com>
    Closes: https://lore.kernel.org/r/202607122241.qzP3QAXF-lkp@intel.com/
    Signed-off-by: Praveen Talari <praveen.talari@oss.qualcomm.com>
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Acked-by: Mukesh Kumar Savaliya <mukesh.savaliya@oss.qualcomm.com>
    Link: https://patch.msgid.link/20260716-fix_return_error_code-v1-1-3295003aacd5@oss.qualcomm.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/spi/spi-geni-qcom.c
+++ drivers/spi/spi-geni-qcom.c
@@ -625,7 +625,7 @@ static int spi_geni_init(struct spi_geni_master *mas)
 	if (spi->target) {
 		if (proto != GENI_SE_SPI_SLAVE) {
 			dev_err(mas->dev, "Invalid proto %d\n", proto);
-			return ret;
+			return -EINVAL;
 		}
 		spi_slv_setup(mas);
 	} else if (proto == GENI_SE_INVALID_PROTO) {
@@ -636,7 +636,7 @@ static int spi_geni_init(struct spi_geni_master *mas)
 		}
 	} else if (proto != GENI_SE_SPI) {
 		dev_err(mas->dev, "Invalid proto %d\n", proto);
-		return ret;
+		return -EINVAL;
 	}
 	mas->tx_fifo_depth = geni_se_get_tx_fifo_depth(se);
 
