62dde71ca2c8cfe74d7d01699bb14b81b8d4afb4
Rochan Avlur <rochan.avlur@gmail.com>
2026-07-16 09:51:27 -0700

    exfat: preserve benign secondary entries during rename and move
    [ Upstream commit 942296784b2a9439651750c42f540bf2579b330f ]
    
    Commit 8258ef28001a ("exfat: handle unreconized benign secondary
    entries") added cluster freeing for benign secondary entries inside
    exfat_remove_entries().  However, exfat_remove_entries() is also called
    from the rename and move paths (exfat_rename_file and exfat_move_file),
    where the old entry set is being relocated rather than deleted.  This
    causes benign secondary entries such as vendor extension entries to be
    silently destroyed on rename or cross-directory move, violating the
    exFAT spec requirement (section 8.2) that implementations preserve
    unrecognized benign secondary entries.
    
    Fix this by adding a free_benign parameter to exfat_remove_entries()
    so callers can suppress cluster freeing during relocation, and
    extending exfat_init_ext_entry() to copy trailing benign secondary
    entries from the old entry set into the new one internally.  Also
    clean up the error paths to delete newly allocated entries on failure.
    
    Fixes: 8258ef28001a ("exfat: handle unreconized benign secondary entries")
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/linux-fsdevel/CAG7tbBV--waov7XVu2FHQEc6paR92dufS=em9DW5Kzsrpu3iQg@mail.gmail.com/
    Signed-off-by: Rochan Avlur <rochan.avlur@gmail.com>
    Reviewed-by: Yuezhang Mo <Yuezhang.Mo@sony.com>
    Signed-off-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Rochan Avlur <rochan.avlur@gmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/exfat/exfat_fs.h
+++ fs/exfat/exfat_fs.h
@@ -496,9 +496,10 @@ void exfat_init_dir_entry(struct exfat_entry_set_cache *es,
 		unsigned int type, unsigned int start_clu,
 		unsigned long long size, struct timespec64 *ts);
 void exfat_init_ext_entry(struct exfat_entry_set_cache *es, int num_entries,
-		struct exfat_uni_name *p_uniname);
+		struct exfat_uni_name *p_uniname,
+		struct exfat_entry_set_cache *old_es, int num_extra);
 void exfat_remove_entries(struct inode *inode, struct exfat_entry_set_cache *es,
-		int order);
+		int order, bool free_benign);
 void exfat_update_dir_chksum(struct exfat_entry_set_cache *es);
 int exfat_calc_num_entries(struct exfat_uni_name *p_uniname);
 int exfat_find_dir_entry(struct super_block *sb, struct exfat_inode_info *ei,
