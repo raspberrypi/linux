d8f5b285dd318fc7bc83de432622410482d77949
Kuniyuki Iwashima <kuniyu@amazon.com>
2026-07-17 19:29:17 +0200

    af_unix: Set drop reason in unix_stream_read_skb().
    [ Upstream commit bace4b468049a558295a0f59460fcb51e28f8fde ]
    
    unix_stream_read_skb() is called when BPF SOCKMAP reads some data
    from a socket in the map.
    
    SOCKMAP does not support MSG_OOB, and reading OOB results in a drop.
    
    Let's set drop reasons respectively.
    
      * SOCKET_CLOSE  : the socket in SOCKMAP was close()d
      * UNIX_SKIP_OOB : OOB was read from the socket in SOCKMAP
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Link: https://patch.msgid.link/20250116053441.5758-7-kuniyu@amazon.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Heiko Stuebner <heiko.stuebner@cherry.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -2743,7 +2743,7 @@ static int unix_stream_read_skb(struct sock *sk, skb_read_actor_t recv_actor)
 
 		if (sock_flag(sk, SOCK_DEAD)) {
 			unix_state_unlock(sk);
-			kfree_skb(skb);
+			kfree_skb_reason(skb, SKB_DROP_REASON_SOCKET_CLOSE);
 			return -ECONNRESET;
 		}
 
@@ -2757,7 +2757,7 @@ static int unix_stream_read_skb(struct sock *sk, skb_read_actor_t recv_actor)
 		unix_state_unlock(sk);
 
 		if (drop) {
-			kfree_skb(skb);
+			kfree_skb_reason(skb, SKB_DROP_REASON_UNIX_SKIP_OOB);
 			return -EAGAIN;
 		}
 	}
