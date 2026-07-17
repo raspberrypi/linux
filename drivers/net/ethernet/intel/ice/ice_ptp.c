33dc0dfb480e67ac0f9d130f31efc78ae3fcc264
Sergey Temerkhanov <sergey.temerkhanov@intel.com>
2026-07-17 11:53:30 -0700

    ice: use READ_ONCE() to access cached PHC time
    commit 2915681b89f817677ab9f1166d95b595bc144f5f upstream.
    
    ptp.cached_phc_time is a 64-bit value updated by a periodic work item
    on one CPU and read locklessly on another.  On 32-bit or non-atomic
    architectures this can result in a torn read.  Use READ_ONCE() to
    enforce a single atomic load.
    
    Fixes: 77a781155a65 ("ice: enable receive hardware timestamping")
    Cc: stable@vger.kernel.org
    Signed-off-by: Sergey Temerkhanov <sergey.temerkhanov@intel.com>
    Signed-off-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Tested-by: Rinitha S <sx.rinitha@intel.com> (A Contingent worker at Intel)
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Link: https://patch.msgid.link/20260717185340.3595286-9-anthony.l.nguyen@intel.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/net/ethernet/intel/ice/ice_ptp.c
+++ drivers/net/ethernet/intel/ice/ice_ptp.c
@@ -350,7 +350,7 @@ static u64 ice_ptp_extend_40b_ts(struct ice_pf *pf, u64 in_tstamp)
 		return 0;
 	}
 
-	return ice_ptp_extend_32b_ts(pf->ptp.cached_phc_time,
+	return ice_ptp_extend_32b_ts(READ_ONCE(pf->ptp.cached_phc_time),
 				     (in_tstamp >> 8) & mask);
 }
 
