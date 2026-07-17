35797a7730be7e72e2785e86eb031c0c6ae76976
Koichiro Den <den@valinux.co.jp>
2026-07-18 03:06:34 +0900

    dmaengine: dw-edma: Complete descriptors before pausing
    commit c154060016a9db2ac889bfdb0a3c1322f9be8ded upstream.
    
    If PAUSE is requested while the final burst of a descriptor is in
    flight, the DONE interrupt takes the PAUSE path without checking whether
    the descriptor has been depleted. The depleted descriptor remains on the
    issued list and the channel enters EDMA_ST_PAUSE.
    
    On resume, dw_edma_start_transfer() can select that depleted descriptor
    again even though no burst remains, leaving the channel in an invalid
    busy state.
    
    Check for descriptor completion before acknowledging PAUSE. If there is
    no work to start on resume, leave the channel idle. Also ignore DONE
    interrupts while the channel is paused so a stale or repeated interrupt
    cannot change its state or start queued work.
    
    Fixes: e63d79d1ffcd ("dmaengine: Add Synopsys eDMA IP core driver")
    Cc: stable@vger.kernel.org
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: Koichiro Den <den@valinux.co.jp>
    Link: https://patch.msgid.link/20260717180639.2643243-5-den@valinux.co.jp
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/dma/dw-edma/dw-edma-core.c
+++ drivers/dma/dw-edma/dw-edma-core.c
@@ -271,7 +271,8 @@ static int dw_edma_device_resume(struct dma_chan *dchan)
 		err = -EPERM;
 	} else {
 		chan->status = EDMA_ST_BUSY;
-		dw_edma_start_transfer(chan);
+		if (!dw_edma_start_transfer(chan))
+			chan->status = EDMA_ST_IDLE;
 	}
 
 	return err;
@@ -621,16 +622,28 @@ static void dw_edma_done_interrupt(struct dw_edma_chan *chan)
 	unsigned long flags;
 
 	spin_lock_irqsave(&chan->vc.lock, flags);
+	if (chan->status == EDMA_ST_PAUSE) {
+		spin_unlock_irqrestore(&chan->vc.lock, flags);
+		return;
+	}
+
 	vd = vchan_next_desc(&chan->vc);
 	if (vd) {
 		switch (chan->request) {
 		case EDMA_REQ_NONE:
+		case EDMA_REQ_PAUSE:
 			desc = vd2dw_edma_desc(vd);
 			if (!desc->chunks_alloc) {
 				dw_hdma_set_callback_result(vd,
 							    DMA_TRANS_NOERROR);
 				list_del(&vd->node);
 				vchan_cookie_complete(vd);
+			}
+
+			if (chan->request == EDMA_REQ_PAUSE) {
+				chan->request = EDMA_REQ_NONE;
+				chan->status = EDMA_ST_PAUSE;
+				break;
 			}
 
 			/* Continue transferring if there are remaining chunks or issued requests.
@@ -643,11 +656,6 @@ static void dw_edma_done_interrupt(struct dw_edma_chan *chan)
 			vchan_cookie_complete(vd);
 			chan->request = EDMA_REQ_NONE;
 			chan->status = EDMA_ST_IDLE;
-			break;
-
-		case EDMA_REQ_PAUSE:
-			chan->request = EDMA_REQ_NONE;
-			chan->status = EDMA_ST_PAUSE;
 			break;
 
 		default:
