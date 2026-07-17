e20c6539394ac8d70240b85bb5963c74a3a49789
Koichiro Den <den@valinux.co.jp>
2026-07-18 03:06:33 +0900

    dmaengine: dw-edma: Serialize abort state updates
    [ Upstream commit dd80e259f65d932634e26d366570d71669ef6654 ]
    
    dw_edma_abort_interrupt() drops vc.lock before changing request and
    status. issue_pending() can acquire the lock in that small window,
    observe the old busy state, and skip starting queued descriptors. Then
    the abort handler overwrites the channel status as idle, leaving the new
    descriptors stranded for good.
    
    Keep descriptor completion and the state transition in the same critical
    section.
    
    Fixes: e63d79d1ffcd ("dmaengine: Add Synopsys eDMA IP core driver")
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Signed-off-by: Koichiro Den <den@valinux.co.jp>
    Link: https://patch.msgid.link/20260717180639.2643243-4-den@valinux.co.jp
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/dma/dw-edma/dw-edma-core.c
+++ drivers/dma/dw-edma/dw-edma-core.c
@@ -710,9 +710,9 @@ static void dw_edma_abort_interrupt(struct dw_edma_chan *chan)
 		list_del(&vd->node);
 		vchan_cookie_complete(vd);
 	}
-	spin_unlock_irqrestore(&chan->vc.lock, flags);
 	chan->request = EDMA_REQ_NONE;
 	chan->status = EDMA_ST_IDLE;
+	spin_unlock_irqrestore(&chan->vc.lock, flags);
 }
 
 static inline irqreturn_t dw_edma_interrupt_write(int irq, void *data)
