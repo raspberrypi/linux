bce232923aa9e8ca0f80b023c179977bbe977878
Christoph Hellwig <hch@lst.de>
2026-07-18 11:47:52 -0400

    xfs: use bio_reuse in the zone GC code
    [ Upstream commit 0506d32f7c52e41f6e8db7c337e0ce6374c6ffbb ]
    
    Replace our somewhat fragile code to reuse the bio, which caused a
    regression in the past with the block layer bio_reuse helper.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Hans Holmberg <hans.holmberg@wdc.com>
    Reviewed-by: Carlos Maiolino <cmaiolino@redhat.com>
    Reviewed-by: Damien Le Moal <dlemoal@kernel.org>
    Signed-off-by: Carlos Maiolino <cem@kernel.org>
    [ sashal: open-coded the bio_reuse logic at the call site as 6.18 lacks
      commit 7ca44303f9f6 ("block: add a bio_reuse helper") ]
    Stable-dep-of: ae3692c7f440 ("xfs: add newly added RTGs to the free pool in growfs")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/xfs/xfs_zone_gc.c
+++ fs/xfs/xfs_zone_gc.c
@@ -812,9 +812,8 @@ xfs_zone_gc_write_chunk(
 {
 	struct xfs_zone_gc_data	*data = chunk->data;
 	struct xfs_mount	*mp = chunk->ip->i_mount;
-	phys_addr_t		bvec_paddr =
-		bvec_phys(bio_first_bvec_all(&chunk->bio));
 	struct xfs_gc_bio	*split_chunk;
+	unsigned short		vcnt, i;
 
 	if (chunk->bio.bi_status)
 		xfs_force_shutdown(mp, SHUTDOWN_META_IO_ERROR);
@@ -826,10 +825,11 @@ xfs_zone_gc_write_chunk(
 	WRITE_ONCE(chunk->state, XFS_GC_BIO_NEW);
 	list_move_tail(&chunk->entry, &data->writing);
 
+	vcnt = chunk->bio.bi_vcnt;
 	bio_reset(&chunk->bio, mp->m_rtdev_targp->bt_bdev, REQ_OP_WRITE);
-	bio_add_folio_nofail(&chunk->bio, chunk->scratch->folio, chunk->len,
-			offset_in_folio(chunk->scratch->folio, bvec_paddr));
-
+	for (i = 0; i < vcnt; i++)
+		chunk->bio.bi_iter.bi_size += chunk->bio.bi_io_vec[i].bv_len;
+	chunk->bio.bi_vcnt = vcnt;
 	while ((split_chunk = xfs_zone_gc_split_write(data, chunk)))
 		xfs_zone_gc_submit_write(data, split_chunk);
 	xfs_zone_gc_submit_write(data, chunk);
