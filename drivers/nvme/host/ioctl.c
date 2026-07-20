95b0591d51dbf9e618d7845883d535e88ad7c8af
Yang Xiuwei <yangxiuwei@kylinos.cn>
2026-07-20 14:03:05 +0800

    nvme/ioctl: check SUBMIT_IO with nvme_cmd_allowed()
    [ Upstream commit b53d495c7f0db46b6748b5ade48371a10dd5d3bc ]
    
    Unlike IO_CMD / IO64_CMD, NVME_IOCTL_SUBMIT_IO never calls
    nvme_cmd_allowed(). Unprivileged callers can thus issue I/O on a
    partition device or write through a read-only file descriptor.
    
    Pass flags and open_for_write through and reject disallowed commands
    with -EACCES.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Stable-dep-of: 1161be71d1ec ("nvme: reject passthrough of driver-managed Set Features")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/ioctl.c
+++ drivers/nvme/host/ioctl.c
@@ -220,7 +220,8 @@ static int nvme_submit_user_cmd(struct request_queue *q,
 	return ret;
 }
 
-static int nvme_submit_io(struct nvme_ns *ns, struct nvme_user_io __user *uio)
+static int nvme_submit_io(struct nvme_ns *ns, struct nvme_user_io __user *uio,
+		unsigned int flags, bool open_for_write)
 {
 	struct nvme_user_io io;
 	struct nvme_command c;
@@ -277,6 +278,9 @@ static int nvme_submit_io(struct nvme_ns *ns, struct nvme_user_io __user *uio)
 	c.rw.reftag = cpu_to_le32(io.reftag);
 	c.rw.lbat = cpu_to_le16(io.apptag);
 	c.rw.lbatm = cpu_to_le16(io.appmask);
+
+	if (!nvme_cmd_allowed(ns, &c, flags, open_for_write))
+		return -EACCES;
 
 	return nvme_submit_user_cmd(ns->queue, &c, io.addr, length, metadata,
 			meta_len, NULL, 0, 0);
@@ -582,7 +586,7 @@ static int nvme_ns_ioctl(struct nvme_ns *ns, unsigned int cmd,
 	case NVME_IOCTL_SUBMIT_IO32:
 #endif
 	case NVME_IOCTL_SUBMIT_IO:
-		return nvme_submit_io(ns, argp);
+		return nvme_submit_io(ns, argp, flags, open_for_write);
 	case NVME_IOCTL_IO64_CMD_VEC:
 		flags |= NVME_IOCTL_VEC;
 		fallthrough;
