6b4f521e01257387906f8b969ad3450d8208d4e2
Sven Eckelmann <sven@narfation.org>
2026-07-20 15:29:51 +0200

    batman-adv: ensure minimal ethernet header on TX
    commit 49df66b7993c80b80c7eb9a84ba5b3410c8296a0 upstream.
    
    As documented in commit 8bd67ebb50c0 ("net: bridge: xmit: make sure we have
    at least eth header len bytes"), it is possible by for a local user with
    eBPF TC hook access to attach a tc filter which truncates the packet and
    redirects to an batadv interface. But the code assumes that at least
    ETH_HLEN bytes are available and thus might read outside of the available
    buffer.
    
    The batadv_interface_tx() must therefore always check itself if enough data
    is available for the ethernet header and don't rely on min_header_len.
    
    Cc: stable@vger.kernel.org
    Fixes: c6c8fea29769 ("net: Add batman-adv meshing protocol")
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    [ move from mesh- to soft-interface.c ]
    Signed-off-by: Sven Eckelmann <sven@narfation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/batman-adv/soft-interface.c
+++ net/batman-adv/soft-interface.c
@@ -206,6 +206,9 @@ static netdev_tx_t batadv_interface_tx(struct sk_buff *skb,
 	if (atomic_read(&bat_priv->mesh_state) != BATADV_MESH_ACTIVE)
 		goto dropped;
 
+	if (!pskb_may_pull(skb, ETH_HLEN))
+		goto dropped;
+
 	/* reset control block to avoid left overs from previous users */
 	memset(skb->cb, 0, sizeof(struct batadv_skb_cb));
 
