b4a7e615a52195cf3a1fdc53972d179f1205ef26
David Howells <dhowells@redhat.com>
2026-07-22 12:08:04 -0400

    rxrpc: Fix notification vs call-release vs recvmsg
    [ Upstream commit 2fd895842d49c23137ae48252dd211e5d6d8a3ed ]
    
    When a call is released, rxrpc takes the spinlock and removes it from
    ->recvmsg_q in an effort to prevent racing recvmsg() invocations from
    seeing the same call.  Now, rxrpc_recvmsg() only takes the spinlock when
    actually removing a call from the queue; it doesn't, however, take it in
    the lead up to that when it checks to see if the queue is empty.  It *does*
    hold the socket lock, which prevents a recvmsg/recvmsg race - but this
    doesn't prevent sendmsg from ending the call because sendmsg() drops the
    socket lock and relies on the call->user_mutex.
    
    Fix this by firstly removing the bit in rxrpc_release_call() that dequeues
    the released call and, instead, rely on recvmsg() to simply discard
    released calls (done in a preceding fix).
    
    Secondly, rxrpc_notify_socket() is abandoned if the call is already marked
    as released rather than trying to be clever by setting both pointers in
    call->recvmsg_link to NULL to trick list_empty().  This isn't perfect and
    can still race, resulting in a released call on the queue, but recvmsg()
    will now clean that up.
    
    Fixes: 17926a79320a ("[AF_RXRPC]: Provide secure RxRPC sockets for use by userspace and kernel both")
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-by: Jeffrey Altman <jaltman@auristor.com>
    cc: Marc Dionne <marc.dionne@auristor.com>
    cc: Junvyyang, Tencent Zhuque Lab <zhuque@tencent.com>
    cc: LePremierHomme <kwqcheii@proton.me>
    cc: Simon Horman <horms@kernel.org>
    cc: linux-afs@lists.infradead.org
    Link: https://patch.msgid.link/20250717074350.3767366-4-dhowells@redhat.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Stable-dep-of: e66f8f32f501 ("rxrpc: Fix socket notification race")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/trace/events/rxrpc.h
+++ include/trace/events/rxrpc.h
@@ -278,10 +278,10 @@
 	EM(rxrpc_call_put_poke,			"PUT poke    ") \
 	EM(rxrpc_call_put_recvmsg,		"PUT recvmsg ") \
 	EM(rxrpc_call_put_recvmsg_peek_nowait,	"PUT peek-nwt") \
+	EM(rxrpc_call_put_release_recvmsg_q,	"PUT rls-rcmq") \
 	EM(rxrpc_call_put_release_sock,		"PUT rls-sock") \
 	EM(rxrpc_call_put_release_sock_tba,	"PUT rls-sk-a") \
 	EM(rxrpc_call_put_sendmsg,		"PUT sendmsg ") \
-	EM(rxrpc_call_put_unnotify,		"PUT unnotify") \
 	EM(rxrpc_call_put_userid_exists,	"PUT u-exists") \
 	EM(rxrpc_call_put_userid,		"PUT user-id ") \
 	EM(rxrpc_call_see_accept,		"SEE accept  ") \
@@ -294,6 +294,7 @@
 	EM(rxrpc_call_see_disconnected,		"SEE disconn ") \
 	EM(rxrpc_call_see_distribute_error,	"SEE dist-err") \
 	EM(rxrpc_call_see_input,		"SEE input   ") \
+	EM(rxrpc_call_see_notify_released,	"SEE nfy-rlsd") \
 	EM(rxrpc_call_see_recvmsg,		"SEE recvmsg ") \
 	EM(rxrpc_call_see_recvmsg_requeue,	"SEE recv-rqu") \
 	EM(rxrpc_call_see_recvmsg_requeue_first, "SEE recv-rqF") \
