db5c554b36d50ea2eb1cacd18116572e78e8c2bf
Eugenio Pérez <eperezma@redhat.com>
2026-07-22 12:07:29 -0400

    vduse: remove unused vaddr parameter of vduse_domain_free_coherent
    [ Upstream commit 766e1749c0ef6a09651be9b8a8283d508c322b58 ]
    
    We will modify the function in next patches so let's clean it first.
    
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20260119143306.1818855-9-eperezma@redhat.com>
    Stable-dep-of: 9c1523803445 ("VDUSE: avoid leaking information to userspace")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/vdpa/vdpa_user/iova_domain.h
+++ drivers/vdpa/vdpa_user/iova_domain.h
@@ -71,8 +71,7 @@ dma_addr_t vduse_domain_alloc_coherent(struct vduse_iova_domain *domain,
 				       size_t size, void *orig);
 
 void vduse_domain_free_coherent(struct vduse_iova_domain *domain, size_t size,
-				void *vaddr, dma_addr_t dma_addr,
-				unsigned long attrs);
+				dma_addr_t dma_addr, unsigned long attrs);
 
 void vduse_domain_reset_bounce_map(struct vduse_iova_domain *domain);
 
