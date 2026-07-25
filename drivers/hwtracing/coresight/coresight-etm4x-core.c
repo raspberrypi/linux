4718988db1b0ee0292cfa948d08c1e24f7bb2208
Yeoreum Yun <yeoreum.yun@arm.com>
2026-07-25 12:36:35 +0100

    coresight: etm4x: fix leaked trace id
    [ Upstream commit 467e5862ccb0eed907002f4c6d3badfe34360940 ]
    
    If etm4_enable_sysfs() fails in cscfg_csdev_enable_active_config(),
    the trace ID may be leaked because it is not released.
    
    To address this, call etm4_release_trace_id() when etm4_enable_sysfs()
    fails in cscfg_csdev_enable_active_config().
    
    Fixes: 7ebd0ec6cf94 ("coresight: configfs: Allow configfs to activate configuration")
    Reviewed-by: Jie Gan <jie.gan@oss.qualcomm.com>
    Reviewed-by: Leo Yan <leo.yan@arm.com>
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://lore.kernel.org/r/20260725113645.57519-4-yeoreum.yun@arm.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -854,8 +854,10 @@ static int etm4_enable_sysfs(struct coresight_device *csdev)
 	cscfg_config_sysfs_get_active_cfg(&cfg_hash, &preset);
 	if (cfg_hash) {
 		ret = cscfg_csdev_enable_active_config(csdev, cfg_hash, preset);
-		if (ret)
+		if (ret) {
+			etm4_release_trace_id(drvdata);
 			return ret;
+		}
 	}
 
 	spin_lock(&drvdata->spinlock);
