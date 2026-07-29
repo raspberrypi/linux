a556189c067569c3e849e05ca59d2a2a721271a2
Eric Biggers <ebiggers@kernel.org>
2026-07-28 22:19:06 -0700

    dm-verity-fec: fix the size of dm_verity_fec_io::erasures
    commit a7fca324d7d90f7b139d4d32747c83a629fdb446 upstream.
    
    At most 25 entries in dm_verity_fec_io::erasures are used: the maximum
    number of FEC roots plus one.  Therefore, set the array size
    accordingly.  This reduces the size of dm_verity_fec_io by 912 bytes.
    
    Note: a later commit introduces a constant DM_VERITY_FEC_MAX_ROOTS,
    which allows the size to be more clearly expressed as
    DM_VERITY_FEC_MAX_ROOTS + 1.  This commit just fixes the size first.
    
    Fixes: a739ff3f543a ("dm verity: add support for forward error correction")
    Cc: stable@vger.kernel.org
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/md/dm-verity-fec.h
+++ drivers/md/dm-verity-fec.h
@@ -50,7 +50,8 @@ struct dm_verity_fec {
 /* per-bio data */
 struct dm_verity_fec_io {
 	struct rs_control *rs;	/* Reed-Solomon state */
-	int erasures[DM_VERITY_FEC_MAX_RSN];	/* erasures for decode_rs8 */
+	/* erasures for decode_rs8 */
+	int erasures[DM_VERITY_FEC_RSM - DM_VERITY_FEC_MIN_RSN + 1];
 	u8 *bufs[DM_VERITY_FEC_BUF_MAX];	/* bufs for deinterleaving */
 	unsigned int nbufs;		/* number of buffers allocated */
 	u8 *output;		/* buffer for corrected output */
