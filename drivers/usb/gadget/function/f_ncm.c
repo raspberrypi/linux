b6032b9ca78351babdff603443e2f1c73128eb1d
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2026-07-29 22:30:59 -0400

    USB: gadget: Use str_enable_disable-like helpers
    [ Upstream commit 5b6dc50e9ed870fffbf2ae6de77b30fb0d15eab8 ]
    
    Replace ternary (condition ? "enable" : "disable") syntax with helpers
    from string_choices.h because:
    1. Simple function call with one argument is easier to read.  Ternary
       operator has three arguments and with wrapping might lead to quite
       long code.
    2. Is slightly shorter thus also easier to read.
    3. It brings uniformity in the text - same string.
    4. Allows deduping by the linker, which results in a smaller binary
       file.
    
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20250114-str-enable-disable-usb-v1-5-c8405df47c19@linaro.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Stable-dep-of: c4dd150fceab ("USB: gadget: fsl-udc: fix dev_printk() device")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/gadget/function/f_ncm.c
+++ drivers/usb/gadget/function/f_ncm.c
@@ -18,6 +18,7 @@
 #include <linux/device.h>
 #include <linux/etherdevice.h>
 #include <linux/crc32.h>
+#include <linux/string_choices.h>
 
 #include <linux/usb/cdc.h>
 #include <linux/usb/gadget.h>
@@ -560,7 +561,7 @@ static void ncm_do_notify(struct f_ncm *ncm)
 		req->length = sizeof *event;
 
 		DBG(cdev, "notify connect %s\n",
-				ncm->is_open ? "true" : "false");
+				str_true_false(ncm->is_open));
 		ncm->notify_state = NCM_NOTIFY_NONE;
 		break;
 
