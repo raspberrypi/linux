6e32f84b63c054e09392153125d7202abab2d14b
Cheng Xu <chengyou@linux.alibaba.com>
2026-07-30 20:43:54 +0800

    RDMA/erdma: Hold QP references for AE and CM processing
    [ Upstream commit a52eeff32024f190b3bdc99088c7becccd4fa60b ]
    
    AE QP fatal events and iWARP CM paths load QPs from dev->qp_xa
    and then use or reference them outside the xarray lock.
    erdma_destroy_qp() can drop the destroy-path reference and free QP
    resources while such a lookup is in flight.
    
    Add erdma_qp_get_by_qpn() to acquire a kref under the xarray
    lock with kref_get_unless_zero(). Remove the QP from the xarray
    before dropping the destroy-path reference so no new lookup can acquire
    it while destruction waits for existing users.
    
    Fixes: 155055771704 ("RDMA/erdma: Add verbs implementation")
    Signed-off-by: Cheng Xu <chengyou@linux.alibaba.com>
    Link: https://patch.msgid.link/20260730124357.12976-2-chengyou@linux.alibaba.com
    Signed-off-by: Leon Romanovsky <leon@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/infiniband/hw/erdma/erdma_verbs.c
+++ drivers/infiniband/hw/erdma/erdma_verbs.c
@@ -1321,6 +1321,7 @@ int erdma_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata)
 		udata, struct erdma_ucontext, ibucontext);
 	struct erdma_cmdq_destroy_qp_req req;
 	union erdma_mod_qp_params params;
+	unsigned long flags;
 	int err;
 
 	down_write(&qp->state_lock);
@@ -1348,6 +1349,10 @@ int erdma_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata)
 				       "failed to destroy QP %u: %d\n",
 				       QP_ID(qp), err);
 
+	xa_lock_irqsave(&dev->qp_xa, flags);
+	__xa_erase(&dev->qp_xa, QP_ID(qp));
+	xa_unlock_irqrestore(&dev->qp_xa, flags);
+
 	erdma_qp_put(qp);
 	wait_for_completion(&qp->safe_free);
 
@@ -1361,7 +1366,6 @@ int erdma_destroy_qp(struct ib_qp *ibqp, struct ib_udata *udata)
 
 	if (qp->cep)
 		erdma_cep_put(qp->cep);
-	xa_erase(&dev->qp_xa, QP_ID(qp));
 
 	return 0;
 }
