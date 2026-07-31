d4c139fe4548ec3aa6fe44ba9aa1353209462e87
Michal Wajdeczko <michal.wajdeczko@intel.com>
2026-07-31 12:56:55 -0400

    drm/xe: Move xe->info.force_execlist initialization
    [ Upstream commit f1d581bb50ed54b71a8121d3d46fe2627fddd147 ]
    
    The xe_info_init_early() is a place where we initialize those of
    the xe->info fields that do not require any additional hardware
    probes. Move the initialization of the force_execlist flag there.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Link: https://patch.msgid.link/20260526195452.20545-4-michal.wajdeczko@intel.com
    Stable-dep-of: ba7fd1634228 ("drm/xe: Set TTM device beneficial_order to 9 (2M)")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -475,7 +475,6 @@ struct xe_device *xe_device_create(struct pci_dev *pdev)
 
 	xe->info.devid = pdev->device;
 	xe->info.revid = pdev->revision;
-	xe->info.force_execlist = xe_modparam.force_execlist;
 	xe->atomic_svm_timeslice_ms = 5;
 	xe->min_run_period_lr_ms = 5;
 
--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -774,6 +774,7 @@ static int xe_info_init_early(struct xe_device *xe,
 	xe->info.probe_display = IS_ENABLED(CONFIG_DRM_XE_DISPLAY) &&
 				 xe_modparam.probe_display &&
 				 desc->has_display;
+	xe->info.force_execlist = xe_modparam.force_execlist;
 
 	xe_assert(xe, desc->max_gt_per_tile > 0);
 	xe_assert(xe, desc->max_gt_per_tile <= XE_MAX_GT_PER_TILE);
