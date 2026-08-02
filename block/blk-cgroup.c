d8c872901e6459339374e9eea80aa919176c2ccd
Zheng Qixing <zhengqixing@huawei.com>
2026-08-02 19:25:19 +0800

    blk-cgroup: skip dying blkg in blkcg_activate_policy()
    [ Upstream commit 5e9220389920f33b6a804d50c548cd0cd1b04634 ]
    
    When switching IO schedulers on a block device, blkcg_activate_policy()
    can race with concurrent blkcg deletion, leading to a use-after-free in
    rcu_accelerate_cbs.
    
    T1:                               T2:
                                      blkg_destroy
                                      kill(&blkg->refcnt) // blkg->refcnt=1->0
                                      blkg_release // call_rcu(__blkg_release)
                                      ...
                                      blkg_free_workfn
                                      ->pd_free_fn(pd)
    elv_iosched_store
    elevator_switch
    ...
    iterate blkg list
    blkg_get(blkg) // blkg->refcnt=0->1
                                      list_del_init(&blkg->q_node)
    blkg_put(pinned_blkg) // blkg->refcnt=1->0
    blkg_release // call_rcu again
    rcu_accelerate_cbs // uaf
    
    Fix this by checking hlist_unhashed(&blkg->blkcg_node) before getting
    a reference to the blkg. This is the same check used in blkg_destroy()
    to detect if a blkg has already been destroyed. If the blkg is already
    unhashed, skip processing it since it's being destroyed.
    
    Fixes: f1c006f1c685 ("blk-cgroup: synchronize pd_free_fn() from blkg_free_workfn() and blkcg_deactivate_policy()")
    Signed-off-by: Zheng Qixing <zhengqixing@huawei.com>
    Reviewed-by: Tang Yizhou <yizhou.tang@shopee.com>
    Signed-off-by: Yu Kuai <yukuai@fygo.io>
    Reviewed-by: Tao Cui <cuitao@kylinos.cn>
    Reviewed-by: Nilay Shroff <nilay@linux.ibm.com>
    Link: https://patch.msgid.link/20260802112525.3933753-4-yukuai@kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- block/blk-cgroup.c
+++ block/blk-cgroup.c
@@ -1574,6 +1574,8 @@ retry:
 
 		if (blkg->pd[pol->plid])
 			continue;
+		if (hlist_unhashed(&blkg->blkcg_node))
+			continue;
 
 		/* If prealloc matches, use it; otherwise try GFP_NOWAIT */
 		if (blkg == pinned_blkg) {
