1047a7196e99a4bf3155560462b80813d86bdde0
Dom Cobley <popcornmix@gmail.com>
2026-08-03 13:19:33 +0100

    usb: xhci: Partial revert to upstream

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -460,11 +460,6 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 	if (pdev->vendor == PCI_VENDOR_ID_VIA && pdev->device == PCI_DEVICE_ID_VIA_VL805) {
 		xhci->quirks |= XHCI_LPM_SUPPORT;
 		xhci->quirks |= XHCI_TRB_OVERFETCH;
-		xhci->quirks |= XHCI_EP_CTX_BROKEN_DCS;
-		xhci->quirks |= XHCI_AVOID_DQ_ON_LINK;
-		xhci->quirks |= XHCI_VLI_SS_BULK_OUT_BUG;
-		if (xhci_vl805_get_fw_version(pdev) < VL805_FW_VER_0138C0)
-			xhci->quirks |= XHCI_VLI_HUB_TT_QUIRK;
 	}
 
 	if (pdev->vendor == PCI_VENDOR_ID_ASMEDIA &&
