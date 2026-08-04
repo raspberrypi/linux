fdbdc5c016a818b970e01accdb351d100009ae71
Christoph Hellwig <hch@lst.de>
2026-08-04 05:43:58 -0700

    iomap: iomap_bio_read_folio_range_sync is missing a call to bio_uninit
    [ Upstream commit accb6624e383872e7703974a5d5826755fb27032 ]
    
    Which could leak blkg references.
    
    Fixes: c03cea42149d ("iomap: add initial support for writes without buffer heads")
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://patch.msgid.link/20260804124404.737145-3-hch@lst.de
    Reviewed-by: "Darrick J. Wong" <djwong@kernel.org>
    Reviewed-by: Anuj Gupta <anuj20.g@samsung.com>
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/iomap/bio.c
+++ fs/iomap/bio.c
@@ -184,5 +184,6 @@ int iomap_bio_read_folio_range_sync(const struct iomap_iter *iter,
 			error = fs_bio_integrity_verify(&bio, sector, len);
 		fs_bio_integrity_free(&bio);
 	}
+	bio_uninit(&bio);
 	return error;
 }
