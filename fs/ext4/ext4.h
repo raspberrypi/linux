c391af5743a447760c341ad5f6c2d1e984dbc484
Jan Kara <jack@suse.cz>
2026-08-05 17:35:47 +0200

    ext4: teach ext4_meta_trans_blocks() about number of allocated extents
    [ Upstream commit 25b2a7e8d420c49fc46f25b6f953603e4dae1dbf ]
    
    So far ext4_meta_trans_blocks() expects that each extent counted in
    @pextents will be allocated in the transaction we estimate credits for.
    This is correct for the use in ext4_chunk_trans_blocks() and
    ext4_chunk_trans_extent() however the use in atomic write path
    (ext4_convert_unwritten_extents_atomic() and ext4_iomap_alloc() for
    IOMAP_ATOMIC) unnecessarily overestimates the number of necessary
    credits as neither of them allocates any data. Add argument to
    ext4_meta_trans_blocks() for number of extents that are going to be
    allocated in the transaction.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20260805153605.166545-4-jack@suse.cz
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Stable-dep-of: 46e8e31771f4 ("ext4: fix transaction overflow during writeback")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -3186,7 +3186,7 @@ extern int ext4_normal_submit_inode_data_buffers(struct jbd2_inode *jinode);
 extern int ext4_chunk_trans_blocks(struct inode *, int nrblocks);
 extern int ext4_chunk_trans_extent(struct inode *inode, int nrblocks);
 extern int ext4_meta_trans_blocks(struct inode *inode, int lblocks,
-				  int pextents);
+				  int pextents, int alloc_extents);
 extern int ext4_block_zero_eof(struct inode *inode, loff_t from, loff_t end);
 extern int ext4_zero_partial_blocks(struct inode *inode, loff_t lstart,
 				    loff_t length, bool *did_zero);
