2dcd2cab775faf638df7ea3f4f42f77b75234300
Sven Peter <sven@kernel.org>
2026-08-06 17:27:35 +0200

    nvme-apple: Never set the opcode in the NVMMU TCB
    [ Upstream commit cc0fec9b42cfbc69d70cb4c4b616408a7037b445 ]
    
    macOS always sets this to zero and the firmware starting with macOS 15
    has started to complain about what we're doing here.
    
    Fixes: 5bd2927aceba ("nvme-apple: Add initial Apple SoC NVMe driver")
    Tested-by: Joshua Peisach <jpeisach@ubuntu.com>
    Tested-by: Janne Grunau <j@jannau.net>
    Tested-by: Nick Chan <towinchenmi@gmail.com>
    Signed-off-by: Sven Peter <sven@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/nvme/host/apple.c
+++ drivers/nvme/host/apple.c
@@ -304,7 +304,7 @@ static void apple_nvme_submit_cmd(struct apple_nvme_queue *q,
 	u32 tag = nvme_tag_from_cid(cmd->common.command_id);
 	struct apple_nvmmu_tcb *tcb = &q->tcbs[tag];
 
-	tcb->opcode = cmd->common.opcode;
+	tcb->opcode = 0;
 	tcb->prp1 = cmd->common.dptr.prp1;
 	tcb->prp2 = cmd->common.dptr.prp2;
 	tcb->length = cmd->rw.length;
