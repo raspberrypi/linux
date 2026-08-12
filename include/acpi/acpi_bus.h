5657859851abb65105220a6cdb5804926249f714
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-08-12 14:41:44 +0200

    ACPI: bus: Introduce acpi_bus_get_primary_device()
    [ Upstream commit 72530e1f72b0515a73fd88292254d04fecf03649 ]
    
    The function used for obtaining the first "physical" device for which
    the given ACPI one is the ACPI companion, acpi_get_first_physical_node(),
    may return a stale device pointer (mostly in theory) because
    acpi_unbind_one() may run as a whole after dropping the ACPI device's
    physical_node_lock in acpi_get_first_physical_node() and before it
    returns.  The last reference to the "physical" device may be dropped
    then before the pointer to it is returned to the caller.
    
    If that happens and the acpi_get_first_physical_node() caller invokes
    get_device() on the pointer obtained from it, which is done by the
    majority of its callers, a use-after-free will occur.
    
    To prepare for addressing this problem, introduce a new function for
    getting the first "physical" device associated with the given ACPI one
    (the "primary physical device") that will also reference count the
    device in question before returning a pointer to it.
    
    Make that new function and acpi_get_first_physical_node() share the
    physical node list lookup code.
    
    No intentional functional impact.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://patch.msgid.link/2843318.mvXUDI8C0e@rafael.j.wysocki
    Stable-dep-of: a9ba4dd2f18b ("ACPI: platform: Use acpi_bus_get_primary_device()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -686,6 +686,7 @@ void acpi_bus_unregister_driver(struct acpi_driver *driver);
 int acpi_bus_scan(acpi_handle handle);
 void acpi_bus_trim(struct acpi_device *start);
 acpi_status acpi_bus_get_ejd(acpi_handle handle, acpi_handle * ejd);
+struct device *acpi_bus_get_primary_device(struct acpi_device *adev);
 int acpi_match_device_ids(struct acpi_device *device,
 			  const struct acpi_device_id *ids);
 void acpi_set_modalias(struct acpi_device *adev, const char *default_id,
@@ -999,6 +1000,11 @@ int acpi_wait_for_acpi_ipmi(void);
 int acpi_scan_add_dep(acpi_handle handle, struct acpi_handle_list *dep_devices);
 u32 arch_acpi_add_auto_dep(acpi_handle handle);
 #else	/* CONFIG_ACPI */
+
+static inline struct device *acpi_bus_get_primary_device(struct acpi_device *adev)
+{
+	return NULL;
+}
 
 static inline bool acpi_of_match_device(const struct acpi_device *adev,
 					const struct of_device_id *of_match_table,
