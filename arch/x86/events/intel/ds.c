a5fe19dd8b3ed5fad6e5e0f0c58c7245043ee4af
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-08-13 14:43:46 +0800

    perf/x86/intel: Prevent drain_pebs() reentry
    [ Upstream commit a56c03a397e2cd0c4cf8da96dcd6214f7d0e7d8c ]
    
    The PEBS buffer is shared by all events on a CPU, so drain_pebs() must
    not be reentered. If so, one instance may observe stale buffer state and
    potentially access out-of-bound memory.
    
    Most invocations happen in NMI context, which naturally prevents reentry.
    However, drain_pebs() is also reachable from process context via
    intel_pmu_drain_pebs_buffer().
    
    In those paths, the PMU is often already disabled, but not guaranteed.
    For example, __intel_pmu_pebs_disable() only disables the target counter,
    so other active counters can still raise a PMI and interrupt an in-flight
    drain_pebs(). Here is an example,
    
    __perf_addr_filters_adjust()
      perf_event_stop()
        __perf_event_stop()
          x86_pmu_stop() (event->pmu->stop)
            intel_pmu_disable_event()
              intel_pmu_pebs_disable()
                __intel_pmu_pebs_disable()
                  intel_pmu_drain_large_pebs()
                    intel_pmu_drain_pebs_buffer()
    
    Introduce __intel_pmu_quiesce() and __intel_pmu_resume() helpers and
    use them in intel_pmu_drain_large_pebs() to disable the full PMU
    around the intel_pmu_drain_pebs_buffer() call, preventing reentry.
    
    Also add a warning in intel_pmu_drain_pebs_buffer() when the full PMU is
    not disabled.
    
    Fixes: b752ea0c28e3 ("perf/x86/intel/ds: Flush PEBS DS when changing PEBS_DATA_CFG")
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://patch.msgid.link/20260813064346.335458-1-dapeng1.mi@linux.intel.com
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -961,7 +961,10 @@ unlock:
 
 void intel_pmu_drain_pebs_buffer(void)
 {
+	struct cpu_hw_events *cpuc = this_cpu_ptr(&cpu_hw_events);
 	struct perf_sample_data data;
+
+	WARN_ON_ONCE(cpuc->enabled);
 
 	static_call(x86_pmu_drain_pebs)(NULL, &data);
 }
@@ -1528,8 +1531,11 @@ static void intel_pmu_pebs_via_pt_enable(struct perf_event *event)
 static inline void intel_pmu_drain_large_pebs(struct cpu_hw_events *cpuc)
 {
 	if (cpuc->n_pebs == cpuc->n_large_pebs &&
-	    cpuc->n_pebs != cpuc->n_pebs_via_pt)
+	    cpuc->n_pebs != cpuc->n_pebs_via_pt) {
+		int enabled = __intel_pmu_quiesce();
 		intel_pmu_drain_pebs_buffer();
+		__intel_pmu_resume(enabled);
+	}
 }
 
 void intel_pmu_pebs_enable(struct perf_event *event)
