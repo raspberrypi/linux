4d35c6d6980855b2053d42d3d0ac69f222b3dd4d
Nguyen Dinh Phi <phind.uet@gmail.com>
2026-08-14 01:30:20 +0800

    vsock: use sock_error() to consume sk_err after a failed connect
    [ Upstream commit 96cbf89993091a163bfedec52a3bd683dc94b3b4 ]
    
    vsock_connect() returns sk_err to userspace but does not clear it:
      if (sk->sk_err) {
                err = -sk->sk_err;
    
    For a blocking connect() the error has already been delivered as
    connect()'s return value, so leaving it set causes subsequent operations
    like poll()/epoll() to keep reporting POLLERR even though the connect
    failure was already delivered.
    
    The error should be consumed once it has been returned to userspace.
    Switch to sock_error(), which reads and clears sk_err atomically,
    matching the behavior of other protocol implementations such as
    __inet_stream_connect().
    
    Fixes: d021c344051a ("VSOCK: Introduce VM Sockets")
    Tested-by: Wupeng Ma <mawupeng1@huawei.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Nguyen Dinh Phi <phind.uet@gmail.com>
    Link: https://patch.msgid.link/20260813173024.2362935-4-phind.uet@gmail.com
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/vmw_vsock/af_vsock.c
+++ net/vmw_vsock/af_vsock.c
@@ -1716,12 +1716,10 @@ static int vsock_connect(struct socket *sock, struct sockaddr *addr,
 		prepare_to_wait(sk_sleep(sk), &wait, TASK_INTERRUPTIBLE);
 	}
 
-	if (sk->sk_err) {
-		err = -sk->sk_err;
+	err = sock_error(sk);
+	if (err) {
 		sk->sk_state = TCP_CLOSE;
 		sock->state = SS_UNCONNECTED;
-	} else {
-		err = 0;
 	}
 
 out_wait:
