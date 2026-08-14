b6bb334b0e9348887e3e55e1f494b0c3b8fbf59f
Jens Axboe <axboe@kernel.dk>
2026-08-14 06:58:43 -0600

    io_uring: defer eventfd signaling when queued from a wakeup handler
    commit cd305ee3633a45fcf5f3a5d83f99f3cb77d87b6e upstream.
    
    io_req_local_work_add() signals the CQ ring eventfd inline when it is the
    one to push the first entry onto ->work_list. For DEFER_TASKRUN rings that
    add is frequently done from a waitqueue wakeup handler, where an
    arbitrary waitqueue lock is held.
    
    eventfd_signal_mask() only refuses to recurse when current->in_eventfd
    is set, but that bit is set by eventfd_signal_mask() itself. If the wake
    chain starts somewhere else, signal goes out inline and can feed back
    into epoll.
    
    Add IOU_F_TWQ_IN_WAKE, set it on the task_work add done from the three
    waitqueue callbacks, and use it to force io_eventfd_signal() down the
    existing call_rcu_hurry() deferral instead of signaling inline.
    
    Fixes: 21a091b970cd ("io_uring: signal registered eventfd to process deferred task work")
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/all/20260813133843.2933127-1-4ncienth@gmail.com/
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- io_uring/tw.c
+++ io_uring/tw.c
@@ -223,7 +223,7 @@ void io_req_local_work_add(struct io_kiocb *req, unsigned flags)
 	if (!head) {
 		io_ctx_mark_taskrun(ctx);
 		if (data_race(ctx->int_flags) & IO_RING_F_HAS_EVFD)
-			io_eventfd_signal(ctx, false);
+			io_eventfd_signal(ctx, false, flags & IOU_F_TWQ_IN_WAKE);
 	}
 
 	nr_wait = atomic_read(&ctx->cq_wait_nr);
