bb4da45766d16503821f167054db76b735d89e94
Qu Wenruo <wqu@suse.com>
2026-08-17 14:43:53 +0930

    btrfs: fix the possible bioc_list memory leak during error
    [ Upstream commit afbe73778338e6d1ac8c4486fbdf33f0cc1f2624 ]
    
    There are two possible ways to leak bioc memory on
    btrfs_ordered_extent::bioc_list:
    
    - An error occurred for btrfs_insert_one_raid_extent()
      Then the function btrfs_insert_raid_extent() immediately return
      without freeing any bioc in the bioc_list.
    
    - An ordered extent hit an IO error
      In that case the ordered extent will have BTRFS_ORDERED_IOERR set, and
      skip the call on btrfs_insert_raid_extent() completely.
    
    Fix the problem by:
    
    - Introduce a new helper, btrfs_cleanup_ordered_bioc_list()
      Which will remove all bioc from the bioc_list, and release the bioc.
    
    - Call the above helper for btrfs_insert_raid_extent()
      So that the cleanup helper is always called no matter what.
    
    - Call the above helper for btrfs_finish_one_ordered()
      This is called just before the final release on the ordered extent.
    
    This was reported by Sashiko when reviewing another patch.
    
    Link: https://sashiko.dev/#/patchset/20260817021512.3010812-1-shuangpeng.kernel%40gmail.com
    Fixes: 02c372e1f016 ("btrfs: add support for inserting raid stripe extents")
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -3435,6 +3435,9 @@ out:
 	 */
 	btrfs_remove_ordered_extent(ordered_extent);
 
+	/* Cleanup any remaining biocs attached to the OE. */
+	btrfs_cleanup_ordered_bioc_list(ordered_extent);
+
 	/* once for us */
 	btrfs_put_ordered_extent(ordered_extent);
 	/* once for the tree */
