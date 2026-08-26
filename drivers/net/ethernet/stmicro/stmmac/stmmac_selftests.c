5a106e59b0519710a87a23da05caf5b03c92a296
Maxime Chevallier <maxime.chevallier@bootlin.com>
2026-08-26 16:04:57 +0200

    net: stmmac: selftests: Account for the UC filter list for filtering tests
    [ Upstream commit cd8c3b2752c684141eab2282e294cae2971a9759 ]
    
    On dwmac, one of the Unicast filter entries is used to store the local
    HW addr. This means that we have to use promisc mode for any kind of
    unicast filtering if we only have one slot in our unicast filter.
    
    The number of slots available depends on how the IP is integrated, and
    we can't autodiscover how many of these slots we have available, so
    the DT property snps,perfect-filter-entries can be used to specify how
    many are available.
    
    Most IP variants default to 1 if this isn't specified, which is the case
    for the amlogic variants (in this case, S905X3).
    
    The stmmac selftests for UC filtering look if we have enough slots in
    the filter to store the dev->uc list, but doesn't account for the
    device's own MAC address. The dev->uc list's size we get with
    netdev_uc_count() also doesn't account for the HW addr.
    
    As the selftest only requires one available slot, in the case of
    single-slot platforms, that means we erroneously consider we have enough
    room for the test, when we actually don't, and the filtering test fails.
    
    Fixes: 091810dbded9 ("net: stmmac: Introduce selftests support")
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>
    Link: https://patch.msgid.link/20260826140500.616466-6-maxime.chevallier@bootlin.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_selftests.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_selftests.c
@@ -499,6 +499,21 @@ static int stmmac_filter_check(struct stmmac_priv *priv)
 	return -EOPNOTSUPP;
 }
 
+static int stmmac_uc_filter_check(struct stmmac_priv *priv)
+{
+	/* For tests involving the UC filter, we need at least one empty
+	 * slot in the UC filter. The UC filters contains netdev_uc_count() + 1
+	 * entries: The dev->uc list + one entry for the HW address.
+	 *
+	 * Having an empty slot therefore means netdev_uc_count() + 2 entries
+	 * can fit in the filter
+	 */
+	if (netdev_uc_count(priv->dev) + 2 > priv->hw->unicast_filter_entries)
+		return -EOPNOTSUPP;
+
+	return 0;
+}
+
 static bool stmmac_hash_check(struct stmmac_priv *priv, unsigned char *addr)
 {
 	int mc_offset = 32 - priv->hw->mcast_bits_log2;
@@ -590,7 +605,7 @@ static int stmmac_test_pfilt(struct stmmac_priv *priv)
 
 	if (stmmac_filter_check(priv))
 		return -EOPNOTSUPP;
-	if (netdev_uc_count(priv->dev) >= priv->hw->unicast_filter_entries)
+	if (stmmac_uc_filter_check(priv))
 		return -EOPNOTSUPP;
 
 	while (--tries) {
@@ -634,7 +649,7 @@ static int stmmac_test_mcfilt(struct stmmac_priv *priv)
 
 	if (stmmac_filter_check(priv))
 		return -EOPNOTSUPP;
-	if (netdev_uc_count(priv->dev) >= priv->hw->unicast_filter_entries)
+	if (stmmac_uc_filter_check(priv))
 		return -EOPNOTSUPP;
 	if (netdev_mc_count(priv->dev) >= priv->hw->multicast_filter_bins)
 		return -EOPNOTSUPP;
@@ -680,7 +695,7 @@ static int stmmac_test_ucfilt(struct stmmac_priv *priv)
 
 	if (stmmac_filter_check(priv))
 		return -EOPNOTSUPP;
-	if (netdev_uc_count(priv->dev) >= priv->hw->unicast_filter_entries)
+	if (stmmac_uc_filter_check(priv))
 		return -EOPNOTSUPP;
 	if (netdev_mc_count(priv->dev) >= priv->hw->multicast_filter_bins)
 		return -EOPNOTSUPP;
