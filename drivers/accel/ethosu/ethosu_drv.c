e5576641b79ecd36acaf91fec4a5333b1ad19c57
Rob Herring (Arm) <robh@kernel.org>
2026-08-27 15:33:03 -0500

    accel: ethosu: Ensure SRAM size is 0 on mapping failure
    commit f5376d7e0fb703876199d3b6f9f97e128fa2f8a4 upstream.
    
    On a mapping failure of the SRAM, the SRAM size is left as non-zero. The
    probe will succeed as the error return is not checked since having SRAM is
    not a hard requirement. The non-zero size allows jobs to access SRAM which
    is left pointing to physical base address 0x0.
    
    Fixes: 5a5e9c0228e6 ("accel: Add Arm Ethos-U NPU driver")
    Cc: stable@vger.kernel.org
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260827-ethosu-fixes-v1-4-346f9ea8791c@kernel.org
    Signed-off-by: Rob Herring (Arm) <robh@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/accel/ethosu/ethosu_drv.c
+++ drivers/accel/ethosu/ethosu_drv.c
@@ -274,8 +274,6 @@ static int ethosu_device_suspend(struct device *dev)
 
 static int ethosu_sram_init(struct ethosu_device *ethosudev)
 {
-	ethosudev->npu_info.sram_size = 0;
-
 	ethosudev->srampool = of_gen_pool_get(ethosudev->base.dev->of_node, "sram", 0);
 	if (!ethosudev->srampool)
 		return 0;
@@ -286,6 +284,7 @@ static int ethosu_sram_init(struct ethosu_device *ethosudev)
 							     ethosudev->npu_info.sram_size,
 							     &ethosudev->sramphys);
 	if (!ethosudev->sram) {
+		ethosudev->npu_info.sram_size = 0;
 		dev_err(ethosudev->base.dev, "failed to allocate from SRAM pool\n");
 		return -ENOMEM;
 	}
