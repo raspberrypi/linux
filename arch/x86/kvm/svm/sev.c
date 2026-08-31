55ac641b73f78ac6bbff988667287537504b1a27
Sean Christopherson <seanjc@google.com>
2026-08-31 14:31:52 -0400

    KVM: SVM: Mark VMCB dirty before processing incoming snp_vmsa_gpa
    [ Upstream commit e268beee4a25ce58807a96253e95e96c2e3a5c1a ]
    
    Mark the VMCB dirty, i.e. zero control.clean, prior to handling the new
    VMSA.  Nothing in the VALID_PAGE() case touches control.clean, and
    isolating the VALID_PAGE() code will allow simplifying the overall logic.
    
    Note, the VMCB probably doesn't need to be marked dirty when the VMSA is
    invalid, as KVM will disallow running the vCPU in such a state.  But it
    also doesn't hurt anything.
    
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Link: https://lore.kernel.org/r/20250227012541.3234589-9-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Stable-dep-of: 42a39ad5d592 ("KVM: SEV: Track the GPA of the guest-controlled VMSA used for SNP guests")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -3924,6 +3924,12 @@ static int __sev_snp_update_protected_guest_state(struct kvm_vcpu *vcpu)
 	/* Clear use of the VMSA */
 	svm->vmcb->control.vmsa_pa = INVALID_PAGE;
 
+	/*
+	 * When replacing the VMSA during SEV-SNP AP creation,
+	 * mark the VMCB dirty so that full state is always reloaded.
+	 */
+	vmcb_mark_all_dirty(svm->vmcb);
+
 	if (VALID_PAGE(svm->sev_es.snp_vmsa_gpa)) {
 		gfn_t gfn = gpa_to_gfn(svm->sev_es.snp_vmsa_gpa);
 		struct kvm_memory_slot *slot;
@@ -3968,12 +3974,6 @@ static int __sev_snp_update_protected_guest_state(struct kvm_vcpu *vcpu)
 		 */
 		kvm_release_pfn_clean(pfn);
 	}
-
-	/*
-	 * When replacing the VMSA during SEV-SNP AP creation,
-	 * mark the VMCB dirty so that full state is always reloaded.
-	 */
-	vmcb_mark_all_dirty(svm->vmcb);
 
 	return 0;
 }
