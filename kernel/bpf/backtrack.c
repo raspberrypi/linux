f83890dfed9efc5e19df1dbc1ad707a142936923
Eduard Zingerman <eddyz87@gmail.com>
2026-08-31 18:36:09 -0700

    bpf: backtracking shouldn't clear outer frame R1-R5 for callbacks
    [ Upstream commit e3e4f66cc4b72333d0886ae2673c360248987889 ]
    
    When processing calls to bpf_loop() verifier marks R1 (and R4) as
    precise. R1 tracks loop iterations number and because of the
    'callback_depth < R1' mechanics in check_helper_call() must be marked
    precise. However, precision propagation for R1 was broken,
    when bpf_loop() call was verified on a second iteration.
    
    Consider the following verification trace:
    - main: bpf_loop(nr_loops, callback ...)
    - callback: BPF_EXIT
    - main: bpf_loop(nr_loops, callback ...)
    - ...
    
    While the first visit of the call to bpf_loop() propagated R1
    precision as expected, the second call to mark_chain_precision() in
    the check_helper_call() set R1, but it was immediately reset when
    backtrack_insn() processed preceding BPF_EXIT in the loop deleted in
    this patch.
    
    Because of that, the second visit of the call to bpf_loop() injected
    checkpoint with R1 not marked as precise. Which could trick the
    verifier into accepting unsafe programs. See the next patch for an
    example of such program.
    
    Commit is structured in a way to minimize conflicts when
    'bpf' would be eventually merged with 'bpf-next'.
    
    Fixes: ab5cfac139ab ("bpf: verify callbacks as if they are called unknown number of times")
    Reported-by: Nicholas Carlini <npc@anthropic.com>
    Suggested-by: Nicholas Carlini <npc@anthropic.com>
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Link: https://lore.kernel.org/r/20260831-bug-015-backtrack-cb-args-precise-v1-1-68a8e2a821e0@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/bpf/backtrack.c
+++ kernel/bpf/backtrack.c
@@ -521,24 +521,7 @@ static int backtrack_insn(struct bpf_verifier_env *env, int idx, int subseq_idx,
 					return -EFAULT;
 			}
 		} else if (opcode == BPF_EXIT) {
-			bool r0_precise;
-
-			/* Backtracking to a nested function call, 'idx' is a part of
-			 * the inner frame 'subseq_idx' is a part of the outer frame.
-			 * In case of a regular function call, instructions giving
-			 * precision to registers R1-R5 should have been found already.
-			 * In case of a callback, it is ok to have R1-R5 marked for
-			 * backtracking, as these registers are set by the function
-			 * invoking callback.
-			 */
-			if (subseq_idx >= 0 && bpf_calls_callback(env, subseq_idx))
-				for (i = BPF_REG_1; i <= BPF_REG_5; i++)
-					bt_clear_reg(bt, i);
-			if (bt_reg_mask(bt) & BPF_REGMASK_ARGS) {
-				verifier_bug(env, "backtracking exit unexpected regs %x",
-					     bt_reg_mask(bt));
-				return -EFAULT;
-			}
+			bool from_subprog_call, r0_precise;
 
 			/* BPF_EXIT in subprog or callback always returns
 			 * right after the call instruction, so by checking
@@ -548,9 +531,23 @@ static int backtrack_insn(struct bpf_verifier_env *env, int idx, int subseq_idx,
 			 * case, we need to propagate r0 precision, if
 			 * necessary. In the former we never do that.
 			 */
-			r0_precise = subseq_idx - 1 >= 0 &&
-				     bpf_pseudo_call(&env->prog->insnsi[subseq_idx - 1]) &&
-				     bt_is_reg_set(bt, BPF_REG_0);
+			from_subprog_call = subseq_idx - 1 >= 0 &&
+					    bpf_pseudo_call(&env->prog->insnsi[subseq_idx - 1]);
+
+			r0_precise = from_subprog_call && bt_is_reg_set(bt, BPF_REG_0);
+
+			/* Backtracking to a nested function call, 'idx' is a part of
+			 * the inner frame 'subseq_idx' is a part of the outer frame.
+			 * In case of a regular function call, instructions giving
+			 * precision to registers R1-R5 should have been found already.
+			 * In case of a callback from bpf_loop(), R{1,4} in the calling
+			 * frame would be set as precise and that is correct.
+			 */
+			if (from_subprog_call && (bt_reg_mask(bt) & BPF_REGMASK_ARGS)) {
+				verifier_bug(env, "backtracking exit unexpected regs %x",
+					     bt_reg_mask(bt));
+				return -EFAULT;
+			}
 
 			bt_clear_reg(bt, BPF_REG_0);
 			if (bt_subprog_enter(bt))
