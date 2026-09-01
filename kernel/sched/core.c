b715872ef0ccc00daaa4b714c5a60a5067ac1e9b
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-09-01 15:54:51 +0200

    futex: Provide rt_mutex_.*_schedule() equivalents for futex scheduling
    commit 912edebe8501a36c6bedcef03bd238ab90a7e060 upstream.
    
    There is rt_mutex_{pre|post}_schedule() around
    rt_mutex_wait_proxy_lock() to ensure that sched_submit_work()/
    sched_update_worker() is invoked before we schedule out and block on
    rt_mutex while waiting for it become available.
    
    The reason is that blocking on rt_mutex assigns a pi_waiter for the PI
    chain and sched_submit_work() will also assign a pi_waiter if it blocks
    on lock but a this point we already have a waiter assigned.
    We can't skip sched_submit_work() entirely because I/O relies on the
    fact that I/O queue is flushed while it blocks on a sleeping lock.
    Therefore sched_submit_work() is moved before we block on the lock.
    
    Sleeping lock in this context means mutex or rw_semaphore not spinlock_t
    on PREEMPT_RT. Because the mutex abstraction on PREEMPT_RT uses the same
    abstraction as the futex proxy lock, the futex code ended up using
    rt_mutex_{pre|post}_schedule(), too.
    Using it is/ was just to keep the task_struct::sched_rt_mutex assertion
    happy. Futex proxy lock is used only in the syscall context of a task.
    At this point it never got any I/O that needs to be flushed and it can't
    be a workqueue that needs to notify that it will be scheduled out.
    Therefore sched_submit_work() does nothing here.
    
    By mistake futex_wait_requeue_pi() -> rt_mutex_wait_proxy_lock() did not
    get the rt_mutex_{pre|post}_schedule() annotation. This was not noticed
    because in this callchain the lock is (usually) not contended and so
    rt_mutex_slowlock_block() does not schedule, triggering the assert.
    
    Adding rt_mutex_pre_schedule() here looks wrong (as noted by PeterZ)
    because at this point there is a pi_waiter recorded and invoking
    sched_submit_work() with a possible lock contention would be wrong.
    
    Add rt_mutex_futex_{pre|post}_schedule() which toggles the
    sched_rt_mutex assert and does not involve sched_submit_work(). Add
    asserts here to ensure that sched_submit_work() would do nothing. Use it
    only in futex proxy lock case which is rt_mutex_wait_proxy_lock().
    Remove it from futex_lock_pi().
    
    Fixes: d14f9e930b90 ("locking/rtmutex: Use rt_mutex specific scheduler helpers")
    Reported-by: Yao Kai <yaokai34@huawei.com>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20260901135453.3121948-2-bigeasy@linutronix.de
    Closes: https://lore.kernel.org/all/20260717084922.4153317-2-yaokai34@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7633,6 +7633,17 @@ void rt_mutex_pre_schedule(void)
 	sched_submit_work(current);
 }
 
+/*
+ * Used within the futex syscall context, skips sched_submit_work() because none
+ * its work will be done. Asserts ensure that it is indeed the case.
+ */
+void rt_mutex_futex_pre_schedule(void)
+{
+	lockdep_assert(!(current->flags & (PF_WQ_WORKER | PF_IO_WORKER)));
+	lockdep_assert(!current->plug);
+	lockdep_assert(!fetch_and_set(current->sched_rt_mutex, 1));
+}
+
 void rt_mutex_schedule(void)
 {
 	lockdep_assert(current->sched_rt_mutex);
@@ -7642,6 +7653,11 @@ void rt_mutex_schedule(void)
 void rt_mutex_post_schedule(void)
 {
 	sched_update_worker(current);
+	lockdep_assert(fetch_and_set(current->sched_rt_mutex, 0));
+}
+
+void rt_mutex_futex_post_schedule(void)
+{
 	lockdep_assert(fetch_and_set(current->sched_rt_mutex, 0));
 }
 
