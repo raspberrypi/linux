ef41e8e4ae5b737861db2dfdb540485affb3b753
Matthew Auld <matthew.auld@intel.com>
2026-09-02 13:41:20 +0100

    drm/xe/vram: report FLAT_CCS base misalignment
    commit 0e68c74e44da81a4599c52437ee1f63a2c234470 upstream.
    
    So we can easily check if a machine had the CCS bug, when looking back
    over bug reports where we have the same machine with newer kernel.
    
    Example print for a machine with the CCS bug:
    
      FLAT_CCS base:27bbff800, aligned:no
    
    v2 (Matt B):
      - Unconditionally print the base + alignment
    
    Fixes: 37173392741c ("drm/xe/vram: fix ccs offset calculation")
    Signed-off-by: Matthew Auld <matthew.auld@intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Cc: stable@kernel.org
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>
    Link: https://patch.msgid.link/20260902124117.918018-9-matthew.auld@intel.com
    (cherry picked from commit d00b7f4f03bbeb2efad872f1686130e18c2b4141)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/xe/xe_vram.c
+++ drivers/gpu/drm/xe/xe_vram.c
@@ -207,6 +207,9 @@ static inline u64 get_flat_ccs_offset(struct xe_gt *gt, u64 tile_size)
 		offset |= offset_lo << 6; /* HW view bits 31:6 */
 		offset *= num_enabled; /* convert to SW view */
 
+		drm_info(&xe->drm, "FLAT_CCS base:%llx, aligned:%s\n", offset,
+			 str_yes_no(IS_ALIGNED(offset, SZ_128K)));
+
 		/*
 		 * Everything below this offset is handed to the VRAM
 		 * allocator, so it has to be the *first* address the
