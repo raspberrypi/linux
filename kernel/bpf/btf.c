0e78cb242a57e481061fcb542fed4d51afba25c4
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2026-09-03 16:44:19 +0200

    bpf: Mark signal tracepoint siginfo arguments as scalar
    [ Upstream commit 77515ab12e4983e6416f8c35039a3f0c0822ac70 ]
    
    The signal_generate and signal_deliver tracepoints declare their info
    argument as a struct kernel_siginfo pointer. btf_ctx_access() therefore
    treats it as a trusted pointer for tp_btf programs.
    
    Signal delivery also uses SEND_SIG_NOINFO and SEND_SIG_PRIV as special
    values for this argument. Those values are zero and one respectively,
    and are not pointers. A tp_btf program can currently dereference either
    value and fault the kernel. In particular, signal_generate can run from
    timer interrupt context, turning the fault into a kernel panic.
    
    Record both tracepoints in raw_tp_null_args[] and mark argument one as
    a non-pointer. This preserves scalar access to the cookie while rejecting
    direct and helper-mediated pointer use. Merely marking it nullable would
    not suffice because SEND_SIG_PRIV is nonzero.
    
    Fixes: 838a10bd2ebf ("bpf: Augment raw_tp arguments with PTR_MAYBE_NULL")
    Reported-by: Nicholas Carlini <npc@anthropic.com>
    Suggested-by: Nicholas Carlini <npc@anthropic.com>
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/r/20260903144433.1716731-2-memxor@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/bpf/btf.c
+++ kernel/bpf/btf.c
@@ -6561,6 +6561,9 @@ static const struct bpf_raw_tp_null_args raw_tp_null_args[] = {
 	{ "rxrpc_resend", 0x10 },
 	{ "rxrpc_tq", 0x10 },
 	{ "rxrpc_client", 0x1 },
+	/* signal */
+	{ "signal_generate", 0x20 },
+	{ "signal_deliver", 0x20 },
 	/* skb */
 	{"kfree_skb", 0x1000},
 	/* sunrpc */
