69d502457e3f76142f7fcf6e2625e80e0ce8a60f
Steven Rostedt <rostedt@goodmis.org>
2026-09-03 13:27:28 -0400

    ring-buffer: Add checking nr_subbufs to persistent ring buffer validation
    [ Upstream commit 6c001a62c34f13fe1c6a24304c289b387d9e697d ]
    
    Sashiko reported that the code was using meta->nr_subbufs without making
    sure that it matched the nr_pages + 1 on data that was assuming the two
    were the same.
    
    Add a check to the persistent ring buffer validation code to make sure
    that the saved nr_subbufs matches what we expect.
    
    Link: https://patch.msgid.link/20260903132728.7fb27d34@gandalf.local.home
    Fixes: f5b95f1fa2ef3 ("ring-buffer: Validate the persistent meta data subbuf array")
    Reported-by: sashiko-bot@kernel.org
    Closes: https://lore.kernel.org/all/20260901164836.D962D1F000E9@smtp.kernel.org/
    Reviewed-by: Vincent Donnefort <vdonnefort@google.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- kernel/trace/ring_buffer.c
+++ kernel/trace/ring_buffer.c
@@ -1836,6 +1836,11 @@ static bool rb_cpu_meta_valid(struct ring_buffer_cpu_meta *meta, int cpu,
 		return false;
 	}
 
+	if (meta->nr_subbufs != nr_pages + 1) {
+		pr_info("Ring buffer boot meta [%d] invalid nr_subbufs\n", cpu);
+		return false;
+	}
+
 	buffers_start = meta->first_buffer;
 	buffers_end = meta->first_buffer + (subbuf_size * meta->nr_subbufs);
 
