b0a253780a01bdf1e76f85f61f79af1c2c177d83
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-09-04 12:15:37 -0400

    zram: fixup read_block_state()
    [ Upstream commit 8b05d2d8af817c6a1e23032df51e7ad83030d543 ]
    
    ac_time is now in seconds, do not use ktime_to_timespec64()
    
    [akpm@linux-foundation.org: remove now-unused local `ts']
    [akpm@linux-foundation.org: fix build]
    Link: https://lkml.kernel.org/r/20260115033031.3818977-1-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reported-by: Chris Mason <clm@meta.com>
    Closes: https://lkml.kernel.org/r/20260114124522.1326519-1-clm@meta.com
    Cc: Brian Geffon <bgeffon@google.com>
    Cc: David Stevens <stevensd@google.com>
    Cc: Minchan Kim <minchan@google.com>
    Cc: Richard Chang <richardycc@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    [ Stable tree note: The ac_time conversion described above is not present
      here, so retain the ktime_t conversion and microsecond output.  Move the
      timespec64 temporary into the scan loop instead, allowing the follow-up
      bounds fix to move nr_pages initialization under init_lock cleanly. ]
    
    Stable-dep-of: 391f057f44a5 ("zram: fix out-of-bounds access in read_block_state()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -869,7 +869,6 @@ static ssize_t read_block_state(struct file *file, char __user *buf,
 	ssize_t index, written = 0;
 	struct zram *zram = file->private_data;
 	unsigned long nr_pages = zram->disksize >> PAGE_SHIFT;
-	struct timespec64 ts;
 
 	kbuf = kvmalloc(count, GFP_KERNEL);
 	if (!kbuf)
@@ -884,6 +883,7 @@ static ssize_t read_block_state(struct file *file, char __user *buf,
 
 	for (index = *ppos; index < nr_pages; index++) {
 		int copied;
+		struct timespec64 ts;
 
 		zram_slot_lock(zram, index);
 		if (!zram_allocated(zram, index))
