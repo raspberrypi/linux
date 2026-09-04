efb4f24fc2b9bcef1c41ff3392894f1fa8fba2a0
Günther Noack <gnoack@google.com>
2026-09-04 20:13:27 +0200

    landlock: Require LANDLOCK_ACCESS_FS_MAKE_REG for whiteout creation
    [ Upstream commit 672fa082d48b21e1fb62cdb184fee41513e53421 ]
    
    Whiteout objects are used in the upper layer of an OverlayFS to
    indicate that the file with this name does not exist in the unified
    view, even if it is present in one of the lower layer file systems.
    
    For the userspace implementations of OverlayFS (fuse-overlayfs),
    whiteout objects can be created from userspace as well:
    
    * mknod(2) with S_IFCHR and makedev(0, 0)
    * renameat2(2) with RENAME_WHITEOUT,
      creating the whiteout in the old place of the moved file.
    
    This commit guards whiteout creation in both of these cases with
    LANDLOCK_ACCESS_FS_MAKE_REG.  Whiteout objects are *not* considered
    character devices and are not bound to a driver.
    
    LANDLOCK_ACCESS_FS_MAKE_REG describes the same permission class as a
    whiteout object: creating one is the only S_IFCHR creation that the VFS
    exempts from CAP_MKNOD, so it is as unprivileged as creating a regular
    file, while LANDLOCK_ACCESS_FS_MAKE_CHAR and
    LANDLOCK_ACCESS_FS_MAKE_BLOCK keep meaning the creation of devices that
    expose a kernel interface [1].
    
    For the mknod(2) case, introduce a Landlock erratum.  The creation of
    whiteout objects through mknod(2) was previously guarded using
    LANDLOCK_ACCESS_FS_MAKE_CHAR, and it is now guarded using
    LANDLOCK_ACCESS_FS_MAKE_REG.
    
    For the renameat2(2) case, fix a bug: Before this commit, renameat2(2)
    with RENAME_WHITEOUT would create a directory entry even when all
    LANDLOCK_ACCESS_FS_MAKE_* rights were denied.
    
    This does not affect normal renames within layered OverlayFS mounts:
    When doing a regular rename() on a mounted fuse-overlayfs, it is the
    fuse-overlayfs daemon that exercises renameat2() with RENAME_WHITEOUT,
    and only the Landlock domain of that daemon is checked there.
    
    Suggested-by: Christian Brauner <brauner@kernel.org>
    Suggested-by: Mickaël Salaün <mic@digikod.net>
    Cc: stable@vger.kernel.org
    Fixes: cb2c7d1a1776 ("landlock: Support filesystem access-control")
    Depends-on: 49c9e09d9610 ("landlock: Fix handling of disconnected directories")
    Depends-on: fe72ce6710cb ("landlock: Add errata documentation section")
    Signed-off-by: Günther Noack <gnoack@google.com>
    Link: https://patch.msgid.link/20260720.chow9ohYie5b@digikod.net [1]
    Link: https://patch.msgid.link/20260813093157.1436894-3-gnoack@google.com
    [mic: Record why LANDLOCK_ACCESS_FS_MAKE_REG is the matching right, and
    add link(2) to the user doc]
    Signed-off-by: Mickaël Salaün <mic@digikod.net>
    Signed-off-by: Günther Noack <gnoack3000@gmail.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- security/landlock/fs.c
+++ security/landlock/fs.c
@@ -20,6 +20,7 @@
 #include <linux/falloc.h>
 #include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/kdev_t.h>
 #include <linux/kernel.h>
 #include <linux/limits.h>
 #include <linux/list.h>
@@ -954,7 +955,7 @@ static int current_check_access_path(const struct path *const path,
 	return check_access_path(dom, path, access_request);
 }
 
-static access_mask_t get_mode_access(const umode_t mode)
+static access_mask_t get_mode_access(const umode_t mode, const dev_t dev)
 {
 	switch (mode & S_IFMT) {
 	case S_IFLNK:
@@ -962,6 +963,9 @@ static access_mask_t get_mode_access(const umode_t mode)
 	case S_IFDIR:
 		return LANDLOCK_ACCESS_FS_MAKE_DIR;
 	case S_IFCHR:
+		/* Whiteout objects are guarded with MAKE_REG. */
+		if (dev == WHITEOUT_DEV)
+			return LANDLOCK_ACCESS_FS_MAKE_REG;
 		return LANDLOCK_ACCESS_FS_MAKE_CHAR;
 	case S_IFBLK:
 		return LANDLOCK_ACCESS_FS_MAKE_BLOCK;
@@ -976,6 +980,13 @@ static access_mask_t get_mode_access(const umode_t mode)
 		/* Treats weird files as regular files. */
 		return LANDLOCK_ACCESS_FS_MAKE_REG;
 	}
+}
+
+static access_mask_t get_dentry_access(const struct dentry *const dentry)
+{
+	const struct inode *const inode = d_backing_inode(dentry);
+
+	return get_mode_access(inode->i_mode, inode->i_rdev);
 }
 
 static access_mask_t maybe_remove(const struct dentry *const dentry)
@@ -1067,6 +1078,7 @@ static bool collect_domain_accesses(
  * @new_dentry: Destination file or directory.
  * @removable: Sets to true if it is a rename operation.
  * @exchange: Sets to true if it is a rename operation with RENAME_EXCHANGE.
+ * @whiteout: Sets to true if it is a rename operation with RENAME_WHITEOUT.
  *
  * Because of its unprivileged constraints, Landlock relies on file hierarchies
  * (and not only inodes) to tie access rights to files.  Being able to link or
@@ -1115,7 +1127,8 @@ static bool collect_domain_accesses(
 static int current_check_refer_path(struct dentry *const old_dentry,
 				    const struct path *const new_dir,
 				    struct dentry *const new_dentry,
-				    const bool removable, const bool exchange)
+				    const bool removable, const bool exchange,
+				    const bool whiteout)
 {
 	const struct landlock_ruleset *const dom = get_current_fs_domain();
 	bool allow_parent1, allow_parent2;
@@ -1134,17 +1147,24 @@ static int current_check_refer_path(struct dentry *const old_dentry,
 	if (exchange) {
 		if (unlikely(d_is_negative(new_dentry)))
 			return -ENOENT;
-		access_request_parent1 =
-			get_mode_access(d_backing_inode(new_dentry)->i_mode);
+		access_request_parent1 = get_dentry_access(new_dentry);
 	} else {
 		access_request_parent1 = 0;
 	}
-	access_request_parent2 =
-		get_mode_access(d_backing_inode(old_dentry)->i_mode);
+	access_request_parent2 = get_dentry_access(old_dentry);
 	if (removable) {
 		access_request_parent1 |= maybe_remove(old_dentry);
 		access_request_parent2 |= maybe_remove(new_dentry);
 	}
+
+	/*
+	 * In case of renameat2(2) with RENAME_WHITEOUT, a whiteout object is
+	 * created in the source location, so we require an additional access
+	 * right there.
+	 */
+	if (whiteout)
+		access_request_parent1 |=
+			get_mode_access(S_IFCHR | WHITEOUT_MODE, WHITEOUT_DEV);
 
 	/* The mount points are the same for old and new paths, cf. EXDEV. */
 	if (old_dentry->d_parent == new_dir->dentry) {
@@ -1417,7 +1437,7 @@ static int hook_path_link(struct dentry *const old_dentry,
 			  struct dentry *const new_dentry)
 {
 	return current_check_refer_path(old_dentry, new_dir, new_dentry, false,
-					false);
+					false, false);
 }
 
 static int hook_path_rename(const struct path *const old_dir,
@@ -1428,7 +1448,8 @@ static int hook_path_rename(const struct path *const old_dir,
 {
 	/* old_dir refers to old_dentry->d_parent and new_dir->mnt */
 	return current_check_refer_path(old_dentry, new_dir, new_dentry, true,
-					!!(flags & RENAME_EXCHANGE));
+					!!(flags & RENAME_EXCHANGE),
+					!!(flags & RENAME_WHITEOUT));
 }
 
 static int hook_path_mkdir(const struct path *const dir,
@@ -1445,7 +1466,8 @@ static int hook_path_mknod(const struct path *const dir,
 
 	if (!dom)
 		return 0;
-	return check_access_path(dom, dir, get_mode_access(mode));
+	return check_access_path(dom, dir,
+				 get_mode_access(mode, new_decode_dev(dev)));
 }
 
 static int hook_path_symlink(const struct path *const dir,
