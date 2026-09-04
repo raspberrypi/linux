5cec3e60e9f2d1324179df3aa91656f90095cf8f
Sergey Senozhatsky <senozhatsky@chromium.org>
2026-09-04 16:32:11 -0400

    zram: set default primary compressor in zram_destroy_comps()
    [ Upstream commit dde75313eed0b014c437f48dd75c0308b592cbf9 ]
    
    Patch series "zram: fix zram issues reported by sashiko".
    
    Sashiko drove by and reported [1] a couple of zram issues:
    a possible BUG_ON() in zlib code due to missing winbits range
    validation and one possible NULL-ptr dereference in zcomp.
    Both are low risk yet still worth fixing.
    
    This patch (of 2):
    
    zram_destroy_comps() resets all compressors and leaves them set to NULL,
    including the primary one, which is invalid device state, as now
    comp_algorithm_show()->strcmp() can be called on a NULL compressor.  Set
    default primary compressor in zram_destroy_comps().
    
    Link: https://lore.kernel.org/20260729045745.775973-2-senozhatsky@chromium.org
    Fixes: 486fd58af7ac ("zram: don't free statically defined names")
    Link: https://sashiko.dev/#/patchset/20260728092935.31139-1-haoqinhuang7@gmail.com [1]
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Haoqin Huang <haoqinhuang7@gmail.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -2464,6 +2464,7 @@ static void zram_destroy_comps(struct zram *zram)
 	}
 
 	zram_comp_params_reset(zram);
+	comp_algorithm_set(zram, ZRAM_PRIMARY_COMP, default_compressor);
 }
 
 static void zram_reset_device(struct zram *zram)
@@ -2482,8 +2483,6 @@ static void zram_reset_device(struct zram *zram)
 	memset(&zram->stats, 0, sizeof(zram->stats));
 	atomic_set(&zram->pp_in_progress, 0);
 	reset_bdev(zram);
-
-	comp_algorithm_set(zram, ZRAM_PRIMARY_COMP, default_compressor);
 }
 
 static ssize_t disksize_store(struct device *dev,
