110747ff535e3d98ac17d68dbd056bb6ef000e23
Sam Edwards <cfsworks@gmail.com>
2026-09-06 13:42:14 -0400

    ceph: properly decrypt filenames in vmalloc() buffers
    [ Upstream commit e939fc6a7bd969a58a150b7f188c1047138403e3 ]
    
    The fscrypt subsystem uses the scatterlist crypto API, inheriting its
    requirement that any buffers are in the linear mapping region. However,
    the messenger client uses kvmalloc() to create buffers for messages,
    which will occasionally place those buffers in the vmalloc() region when
    physical memory fragmentation doesn't permit a large enough kmalloc().
    The various callers of ceph_fname_to_usr() directly pass (slices of) raw
    messages from the MDS without considering that the messages may be in
    vmalloc() buffers, resulting in oopses especially on non-x86 platforms
    (see 'Closes:' for more details and a reproducer).
    
    Make ceph_fname_to_usr() explicitly tolerant of vmalloc()-allocated
    fname->ctext, fname->name, and/or oname->name buffers, using `tname`
    (which, when non-null, must be a linear address; when null, is briefly
    allocated as necessary) as a bounce buffer to avoid passing any
    inappropriate addresses to fscrypt_fname_disk_to_usr().
    
    Additionally change parse_reply_info_readdir() -- the only function to
    supply its own `tname` -- to follow the new "tname must never come from
    vmalloc()" rule by passing NULL when the message is not in the linear
    region. Though this causes a per-dentry kmalloc()+kfree(), this overhead
    exists only when processing the minority of messages that spill into
    vmalloc(). My (crude) testing puts this at only about 1 in 8,000 readdir
    messages. Still, if the overhead proves unreasonable in the future, it
    is easy enough to mitigate: a future change could allocate a bounce
    buffer in parse_reply_info_readdir() and use that as `tname` instead.
    
    Cc: stable@vger.kernel.org # 888d33b208bd: ceph: pass fscrypt `tname` buffers directly
    Cc: stable@vger.kernel.org
    Fixes: 457117f077c6 ("ceph: add helpers for converting names for userland presentation")
    Closes: https://lore.kernel.org/ceph-devel/20260415034020.11530-1-CFSworks@gmail.com/
    Signed-off-by: Sam Edwards <CFSworks@gmail.com>
    Reviewed-by: Alex Markuze <amarkuze@redhat.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    [ adapted raw tname buffer accesses to the legacy tname->name interface ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ceph/crypto.c
+++ fs/ceph/crypto.c
@@ -351,6 +351,11 @@ out:
  * Otherwise, base64 decode the string, and then ask fscrypt to format it
  * for userland presentation.
  *
+ * Though the fscrypt/crypto subsystems broadly expect all buffers to be in the
+ * linear-mapped region, this function slightly relaxes those requirements:
+ * fname->ctext, fname->name, and oname->name may be vmalloc(), but not
+ * tname->name.
+ *
  * Returns 0 on success or negative error code on error.
  */
 int ceph_fname_to_usr(const struct ceph_fname *fname, struct fscrypt_str *tname,
@@ -358,10 +363,14 @@ int ceph_fname_to_usr(const struct ceph_fname *fname, struct fscrypt_str *tname,
 {
 	struct inode *dir = fname->dir;
 	struct fscrypt_str _tname = FSTR_INIT(NULL, 0);
+	struct fscrypt_str _oname;
 	struct fscrypt_str iname;
 	char *name = fname->name;
 	int name_len = fname->name_len;
 	int ret;
+
+	if (WARN_ON_ONCE(tname && is_vmalloc_addr(tname->name)))
+		return -EIO;
 
 	/* Sanity check that the resulting name will fit in the buffer */
 	if (fname->name_len > NAME_MAX || fname->ctext_len > NAME_MAX)
@@ -403,15 +412,18 @@ int ceph_fname_to_usr(const struct ceph_fname *fname, struct fscrypt_str *tname,
 		goto out_inode;
 	}
 
+	if ((!tname || !tname->name) &&
+	    (fname->ctext_len == 0 ||
+	     unlikely(is_vmalloc_addr(fname->ctext)) ||
+	     unlikely(is_vmalloc_addr(oname->name)))) {
+		ret = fscrypt_fname_alloc_buffer(NAME_MAX, &_tname);
+		if (ret)
+			goto out_inode;
+		tname = &_tname;
+	}
+
 	if (fname->ctext_len == 0) {
 		int declen;
-
-		if (!tname) {
-			ret = fscrypt_fname_alloc_buffer(NAME_MAX, &_tname);
-			if (ret)
-				goto out_inode;
-			tname = &_tname;
-		}
 
 		declen = ceph_base64_decode(name, name_len, tname->name);
 		if (declen <= 0) {
@@ -420,13 +432,29 @@ int ceph_fname_to_usr(const struct ceph_fname *fname, struct fscrypt_str *tname,
 		}
 		iname.name = tname->name;
 		iname.len = declen;
+	} else if (unlikely(is_vmalloc_addr(fname->ctext))) {
+		memcpy(tname->name, fname->ctext, fname->ctext_len);
+
+		iname.name = tname->name;
+		iname.len = fname->ctext_len;
 	} else {
 		iname.name = fname->ctext;
 		iname.len = fname->ctext_len;
 	}
 
-	ret = fscrypt_fname_disk_to_usr(dir, 0, 0, &iname, oname);
-	if (!ret && (dir != fname->dir)) {
+	_oname.name = unlikely(is_vmalloc_addr(oname->name)) ?
+			tname->name : oname->name;
+	_oname.len = oname->len;
+
+	ret = fscrypt_fname_disk_to_usr(dir, 0, 0, &iname, &_oname);
+	if (ret)
+		goto out;
+
+	if (unlikely(is_vmalloc_addr(oname->name)))
+		memcpy(oname->name, _oname.name, _oname.len);
+	oname->len = _oname.len;
+
+	if (dir != fname->dir) {
 		char tmp_buf[CEPH_BASE64_CHARS(NAME_MAX)];
 
 		name_len = snprintf(tmp_buf, sizeof(tmp_buf), "_%.*s_%ld",
