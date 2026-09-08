8ee7286f6854d37e3235f32d6c09899533f16f14
Miguel Ojeda <ojeda@kernel.org>
2026-09-08 19:05:39 +0200

    rust: allow `unknown_lints` in generated bindings for Rust < 1.88
    commit f4c3e38111fd84c2c7ae5785755f4a4d476e1cba upstream.
    
    Starting with bindgen 0.73.2 [1], `#[allow(unnecessary_transmutes)]`
    are used, even when `--rust-target 1.85` is passed.
    
    However, the lint was introduced in Rust 1.88.0. Thus building with
    older Rust versions warns like:
    
        error: unknown lint: `unnecessary_transmutes`
             --> rust/uapi/uapi_generated.rs:26294:13
              |
        26294 |     #[allow(unnecessary_transmutes)]
              |             ^^^^^^^^^^^^^^^^^^^^^^
              |
              = note: `-D unknown-lints` implied by `-D warnings`
              = help: to override `-D warnings` add `#[allow(unknown_lints)]`
    
    Thus allow `unknown_lints` in the generated bindings -- only when building
    with older Rust versions.
    
    I have asked upstream if this is intentional [1], i.e. if we are supposed
    to always allow unknown lints in case `bindgen` uses such attributes,
    or whether it is an oversight.
    
    [ Emilio said it wasn't intentional -- we will work around it for now
      on the kernel side. - Miguel ]
    
    Cc: stable@vger.kernel.org # Needed in 6.12.y and later (Rust is pinned in older LTSs).
    Cc: Emilio Cobos Álvarez <emilio@crisal.io>
    Link: https://github.com/rust-lang/rust-bindgen/pull/3455#issuecomment-5588526559 [1]
    Assisted-by: LLM
    Link: https://patch.msgid.link/20260908170539.345207-1-ojeda@kernel.org
    [ Removed the `cfg` for `allow(unnecessary_transmutes)` as suggested by
      Gary. - Miguel ]
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- rust/bindings/lib.rs
+++ rust/bindings/lib.rs
@@ -27,7 +27,8 @@
 #[allow(clippy::ptr_as_ptr)]
 #[allow(clippy::ref_as_ptr)]
 #[allow(clippy::undocumented_unsafe_blocks)]
-#[cfg_attr(CONFIG_RUSTC_HAS_UNNECESSARY_TRANSMUTES, allow(unnecessary_transmutes))]
+#[cfg_attr(not(CONFIG_RUSTC_HAS_UNNECESSARY_TRANSMUTES), allow(unknown_lints))]
+#[allow(unnecessary_transmutes)]
 #[cfg_attr(
     CONFIG_RUSTC_HAS_SUSPICIOUS_RUNTIME_SYMBOL_DEFINITIONS,
     allow(suspicious_runtime_symbol_definitions)
