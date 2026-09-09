fe3772ea9d269890e06d6102e67a83c9b29fb936
Akhmed Zhitaev <zhitaevakh@gmail.com>
2026-09-09 20:25:20 +0500

    drm/amd/display: Scale custom brightness curve from full range
    [ Upstream commit 6fd83a1c2cdea48c396f600795217fbdfb8124f6 ]
    
    Custom brightness curves use an 8-bit input signal.  After exporting the
    full PWM range to userspace, the curve normalizer still divides requests
    by the physical PWM span.  On panels with a nonzero minimum PWM level,
    this can produce a curve input greater than 255 and send an invalid
    backlight level to DC.
    
    Scale the userspace [0..max] range to the curve's [0..255] range
    instead.  This retains the full advertised range and keeps the reverse
    readback conversion unchanged.
    
    Fixes: 8dbd72cb7900 ("drm/amd/display: Export full brightness range to userspace")
    Cc: stable@vger.kernel.org
    Signed-off-by: Akhmed Zhitaev <zhitaevakh@gmail.com>
    Reviewed-by: Mario Limonciello (AMD) <superm1@kernel.org>
    (Move to amdgpu_dm_backlight.c)
    Link: https://patch.msgid.link/20260813170959.22073-1-zhitaevakh@gmail.com
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -5271,10 +5271,10 @@ static int get_brightness_range(const struct amdgpu_dm_backlight_caps *caps,
 	return 1;
 }
 
-/* Rescale from [min..max] to [0..AMDGPU_MAX_BL_LEVEL] */
-static inline u32 scale_input_to_fw(int min, int max, u64 input)
+/* Rescale userspace [0..max] to the firmware curve's [0..255]. */
+static inline u32 scale_input_to_fw(int max, u64 input)
 {
-	return DIV_ROUND_CLOSEST_ULL(input * AMDGPU_MAX_BL_LEVEL, max - min);
+	return DIV_ROUND_CLOSEST_ULL(input * AMDGPU_MAX_BL_LEVEL, max);
 }
 
 /* Rescale from [0..AMDGPU_MAX_BL_LEVEL] to [min..max] */
@@ -5287,7 +5287,7 @@ static void convert_custom_brightness(const struct amdgpu_dm_backlight_caps *cap
 				      unsigned int min, unsigned int max,
 				      uint32_t *user_brightness)
 {
-	u32 brightness = scale_input_to_fw(min, max, *user_brightness);
+	u32 brightness = scale_input_to_fw(max, *user_brightness);
 	u8 lower_signal, upper_signal, upper_lum, lower_lum, lum;
 	int left, right;
 
