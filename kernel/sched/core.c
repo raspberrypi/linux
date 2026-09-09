c522333c35d483123e365ed0ae4ea34ff5e6c06f
John Stultz <jstultz@google.com>
2026-09-09 05:26:29 -0400

    sched: Add assert_balance_callbacks_empty helper
    [ Upstream commit f9530b3183358bbf945f7c20d4a6e2048061ec50 ]
    
    With proxy-exec utilizing pick-again logic, we can end up having
    balance callbacks set by the preivous pick_next_task() call left
    on the list.
    
    So pull the warning out into a helper function, and make sure we
    check it when we pick again.
    
    Suggested-by: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: John Stultz <jstultz@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://patch.msgid.link/20260324191337.1841376-8-jstultz@google.com
    Stable-dep-of: f3629c63a4af ("sched/core: Make core-sched flips wait for in-flight selections")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6880,6 +6880,7 @@ static void __sched notrace __schedule(int sched_mode)
 	}
 
 pick_again:
+	assert_balance_callbacks_empty(rq);
 	next = pick_next_task(rq, rq->donor, &rf);
 	rq_set_donor(rq, next);
 	if (unlikely(task_is_blocked(next))) {
