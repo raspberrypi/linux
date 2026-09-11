9b046de62b8098af6e2ba820b125ec9dc5f162c8
Christian Borntraeger <borntraeger@linux.ibm.com>
2026-09-11 07:49:27 -0400

    KVM: s390: Zero initialize data structures for inject_pfault_token
    [ Upstream commit 4e2c7f7cbc27418f9a290399b986c1b85ff93b90 ]
    
    __kvm_inject_pfault_token() only sets .type and .u.ext.ext_params2 of
    the on-stack struct kvm_s390_irq but the full ext substructure is copied
    into the cpu local variable on inject. ext_params and pad contain stale
    stack values.
    
    Interrupt delivery only uses ext_params2, so nothing leaks to the guest,
    but a host user can use the migration ioctls to get to the data.
    
    Fix by zero-initializing the irq struct.
    Do the same for the inti data structure.
    
    Fixes: 383d0b050106 ("KVM: s390: handle pending local interrupts via bitmap")
    Cc: stable@vger.kernel.org
    Signed-off-by: Christian Borntraeger <borntraeger@linux.ibm.com>
    Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com>
    Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Message-ID: <20260805110455.7200-3-borntraeger@linux.ibm.com>
    [ Adjusted context for missing inti_mem and ret declarations in __kvm_inject_pfault_token(). ]
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/s390/kvm/kvm-s390.c
+++ arch/s390/kvm/kvm-s390.c
@@ -4705,8 +4705,8 @@ int kvm_s390_try_set_tod_clock(struct kvm *kvm, const struct kvm_s390_vm_tod_clo
 static void __kvm_inject_pfault_token(struct kvm_vcpu *vcpu, bool start_token,
 				      unsigned long token)
 {
-	struct kvm_s390_interrupt inti;
-	struct kvm_s390_irq irq;
+	struct kvm_s390_interrupt inti = {};
+	struct kvm_s390_irq irq = {};
 
 	if (start_token) {
 		irq.u.ext.ext_params2 = token;
