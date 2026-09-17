16971cac56f2c2df32b12720be7ab1b5f0d7a682
Ralph Boehme <slow@samba.org>
2026-09-16 23:54:31 -0400

    smb: client: refactor ACL setting control flow in id_mode_to_cifs_acl()
    [ Upstream commit a540a64c4801fe02e452ee37e961018106418260 ]
    
    Refactor the control flow in id_mode_to_cifs_acl() to reduce nesting and
    prevent error code overwriting.
    
    Instead of wrapping the call to ops->set_acl() in a conditional block,
    introduce early exits (goto id_mode_to_cifs_acl_exit) when build_sec_desc()
    fails or ops->set_acl is NULL. This ensures that any actual error returned
    by build_sec_desc() is not overwritten with -EOPNOTSUPP.
    
    Signed-off-by: Ralph Boehme <slow@samba.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Stable-dep-of: d05045177a85 ("smb: client: fail DACL rewrite when the new DACL exceeds 64K")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/cifsacl.c
+++ fs/smb/client/cifsacl.c
@@ -1799,14 +1799,18 @@ id_mode_to_cifs_acl(struct inode *inode, const char *path, __u64 *pnmode,
 
 	cifs_dbg(NOISY, "build_sec_desc rc: %d\n", rc);
 
-	if (ops->set_acl == NULL)
-		rc = -EOPNOTSUPP;
+	if (rc != 0)
+		goto id_mode_to_cifs_acl_exit;
 
-	if (!rc) {
-		/* Set the security descriptor */
-		rc = ops->set_acl(pnntsd, nsecdesclen, inode, path, aclflag);
-		cifs_dbg(NOISY, "set_cifs_acl rc: %d\n", rc);
+	if (ops->set_acl == NULL) {
+		rc = -EOPNOTSUPP;
+		goto id_mode_to_cifs_acl_exit;
 	}
+
+	/* Set the security descriptor */
+	rc = ops->set_acl(pnntsd, nsecdesclen, inode, path, aclflag);
+	cifs_dbg(NOISY, "set_cifs_acl rc: %d\n", rc);
+
 id_mode_to_cifs_acl_exit:
 	cifs_put_tlink(tlink);
 
